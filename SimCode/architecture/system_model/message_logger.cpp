59db723087d75b4964f8161cb0fcf7a78b0642da
Scott Piggott <scott.piggott@lasp.colorado.edu>
2015-08-20 09:21:55 -0600

    Committing set of updates that allow us to log system messages at runtime.  The logging read call is a little cumbersome still, but it is completely functional.
    Former-commit-id: 008b41ddc2113409af54a0758ddc64cf708549dc

---
+++ SimCode/architecture/system_model/message_logger.cpp
@@ -0,0 +1,164 @@
+
+#include "architecture/system_model/message_logger.h"
+#include <cstring>
+#include <iostream>
+
+/*! This constructor is used to initialize the message logging data.  It clears 
+    out the message log list and resets the logger to a clean state.
+*/
+messageLogger::messageLogger()
+{
+   logData.clear();
+   allLogsLinked = true;
+   initBufferSize = 5000000; 
+}
+
+/*! Nothing to destroy really */
+messageLogger::~messageLogger()
+{
+}
+
+/*! This method inserts a new message onto the logging vector so that it gets 
+    logged out appropriately.  If the message has already been added once 
+    already it will just update the log period and return from there.
+    @return void
+    @param messageName -- The name of the message that we want to log
+    @param messagePeriod ns The minimum time between messages that we want to allow
+*/
+void messageLogger::addMessageLog(std::string messageName, uint64_t messagePeriod)
+{
+   //! Begin  method steps
+   //! - Check to see if we have already commanded a log for this message, if so just update the rate
+   std::vector<messageLogContainer>::iterator it;
+   for(it=logData.begin(); it != logData.end(); it++)
+   {
+      if(it->messageName ==  messageName)
+      {
+         it->writeDelta = messagePeriod;
+         return;
+      }
+   }
+   //! - If the message hasn't been added yet, allocate new container, init data, and add to log vector
+   messageLogContainer newContainer;
+   newContainer.messageName = messageName;
+   newContainer.messageID = -1;
+   newContainer.lastLogTime = 0;
+   newContainer.logInstanceCount = 0;
+   newContainer.lastWriteCheck = 0;
+   newContainer.writeDelta = messagePeriod;
+   newContainer.bufferOffset = 0;
+   newContainer.storOff.clear();
+   logData.push_back(newContainer);
+   //! - Since we have a new message, note that we need to link it durin run
+   allLogsLinked = false;
+}
+/*! This method is used to find the appropriate messaged ID associated with 
+    each message name that has been added to logging.  It warns the user if any 
+    of the added messages have not been successfully linked.
+    @return void
+*/
+void messageLogger::linkMessages()
+{
+   //! Begin  method steps
+   //! - Since we are linking, go ahead and just relink all messages
+   std::vector<messageLogContainer>::iterator it;
+   //! - Set the linker indicator to good and only falsify it if we don't find a message
+   allLogsLinked = true;
+   for(it=logData.begin(); it != logData.end(); it++)
+   {
+      it->messageID = SystemMessaging::GetInstance()->
+         FindMessageID(it->messageName);
+      //! - Warn the user if linking failed and note that logging won't work for that message
+      if(it->messageID < 0)
+      {
+         std::cout << "Warning, failed to find message: " << it->messageName;
+         std::cout << std::endl << "Disabling logging for it"<<std::endl;
+      }
+      else
+      {
+         it->messageBuffer.IncreaseStorage(initBufferSize);
+      }
+   }
+}
+/*! This method executes the actual log generation activity.  It is focused on 
+    simplicity currently so there is room to speed things up if this 
+    functionality begins to consume significant parts of our runtime
+    @return void
+*/
+void messageLogger::logAllMessages()
+{
+   //! Begin  method steps
+   //! - Iterate through the message list and log any needed messages
+   std::vector<messageLogContainer>::iterator it;
+   for(it=logData.begin(); it != logData.end(); it++)
+   {
+      //! - Message ID is invalid if it is less than zero
+      if(it->messageID <0)
+      {
+         continue;
+      }
+      //! - Get the current message header and check to see if it is new and if enough time has elapsed since the last log
+      MessageHeaderData* localHeader = SystemMessaging::GetInstance()->
+         FindMsgHeader(it->messageID);
+      bool bufferNew = it->lastWriteCheck != localHeader->UpdateCounter;
+      bufferNew = bufferNew ? (localHeader->CurrentReadTime - it->lastLogTime) 
+         >= it->writeDelta || it->lastWriteCheck == 0 : bufferNew;
+      it->lastWriteCheck = localHeader->UpdateCounter;
+      if(bufferNew)
+      {
+         //! - For valid message logging instance, increase storage buffer if necessary
+         while((it->bufferOffset + sizeof(SingleMessageHeader) + 
+            localHeader->CurrentReadSize) > it->messageBuffer.GetCurrentSize())
+         {
+            it->messageBuffer.IncreaseStorage(
+               it->messageBuffer.GetCurrentSize()*2+1);
+         }
+         //! - Read out current message and reset log parameter so that we grab the appropriate message next time
+         uint8_t * localPtr = 
+            &(it->messageBuffer.StorageBuffer[it->bufferOffset]);
+         SystemMessaging::GetInstance()->ReadMessage(it->messageID, 
+            reinterpret_cast<SingleMessageHeader *> (localPtr),
+            localHeader->CurrentReadSize, &localPtr[sizeof(SingleMessageHeader)]);
+         it->storOff.push_back(it->bufferOffset);
+         it->bufferOffset += sizeof(SingleMessageHeader) + 
+            localHeader->CurrentReadSize;
+         it->lastLogTime = localHeader->CurrentReadTime;
+         it->logInstanceCount++;
+      }
+   }
+}
+bool messageLogger::readLog(int64_t messageID, SingleMessageHeader *dataHeader,
+      uint64_t maxBytes, uint8_t *msgPayload, uint64_t currentOffset)
+{
+   //! Begin  method steps
+   //! - Iterate through the message list and find the requested ID
+   std::vector<messageLogContainer>::iterator it;
+   SingleMessageHeader *headPtr;
+   for(it=logData.begin(); it != logData.end(); it++)
+   {
+      if(it->messageID != messageID)
+      {
+         continue;
+      }
+      int64_t currentIndex = it->logInstanceCount;
+      currentIndex -= (1 + currentOffset);
+      while(currentIndex < 0)
+      {
+         currentIndex += it->logInstanceCount;
+      }
+      std::vector<uint64_t>::iterator storIt;
+      storIt = it->storOff.begin();
+      storIt += currentIndex;
+      uint8_t *dataPtr = &(it->messageBuffer.StorageBuffer[*storIt]);
+      headPtr = reinterpret_cast<SingleMessageHeader*> (dataPtr);
+      memcpy(dataHeader, headPtr, sizeof(SingleMessageHeader));
+      dataPtr += sizeof(SingleMessageHeader);
+      uint64_t bytesUse = maxBytes > headPtr->WriteSize ? headPtr->WriteSize : 
+         maxBytes;
+      memcpy(msgPayload, dataPtr, bytesUse);
+      return(true);
+   }
+
+   return false;
+}
+
---
+++ SimCode/architecture/system_model/message_logger.h
@@ -0,0 +1,46 @@
+#ifndef _MessageLogger_HH_
+#define _MessageLogger_HH_
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+#include "architecture/messaging/system_messaging.h"
+
+/*! \addtogroup SimArchGroup
+ * @{
+ */
+
+typedef struct {
+   std::string messageName;    //!< -- The message name associated with the log
+   int32_t messageID;          //!< -- The message ID associated with the log
+   uint64_t lastLogTime;       //!< ns The last valid log time observed
+   uint64_t logInstanceCount;  //!< -- The number of message logs that we have made
+   uint64_t lastWriteCheck;    //!< ns The last write count that we examined
+   uint64_t writeDelta;        //!< ns The minimum time between log steps
+   uint64_t bufferOffset;      //!< -- The current offset in the log to access
+   BlankStorage messageBuffer; //!< The storage buffer associated with the log
+   std::vector<uint64_t> storOff; //!< -- Vector of storage buffer offset offsets for access
+}messageLogContainer;
+
+//! The top-level container for an entire simulation
+class messageLogger
+{
+ public:
+   messageLogger(); //!< The MessageLogger constructor
+   ~messageLogger();//!< MessageLogger destructor
+   void addMessageLog(std::string messageName, uint64_t messagePeriod=0);
+   void linkMessages();
+   bool messagesLinked() {return allLogsLinked;} //!< Getter for link success
+   void logAllMessages();
+   bool readLog(int64_t messageID, SingleMessageHeader *dataHeader, 
+      uint64_t maxBytes, uint8_t *msgPayload, uint64_t currentOffset=0);
+
+ public:
+   uint64_t initBufferSize; //!< Default buffer size fo message log storage
+    std::vector<messageLogContainer> logData; //!< Vector of log elements
+ private:
+    bool allLogsLinked; //!< Indicator of whether or not messages are all linked
+};
+
+/*! @} */
+#endif /* _MessageLogger_H_ */
