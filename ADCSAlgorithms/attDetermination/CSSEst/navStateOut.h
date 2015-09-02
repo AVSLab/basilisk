4f4ab7b9dd7c2e726fe534978b590a6a40dcea6a
Scott Piggott <Scott.Piggott@lasp.colorado.edu>
2015-09-02 09:05:10 -0600

    Committing updates with nav output header present
    Former-commit-id: 987503f6eeaa07de780706d8066dbba822d097bd

---
+++ ADCSAlgorithms/attDetermination/CSSEst/navStateOut.h
@@ -0,0 +1,23 @@
+
+#ifndef _NAV_STATE_OUT_H_
+#define _NAV_STATE_OUT_H_
+
+#include <stdint.h>
+
+/*! \addtogroup ADCSAlgGroup
+ * @{
+ */
+
+/*! @brief Structure used to define the output definition for attitude guidance*/
+typedef struct {
+    double vehPosition[3];   /*!< m  Current position of the spacecraft*/
+    double vehVelocity[3];   /*!< m/s Current velocity of the spacecraft*/
+    double vehAccumDV[3];    /*!< m/s Total accumulated delta-velocity for s/c*/
+    double vehSigma[3];      /*!< -- Current spacecraft attitude (MRPs)*/
+    double vehBodyRate[3];   /*!< r/s Current spacecraft body rate wrt reference*/
+    double sunPointBdy[3];   /*!< -- Current sun pointing vector in body frame*/
+}NavStateOut;
+
+/*! @} */
+
+#endif
