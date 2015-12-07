aad2e85a4d48b7895ca04f86de455222d8d409fc
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2015-12-07 14:57:02 -0700

    first cut of making an ADCS Algorithm template module folder and and sub-module folder - includes new README files inside the module and sub-module - The CMake still needs to pull in the files in the "_GeneralModuleFiles" sub-folder - needs a generate output message declaration file that is pulled into the one template sub-folder
    Former-commit-id: 9a36321fefae48b0c32928d7b6b20340036ec604
    Former-commit-id: e5e0e8a7c8b7f34e8b5edd4e4a7c4a00d75a3df0

---
+++ ADCSAlgorithms/ moduleTemplate/ subModuleTemplate/subModuleTemplate.c
@@ -0,0 +1,97 @@
+/*
+    SUB_MODULE Template
+ 
+ * University of Colorado, Autonomous Vehicle Systems (AVS) Lab
+ * Unpublished Copyright (c) 2012-2015 University of Colorado, All Rights Reserved
+
+ */
+
+/* modify the path to reflect the new module names */
+#include " moduleTemplate/ subModuleTemplate/subModuleTemplate.h"
+
+/*
+ Pull in common Module support files
+ */
+//#include "../_GeneralModuleFiles/dummy.h"
+
+/*
+ Pull in supprt files from other modules.  Be sure to use the absolute path.
+ */
+//#include "Utilities/rigidBodyKinematics.h"
+
+
+/*! This method initializes the ConfigData for this module.
+ It checks to ensure that the inputs are sane and then creates the
+ output message
+ @return void
+ @param ConfigData The configuration data associated with this module
+ */
+void SelfInit_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t moduleID)
+{
+    
+    /*! Begin method steps */
+    /*! - Create output message for module */
+//    ConfigData->outputMsgID = CreateNewMessage(ConfigData->outputDataName,
+//        sizeof(vehControlOut), "vehControlOut", moduleID);
+
+}
+
+/*! This method performs the second stage of initialization for this module.
+ It's primary function is to link the input messages that were created elsewhere.
+ @return void
+ @param ConfigData The configuration data associated with this module
+ */
+void CrossInit_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t moduleID)
+{
+    /*! - Get the control data message ID*/
+//    ConfigData->inputGuidID = subscribeToMessage(ConfigData->inputGuidName,
+//                                                 sizeof(attGuidOut), moduleID);
+
+}
+
+/*! This method performs a complete reset of the module.  Local module variables that retain
+ time varying states between function calls are reset to their default values.
+ @return void
+ @param ConfigData The configuration data associated with the MRP steering control
+ */
+void Reset_subModuleTemplate(subModuleTemplateConfig *ConfigData)
+{
+    ConfigData->dummy = 0;              /* reset any required variables */
+}
+
+/*! This method takes the attitude and rate errors relative to the Reference frame, as well as
+    the reference frame angular rates and acceleration, and computes the required control torque Lr.
+ @return void
+ @param ConfigData The configuration data associated with the MRP Steering attitude control
+ @param callTime The clock time at which the function was called (nanoseconds)
+ */
+void Update_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t callTime,
+    uint64_t moduleID)
+{
+//    uint64_t            clockTime;
+//    uint32_t            readSize;
+//    double              variable;           /*!< [unit] variable description */
+
+
+    /*! Begin method steps*/
+    /*! - Read the input messages */
+//    ReadMessage(ConfigData->inputMsgID, &clockTime, &readSize,
+//                sizeof(attGuidOut), (void*) &(guidCmd));
+
+
+
+    /*
+        Add the module specific code
+     */
+
+
+    /*
+     store the output message 
+     */
+//    v3Copy(Lr, ConfigData->controlOut.torqueRequestBody);
+//    
+//    WriteMessage(ConfigData->outputMsgID, callTime, sizeof(vehControlOut),
+//                 (void*) &(ConfigData->controlOut), moduleID);
+
+    return;
+}
---
+++ ADCSAlgorithms/ moduleTemplate/ subModuleTemplate/subModuleTemplate.h
@@ -0,0 +1,41 @@
+
+#ifndef _SUB_MODULE_TEMPLATE_H_
+#define _SUB_MODULE_TEMPLATE_H_
+
+#include "messaging/static_messaging.h"
+#include <stdint.h>
+
+
+/*! \addtogroup ADCSAlgGroup
+ * @{
+ */
+
+/*! @brief Top level structure for the sub-module routines. */
+typedef struct {
+    /* declare module private variables */
+    double dummy;          /*!< [rad/sec] Proportional gain applied to MRP errors */
+
+    /* declare module IO interfaces */
+    char outputDataName[MAX_STAT_MSG_LENGTH];       /*!< The name of the output message*/
+    char inputDataName[MAX_STAT_MSG_LENGTH];        /*!< The name of the Input message*/
+    int32_t outputMsgID;                            /*!< [] ID for the outgoing body accel requests*/
+    int32_t inputMsgID;                             /*!< [] ID for the incoming guidance errors*/
+
+}subModuleTemplateConfig;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    void SelfInit_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t moduleID);
+    void CrossInit_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t moduleID);
+    void Update_subModuleTemplate(subModuleTemplateConfig *ConfigData, uint64_t callTime, uint64_t moduleID);
+    void Reset_subModuleTemplate(subModuleTemplateConfig *ConfigData);
+    
+#ifdef __cplusplus
+}
+#endif
+
+/*! @} */
+
+#endif
