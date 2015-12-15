ee5dd3a536ee195444ad261e33060ce3d34e2099
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2015-12-15 12:15:07 -0700

    updated the subModuleTemplate folder with new unitTest python script (IN PROGRESS), and a new TeX documentation template I added a new python unit test support script file to the PythonModules folder
    Former-commit-id: e016133fd58604c5836ba6526777ccf8fee71570
    Former-commit-id: 3294998f9266f70d1ec88903777dbfc8463aa94f

---
+++ PythonModules/unitTestSupport.py
@@ -0,0 +1,60 @@
+#
+#   Unit Test Support Script
+#
+
+import math
+
+#
+# function to convert seconds to an integer nanoseconds value
+#
+def sec2nano(time):
+    return int(time*1E9)
+
+# variable to convert nano-seconds to seconds
+NANO2SEC = 1E-9
+
+
+#
+# function to check if an array of values is the same as the truth values
+#
+def isArrayEqual(result, truth, dim, accuracy):
+    # the result array is of dimension dim+1, as the first entry is the time stamp
+    # the truth array is of dimesion dim, no time stamp
+    if dim < 1:
+        print "Incorrect array dimension " + dim + " sent to isArrayEqual"
+        return 0
+
+    for i in range(0,dim):
+        if (math.fabs(result[i+1] - truth[i]) > accuracy):
+            return 0    # return 0 to indicate the array's are not equal
+
+    return 1            # return 1 to indicate the two array's are equal
+
+
+#
+# function to check if an array of values are zero
+#
+def isArrayZero(result, dim, accuracy):
+    # the result array is of dimension dim+1, as the first entry is the time stamp
+    if dim < 1:
+        print "Incorrect array dimension " + dim + " sent to isArrayEqual"
+        return 0
+
+    for i in range(0,dim):
+        if (math.fabs(result[i+1]) > accuracy):
+            return 0    # return 0 to indicate the array's are not equal
+
+    return 1            # return 1 to indicate the two array's are equal
+
+
+#
+# function to check if a double equals a truth value
+#
+def isDoubleEqual(result, truth, accuracy):
+    # the result array is of dimension dim+1, as the first entry is the time stamp
+    if (math.fabs(result[1] - truth) > accuracy):
+        return 0    # return 0 to indicate the doubles are not equal
+
+    return 1        # return 1 to indicate the doubles are equal
+
+
