1cedbc085870aa58baae258c068d59af989adc60
Scott Piggott <Scott.Piggott@lasp.colorado.edu>
2015-12-21 10:42:27 -0700

    Committing updates (last non-branch commit) that contain dynamics updates and a couple little power fixes.
    Former-commit-id: 39ce737fb402ccd7d0aff194d7a555a54e49fb68
    Former-commit-id: f4357b8e8f1a97d834c93253862c24cb815737b3

--- TrajectoryScenarios/PowerScienceOrbit.py
+++ TrajectoryScenarios/PowerScienceOrbit.py
@@ -1,4 +1,4 @@
-﻿import sys, os, inspect #Don't worry about this, standard stuff plus file discovery
+import sys, os, inspect #Don't worry about this, standard stuff plus file discovery
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
 sys.path.append(path + '/../PythonModules/')
@@ -13,6 +13,8 @@ import numpy #Lets us interact with nump objects which work like matlab arrays
 import logging #Handy logging feature for test issues
 import SimulationBaseClass #Need this to access some pointer manipulation
 import spice_interface #Need a local copy of the SPICE interface for file manipulation
+import RigidBodyKinematics
+import csv
 
 #O1	38.5
 #O2	71.5
@@ -38,20 +40,22 @@ import spice_interface #Need a local copy of the SPICE interface for file manipu
 #C16	99.32
 
 class SequenceElement:
- def __init__(self, times, rasterSet, rateSet, modeSet):
+ def __init__(self, times, modeSet = 'sunPoint', rasterSet=[[0.0, 0.0, 0.0]], rateSet=[[0.0, 0.0, 0.0]]):
      self.rasterAngles = rasterSet
      self.rasterRates = rateSet
      self.rasterMode = modeSet
      self.rasterTimes = times
 
-class PowerSimBase(SimulationBaseClass.SimBaseClass):
+class powerSimBase(SimulationBaseClass.SimBaseClass):
  def __init__(self):
+    self.modeData = [[0, 'Unknown']]
     self.TheEMMSim = EMMSim.EMMSim()
     self.TheEMMSim.TotalSim.logThisMessage("inertial_state_output", int(1E9)) #inertial states
-    self.TheEMMSim.TotalSim.logThisMessage("solar_array_sun_bore", int(1E9)) #solar array boresight angles
+    self.TheEMMSim.TotalSim.logThisMessage("solar_array_sun_bore", int(60E9)) #solar array boresight angles
     self.TheEMMSim.TotalSim.logThisMessage("high_gain_earth_bore", int(1E9)) #solar array boresight angles
     self.TheEMMSim.TotalSim.logThisMessage("instrument_mars_bore", int(1E9)) #solar array boresight angles
     self.TheEMMSim.TotalSim.logThisMessage("att_cmd_output", int(1E9)) #inertial states
+    self.TheEMMSim.TotalSim.logThisMessage("OrbitalElements", int(1E10)) #orbital elements
     self.TheEMMSim.AddVectorForLogging('instrumentBoresight.boreVecPoint', 'double', 0, 2, int(1E9))
 
     self.TheEMMSim.VehDynObject.GravData[0].IsCentralBody = False
@@ -88,15 +92,25 @@ class PowerSimBase(SimulationBaseClass.SimBaseClass):
     self.spacecraftMessageName = self.amalName + "_planet_data"
 
     self.orbitSequence = {}
-    self.orbitSequence['C1'] = SequenceElement([int(112.86*60.0)], [[0.0, 0.0, 0.0]], 
-                                               [[0.0, 0.0, 0.0]], 'sunPoint')
-    self.orbitSequence['C2'] = SequenceElement([int(86.32*60.0)], [[0.0, 0.0, 0.0]], 
-                                               [[0.0, 0.0, 0.0]], 'sunPoint')
-    self.orbitSequence['C3'] = SequenceElement([int(59.82*60.0)], [[0.0, 0.0, 0.0]], 
-                                               [[0.0, 0.0, 0.0]], 'sunPoint')
+    self.orbitSequence['C1'] = SequenceElement([int((112.82+5)*60.0)])
+    self.orbitSequence['C2'] = SequenceElement([int((86.32+5)*60.0)])
+    self.orbitSequence['C3'] = SequenceElement([int((59.82+5)*60.0)])
+    self.orbitSequence['C4'] = SequenceElement([int((72.57+5)*60.0)])
+    self.orbitSequence['C5'] = SequenceElement([int((105.57+5)*60.0)])
+    self.orbitSequence['C6'] = SequenceElement([int((85.32+5)*60.0)])
+    self.orbitSequence['C7'] = SequenceElement([int((92.82+5)*60.0)])
+    self.orbitSequence['C8'] = SequenceElement([int((119.32+5)*60.0)])
+    self.orbitSequence['C9'] = SequenceElement([int((99.07+5)*60.0)])
+    self.orbitSequence['C10'] = SequenceElement([int((125.82+5)*60.0)])
+    self.orbitSequence['C11'] = SequenceElement([int((79.82+5)*60.0)])
+    self.orbitSequence['C12'] = SequenceElement([int((96.32+5)*60.0)])
+    self.orbitSequence['C13'] = SequenceElement([int((119.32+5)*60.0)])
+    self.orbitSequence['C14'] = SequenceElement([int((102.82+5)*60.0)])
+    self.orbitSequence['C15'] = SequenceElement([int((105.82+5)*60.0)])
+    self.orbitSequence['C16'] = SequenceElement([int((99.32+5)*60.0)])
     longTimes = [int(30*60), int(1.5*60), int(26.5*60), int(1.5*60), int(26.5*60)]
     rastAngRad = 10.0*math.pi/180.0
-    discAngleRad = 30.0*math.pi/180.0
+    discAngleRad = 15.0*math.pi/180.0
     rasterTime = 26.5*60.0
     discAngRate = 2.0*discAngleRad/rasterTime
     longAngles = [ \
@@ -113,13 +127,90 @@ class PowerSimBase(SimulationBaseClass.SimBaseClass):
                     [0.0, 0.0, 0.0],
                     [0.0, 0.0, discAngRate]\
                     ]
-    self.orbitSequence['O3'] = SequenceElement(longTimes, longAngles, 
-                                               longRates, 'marsPoint')
-    self.orbitSequence['O4'] = SequenceElement(longTimes[1:4], longAngles[1:4], 
-                                               longRates[1:4], 'marsPoint')
+    self.orbitSequence['O3'] = SequenceElement(longTimes, 'marsPoint', longAngles,
+                                               longRates)
+    self.orbitSequence['O1'] = SequenceElement(longTimes[1:4], 'marsPoint', longAngles[1:4],
+                                               longRates[1:4])
+ 
+    mediumTimes = [int(15.0*60), int(11*60), int(11*60), int(1.5*60), int(26.5*60)]
+    medUpAngRad = 10.0*math.pi/180.0
+    medScanAngRad = 11.4*math.pi/180.0
+    mediumTime = 10.0*60.0
+    medAngRate = 2.0*medScanAngRad/mediumTime
+    mediumAngles = [ \
+                    [medUpAngRad, 0.0, -medScanAngRad],
+                    [0.0, 0.0, medScanAngRad],
+                    [-medUpAngRad, 0.0, -medScanAngRad],
+                    [0.0, 0.0, 0.0],
+                    [0.0, 0.0, -discAngleRad],
+                    [0.0, 0.0, 0.0] \
+                    ]
+    mediumRates = [ \
+                    [0.0, 0.0, -medAngRate],
+                    [0.0, 0.0, medAngRate],
+                    [0.0, 0.0, -medAngRate],
+                    [0.0, 0.0, 0.0],
+                    [0.0, 0.0, -discAngRate],
+                    [0.0, 0.0, 0.0]
+                ]
+    self.orbitSequence['O2'] = SequenceElement(mediumTimes, 'marsPoint', mediumAngles,
+                            mediumRates)
+ 
+    BOSshortTimes = [int(5.0*60), int(26.5*60), int(1.5*60), int(26.5*60.0)]
+    BOSshortAngles = [ \
+                        [0.0, 0.0, 0.0],
+                        [rastAngRad/2.0, 0.0, -discAngleRad],
+                        [0.0, 0.0, 0.0],
+                        [-rastAngRad/2.0, 0.0, -discAngleRad] \
+                      ]
+    BOSshortRates = [ \
+                    [0.0, 0.0, 0.0],
+                    [0.0, 0.0, -discAngRate],
+                    [0.0, 0.0, 0.0],
+                    [0.0, 0.0, -discAngRate]\
+                    ]
+    self.orbitSequence['O6'] = SequenceElement(BOSshortTimes, 'marsPoint', BOSshortAngles,
+                                                   BOSshortRates)
+    MOSshortTimes = [int(15*60), int(11*60), int(1.5*60), int(26.5*60), int(1.5*60)]
+    MOSshortAngles = [ \
+                      [medUpAngRad/2.0, 0.0, -medScanAngRad],
+                      [-medUpAngRad/2.0, 0.0, medScanAngRad],
+                      [0.0, 0.0, 0.0],
+                      [0.0, 0.0, -discAngleRad],
+                      [0.0, 0.0, 0.0] \
+                      ]
+    MOSshortRates = [ \
+                    [0.0, 0.0, -medAngRate],
+                    [0.0, 0.0, medAngRate],
+                    [0.0, 0.0, 0.0],
+                    [0.0, 0.0, -discAngRate],
+                    [0.0, 0.0, 0.0]\
+                    ]
+    self.orbitSequence['O5'] = SequenceElement(MOSshortTimes, 'marsPoint', MOSshortAngles,
+                                                MOSshortRates)
+    SOSShortAngle = 7.0*math.pi/180.0
+    SOSShortTime = 12.0*60.0
+    SOSShortRate = 2.0*SOSShortAngle/SOSShortTime
+    SOSshortTimes = [int(5.5*60), int(12*60), int(1.5*60)]
+    SOSshortAngles = [ \
+                      [0.0, 0.0, 0.0],
+                      [0.0, 0.0, -SOSShortAngle],
+                      [0.0, 0.0, 0.0] \
+                      ]
+    SOSshortRates = [ \
+                     [0.0, 0.0, 0.0],
+                     [0.0, 0.0, -SOSShortRate],
+                     [0.0, 0.0, 0.0]\
+                     ]
+    self.orbitSequence['O4'] = SequenceElement(SOSshortTimes, 'marsPoint', SOSshortAngles,
+                                                   SOSshortRates)
+    self.orbitSequence['EP'] = SequenceElement([int(6*60*60)], 'earthPoint')
+ 
+
 
  def updateStartCalendar(self, newCalendar):
      self.spiceLocal.UTCCalInit = newCalendar
+     self.TheEMMSim.SpiceObject.UTCCalInit = newCalendar
  def applyInitialState(self):
     self.spiceLocal.SelfInit()
     self.spiceLocal.UpdateState(0)
@@ -137,40 +228,119 @@ class PowerSimBase(SimulationBaseClass.SimBaseClass):
         self.TheEMMSim.scanRate = opElement.rasterRates
         self.TheEMMSim.rasterTimes = opElement.rasterTimes
         self.TheEMMSim.modeRequest = opElement.rasterMode
+        self.modeData.append([self.TheEMMSim.TotalSim.CurrentNanos, opElement.rasterMode])
         self.TheEMMSim.ExecuteSimulation()
+ def generateMissCSV(self, outputName):
+    with open(outputName, 'w') as csvfile:
+        missWriter = csv.writer(csvfile, delimiter=',')
+        missWriter.writerow(['Time (s)', 'Solar Array Miss (d)', 'ADCS State'])
+        modeIndex = 0
+        currentMode = self.modeData[modeIndex]
+        nextMode = self.modeData[modeIndex+1]
+        solarArrayMiss = self.TheEMMSim.pullMessageLogData("solar_array_sun_bore.missAngle")
+        for i in range(solarArrayMiss.shape[0]):
+            currentRow = solarArrayMiss[i, :]
+            while(currentRow[0] >= nextMode[0] and modeIndex+1 < len(self.modeData)):
+                modeIndex += 1
+                currentMode = nextMode
+                if(modeIndex+1 < len(self.modeData)):
+                    nextMode = self.modeData[modeIndex+1]
+            missWriter.writerow([currentRow[0]*1.0E-9/60.0, currentRow[1]*180.0/math.pi,
+               currentMode[1]])
+
+
+
 
 #Instantiate a copy of the EMM vehicle/FSW
-powerSim = PowerSimBase()
-powerSim.updateStartCalendar('MAY 10,2021  06:37:31.4219 (UTC)')
-powerSim.applyInitialState()
-powerSim.TheEMMSim.InitializeSimulation()
-powerSim.ConfigureStopTime(int(10*1E9))
-powerSim.TheEMMSim.modeRequest = 'safeMode'
-powerSim.TheEMMSim.ConfigureStopTime(int(30*1E9))
-powerSim.TheEMMSim.ExecuteSimulation()
-powerSim.iterateThroughSequence(['C1', 'O3', 'C2', 'O4', 'C3'])
+obsSequence = ['O4', 'C10', 'O4', 'C10', 'O4', 'C5', 'O6', 'C5', 'O4',
+                 'C10', 'O4', 'C10', 'O4', 'C10', 'O4', 'C7', 'O3', 'C2', 'O1', 'C2', 'O3', 'C2',
+                  'O1', 'C2', 'O3', 'EP', 'O1', 'C9', 'O6', 'C5', 'O4', 'C5', 'O6',
+                  'C5', 'O4', 'C10', 'O4', 'C10', 'O4', 'C10']
 
-solarArrayMiss = powerSim.TheEMMSim.pullMessageLogData("solar_array_sun_bore.missAngle")
-dataInstBore = powerSim.TheEMMSim.GetLogVariableData('instrumentBoresight.boreVecPoint')
-sigmaCMD = powerSim.TheEMMSim.pullMessageLogData('att_cmd_output.sigma_BR', range(3))
-sigmaTruth = powerSim.TheEMMSim.pullMessageLogData('inertial_state_output.sigma', range(3))
+powerSimOP = powerSimBase()
+powerSimOP.updateStartCalendar('AUG 27,2021  01:30:15.0 (UTC)')
+powerSimOP.applyInitialState()
+powerSimOP.TheEMMSim.InitializeSimulation()
+powerSimOP.ConfigureStopTime(int(10*1E9))
+powerSimOP.TheEMMSim.modeRequest = 'safeMode'
+powerSimOP.TheEMMSim.ConfigureStopTime(int(30*1E9))
+powerSimOP.TheEMMSim.ExecuteSimulation()
+
+powerSimOP.iterateThroughSequence(obsSequence)# 'C2', 'O4', 'C3', 'O2'])
+powerSimOP.generateMissCSV('optimalZeroNuPrime.csv')
+powerSimOP.TheEMMSim.TotalSim.terminateSimulation()
+
+powerSimSub = powerSimBase()
+powerSimSub.updateStartCalendar('AUG 27,2021  01:30:15.0 (UTC)')
+baseMarsTrans = numpy.array([0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]).reshape((3,3))
+powerSimSub.TheEMMSim.marsPointData.inputSecMessName = "" #No constraint
+betaPrime = -math.pi/2.0 +math.pi - 27.5*math.pi/180.0
+transMat = RigidBodyKinematics.Euler1232C(numpy.array([0.0, betaPrime, 0.0]).reshape((3,1)))
+totalTrans = transMat*baseMarsTrans
+powerSimSub.TheEMMSim.baseMarsTrans = totalTrans.reshape((1,9)).tolist()[0]
+SimulationBaseClass.SetCArray(totalTrans.reshape((1,9)).tolist()[0], 'double', powerSimSub.TheEMMSim.marsPointData.TPoint2Bdy)
+powerSimSub.TheEMMSim.VehOrbElemObject.CurrentElem.f = 10.0
+
+powerSimSub.TheEMMSim.createNewEvent("constFlip", int(1E10), True, ["self.VehOrbElemObject.CurrentElem.f < 3.14/2.0"],
+                                     ["import math",
+                                      "betaPrime = -math.pi/2.0 + 27.5*math.pi/180.0",
+                                      "import RigidBodyKinematics",
+                                      "baseMarsTrans = numpy.array([0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]).reshape((3,3))",
+                                      "transMat = RigidBodyKinematics.Euler1232C(numpy.array([0.0, betaPrime, 0.0]).reshape((3,1)))",
+                                      "totalTrans = transMat*baseMarsTrans",
+                                      "self.baseMarsTrans = totalTrans.reshape((1,9)).tolist()[0]",
+                                      "print 'Oh I made it'"])
+
+powerSimSub.applyInitialState()
+powerSimSub.TheEMMSim.InitializeSimulation()
+powerSimSub.ConfigureStopTime(int(10*1E9))
+powerSimSub.TheEMMSim.modeRequest = 'safeMode'
+powerSimSub.TheEMMSim.ConfigureStopTime(int(30*1E9))
+powerSimSub.TheEMMSim.ExecuteSimulation()
+powerSimSub.iterateThroughSequence(obsSequence)
+powerSimSub.generateMissCSV('constantZeroNuPrime.csv')
+
+
+trueAnomOP = powerSimSub.TheEMMSim.pullMessageLogData("OrbitalElements.f")
+solarArrayMissOP = powerSimOP.TheEMMSim.pullMessageLogData("solar_array_sun_bore.missAngle")
+dataInstBoreOP = powerSimOP.TheEMMSim.GetLogVariableData('instrumentBoresight.boreVecPoint')
+sigmaCMDOP = powerSimOP.TheEMMSim.pullMessageLogData('att_cmd_output.sigma_BR', range(3))
+sigmaTruthOP = powerSimOP.TheEMMSim.pullMessageLogData('inertial_state_output.sigma', range(3))
+
+highGainMess = powerSimOP.TheEMMSim.pullMessageLogData("high_gain_earth_bore.missAngle")
+
+solarArrayMissSub = powerSimSub.TheEMMSim.pullMessageLogData("solar_array_sun_bore.missAngle")
+dataInstBoreSub = powerSimSub.TheEMMSim.GetLogVariableData('instrumentBoresight.boreVecPoint')
+sigmaCMDSub = powerSimSub.TheEMMSim.pullMessageLogData('att_cmd_output.sigma_BR', range(3))
+sigmaTruthSub = powerSimSub.TheEMMSim.pullMessageLogData('inertial_state_output.sigma', range(3))
 
 plt.figure(1)
-plt.plot(solarArrayMiss[:,0]*1.0E-9, solarArrayMiss[:,1]*180/math.pi)
+plt.plot(solarArrayMissOP[:,0]*1.0E-9, solarArrayMissOP[:,1]*180/math.pi)
+plt.plot(solarArrayMissSub[:,0]*1.0E-9, solarArrayMissSub[:,1]*180/math.pi, 'g--')
 plt.xlabel('Time (s)')
 plt.ylabel('Solar Array Miss (d)')
 
 plt.figure(2)
-plt.plot(dataInstBore[:,2], dataInstBore[:,3])
+plt.plot(dataInstBoreOP[:,2], dataInstBoreOP[:,3], dataInstBoreSub[:,2], dataInstBoreSub[:,3], 'g--')
 plt.xlabel('y-component (-)')
 plt.ylabel('z-component (-)')
 
 plt.figure(3)
-plt.plot(sigmaCMD[:,0]*1.0E-9, sigmaCMD[:,1], 'b', sigmaTruth[:,0]*1.0E-9, sigmaTruth[:,1], 'b--')
-plt.plot(sigmaCMD[:,0]*1.0E-9, sigmaCMD[:,2], 'g', sigmaTruth[:,0]*1.0E-9, sigmaTruth[:,2], 'g--')
-plt.plot(sigmaCMD[:,0]*1.0E-9, sigmaCMD[:,3], 'r', sigmaTruth[:,0]*1.0E-9, sigmaTruth[:,3], 'r--')
+plt.plot(sigmaTruthOP[:,0]*1.0E-9, sigmaTruthOP[:,1], 'b')
+plt.plot(sigmaTruthOP[:,0]*1.0E-9, sigmaTruthOP[:,2], 'g')
+plt.plot(sigmaTruthOP[:,0]*1.0E-9, sigmaTruthOP[:,3], 'r')
 plt.xlabel('Time (s)')
 plt.ylabel('Attitude MRP (-)')
+
+plt.figure(4)
+plt.plot(trueAnomOP[:,0]*1.0E-9, trueAnomOP[:,1]*180.0/math.pi)
+plt.xlabel('Time (s)')
+plt.ylabel('True Anomaly (d)')
+
+plt.figure(5)
+plt.plot(highGainMess[:,0]*1.0E-9, highGainMess[:,1]*180/math.pi)
+plt.xlabel('Time (s)')
+plt.ylabel('Solar Array Miss (d)')
 
 plt.show()
 ##Log a handful of messages to examine vehicle performance
