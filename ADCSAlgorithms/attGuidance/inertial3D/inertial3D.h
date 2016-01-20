6e29922300807f6ec895786d489d6693a16fc8e9
Mar Cols Margenet <maria.colsmargenet@colorado.edu>
2016-01-20 11:49:39 -0700

    inertial pointing reference generation
    Former-commit-id: 885a8e99ac0fed52932ac3c44a7fae59f5e5068b
    Former-commit-id: ba40921046288784ec68bdec55ec85c497cd9173

--- ADCSAlgorithms/attGuidance/inertial3D/inertial3D.h
+++ ADCSAlgorithms/attGuidance/inertial3D/inertial3D.h
@@ -14,26 +14,15 @@
 /*! @brief Top level structure for the sub-module routines. */
 typedef struct {
     /* declare module private variables */
-    double sigma_R0N[3];                            /*!<        MRP from inertial frame N to original Reference frame R0
-                                                                at initial time t0 */
-    double sigma_R0R[3];                            /*!<        MRP from corrected reference frame to original reference frame R0
-                                                                This is the same as [BcB] going from primary body frame B
-                                                                to the corrected body frame Bc */
-    double *sigma_BcB;                              /*!<        MRP from primary body frame B to corrected body frame Bc */
-    double omega_RN_N[3];                           /*!< [r/s]  angular velocity vector of R relative to inertial N
-                                                                in N-frame components */
     double sigma_RN[3];                             /*!<        MRP from inertial frame N to corrected reference frame R */
-    uint64_t priorTime;                             /*!< [ns]   Last time the guidance module is called */
-
+    uint64_t priorTime;                             /*!<        [ns]   Last time the guidance module is called */
     
     /* declare module IO interfaces */
-    char outputDataName[MAX_STAT_MSG_LENGTH];       /*!< The name of the output message*/
-    char inputNavName[MAX_STAT_MSG_LENGTH];         /*!< The name of the Navigation Input message */
-    int32_t outputMsgID;                            /*!< ID for the outgoing message */
-    int32_t inputNavID;                             /*!< ID for the incoming navigation message */
-
-
-    attGuidOut attGuidOut;                          /*!< -- copy of the output message */
+    char outputDataName[MAX_STAT_MSG_LENGTH];       /*!<        The name of the output message */
+    int32_t outputMsgID;                            /*!<        ID for the outgoing message */
+    
+    /* copy of the output message */
+    attRefOut attRefOut;
 
 }inertial3DConfig;
 
@@ -46,13 +35,10 @@ extern "C" {
     void Update_inertial3D(inertial3DConfig *ConfigData, uint64_t callTime, uint64_t moduleID);
     void Reset_inertial3D(inertial3DConfig *ConfigData);
 
-    void computeInertial3DAttitudeError(double sigma_BN[3],
-                                          double omega_BN_B[3],
-                                          inertial3DConfig *ConfigData,
-                                          double sigma_BR[3],
-                                          double omega_BR_B[3],
-                                          double omega_RN_B[3],
-                                          double domega_RN_B[3]);
+    void computeInertialPointingReference(inertial3DConfig *ConfigData,
+                                          double sigma_RN[3],
+                                          double omega_RN_N[3],
+                                          double domega_RN_N[3]);
 
 #ifdef __cplusplus
 }
