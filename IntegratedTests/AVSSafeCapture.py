7d9a6f7aefdd5b5f711f801f9eb02d36a6418418
Patrick Kenneally <patrick.kenneally@colorado.edu>
2016-02-20 13:58:17 -0700

    Formatting and re-fixing monte-carlo errors
    Former-commit-id: 7370b0a02c8e6b7e85f6091ab9c22597a468bc8c
    Former-commit-id: 3fa3cd323e6526b2793da71c4deb941415cb7dc5

--- IntegratedTests/AVSSafeCapture.py
+++ IntegratedTests/AVSSafeCapture.py
@@ -1,4 +1,5 @@
 ﻿import sys, os, inspect
+
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
 sys.path.append(path + '/../PythonModules/')
@@ -54,61 +55,40 @@ def checkSlewAccuracy(DataCSSTruth, FSWsHat, CSSEstAccuracyThresh,
 
 
 def executeAVSSafeCapture(TheAVSSim):
-    TheAVSSim.TotalSim.logThisMessage("acs_thruster_cmds", int(1E8))
-    TheAVSSim.TotalSim.logThisMessage("inertial_state_output", int(1E9))
-    TheAVSSim.AddVariableForLogging('CSSWlsEst.numActiveCss', int(1E8))
-    TheAVSSim.TotalSim.logThisMessage("OrbitalElements", int(1E9))
-
-    TheAVSSim.VehOrbElemObject.CurrentElem.a = 188767262.18*1000.0
-    TheAVSSim.VehOrbElemObject.CurrentElem.e = 0.207501
-    TheAVSSim.VehOrbElemObject.CurrentElem.i = 0.0
-    TheAVSSim.VehOrbElemObject.CurrentElem.Omega = 0.0
-    TheAVSSim.VehOrbElemObject.CurrentElem.omega = 0.0
-    TheAVSSim.VehOrbElemObject.CurrentElem.f = 70.0*math.pi/180.0
-    # Convert those OEs to cartesian
-    TheAVSSim.VehOrbElemObject.Elements2Cartesian()
-    PosVec = ctypes.cast(TheAVSSim.VehOrbElemObject.r_N.__long__(),
-       ctypes.POINTER(ctypes.c_double))
-    VelVec = ctypes.cast(TheAVSSim.VehOrbElemObject.v_N.__long__(),
-      ctypes.POINTER(ctypes.c_double))
-    TheAVSSim.VehDynObject.PositionInit = sim_model.DoubleVector([PosVec[0], PosVec[1], PosVec[2]])
-    TheAVSSim.VehDynObject.VelocityInit = sim_model.DoubleVector([VelVec[0], VelVec[1], VelVec[2]])
-
     TheAVSSim.TotalSim.logThisMessage("sun_safe_att_err", int(1E9))
     TheAVSSim.TotalSim.logThisMessage("css_wls_est", int(1E9))
     TheAVSSim.TotalSim.logThisMessage("solar_array_sun_bore", int(1E9))  # solar array boresight angles
     TheAVSSim.AddVectorForLogging('CSSPyramid1HeadA.sHatStr', 'double', 0, 2, int(1E9))
 
-
     TheAVSSim.InitializeSimulation()
-    TheAVSSim.ConfigureStopTime(int(30*1E9))
+    TheAVSSim.ConfigureStopTime(int(30 * 1E9))
     TheAVSSim.ExecuteSimulation()
     # TheAVSSim.modeRequest = 'safeMode'
-    # TheAVSSim.ConfigureStopTime(int(60*10*1E9))
+    # TheAVSSim.ConfigureStopTime(int(60 * 10 * 1E9))
     # TheAVSSim.ExecuteSimulation()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     TheAVSSim = AVSSim.AVSSim()
-    # TheAVSSim.TotalSim.logThisMessage("acs_thruster_cmds", int(1E8))
-    # TheAVSSim.TotalSim.logThisMessage("inertial_state_output", int(1E9))
-    # TheAVSSim.AddVariableForLogging('CSSWlsEst.numActiveCss', int(1E8))
-    # TheAVSSim.TotalSim.logThisMessage("OrbitalElements", int(1E9))
-    #
-    # TheAVSSim.VehOrbElemObject.CurrentElem.a = 188767262.18*1000.0
-    # TheAVSSim.VehOrbElemObject.CurrentElem.e = 0.207501
-    # TheAVSSim.VehOrbElemObject.CurrentElem.i = 0.0
-    # TheAVSSim.VehOrbElemObject.CurrentElem.Omega = 0.0
-    # TheAVSSim.VehOrbElemObject.CurrentElem.omega = 0.0
-    # TheAVSSim.VehOrbElemObject.CurrentElem.f = 70.0*math.pi/180.0
-    # # Convert those OEs to cartesian
-    # TheAVSSim.VehOrbElemObject.Elements2Cartesian()
-    # PosVec = ctypes.cast(TheAVSSim.VehOrbElemObject.r_N.__long__(),
-    #    ctypes.POINTER(ctypes.c_double))
-    # VelVec = ctypes.cast(TheAVSSim.VehOrbElemObject.v_N.__long__(),
-    #   ctypes.POINTER(ctypes.c_double))
-    # TheAVSSim.VehDynObject.PositionInit = sim_model.DoubleVector([PosVec[0], PosVec[1], PosVec[2]])
-    # TheAVSSim.VehDynObject.VelocityInit = sim_model.DoubleVector([VelVec[0], VelVec[1], VelVec[2]])
+    TheAVSSim.TotalSim.logThisMessage("acs_thruster_cmds", int(1E8))
+    TheAVSSim.TotalSim.logThisMessage("inertial_state_output", int(1E9))
+    TheAVSSim.AddVariableForLogging('CSSWlsEst.numActiveCss', int(1E8))
+    TheAVSSim.TotalSim.logThisMessage("OrbitalElements", int(1E9))
+
+    TheAVSSim.VehOrbElemObject.CurrentElem.a = 188767262.18 * 1000.0
+    TheAVSSim.VehOrbElemObject.CurrentElem.e = 0.207501
+    TheAVSSim.VehOrbElemObject.CurrentElem.i = 0.0
+    TheAVSSim.VehOrbElemObject.CurrentElem.Omega = 0.0
+    TheAVSSim.VehOrbElemObject.CurrentElem.omega = 0.0
+    TheAVSSim.VehOrbElemObject.CurrentElem.f = 70.0 * math.pi / 180.0
+    # Convert those OEs to cartesian
+    TheAVSSim.VehOrbElemObject.Elements2Cartesian()
+    PosVec = ctypes.cast(TheAVSSim.VehOrbElemObject.r_N.__long__(),
+                         ctypes.POINTER(ctypes.c_double))
+    VelVec = ctypes.cast(TheAVSSim.VehOrbElemObject.v_N.__long__(),
+                         ctypes.POINTER(ctypes.c_double))
+    TheAVSSim.VehDynObject.PositionInit = sim_model.DoubleVector([PosVec[0], PosVec[1], PosVec[2]])
+    TheAVSSim.VehDynObject.VelocityInit = sim_model.DoubleVector([VelVec[0], VelVec[1], VelVec[2]])
 
     executeAVSSafeCapture(TheAVSSim)
 
@@ -117,30 +97,30 @@ if __name__ == "__main__":
     solarArrayMiss = TheAVSSim.pullMessageLogData("solar_array_sun_bore.missAngle")
     numCSSActive = TheAVSSim.GetLogVariableData('CSSWlsEst.numActiveCss')
 
-    CSSEstAccuracyThresh = 17.5*math.pi/180.0
-    accuracyFailCounter = checkCSSEstAccuracy(DataCSSTruth, FSWsHat, 
-       CSSEstAccuracyThresh)
+    CSSEstAccuracyThresh = 17.5 * math.pi / 180.0
+    accuracyFailCounter = checkCSSEstAccuracy(DataCSSTruth, FSWsHat,
+                                              CSSEstAccuracyThresh)
 
     slewFinishTime = 150.0
     desiredSunBdy = [0.0, 0.0, 1.0]
-    controlFailCounter =  checkSlewAccuracy(DataCSSTruth, FSWsHat, CSSEstAccuracyThresh,
-       slewFinishTime, desiredSunBdy)
+    controlFailCounter = checkSlewAccuracy(DataCSSTruth, FSWsHat, CSSEstAccuracyThresh,
+                                           slewFinishTime, desiredSunBdy)
 
     plt.figure(1)
-    plt.plot(FSWsHat[:,0]*1.0E-9, FSWsHat[:,1], 'b', DataCSSTruth[:,0]*1.0E-9, DataCSSTruth[:,1], 'b--')
-    plt.plot(FSWsHat[:,0]*1.0E-9, FSWsHat[:,2], 'g', DataCSSTruth[:,0]*1.0E-9, DataCSSTruth[:,2], 'g--')
-    plt.plot(FSWsHat[:,0]*1.0E-9, FSWsHat[:,3], 'r', DataCSSTruth[:,0]*1.0E-9, DataCSSTruth[:,3], 'r--')
+    plt.plot(FSWsHat[:, 0] * 1.0E-9, FSWsHat[:, 1], 'b', DataCSSTruth[:, 0] * 1.0E-9, DataCSSTruth[:, 1], 'b--')
+    plt.plot(FSWsHat[:, 0] * 1.0E-9, FSWsHat[:, 2], 'g', DataCSSTruth[:, 0] * 1.0E-9, DataCSSTruth[:, 2], 'g--')
+    plt.plot(FSWsHat[:, 0] * 1.0E-9, FSWsHat[:, 3], 'r', DataCSSTruth[:, 0] * 1.0E-9, DataCSSTruth[:, 3], 'r--')
 
     plt.figure(2)
-    plt.plot(numCSSActive[:,0]*1.0E-9, numCSSActive[:,1])
+    plt.plot(numCSSActive[:, 0] * 1.0E-9, numCSSActive[:, 1])
 
     plt.figure(3)
-    plt.plot(solarArrayMiss[:,0]*1.0E-9, solarArrayMiss[:,1]*180/math.pi)
+    plt.plot(solarArrayMiss[:, 0] * 1.0E-9, solarArrayMiss[:, 1] * 180 / math.pi)
     plt.xlabel('Time (s)')
     plt.ylabel('Solar Array Miss (d)')
 
-    if(len(sys.argv) > 1):
-       if(sys.argv[1] == 'True'):
-          plt.show()
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == 'True'):
+            plt.show()
 
-    #sys.exit(accuracyFailCounter + controlFailCounter)
+            # sys.exit(accuracyFailCounter + controlFailCounter)
