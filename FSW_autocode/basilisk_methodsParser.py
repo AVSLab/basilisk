8582bc9dbe1b3de4dab1b9c8c18fdae40fb6116d
Mar Cols Margenet <maria.colsmargenet@colorado.edu>
2016-08-03 16:18:40 -0600

    parser to generate source and header files with all alg methods
    Former-commit-id: b30322ac0752de9181c7b34a1eda2ab796db5c6d
    Former-commit-id: 3e4fb7c28a9dc9705e4428b5f1a4499ff380790c

--- FSW_autocode/basilisk_methodsParser.py
+++ FSW_autocode/basilisk_methodsParser.py
@@ -2,10 +2,12 @@ import sys, os, inspect
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
 sys.path.append(path + '/../PythonModules/')
+
 import AVSSim
 import alg_contain
 import sim_model
 import numpy as np
+np.set_printoptions(precision=9)
 
 # ----------------------------- METHODS ----------------------------- #
 def parseSwigVars(list):
@@ -27,14 +29,92 @@ def evalParsedList(list):
         if type(methodObject).__name__ == "SwigPyObject":
             methodAddress = sim_model.getObjectAddress(methodObject)
             addressDict[methodName] = int(methodAddress)
-    print addressDict
+    return addressDict
+
+def checkMethodType(methodName):
+    str_selfInit = 'SelfInit'
+    str_crossInit = 'CrossInit'
+    str_update = 'Update'
+    str_reset = 'Reset'
+    if methodName[0:len(str_selfInit)] == str_selfInit:
+        return str_selfInit
+    elif methodName[0:len(str_crossInit)] == str_crossInit:
+        return str_crossInit
+    elif methodName[0:len(str_update)] == str_update:
+        return str_update
+    elif methodName[0:len(str_reset)] == str_reset:
+        return str_reset
+    else:
+        raise ValueError('Cannot recognize the method. Parse better.')
+
+def findAddressMatch(methodType, address):
+    dict = eval(methodType + '_dict')
+    for k, v in dict.items():
+        if v == address:
+            return k
+    raise ValueError('Unable to find address match.')
+
+def writeCFiles(sourceFile, headerFile, taskName, str_alg):
+    str_EMMData = '(EMMConfigData *data)'
+    algList = eval(str_alg)
+    if len(algList) > 0:
+        void_def = '\n void ' + taskName + '_' + str_alg + str_EMMData
+        headerFile.write(void_def + ';\n')
+        sourceFile.write('\n')
+        sourceFile.write(void_def)
+        sourceFile.write('\n { \n')
+        for algName in algList:
+            sourceFile.write('\t')
+            sourceFile.write(algName)
+            sourceFile.write('\n')
+        sourceFile.write('}')
+        sourceFile.write('\n')
 
 # ----------------------------- MAIN ----------------------------- #
 if __name__ == "__main__":
+    alg_source = open(path + '/../../Basilisk/FSW_autocode/EMMAlgorithms.c', 'w+')
+    alg_header = open(path + '/../../Basilisk/FSW_autocode/EMMAlgorithms.h', 'w+')
+
     TheAVSSim = AVSSim.AVSSim()
+
+    # Model Wraps
+    SelfInit_dict = {}
+    CrossInit_dict = {}
+    Update_dict = {}
+    Reset_dict = {}
+    TheAVSList = dir(TheAVSSim)
+    for elemName in TheAVSList:
+        elem = eval('TheAVSSim.' + elemName)
+        if type(elem) == alg_contain.AlgContain:
+            SelfInit_dict[elem.ModelTag] = int(elem.getSelfInitAddress())
+            CrossInit_dict[elem.ModelTag] = int(elem.getCrossInitAddress())
+            Update_dict[elem.ModelTag] = int(elem.getUpdateAddress())
+            if (elem.getResetAddress()):
+                Reset_dict[elem.ModelTag] = int(elem.getResetAddress())
+                reset = int(elem.getResetAddress())
+    def printWrap_allDicts():
+        print 'selfInit_ModelDict = ', SelfInit_dict
+        print 'crossInit_ModelDict = ', CrossInit_dict
+        print 'update_ModelDict = ', Update_dict
+        print 'reset_ModelDict = ', Reset_dict
+        print '\n'
+    printWrap_allDicts()
+
+    # Model Data
+    dict_TagID = {}
+    ID_counter = 0
     taskIdxList = [10, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 26]
-    #taskIdxList = [10]
+    # Tasks Out:
+    #   11. attitudeNav
+    #   16. singleAxisSpinTask
+    #   17. orbitAxisSpinTask
+    #   18. axisScanTask
     for i_task in taskIdxList:
+        allAlgSelfInit = []
+        allAlgCrossInit = []
+        allAlgUpdate = []
+        allAlgReset = []
+
         print 'Task Index = ', i_task
         task = TheAVSSim.TaskList[i_task]
         print 'Task Name = ', task.Name
@@ -44,31 +124,44 @@ if __name__ == "__main__":
             sysMod = sys.modules[module]
             dirList = dir(sysMod)
             parsed_dirList = parseSwigVars(dirList)
-            evalParsedList(parsed_dirList)
-        print '\n'
+            addressDict = evalParsedList(parsed_dirList)
+            for k, v in addressDict.items():
+                methodType = checkMethodType(k)
+                modelTag = findAddressMatch(methodType, v)
+                try:
+                    modelID = dict_TagID[modelTag]
+                except:
+                    dict_TagID[modelTag] = ID_counter
+                    modelID = ID_counter
+                    ID_counter += 1
 
-    addressDictMatch = {}
-    TheAVSList = dir(TheAVSSim)
-    for elemName in TheAVSList:
-        elem = eval('TheAVSSim.' + elemName)
-        if type(elem) == alg_contain.AlgContain:
-            addressDictMatch['SelfInit'] = int(elem.getSelfInitAddress())
-            addressDictMatch['CrossInit'] = int(elem.getCrossInitAddress())
-            addressDictMatch['Update'] = int(elem.getUpdateAddress())
-            if (elem.getResetAddress()):
-                addressDictMatch['Reset'] = int(elem.getResetAddress())
-            print 'Wrap = ', elemName
-            print 'Model Tag = ', elem.ModelTag
-            print addressDictMatch
+                theString = k +'(&(data->' + modelTag + '), ' + str(modelID) + ')'
+                theList = eval('allAlg' + methodType)
+                theList.append(theString)
+
+        def printTaskData_AllAlg():
+            print 'allAlgSelfInit = ', allAlgSelfInit
+            print 'allAlgCrossInit = ', allAlgCrossInit
+            print 'allAlgUpdate = ', allAlgUpdate
+            print 'allAlgReset = ', allAlgReset
             print '\n'
-        # elif sys.getsizeof(elem) == 64: # size of a class in the AVSSim
-        #     print elemName
-        # try:
-        #     mod = elem.__module__
-        #     print elemName
-        #     print type(elem)
-        # except:
-        #     continue
+        printTaskData_AllAlg()
+
+        str_algSelfInit = 'allAlgSelfInit'
+        str_algCrossInit = 'allAlgCrossInit'
+        str_algUpdate = 'allAlgUpdate'
+        str_algReset = 'allAlgReset'
+        writeCFiles(alg_source, alg_header, task.Name, str_algSelfInit)
+        writeCFiles(alg_source, alg_header, task.Name, str_algCrossInit)
+        writeCFiles(alg_source, alg_header, task.Name, str_algUpdate)
+        writeCFiles(alg_source, alg_header, task.Name, str_algReset)
+
+    alg_source.close()
+    alg_header.close()
+
+
+
+
 
 
 
