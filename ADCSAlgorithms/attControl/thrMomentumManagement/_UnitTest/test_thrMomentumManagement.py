1b9c2ae9e82ee558a18a71ef88877e8a162e0574
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2016-08-19 11:48:34 -0600

    [BSK-279] WIP, first cut at unit test script, not fully functioning yet.
    Former-commit-id: 33c8e281bbe43d1f776ef748edf797d051a5e331
    Former-commit-id: 38baf560c6f334ff699d392fb863ab7a269ea220

--- ADCSAlgorithms/attControl/thrMomentumManagement/_UnitTest/test_thrMomentumManagement.py
+++ ADCSAlgorithms/attControl/thrMomentumManagement/_UnitTest/test_thrMomentumManagement.py
@@ -16,15 +16,16 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 '''
 #
 #   Unit Test Script
-#   Module Name:        fswModuleTemplateParametrized
-#   Author:             (First Name) (Last Name)
-#   Creation Date:      Month Day, Year
+#   Module Name:        thrMomentumManagement
+#   Author:             Hanspeter Schaub
+#   Creation Date:      August 18, 2016
 #
 
-import pytest
 import sys, os, inspect
 import matplotlib.pyplot as plt
-# import packages as needed e.g. 'numpy', 'ctypes, 'math' etc.
+import numpy as np
+import pytest
+
 
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
@@ -36,9 +37,12 @@ sys.path.append(splitPath[0] + '/PythonModules')
 import SimulationBaseClass
 import alg_contain
 import unitTestSupport                  # general support file with common unit test functions
-import fswModuleTemplate                # import the module that is to be tested
-import MRP_Steering                     # import module(s) that creates the needed input message declaration
+import thrMomentumManagement            # import the module that is to be tested
 import macros
+import fswSetupRW
+import vehicleConfigData
+import rwNullSpace
+
 
 # Uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed.
 # @pytest.mark.skipif(conditionstring)
@@ -47,20 +51,18 @@ import macros
 # Provide a unique test method name, starting with 'test_'.
 # The following 'parametrize' function decorator provides the parameters and expected results for each
 #   of the multiple test runs for this test.
-@pytest.mark.parametrize("param1, param2", [
-    (1, 1),
-    (1, 3),
-    (2, 2),
+@pytest.mark.parametrize("param1", [
+    (1)
 ])
 
 # update "module" in this function name to reflect the module name
-def test_module(show_plots, param1, param2):
+def test_thrMomentumManagement(show_plots, param1):
     # each test method requires a single assert method to be called
-    [testResults, testMessage] = fswModuleTestFunction(show_plots, param1, param2)
+    [testResults, testMessage] = thrMomentumManagementTestFunction(show_plots, param1)
     assert testResults < 1, testMessage
 
 
-def fswModuleTestFunction(show_plots, param1, param2):
+def thrMomentumManagementTestFunction(show_plots, param1):
     testFailCount = 0                       # zero unit test result counter
     testMessages = []                       # create empty array to store test log messages
     unitTaskName = "unitTask"               # arbitrary name (don't change)
@@ -80,48 +82,77 @@ def fswModuleTestFunction(show_plots, param1, param2):
 
 
     # Construct algorithm and associated C++ container
-    moduleConfig = fswModuleTemplate.fswModuleTemplateConfig()                          # update with current values
+    moduleConfig = thrMomentumManagement.thrMomentumManagementConfig()                          # update with current values
     moduleWrap = alg_contain.AlgContain(moduleConfig,
-                                        fswModuleTemplate.Update_fswModuleTemplate,     # update with current values
-                                        fswModuleTemplate.SelfInit_fswModuleTemplate,   # update with current values
-                                        fswModuleTemplate.CrossInit_fswModuleTemplate,  # update with current values
-                                        fswModuleTemplate.Reset_fswModuleTemplate)      # update with current values
-    moduleWrap.ModelTag = "fswModuleTemplate"                                        # update python name of test module
+                                        thrMomentumManagement.Update_thrMomentumManagement,     # update with current values
+                                        thrMomentumManagement.SelfInit_thrMomentumManagement,   # update with current values
+                                        thrMomentumManagement.CrossInit_thrMomentumManagement,  # update with current values
+                                        thrMomentumManagement.Reset_thrMomentumManagement)      # update with current values
+    moduleWrap.ModelTag = "thrMomentumManagement"                                        # update python name of test module
 
     # Add test module to runtime call list
     unitTestSim.AddModelToTask(unitTaskName, moduleWrap, moduleConfig)
 
     # Initialize the test module configuration data
-    moduleConfig.inputDataName = "sampleInput"          # update with current values
-    moduleConfig.outputDataName = "sampleOutput"        # update with current values
-    moduleConfig.dummy = 1                              # update module parameter with required values
-    vector = [1., 2., 3.]
-    SimulationBaseClass.SetCArray(vector,
-                                  'double',
-                                  moduleConfig.dumVector)
+    moduleConfig.hs_min = 100./6000.*100.               # Nms
 
     # Create input message and size it because the regular creator of that message
     # is not part of the test.
-    inputMessageSize = 3*8                              # 3 doubles
-    unitTestSim.TotalSim.CreateNewMessage(unitProcessName,
-                                          moduleConfig.inputDataName,
-                                          inputMessageSize,
-                                          2)            # number of buffers (leave at 2 as default, don't make zero)
+    moduleConfig.inputVehicleConfigDataName = "vehicleConfigName"
+    moduleConfig.inputRWSpeedsName = "reactionwheel_speeds"
+    moduleConfig.inputRWConfigData = "rwa_config_data"
+    moduleConfig.outputDataName = "outputName"
 
-    inputMessageData = MRP_Steering.vehControlOut()     # Create a structure for the input message
-    sampleInputMessageVariable = [param1, param2, 0.7]       # Set up a list as a 3-vector
-    SimulationBaseClass.SetCArray(sampleInputMessageVariable,           # specify message variable
-                                  'double',                             # specify message variable type
-                                  inputMessageData.torqueRequestBody)   # write torque request to input message
-    unitTestSim.TotalSim.WriteMessageData(moduleConfig.inputDataName,
+    # wheelSpeeds Message
+    inputMessageSize = 36 * 8  # 36 doubles
+    unitTestSim.TotalSim.CreateNewMessage(unitProcessName,
+                                          moduleConfig.inputRWSpeedsName,
+                                          inputMessageSize,
+                                          2)  # number of buffers (leave at 2 as default, don't make zero)
+    rwSpeedMessage = rwNullSpace.RWSpeedData()
+    Omega = [10.0, -25.0, 50.0, 100.]
+    SimulationBaseClass.SetCArray(Omega,
+                                  'double',
+                                  rwSpeedMessage.wheelSpeeds)
+    unitTestSim.TotalSim.WriteMessageData(moduleConfig.inputRWSpeedsName,
                                           inputMessageSize,
                                           0,
-                                          inputMessageData)             # write data into the simulator
+                                          rwSpeedMessage)
+
+    # vehicleConfigData Message:
+    inputMessageSize = 18 * 8 + 8  # 18 doubles + 1 32bit integer
+    unitTestSim.TotalSim.CreateNewMessage(unitProcessName,
+                                          moduleConfig.inputVehicleConfigDataName,
+                                          inputMessageSize,
+                                          2)  # number of buffers (leave at 2 as default, don't make zero)
+    vehicleConfigOut = vehicleConfigData.vehicleConfigData()
+    BS = [1.0, 0.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 0.0, 1.0]
+    SimulationBaseClass.SetCArray(BS,
+                                  'double',
+                                  vehicleConfigOut.BS)
+    unitTestSim.TotalSim.WriteMessageData(moduleConfig.inputVehicleConfigDataName,
+                                          inputMessageSize,
+                                          0,
+                                          vehicleConfigOut)
+
+
+    # wheelConfigData Message
+    fswSetupRW.clearSetup()
+    Js = 0.1
+    fswSetupRW.create([1.0, 0.0, 0.0], Js)
+    fswSetupRW.create([0.0, 1.0, 0.0], Js)
+    fswSetupRW.create([0.0, 0.0, 1.0], Js)
+    fswSetupRW.create([0.5773502691896258, 0.5773502691896258, 0.5773502691896258], Js)
+    fswSetupRW.addToSpacecraft(moduleConfig.inputRWConfigData,
+                               unitTestSim.TotalSim,
+                               unitProcessName)
+
+
 
     # Setup logging on the test module output message so that we get all the writes to it
     unitTestSim.TotalSim.logThisMessage(moduleConfig.outputDataName, testProcessRate)
-    variableName = "dummy"                              # name the module variable to be logged
-    unitTestSim.AddVariableForLogging(moduleWrap.ModelTag + "." + variableName, testProcessRate)
 
     # Need to call the self-init and cross-init methods
     unitTestSim.InitializeSimulation()
@@ -130,65 +161,44 @@ def fswModuleTestFunction(show_plots, param1, param2):
     # NOTE: the total simulation time may be longer than this value. The
     # simulation is stopped at the next logging event on or after the
     # simulation end time.
-    unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))        # seconds to stop simulation
+    unitTestSim.ConfigureStopTime(macros.sec2nano(0.5))        # seconds to stop simulation
 
     # Begin the simulation time run set above
     unitTestSim.ExecuteSimulation()
-    
-    # reset the module to test this functionality
-    moduleWrap.Reset(1)     # this module reset function needs a time input (in NanoSeconds) 
 
-    # run the module again for an additional 1.0 seconds
-    unitTestSim.ConfigureStopTime(macros.sec2nano(2.0))        # seconds to stop simulation
-    unitTestSim.ExecuteSimulation()
-        
+    # # reset the module to test this functionality
+    # moduleWrap.Reset(1)     # this module reset function needs a time input (in NanoSeconds)
+    #
+    # # run the module again for an additional 1.0 seconds
+    # unitTestSim.ConfigureStopTime(macros.sec2nano(2.0))        # seconds to stop simulation
+    # unitTestSim.ExecuteSimulation()
+
 
     # This pulls the actual data log from the simulation run.
     # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
-    moduleOutputName = "outputVector"
+    moduleOutputName = "torqueRequestBody"
     moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
                                                   range(3))
-    variableState = unitTestSim.GetLogVariableData(moduleWrap.ModelTag + "." + variableName)
-    
+    print moduleOutput
+
     # set the filtered output truth states
-    trueVector=[];
     if param1==1:
-        if param2==1:
-            trueVector = [
-                       [2.0, 1.0, 0.7],
-                       [3.0, 1.0, 0.7],
-                       [4.0, 1.0, 0.7],
-                       [2.0, 1.0, 0.7],
-                       [3.0, 1.0, 0.7]
-                       ]
-        else:
-            if param2==3:
-                trueVector = [
-                       [2.0, 3.0, 0.7],
-                       [3.0, 3.0, 0.7],
-                       [4.0, 3.0, 0.7],
-                       [2.0, 3.0, 0.7],
-                       [3.0, 3.0, 0.7]
-                       ]
-            else:
-                testFailCount+=1
-                testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed with unsupported input parameters")
+        trueVector = [
+                   [2.0, 1.0, 0.7],
+                   [2.0, 1.0, 0.7]
+                   ]
     else:
-        if param1==2:
-            trueVector = [
-                       [3.0, 2.0, 0.7],
-                       [4.0, 2.0, 0.7],
-                       [5.0, 2.0, 0.7],
-                       [3.0, 2.0, 0.7],
-                       [4.0, 2.0, 0.7]
-                       ]
-        else:
-            testFailCount+=1
-            testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed with unsupported input parameters")
+        trueVector = [
+                   [2.0, 1.0, 0.7],
+                   [3.0, 1.0, 0.7]
+                   ]
+
+        # else:
+        #     testFailCount+=1
+        #     testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed with unsupported input parameters")
 
     # compare the module results to the truth values
     accuracy = 1e-12
-    dummyTrue = [1.0, 2.0, 3.0, 1.0, 2.0]
     for i in range(0,len(trueVector)):
         # check a vector values
         if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
@@ -198,29 +208,17 @@ def fswModuleTestFunction(show_plots, param1, param2):
                                 str(moduleOutput[i,0]*macros.NANO2SEC) +
                                 "sec\n")
 
-        # check a scalar double value
-        if not unitTestSupport.isDoubleEqual(variableState[i], dummyTrue[i], accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
-                                variableName + " unit test at t=" +
-                                str(variableState[i,0]*macros.NANO2SEC) +
-                                "sec\n")
 
-    # Note that we can continue to step the simulation however we feel like.
-    # Just because we stop and query data does not mean everything has to stop for good
-    unitTestSim.ConfigureStopTime(macros.sec2nano(0.6))    # run an additional 0.6 seconds
-    unitTestSim.ExecuteSimulation()
- 
     # If the argument provided at commandline "--show_plots" evaluates as true,
     # plot all figures
-    if show_plots:
+    # if show_plots:
         # plot a sample variable.
-        plt.figure(1)
-        plt.plot(variableState[:,0]*macros.NANO2SEC, variableState[:,1], label='Sample Variable')
-        plt.legend(loc='upper left')
-        plt.xlabel('Time [s]')
-        plt.ylabel('Variable Description [unit]')
-        plt.show()
+        # plt.figure(1)
+        # plt.plot(variableState[:,0]*macros.NANO2SEC, variableState[:,1], label='Sample Variable')
+        # plt.legend(loc='upper left')
+        # plt.xlabel('Time [s]')
+        # plt.ylabel('Variable Description [unit]')
+        # plt.show()
 
     #   print out success message if no error were found
     if testFailCount == 0:
@@ -236,8 +234,7 @@ def fswModuleTestFunction(show_plots, param1, param2):
 # stand-along python script
 #
 if __name__ == "__main__":
-    test_module(              # update "module" in function name
+    test_thrMomentumManagement(              # update "module" in function name
                  True,
-                 1,           # param1 value
-                 1            # param2 value
+                 1            # param1 value
                )
