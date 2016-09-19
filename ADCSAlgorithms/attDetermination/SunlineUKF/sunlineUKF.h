e23ddae230ff84f38cb53b90f7c3551752c79503
Scott Piggott <swpiggott@gmail.com>
2016-09-19 08:57:56 -0600

    [BSK 338] Added filter variables to the sunline KF header file in order to start writing filter-specific code.
    Former-commit-id: 5ff6676a396aaff02566b5cec8593b1308ffc427
    Former-commit-id: cd914f316bf8f34adea416230ce78ffdd1ed5848

--- ADCSAlgorithms/attDetermination/SunlineUKF/sunlineUKF.h
+++ ADCSAlgorithms/attDetermination/SunlineUKF/sunlineUKF.h
@@ -22,6 +22,9 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include "sensorInterfaces/CSSSensorData/cssComm.h"
 #include <stdint.h>
 
+#define SKF_N_STATES 2
+#define MAX_N_CSS_MEAS 8
+
 /*! \addtogroup ADCSAlgGroup
  * @{
  */
@@ -30,16 +33,42 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  Used to estimate the sun state in the vehicle body frame*/
 typedef struct {
 //    SingleCSSConfig CSSData[MAX_NUM_CSS_SENSORS]; /*!< -- The config data for the estimator*/
-    char OutputDataName[MAX_STAT_MSG_LENGTH]; /*!< The name of the output message*/
-    char InputDataName[MAX_STAT_MSG_LENGTH]; /*!< The name of the Input message*/
-    char InputPropsName[MAX_STAT_MSG_LENGTH]; /*!< [-] The name of the mass props message*/
+    char outputNavStateName[MAX_STAT_MSG_LENGTH]; /*!< The name of the output message*/
+    char inputCSSDataName[MAX_STAT_MSG_LENGTH]; /*!< The name of the Input message*/
+    char inputPropsName[MAX_STAT_MSG_LENGTH]; /*!< [-] The name of the mass props message*/
+
+	int numStates;                /*! [-] Number of states for this filter*/                
+	int countHalfSPs;             /*! [-] Number of sigma points over 2 */
+	int numObs;                   /*! [-] Number of measurements this cycle */
+	double beta;                  /*! [-] Beta parameter for filter */
+	double alpha;                 /*! [-] Alpha parameter for filter*/
+	double kappa;                 /*! [-] Kappa parameter for filter*/
+	double lambdaVal;             /*! [-] Lambda parameter for filter*/
+	double gamma;                 /*! [-] Gamma parameter for filter*/
+
+	double dt;                     /*! [s] seconds since last data epoch */
+	double TimeTag;                /*! [s]  Time tag for statecovar/etc */
+
+	double state[SKF_N_STATES];        // -- State estimate for time TimeTag
+	double sBar[SKF_N_STATES*SKF_N_STATES];         // -- Time updated covariance
+	double covar[SKF_N_STATES*SKF_N_STATES];        // -- covariance
+
+	double obs[MAX_N_CSS_MEAS];          // -- pseudorange observations
+	double yMeas[MAX_N_CSS_MEAS*SKF_N_STATES];        // -- Measurement model data
+
+	double SP[(2*SKF_N_STATES+1)*SKF_N_STATES];          // --    sigma point matrix
+
+	double Qnoise[SKF_N_STATES*SKF_N_STATES];       // -- process noise matrix
+	double SQnoise[SKF_N_STATES*SKF_N_STATES];      // -- cholesky of Qnoise
+
+	double Q_obs[MAX_N_CSS_MEAS];  // -- to put on diagonal of Q obs matrix
+
     uint32_t numActiveCss;   /*!< -- Number of currently active CSS sensors*/
-    uint32_t UseWeights;     /*!< -- Flag indicating whether or not to use weights for least squares*/
-    double SensorUseThresh;  /*!< -- Threshold below which we discount sensors*/
+    double sensorUseThresh;  /*!< -- Threshold below which we discount sensors*/
     //CSSWlsEstOut OutputData; /*!< -- Unit vector to the Sun in the spacecraft body frame*/
-    int32_t OutputMsgID;     /*!< -- ID for the outgoing body estimate message*/
-    int32_t InputMsgID;      /*!< -- ID for the incoming CSS sensor message*/
-    int32_t InputPropsID;    /*!< [-] ID for the incoming mass properties message*/
+    int32_t outputStateID;     /*!< -- ID for the outgoing body estimate message*/
+    int32_t inputCSSDataID;      /*!< -- ID for the incoming CSS sensor message*/
+    int32_t inputPropsID;    /*!< [-] ID for the incoming mass properties message*/
 }SunlineUKFConfig;
 
 #ifdef __cplusplus
