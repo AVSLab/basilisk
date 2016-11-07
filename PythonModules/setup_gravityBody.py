85b1b5dd0929df5207db53b195a19a7a66627203
Mar Cols Margenet <maria.colsmargenet@colorado.edu>
2016-11-07 08:52:44 -0700

    [BSK-321] first cut of new sim with hybrid control
    Former-commit-id: acd2d81f76c260eab38694e248b80e61ef954416
    Former-commit-id: 1c1a5b3ef59ea108b1f3df82b5db9e3a053de471

---
+++ PythonModules/setup_gravityBody.py
@@ -0,0 +1,66 @@
+import csv
+import math
+import six_dof_eom
+
+def LoadGravFromFile(FileName, GravBody, JParamsSelect):
+    csvfile = open(FileName, 'rb')
+    csvreader = csv.reader(csvfile)
+    FirstLine = True
+    NextJindex = 0
+    AllJParams = []
+    for row in csvreader:
+        if (FirstLine == True):
+            GravBody.mu = float(row[1])
+            GravBody.radEquator = float(row[0])
+            FirstLine = False
+        elif (int(row[0]) == JParamsSelect[NextJindex]):
+            LocalJParam = -math.sqrt(2 * JParamsSelect[NextJindex] + 1) * float(row[2])
+            AllJParams.append(LocalJParam)
+            NextJindex += 1
+            if (NextJindex >= len(JParamsSelect)):
+                break
+    return (AllJParams)
+
+
+def Add_sunGravityBody(sim, sun):
+    sun.BodyMsgName = "sun_planet_data"
+    sun.outputMsgName = "sun_display_frame_data"
+    sun.mu = 1.32712440018E20  # meters!
+    sun.IsCentralBody = True
+    sun.IsDisplayBody = True
+    sun.UseJParams = False
+    sim.VehDynObject.AddGravityBody(sun)
+
+def Add_earthGravityBody(sim, earth):
+    JParamsSelect = [2, 3, 4, 5, 6]
+    EarthGravFile = sim.simBasePath + '/External/LocalGravData/GGM03S.txt'
+    earth.BodyMsgName = "earth_planet_data"
+    earth.outputMsgName = "earth_display_frame_data"
+    earth.IsCentralBody = False
+    earth.UseJParams = False
+    JParams = LoadGravFromFile(EarthGravFile, earth, JParamsSelect)
+    earth.JParams = six_dof_eom.DoubleVector(JParams)
+    sim.VehDynObject.AddGravityBody(earth)
+
+def Add_marsGravityBody(self):
+    JParamsSelect = [2, 3, 4, 5, 6]
+    MarsGravFile = self.simBasePath + '/External/LocalGravData/GGM2BData.txt'
+    self.MarsGravBody = six_dof_eom.GravityBodyData()
+    self.MarsGravBody.BodyMsgName = "mars barycenter_planet_data"
+    self.MarsGravBody.outputMsgName = "mars barycenter_display_frame_data"
+    self.MarsGravBody.IsCentralBody = False
+    self.MarsGravBody.UseJParams = True
+    JParams = LoadGravFromFile(MarsGravFile, self.MarsGravBody, JParamsSelect)
+    self.MarsGravBody.JParams = six_dof_eom.DoubleVector(JParams)
+    self.VehDynObject.AddGravityBody(self.MarsGravBody)
+
+def Init_spacecraftVehicle(sc, r0, v0, sigma0_BN, omega0_BN, m, I, DCM_BS, CoM):
+    sc.PositionInit = six_dof_eom.DoubleVector(r0)
+    sc.VelocityInit = six_dof_eom.DoubleVector(v0)
+    sc.AttitudeInit = six_dof_eom.DoubleVector(sigma0_BN)
+    sc.AttRateInit = six_dof_eom.DoubleVector(omega0_BN)
+    sc.baseMass = m
+    sc.baseInertiaInit = six_dof_eom.DoubleVector(I)
+    sc.T_Str2BdyInit = six_dof_eom.DoubleVector(DCM_BS)
+    sc.baseCoMInit = six_dof_eom.DoubleVector(CoM)
+
