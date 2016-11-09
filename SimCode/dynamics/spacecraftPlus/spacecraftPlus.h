776a44f07bd865fbc3c6c0f635dd1b9d00ae9fee
Cody Allard <cody.allard@colorado.edu>
2016-11-09 09:43:33 -0700

    [BSK-417] Made changes to the comments to follow the coding style
    Former-commit-id: eb90446af6859490f273d4d57e60b29a4b0691fb
    Former-commit-id: 5fc4de3817a8b107693516576b619d423753b636

--- SimCode/dynamics/spacecraftPlus/spacecraftPlus.h
+++ SimCode/dynamics/spacecraftPlus/spacecraftPlus.h
@@ -33,43 +33,43 @@
  */
 class SpacecraftPlus : public DynObject2{
 public:
-    HubEffector hub;                     //! [-] The spacecraft hub that effectors spoke off
-    GravityEffector gravField;           //! [-] Gravitational field experienced by spacecraft
-    Eigen::Matrix3d matrixAContr;        //! [-] The contribution of each stateEffetor to matrix A
-    Eigen::Matrix3d matrixBContr;        //! [-] The contribution of each stateEffetor to matrix B
-    Eigen::Matrix3d matrixCContr;        //! [-] The contribution of each stateEffetor to matrix C
-    Eigen::Matrix3d matrixDContr;        //! [-] The contribution of each stateEffetor to matrix D
-    Eigen::Vector3d vecTransContr;       //! [-] The contribution of each stateEffetor to vecTrans
-    Eigen::Vector3d vecRotContr;         //! [-] The contribution of each stateEffetor to vecRot
-    Eigen::MatrixXd *m_SC;               //! [kg] spacecrafts total mass
-    Eigen::MatrixXd *ISCPntB_B;          //! [kg m^2] Inertia of s/c about point B in B frame components
-    Eigen::MatrixXd *c_B;                //! [m] Vector from point B to CoM of s/c in B frame components
-    Eigen::MatrixXd *cPrime_B;           //! [m] Body time derivative of c_B
-    Eigen::MatrixXd *ISCPntBPrime_B;     //! [m] Body time derivative of ISCPntB_B
+    HubEffector hub;                     //!< [-] The spacecraft hub that effectors spoke off
+    GravityEffector gravField;           //!< [-] Gravitational field experienced by spacecraft
+    Eigen::Matrix3d matrixAContr;        //!< [-] The contribution of each stateEffetor to matrix A
+    Eigen::Matrix3d matrixBContr;        //!< [-] The contribution of each stateEffetor to matrix B
+    Eigen::Matrix3d matrixCContr;        //!< [-] The contribution of each stateEffetor to matrix C
+    Eigen::Matrix3d matrixDContr;        //!< [-] The contribution of each stateEffetor to matrix D
+    Eigen::Vector3d vecTransContr;       //!< [-] The contribution of each stateEffetor to vecTrans
+    Eigen::Vector3d vecRotContr;         //!< [-] The contribution of each stateEffetor to vecRot
+    Eigen::MatrixXd *m_SC;               //!< [kg] spacecrafts total mass
+    Eigen::MatrixXd *ISCPntB_B;          //!< [kg m^2] Inertia of s/c about point B in B frame components
+    Eigen::MatrixXd *c_B;                //!< [m] Vector from point B to CoM of s/c in B frame components
+    Eigen::MatrixXd *cPrime_B;           //!< [m] Body time derivative of c_B
+    Eigen::MatrixXd *ISCPntBPrime_B;     //!< [m] Body time derivative of ISCPntB_B
     Eigen::MatrixXd *sysTime;
-    Eigen::Vector3d totSCAngMomentum_N;  //! [kg-m^2/s] Total angular momentum of the s/c in N frame compenents
-    double totSCEnergy;                  //! [J]    Total energy of the spacecraft
-    double totSCAngMomentum;             //! [kg-m^2/s] Magnitude of total angular momentum of the s/c
+    Eigen::Vector3d totSCAngMomentum_N;  //!< [kg-m^2/s] Total angular momentum of the s/c in N frame compenents
+    double totSCEnergy;                  //!< [J]    Total energy of the spacecraft
+    double totSCAngMomentum;             //!< [kg-m^2/s] Magnitude of total angular momentum of the s/c
 	double currTimeStep;
 	double timePrevious;
-    uint64_t simTimePrevious;            //! [-] Previous simulation time
+    uint64_t simTimePrevious;            //!< [-] Previous simulation time
     
-    std::string sysTimePropertyName;     //! [-] Name of the system time property
+    std::string sysTimePropertyName;     //!< [-] Name of the system time property
     
 public:
     SpacecraftPlus();
     ~SpacecraftPlus();
-    void SelfInit();                           //! [-] Lets spacecraft plus create its own msgs
-    void CrossInit();                          //! [-] Hook to tie s/c plus back into provided msgs
-    void UpdateState(uint64_t CurrentSimNanos);//! [-] Runtime hook back into Basilisk arch
-    void equationsOfMotion(double t);         //! [-] Everyone will need to provide this EOM
-    void integrateState(double t);            //! [-] Everyone will need to integrate the state
-    void computeEnergyMomentum();             //! [-] User can implement NRG/moment check
-    void initializeDynamics();                //! [-] Method to link all spacecraft states
+    void SelfInit();                           //!< [-] Lets spacecraft plus create its own msgs
+    void CrossInit();                          //!< [-] Hook to tie s/c plus back into provided msgs
+    void UpdateState(uint64_t CurrentSimNanos);//!< [-] Runtime hook back into Basilisk arch
+    void equationsOfMotion(double t);          //!< [-] Everyone will need to provide this EOM
+    void integrateState(double t);             //!< [-] Everyone will need to integrate the state
+    void computeEnergyMomentum();              //!< [-] User can implement NRG/moment check
+    void initializeDynamics();                 //!< [-] Method to link all spacecraft states
     void linkInStates(DynParamManager& statesIn);
 
 private:
-    StateData *hubSigma;                      //! [-] sigmaBN for the hub
+    StateData *hubSigma;                       //!< [-] sigmaBN for the hub
 };
 
 #endif /* SPACECRAFT_PLUS_H */
