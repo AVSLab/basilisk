82f4eb46309ec6ad0dfb744de5db479e482ba4cb
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2016-11-23 13:15:39 -0700

    [BSK-448] kept the old wrapper logic to upgrade any prior license statement to the new type
    Former-commit-id: 6b3b2256bb5ede87088bde107ea93087c75a542f
    Former-commit-id: 8b2494a193967fc95485a27cbed710886b2e2c91

--- License/addLicense.py
+++ License/addLicense.py
@@ -18,11 +18,11 @@ def update_source(filename, oldcopyright, copyright):
 
     if (filename.endswith(".c") or filename.endswith(".h") or filename.endswith(".cpp") or filename.endswith(".hpp") or filename.endswith(".i")  ):
         copyright    = "/*" + copyright + " */\n"
-        oldcopyright = "/*" + oldcopyright + " */\n"
+        oldcopyright = "/*" + oldcopyright + "*/\n"
         knownFileType = True
     if (filename.endswith(".py")  ):
         copyright    = "''' '''\n'''" + copyright + "'''\n"
-        oldcopyright = "''' '''\n'''" + oldcopyright + "'''\n"
+        oldcopyright = "'''" + oldcopyright + "'''\n"
         knownFileType = True
 
     if (oldcopyright != None):
