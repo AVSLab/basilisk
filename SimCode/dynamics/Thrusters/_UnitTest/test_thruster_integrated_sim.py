ca04daab4d8d1dcd78603dd2f52ff086ee0be59d
Cody Allard <cody.allard@colorado.edu>
2016-11-23 12:33:06 -0700

    [BSK-410] Obviously needed to call the function, idiot!
    Former-commit-id: 7f2ebb635349423f31aed6edb4ba839fb040fc0d
    Former-commit-id: 73ec2cd6ae9bb45dacf331f00b68a4145445749c

--- SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
+++ SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
@@ -39,6 +39,7 @@ import spice_interface
 import simIncludeThruster
 import thrusterDynamicEffector
 import vehicleConfigData
+import fuelTank
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
 # @pytest.mark.skipif(conditionstring)
@@ -85,19 +86,16 @@ def test_thrusterIntegratedTest(show_plots):
 
     # create thruster object container and tie to spacecraft object
     thrustersDynamicEffector = thrusterDynamicEffector.ThrusterDynamicEffector()
+
+    unitTestSim.fuelTankStateEffector = fuelTank.FuelTank()
+    unitTestSim.fuelTankStateEffector.r_TB_B = [[0.0], [0.0], [0.0]]
+    unitTestSim.fuelTankStateEffector.radiusTank = 46.0 / 2.0 / 3.2808399 / 12.0
+
+    # Add tank and thruster
+    scObject.addStateEffector(unitTestSim.fuelTankStateEffector)
     simIncludeThruster.addToSpacecraft(  "Thrusters",
                                        thrustersDynamicEffector,
-                                       scObject)
-
-    # setup the thruster mass properties (This will be in the full tank - still to come!)
-    # thrusterPropCM = [0.0, 0.0, 1.2]
-    # thrusterPropMass = 40.0
-    # thrusterPropRadius = 46.0 / 2.0 / 3.2808399 / 12.0
-    # sphereInerita = 2.0 / 5.0 * thrusterPropMass * thrusterPropRadius * thrusterPropRadius
-    # thrusterPropInertia = [sphereInerita, 0, 0, 0, sphereInerita, 0, 0, 0, sphereInerita]
-    # thrustersDynamicEffector.objProps.Mass = thrusterPropMass
-    # thrustersDynamicEffector.objProps.CoM = thrusterPropCM
-    # thrustersDynamicEffector.objProps.InertiaTensor = thrusterPropInertia
+                                       scObject, unitTestSim.fuelTankStateEffector)
 
     # set thruster commands
     ThrustMessage = thrusterDynamicEffector.ThrustCmdStruct()
@@ -138,15 +136,17 @@ def test_thrusterIntegratedTest(show_plots):
     velRef = scObject.dynManager.getStateObject("hubVelocity")
     sigmaRef = scObject.dynManager.getStateObject("hubSigma")
     omegaRef = scObject.dynManager.getStateObject("hubOmega")
+    massTankRef = scObject.dynManager.getStateObject("fuelTankMass")
 
     posRef.setState([[-4020338.690396649],	[7490566.741852513],	[5248299.211589362]])
     velRef.setState([[-5199.77710904224],	[-3436.681645356935],	[1041.576797498721]])
     sigmaRef.setState([[0.1], [0.2], [-0.3]])
     omegaRef.setState([[0.001], [-0.01], [0.03]])
+    massTankRef.setState([[40.0]])
 
-    scObject.hub.mHub = 790.0
+    scObject.hub.mHub = 750.0
     scObject.hub.rBcB_B = [[0.0], [0.0], [0.0]]
-    scObject.hub.IHubPntBc_B = [[905.46063422339966, 0.0, 0.0], [0.0, 805.46063422339967, 0.0], [0.0, 0.0, 605.46063422339967]]
+    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
 
     stopTime = 60.0*10.0
     unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
@@ -165,11 +165,6 @@ def test_thrusterIntegratedTest(show_plots):
                 [1.4401781243854264e-01, -6.4168702021364002e-02, 3.0166086824900967e-01]
                 ]
 
-    print dataPos
-    print truePos
-    print dataSigma
-    print trueSigma
-
     moduleOutputr_N = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName + '.r_BN_N',
                                                   range(3))
     moduleOutputSigma = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName + '.sigma_BN',
@@ -182,7 +177,7 @@ def test_thrusterIntegratedTest(show_plots):
             testFailCount += 1
             testMessages.append("FAILED: Thruster Integrated Test failed pos unit test")
 
-    accuracy = 1e-5
+    accuracy = 1e-7
     for i in range(0,len(trueSigma)):
         # check a vector values
         if not unitTestSupport.isArrayEqualRelative(dataSigma[i],trueSigma[i],3,accuracy):
