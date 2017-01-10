c80a0a0e40583a2a7cc9eeca1a8b409222d58b69
atharris <anha5550@colorado.edu>
2017-01-10 12:20:34 -0700

    [BSK-429] dataFactory first working version
    First working version of "dataFactory.py". Tested in dataFacTest.py, which does not currently run on Pytest.
    
    Former-commit-id: 4531ee10deb61d55250357f414536676f2ff337b
    Former-commit-id: 3cf0c4c0d73c36e081a26afbe988c0c23057767e

---
+++ External/AtmosphereData/DataFactory/dataFacTest.py
@@ -0,0 +1,26 @@
+from External.AtmosphereData.DataFactory import dataFactory as df
+from matplotlib import pyplot as plt
+
+baseFileName = "sw_index_2016_3hr.txt"
+
+baseParams = df.fileParams("3 hour","Year Day Hour Ap Kp F107 ind1 ind2 ind3 ind4 ind5 ind6 ind7", " ")
+desParams = df.fileParams("1 day", "Year Day Hour Ap Kp F107 ind1 ind2 ind3 ind4 ind5 ind6 ind7", " ")
+
+options = df.options(False)
+
+testFac = df.dataFactory(baseFileName, baseParams, desParams, options)
+
+testFac.getBaseData()
+#print testFac.baseDict
+
+testFac.baseToDes()
+print testFac.desDict['Ap']
+
+plt.figure()
+plt.plot(testFac.desDict['Ap'])
+plt.title('Desired Ap')
+
+plt.figure()
+plt.plot(testFac.baseDict['Ap'])
+plt.title('Base Values')
+plt.show()
