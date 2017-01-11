a63088c887ace1522b707805aac6c3bc6bc6baaa
atharris <anha5550@colorado.edu>
2017-01-11 14:18:30 -0700

    [BSK 429/430] Playing with folder structure, interpoloation
    - Moved NRLMSISE-00 source files to External/AtmosphereData
    - Added ability to get variable values at specific times to dataFactory
    
    Former-commit-id: 3e90b2594b398df334b90052d12a2602ee19bcc5
    Former-commit-id: 58f607cab9b9f739a42fe9bda23554a7dc5e1f8d

--- External/AtmosphereData/DataFactory/dataFacTest.py
+++ External/AtmosphereData/DataFactory/dataFacTest.py
@@ -1,10 +1,11 @@
 from External.AtmosphereData.DataFactory import dataFactory as df
 from matplotlib import pyplot as plt
+import numpy as np
 
 baseFileName = "sw_index_2016_3hr.txt"
 
 baseParams = df.fileParams("3 hour","Year Day Hour Ap Kp F107 ind1 ind2 ind3 ind4 ind5 ind6 ind7", " ")
-desParams = df.fileParams("1 day", "Year Day Hour Ap Kp F107 ind1 ind2 ind3 ind4 ind5 ind6 ind7", " ")
+desParams = df.fileParams("12 hour", "Year Day Hour Ap Kp F107 ind1 ind2 ind3 ind4 ind5 ind6 ind7", " ")
 
 options = df.options(False)
 
@@ -14,7 +15,7 @@ testFac.getBaseData()
 #print testFac.baseDict
 
 testFac.baseToDes()
-print testFac.desDict['Ap']
+#print testFac.desDict['Ap']
 
 plt.figure()
 plt.plot(testFac.desDict['Ap'])
@@ -23,4 +24,15 @@ plt.title('Desired Ap')
 plt.figure()
 plt.plot(testFac.baseDict['Ap'])
 plt.title('Base Values')
+
+timeVec = np.arange(0,48*3600.0,60)
+doy = 1
+testInterp = []
+
+for ind in timeVec:
+    testInterp.append(testFac.getVarVal( doy,int(ind/3600),int(ind/60), ind - int(ind/3600)*3600.0 - int(ind/60)*60, "Ap"))
+
+plt.figure()
+plt.plot(testInterp)
+plt.title('Interpolation test')
 plt.show()
--- External/AtmosphereData/DataFactory/dataFactory.py
+++ External/AtmosphereData/DataFactory/dataFactory.py
@@ -4,10 +4,17 @@ import math
 import scipy as sci
 from collections import defaultdict
 
+def linearInterp(x0, x1, y0, y1, xdes):
+
+    ydes = y0 + (xdes - x0) * (y1 - y0)/(x1-x0)
+
+    return ydes
+
 class fileParams:
-    def __init__(self, fileFreq, columnNames):
+    def __init__(self, fileFreq, columnNames, sepChar):
         self.fileFreq = fileFreq
         self.columnNames = columnNames
+        self.sepChar = sepChar
 
 class options:
     def __init__(self, outputCaching):
@@ -33,42 +40,72 @@ class dataFactory:
 
         #   Iterate over each line in the file
         for ind in range(0, self.baseFileLen):
-            currLine = self.baseFileName.readline()
-            currVals = currLine.split(self.baseFileParams.sepChar)
+            currLine = self.baseFileHandle.readline()
+            currVals = currLine.split()
+            #print currVals
             #   Iterate over each seperated value in the line
             for valInd in range(0, self.numVar):
-                self.baseDict[self.varNames[valInd]].append(currVals[valInd])
+                #print self.varNames[valInd]
+                self.baseDict[self.varNames[valInd]].append(float(currVals[valInd]))
+                #print self.baseDict[self.varNames[valInd]]
+        self.baseFileHandle.seek(0)
 
     def baseToDes(self):
         self.getBaseData()
         #   Figure out the frequency of data in the base file and in the desired output
         #   Example: "3 hour" to "1 day"
-        baseVals = self.baseFileParams.fileFreq.split()
+        baseVals = self.baseParams.fileFreq.split()
         baseFileQuant = float(baseVals[0])
         baseFileUnit = baseVals[1]
-        desVals = self.desiredParams.fileFreq.split()
+        desVals = self.desParams.fileFreq.split()
         desQuant = float(desVals[0])
-        desUnit = baseVals[2]
+        desUnit = desVals[1]
 
         unitList = ['second','minute','hour','day','year']
         convList = [60, 60, 24, 365]
 
         convDict = dict(zip(unitList, range(0,5)))
-
-        baseInd = convDict(baseFileUnit)
-        desInd = convDict(desUnit)
+        baseInd = convDict[baseFileUnit]
+        desInd = convDict[desUnit]
         indDiff = desInd - baseInd
+
         convFactor = 1
         if not indDiff == 0:
             for ind in range(baseInd, desInd):
                 convFactor = convFactor * convList[ind]
+
         indsToAvg = (desQuant / baseFileQuant * convFactor)
+
         indsToAvg = int(indsToAvg)
+
         self.desDict = defaultdict(list)
 
         for ind in range(0,self.baseFileLen,indsToAvg):
             for nameInd in range(0,self.numVar):
-                self.desDict(self.varNames[nameInd]).append(np.mean(self.baseDict[self.varNames[nameInd]][ind:indsToAvg]))
+                self.desDict[self.varNames[nameInd]].append(np.mean(self.baseDict[self.varNames[nameInd]][ind:ind+indsToAvg]))
+
+    def getVarVal(self, doy, hour, min, sec, varName):
+        #   If we haven't already generated the desired dictionary of converted values, do that first.
+        if not hasattr(self, 'desDict'):
+            self.baseToDes()
+
+        fracHour = hour + min/60.0 + sec/ 3600.0
+
+        for ind in range(0,len(self.desDict["Year"])):
+            if self.desDict["Day"][ind] == doy:
+                #   If the time at this index is GREATER than the requested time,
+                if self.desDict["Hour"][ind] >= fracHour:
+                    returnVal = linearInterp(self.desDict["Hour"][ind-1],self.desDict["Hour"][ind], self.desDict[varName][ind-1], self.desDict[varName][ind], fracHour)
+
+        if 'returnVal' in vars():
+            return returnVal
+        else:
+            return 1
+
+
+
+
+
 
 
 
