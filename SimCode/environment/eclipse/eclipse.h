ad873957da70beb5ce4c2b21d58a6fd9330fafeb
Patrick Kenneally <patrick.kenneally@colorado.edu>
2017-02-12 22:55:59 -0700

    [BSK-339] WIP illumination module and demo scenario
    Former-commit-id: 001c90fdf00164152e419d56cca88ea61cd0ed86
    Former-commit-id: 36e9bb0fc133fea8d04e9d1477ced7daabf092d1

---
+++ SimCode/environment/eclipse/eclipse.h
@@ -0,0 +1,83 @@
+/*
+ ISC License
+
+ Copyright (c) 2016-2017, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef Eclipse_H
+#define Eclipse_H
+
+#include <vector>
+#include "_GeneralModuleFiles/sys_model.h"
+#include "environment/spice/spice_planet_state.h"
+#include "utilities/linearAlgebra.h"
+
+/*! \addtogroup SimModelGroup
+ *  This group is used to model parts of the vehicle and the surrounding environment
+ *  in the simulation system.  All components/dynamics/environment models are a
+ *  part of this group.
+ * @{
+ */
+
+
+//! The SPICE interface class gets time and planetary body information from the JPL ephemeris library
+class Eclipse: public SysModel {
+public:
+    Eclipse();
+    ~Eclipse();
+    
+    void UpdateState(uint64_t CurrentSimNanos);
+    int loadSpiceKernel(char *kernelName, const char *dataPath);
+    int unloadSpiceKernel(char *kernelName, const char *dataPath);
+	std::string getCurrentTimeString();
+    void SelfInit();
+    void InitTimeData();
+    void ComputeGPSData();
+    void ComputePlanetData();
+    void writeOutputMessages(uint64_t CurrentClock);
+    
+public:
+    std::string SPICEDataPath;           //!< -- Path on file to SPICE data
+    std::string referenceBase;           //!< -- Base reference frame to use
+    std::string zeroBase;                //!< -- Base zero point to use for states
+	std::string timeOutPicture;          //!< -- Optional parameter used to extract time strings
+    bool SPICELoaded;                    //!< -- Boolean indicating to reload spice
+    uint32_t CharBufferSize;    //!< -- avert your eyes we're getting SPICE
+    uint8_t *SpiceBuffer;       //!< -- General buffer to pass down to spice
+    std::string UTCCalInit;     //!< -- UTC time string for init time
+    std::string OutputTimePort; //!< -- Output time sampling port name to use
+    uint64_t OutputBufferCount; //!< -- Number of output buffers to use
+    std::vector<std::string>PlanetNames;  //!< -- Names of planets we want to track
+    
+    bool TimeDataInit;          //!< -- Flag indicating whether time has been init
+    double J2000ETInit;         //!< s Seconds elapsed since J2000 at init
+    double J2000Current;        //!< s Current J2000 elapsed time
+    double JulianDateCurrent;   //!< s Current JulianDate
+    double GPSSeconds;          //!< s Current GPS seconds
+    uint16_t GPSWeek;           //!< -- Current GPS week value
+    uint64_t GPSRollovers;      //!< -- Count on the number of GPS rollovers
+    
+private:
+    std::string GPSEpochTime;   //!< -- String for the GPS epoch
+    double JDGPSEpoch;          //!< s Epoch for GPS time.  Saved for efficiency
+    int64_t TimeOutMsgID;       //!< -- Output time message ID
+    std::map<uint32_t, SpicePlanetState> PlanetData; //!< -- Internal vector of planets
+    
+};
+
+/*! @} */
+
+#endif
---
+++ SimCode/environment/eclipse/eclipse_data.h
@@ -0,0 +1,13 @@
+//
+//  eclipse_data.h
+//  AVS basilisk
+//
+//  Created by Patrick Kenneally on 2/12/17.
+//
+//
+
+#ifndef eclipse_data_h
+#define eclipse_data_h
+
+
+#endif /* eclipse_data_h */
