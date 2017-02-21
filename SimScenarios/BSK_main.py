6b18abf1e9bb60aeabe0a4fb588460b2571ef7ba
Mar Cols Margenet <maria.colsmargenet@colorado.edu>
2017-02-21 12:33:47 -0700

    [BSK-566] First cut of TCP interface
    Former-commit-id: 8cc9e6b2483169c864152b5a187b7a7b625db407
    Former-commit-id: 1d74e062a5cf928de2f0ac09737f3ce3b3260b40

--- SimScenarios/BSK_main.py
+++ SimScenarios/BSK_main.py
@@ -10,6 +10,10 @@ sys.path.append(bskPath + 'PythonModules')
 
 import SimulationBaseClass
 import sim_model
+import message_router
+
+
+
 import BSK_DKE
 import BSK_FSW
 
@@ -30,11 +34,52 @@ class BSKSim(SimulationBaseClass.SimBaseClass):
         # Define process message interfaces.
         self.dyn2FSWInterface = sim_model.SysInterface()
         self.fsw2DynInterface = sim_model.SysInterface()
-        self.dyn2FSWInterface.addNewInterface(self.DynamicsProcessName, self.FSWProcessName)
-        self.fsw2DynInterface.addNewInterface(self.FSWProcessName, self.DynamicsProcessName)
-        self.dynProc.addInterfaceRef(self.dyn2FSWInterface)
-        self.fswProc.addInterfaceRef(self.fsw2DynInterface)
 
         # Crate Dynamics and FSW classes
         self.DynClass = BSK_DKE.DynamicsClass(self)
         self.FSWClass = BSK_FSW.FSWClass(self)
+
+        # Define TCP/IP interfaces
+        self.DynamicsServer = False
+        self.FSWClient = False
+
+        # Create Synchronization task
+        self.dynProc.addTask(self.CreateNewTask("SynchTask", int(5E8)),)
+        self.fswProc.addTask(self.CreateNewTask("SynchTask", int(5E8)),)
+        self.disableTask("SynchTask")
+
+        if (self.DynamicsServer):
+            self.enableTask('SynchTask')
+            self.router = message_router.MessageRouter("DynamicsProcess", "FSWProcess")
+            self.router.runAsServer = True
+            self.router.linkProcesses()
+            self.dyn2FSWInterface.addNewInterface(self.router)
+            self.AddModelToTask("SynchTask", self.router)
+            self.routIn = message_router.MessageRouter(self.router.getConnection())
+            self.routIn.linkProcesses()
+            self.fsw2DynInterface.addNewInterface(self.routIn)
+            self.dynProc.addInterfaceRef(self.fsw2DynInterface)
+
+        elif (self.FSWClient):
+            self.enableTask('SynchTask')
+            self.router = message_router.MessageRouter()
+            self.router.linkProcesses()
+            self.dyn2FSWInterface.addNewInterface(self.router)
+            self.fswProc.addInterfaceRef(self.dyn2FSWInterface)
+            fromString = "FSWProcess"
+            toString = "DynamicsProcess"
+            connString = fromString + "2" + toString + "Interface"
+            self.routOut = message_router.MessageRouter(fromString, toString, connString, self.router.getConnection())
+            self.routOut.runAsServer = True
+            self.routOut.linkProcesses()
+            self.fsw2DynInterface.addNewInterface(self.routOut)
+            self.AddModelToTask("SynchTask", self.routOut)
+
+        else:
+            self.dyn2FSWInterface.addNewInterface(self.DynamicsProcessName, self.FSWProcessName)
+            self.fsw2DynInterface.addNewInterface(self.FSWProcessName, self.DynamicsProcessName)
+            self.dynProc.addInterfaceRef(self.dyn2FSWInterface)
+            self.fswProc.addInterfaceRef(self.fsw2DynInterface)
+
+
+#TheBSKSim = BSKSim()
