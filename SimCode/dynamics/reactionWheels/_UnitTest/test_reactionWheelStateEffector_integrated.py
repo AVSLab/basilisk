c4a0b8dc3ff2610e1a478708a3de8b17fbede00f
John Alcorn <john.alcorn@colorado.edu>
2017-03-06 22:21:51 -0700

    [BSK-68] Simple jitter integrated test.
    Former-commit-id: 39d81e5869562274eda8b3593b76662422fff1b2
    Former-commit-id: bd2ce5040ac3a9fd0017f5133ace48d7b3f8e7c1

--- SimCode/dynamics/reactionWheels/_UnitTest/test_reactionWheelStateEffector_integrated.py
+++ SimCode/dynamics/reactionWheels/_UnitTest/test_reactionWheelStateEffector_integrated.py
@@ -41,9 +41,10 @@ import reactionWheelStateEffector
 import vehicleConfigData
 
 
-@pytest.mark.parametrize("testCase", [
-    ('BalancedWheels'),
-    ('JitterFullyCoupled')
+@pytest.mark.parametrize("useFlag, testCase", [
+    (False,'BalancedWheels'),
+    (False,'JitterSimple'),
+    (False,'JitterFullyCoupled')
 ])
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
@@ -51,11 +52,11 @@ import vehicleConfigData
 # uncomment this line if this test has an expected failure, adjust message as needed
 # @pytest.mark.xfail() # need to update how the RW states are defined
 # provide a unique test method name, starting with test_
-def reactionWheelIntegratedTest(show_plots,testCase):
-    [testResults, testMessage] = test_reactionWheelIntegratedTest(show_plots,testCase)
+def test_reactionWheelIntegratedTest(show_plots,useFlag,testCase):
+    [testResults, testMessage] = reactionWheelIntegratedTest(show_plots,useFlag,testCase)
     assert testResults < 1, testMessage
 
-def test_reactionWheelIntegratedTest(show_plots,testCase):
+def reactionWheelIntegratedTest(show_plots,useFlag,testCase):
     # The __tracebackhide__ setting influences pytest showing of tracebacks:
     # the mrp_steering_tracking() function will not be shown unless the
     # --fulltrace command line option is specified.
@@ -84,26 +85,30 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
     # The clearRWSetup() is critical if the script is to run multiple times
     simIncludeRW.clearSetup()
     simIncludeRW.options.maxMomentum = 100
-    if testCase == 'JitterFullyCoupled':
-        simIncludeRW.options.U_d = 0
-        simIncludeRW.options.U_s = 0
+
+    if testCase == 'BalancedWheels':
+        simIncludeRW.options.RWModel = 0
+    elif testCase == 'JitterSimple':
+        simIncludeRW.options.RWModel = 1
+    elif testCase == 'JitterFullyCoupled':
         simIncludeRW.options.RWModel = 2
+
     simIncludeRW.create(
             'Honeywell_HR16',
             [1,0,0],                # gsHat_S
-            30.,                     # RPM
+            500.,                     # RPM
             [0.1,0.,0.]
             )
     simIncludeRW.create(
             'Honeywell_HR16',
             [0,1,0],                # gsHat_S
-            20.,                     # RPM
+            200.,                     # RPM
             [0.,0.1,0.]
             )
     simIncludeRW.create(
             'Honeywell_HR16',
             [0,0,1],                # gsHat_S
-            15.,                    # RPM
+            -150.,                    # RPM
             [0.,0.,0.1]
             )
 
@@ -114,9 +119,9 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
     # set RW torque command
     unitTestSim.TotalSim.CreateNewMessage(unitProcessName, rwCommandName, 8*vehicleConfigData.MAX_EFF_CNT, 2)
     cmdArray = sim_model.new_doubleArray(vehicleConfigData.MAX_EFF_CNT)
-    sim_model.doubleArray_setitem(cmdArray, 0, 0*0.020) # RW-1 [Nm]
-    sim_model.doubleArray_setitem(cmdArray, 1, 0*0.010) # RW-2 [Nm]
-    sim_model.doubleArray_setitem(cmdArray, 2,0*-0.050) # RW-3 [Nm]
+    sim_model.doubleArray_setitem(cmdArray, 0, .2) # RW-1 [Nm]
+    sim_model.doubleArray_setitem(cmdArray, 1, .1) # RW-2 [Nm]
+    sim_model.doubleArray_setitem(cmdArray, 2,-.5) # RW-3 [Nm]
     unitTestSim.TotalSim.WriteMessageData(rwCommandName, 8*vehicleConfigData.MAX_EFF_CNT, 1, cmdArray )
     
     # Add test module to runtime call list
@@ -160,14 +165,14 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
 
     posRef.setState([[-4020338.690396649],	[7490566.741852513],	[5248299.211589362]])
     velRef.setState([[-5199.77710904224],	[-3436.681645356935],	[1041.576797498721]])
-    sigmaRef.setState([[0.1], [0.2], [-0.3]])
-    omegaRef.setState([[0.000], [-0.0], [0.0]])
+    sigmaRef.setState([[0.0], [0.0], [0.0]])
+    omegaRef.setState([[0.08], [0.01], [0.0]])
 
     scObject.hub.mHub = 750.0
-    scObject.hub.rBcB_B = [[0.0], [0.0], [0.1]]
+    scObject.hub.rBcB_B = [[-0.0002], [0.0001], [0.1]]
     scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
 
-    stopTime = 0.25
+    stopTime = 0.1
     unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
     unitTestSim.ExecuteSimulation()
 
@@ -191,19 +196,29 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
 
     if testCase == 'BalancedWheels':
         truePos = [
-                    [-4033333.1061706794, 7481965.68525689, 5250896.597134046]
+                    [-4020858.6600723616, 7490223.058718186, 5248403.358783435]
                     ]
 
         trueSigma = [
-                    [0.10278985463139288, 0.18785123312448332, -0.2813107133882423]
+                    [0.0019997597306700616, 0.000249530148702714, 2.000437833943989e-07]
                     ]
+
+    elif testCase == 'JitterSimple':
+        truePos = [
+                    [-4020858.660072413, 7490223.058718171, 5248403.358783543]
+                    ]
+
+        trueSigma = [
+                    [0.0019997597306700616, 0.000249530148702714, 2.000437833943989e-07]
+                    ]
+
     elif testCase == 'JitterFullyCoupled':
         truePos = [
-                    [1,1,1]
+                    [-4020858.6600725227, 7490223.058718106, 5248403.358783446]
                     ]
 
         trueSigma = [
-                    [1,1,1]
+                    [0.001999758465936839, 0.00024952947836825925, 1.983624117403241e-07]
                     ]
 
 
@@ -232,6 +247,7 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
     if show_plots == True:
         plt.show()
 
+
     accuracy = 1e-8
     for i in range(0,len(truePos)):
         # check a vector values
@@ -245,17 +261,18 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
             testFailCount += 1
             testMessages.append("FAILED: Reaction Wheel Integrated Test failed attitude unit test")
 
-    for i in range(0,len(initialOrbAngMom_N)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(finalOrbAngMom[i],initialOrbAngMom_N[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: Reaction Wheel Integrated Test failed orbital angular momentum unit test")
+    if testCase == 'BalancedWheels' or testCase == 'JitterFullyCoupled':
+        for i in range(0,len(initialOrbAngMom_N)):
+            # check a vector values
+            if not unitTestSupport.isArrayEqualRelative(finalOrbAngMom[i],initialOrbAngMom_N[i],3,accuracy):
+                testFailCount += 1
+                testMessages.append("FAILED: Reaction Wheel Integrated Test failed orbital angular momentum unit test")
 
-    for i in range(0,len(initialRotAngMom_N)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(finalRotAngMom[i],initialRotAngMom_N[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: Reaction Wheel Integrated Test failed rotational angular momentum unit test")
+        for i in range(0,len(initialRotAngMom_N)):
+            # check a vector values
+            if not unitTestSupport.isArrayEqualRelative(finalRotAngMom[i],initialRotAngMom_N[i],3,accuracy):
+                testFailCount += 1
+                testMessages.append("FAILED: Reaction Wheel Integrated Test failed rotational angular momentum unit test")
 
     if testFailCount == 0:
         print "PASSED: " + " Reaction Wheel Integrated Sim Test"
@@ -267,5 +284,4 @@ def test_reactionWheelIntegratedTest(show_plots,testCase):
     return [testFailCount, ''.join(testMessages)]
 
 if __name__ == "__main__":
-    test_reactionWheelIntegratedTest(True,'JitterFullyCoupled')
-    # test_reactionWheelIntegratedTest(True,'BalancedWheels')
+    reactionWheelIntegratedTest(True,False,'JitterSimple')
