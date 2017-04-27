227946c080548cf63cec164eb3302362d2fb91ba
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2017-04-27 15:08:41 -0600

    removed some TABs from the source code
    Former-commit-id: 841bdb6ea624b8fe23db7253938ca85ac5ad8a48
    Former-commit-id: cd66ace74fda611e0836a74d73cfb28459f20df4

--- SimCode/simMessages/rwConfigSimMsg.h
+++ SimCode/simMessages/rwConfigSimMsg.h
@@ -28,35 +28,35 @@ enum RWModels { BalancedWheels, JitterSimple, JitterFullyCoupled };
 
 /*! @brief Structure used to define the individual RW configuration data message*/
 typedef struct {
-    std::string typeName;      //!< [-], string containing the RW type name
-    Eigen::Vector3d rWB_S;		//!< [m], position vector of the RW relative to the spacecraft structural frame
-    Eigen::Vector3d gsHat_S;	//!< [-] spin axis unit vector in structural frame
-    Eigen::Vector3d w2Hat0_S;	//!< [-] initial torque axis unit vector in structural
-    Eigen::Vector3d w3Hat0_S;	//!< [-] initial gimbal axis unit vector in structural frame
-    Eigen::Vector3d rWB_B;		//!< [m], position vector of the RW relative to the spacecraft body frame
-    Eigen::Vector3d gsHat_B;	//!< [-] spin axis unit vector in body frame
-    Eigen::Vector3d w2Hat0_B;	//!< [-] initial torque axis unit vector in body frame
-    Eigen::Vector3d w3Hat0_B;	//!< [-] initial gimbal axis unit vector in body frame
-    double mass;               //!< [kg], reaction wheel rotor mass
-    double theta;              //!< [rad], wheel angle
-    double Omega;              //!< [rad/s], wheel speed
-    double Js;                 //!< [kg-m^2], spin axis gsHat rotor moment of inertia
-    double Jt;                 //!< [kg-m^2], gtHat axis rotor moment of inertia
-    double Jg;                 //!< [kg-m^2], ggHat axis rotor moment of inertia
-    double U_s;                //!< [kg-m], static imbalance
-    double U_d;                //!< [kg-m^2], dynamic imbalance
+    std::string typeName;       //!< [-], string containing the RW type name
+    Eigen::Vector3d rWB_S;      //!< [m], position vector of the RW relative to the spacecraft structural frame
+    Eigen::Vector3d gsHat_S;    //!< [-] spin axis unit vector in structural frame
+    Eigen::Vector3d w2Hat0_S;   //!< [-] initial torque axis unit vector in structural
+    Eigen::Vector3d w3Hat0_S;   //!< [-] initial gimbal axis unit vector in structural frame
+    Eigen::Vector3d rWB_B;      //!< [m], position vector of the RW relative to the spacecraft body frame
+    Eigen::Vector3d gsHat_B;    //!< [-] spin axis unit vector in body frame
+    Eigen::Vector3d w2Hat0_B;   //!< [-] initial torque axis unit vector in body frame
+    Eigen::Vector3d w3Hat0_B;   //!< [-] initial gimbal axis unit vector in body frame
+    double mass;                //!< [kg], reaction wheel rotor mass
+    double theta;               //!< [rad], wheel angle
+    double Omega;               //!< [rad/s], wheel speed
+    double Js;                  //!< [kg-m^2], spin axis gsHat rotor moment of inertia
+    double Jt;                  //!< [kg-m^2], gtHat axis rotor moment of inertia
+    double Jg;                  //!< [kg-m^2], ggHat axis rotor moment of inertia
+    double U_s;                 //!< [kg-m], static imbalance
+    double U_d;                 //!< [kg-m^2], dynamic imbalance
     double d;                	//!< [m], wheel center of mass offset from wheel frame origin
     double J13;                	//!< [kg-m^2], x-z inertia of wheel about wheel center in wheel frame (imbalance)
-    double u_current;          //!< [N-m], current motor torque
-    double u_max;              //!< [N-m], Max torque
-    double u_min;              //!< [N-m], Min torque
-    double u_f;                //!< [N-m], Coulomb friction torque magnitude
-    double Omega_max;          //!< [rad/s], max wheel speed
-    double linearFrictionRatio;//!< [%] ratio relative to max speed value up to which the friction behaves linearly
-    RWModels RWModel; //!< [-], Type of imbalance model to use
-    Eigen::Vector3d aOmega; //!< [-], parameter used in coupled jitter back substitution
-    Eigen::Vector3d bOmega; //!< [-], parameter used in coupled jitter back substitution
-    double cOmega; //!< [-], parameter used in coupled jitter back substitution
+    double u_current;           //!< [N-m], current motor torque
+    double u_max;               //!< [N-m], Max torque
+    double u_min;               //!< [N-m], Min torque
+    double u_f;                 //!< [N-m], Coulomb friction torque magnitude
+    double Omega_max;           //!< [rad/s], max wheel speed
+    double linearFrictionRatio; //!< [%] ratio relative to max speed value up to which the friction behaves linearly
+    RWModels RWModel;           //!< [-], Type of imbalance model to use
+    Eigen::Vector3d aOmega;     //!< [-], parameter used in coupled jitter back substitution
+    Eigen::Vector3d bOmega;     //!< [-], parameter used in coupled jitter back substitution
+    double cOmega;              //!< [-], parameter used in coupled jitter back substitution
     Eigen::Matrix3d IRWPntWc_B;
     Eigen::Matrix3d IPrimeRWPntWc_B;
     Eigen::Vector3d rWcB_B;
