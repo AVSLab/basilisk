6c66ef8c0f32631a8451f60960aed3d3862f3678
Thibaud Teil <thibaud.teil@colorado.edu>
2017-06-06 09:29:27 -0600

    [BSK-697] Cleaning up uKF test for EKF
    Former-commit-id: d2080ebf2346cb155ffb0ef3191b593dd55077d5
    Former-commit-id: 5a8e7f23b8c14ed62855df445fdf59622d4177f6

--- FswAlgorithms/attDetermination/sunlineEKF/_UnitTest/test_SunLineEKF.py
+++ FswAlgorithms/attDetermination/sunlineEKF/_UnitTest/test_SunLineEKF.py
@@ -18,7 +18,7 @@
 
 '''
 import sys, os, inspect
-import numpy
+import numpy as np
 import pytest
 import math
 
@@ -47,10 +47,6 @@ def setupFilterData(filterObject):
     filterObject.massPropsInMsgName = "adcs_config_data"
     filterObject.cssConfInMsgName = "css_config_data"
 
-    filterObject.alpha = 0.02
-    filterObject.beta = 2.0
-    filterObject.kappa = 0.0
-
     filterObject.states = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     filterObject.covar = [0.4, 0.0, 0.0, 0.0, 0.0, 0.0,
                           0.0, 0.4, 0.0, 0.0, 0.0, 0.0,
@@ -58,10 +54,14 @@ def setupFilterData(filterObject):
                           0.0, 0.0, 0.0, 0.04, 0.0, 0.0,
                           0.0, 0.0, 0.0, 0.0, 0.04, 0.0,
                           0.0, 0.0, 0.0, 0.0, 0.0, 0.04]
-    qNoiseIn = numpy.identity(6)
-    qNoiseIn[0:3, 0:3] = qNoiseIn[0:3, 0:3]*0.017*0.017
-    qNoiseIn[3:6, 3:6] = qNoiseIn[3:6, 3:6]*0.0017*0.0017
-    filterObject.qNoise = qNoiseIn.reshape(36).tolist()
+    R = np.identity(6)
+    R[0:3, 0:3] = R[0:3, 0:3]*0.017*0.017
+    R[3:6, 3:6] = R[3:6, 3:6]*0.0017*0.0017
+    filterObject.measNoise = R
+
+    Q = np.identity(3)
+    Q[0:3, 0:3] = Q[0:3, 0:3]*0.017*0.017
+    filterObject.procNoise = Q
     filterObject.qObsVal = 0.017*0.017
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
@@ -86,165 +86,17 @@ def sunline_utilities_test(show_plots):
     testFailCount = 0  # zero unit test result counter
     testMessages = []  # create empty list to store test log messages
 
-    # Initialize the test module configuration data
-    AMatrix = [0.488894, 0.888396, 0.325191, 0.319207,
-                1.03469, -1.14707, -0.754928, 0.312859, 
-                0.726885, -1.06887, 1.3703, -0.86488,
-               -0.303441, -0.809499, -1.71152, -0.0300513,
-                0.293871, -2.94428, -0.102242, -0.164879,
-               -0.787283, 1.43838, -0.241447, 0.627707]
-   
-    RVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    AVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    for i in range(len(AMatrix)):
-        sunlineEKF.doubleArray_setitem(AVector, i, AMatrix[i])
-        sunlineEKF.doubleArray_setitem(RVector, i, 0.0)
+    # Testing dynamics matrix computation
+    inputStates = [0,1,0,0.1,0,0.05]
 
-    sunlineEKF.ukfQRDJustR(AVector, 6, 4, RVector)
-    RMatrix = []
-    for i in range(4*4):
-        RMatrix.append(sunlineEKF.doubleArray_getitem(RVector, i))
-    RBaseNumpy = numpy.array(RMatrix).reshape(4,4)
-    AMatNumpy = numpy.array(AMatrix).reshape(6,4)
-    q,r = numpy.linalg.qr(AMatNumpy)
-    for i in range(r.shape[0]):
-        if r[i,i] < 0.0:
-            r[i,:] *= -1.0
-    if numpy.linalg.norm(r - RBaseNumpy) > 1.0E-15:
-        testFailCount += 1
-        testMessages.append("QR Decomposition accuracy failure")
-    
-    AMatrix = [1.09327, 1.10927, -0.863653, 1.32288,
-     -1.21412, -1.1135, -0.00684933, -2.43508,
-     -0.769666, 0.371379, -0.225584, -1.76492,
-     -1.08906, 0.0325575, 0.552527, -1.6256,
-     1.54421, 0.0859311, -1.49159, 1.59683]
+    expDynMat = np.zeros([6,6])
+    expDynMat[0:3, 0:3] = -(np.outer(inputStates[3:6],inputStates[0:3])/np.linalg.norm(inputStates[0:3])**2. +
+                         np.dot(inputStates[3:6],inputStates[0:3])*
+                         (np.linalg.norm(inputStates[0:3])**2.*np.eye(3)- 2*np.outer(inputStates[0:3],inputStates[3:6]))/np.linalg.norm(inputStates[0:3])**4.)
+    expDynMat[0:3, 3:6] = np.eye(3) - np.outer(inputStates[0:3],inputStates[0:3])/np.linalg.norm(inputStates[0:3])**2
 
-    RVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    AVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    for i in range(len(AMatrix)):
-        sunlineEKF.doubleArray_setitem(AVector, i, AMatrix[i])
-        sunlineEKF.doubleArray_setitem(RVector, i, 0.0)
+    sunlineEKF.Dynamics = np.zeros([6,6]).tolist()
 
-    sunlineEKF.ukfQRDJustR(AVector, 5, 4, RVector)
-    RMatrix = []
-    for i in range(4*4):
-        RMatrix.append(sunlineEKF.doubleArray_getitem(RVector, i))
-    RBaseNumpy = numpy.array(RMatrix).reshape(4,4)
-    AMatNumpy = numpy.array(AMatrix).reshape(5,4)
-    q,r = numpy.linalg.qr(AMatNumpy)
-    for i in range(r.shape[0]):
-        if r[i,i] < 0.0:
-            r[i,:] *= -1.0
-    if numpy.linalg.norm(r - RBaseNumpy) > 1.0E-14:
-        testFailCount += 1
-        testMessages.append("QR Decomposition accuracy failure")
-    
-    AMatrix = [ 0.2236,         0,
-               0,    0.2236,
-               -0.2236,         0,
-               0,   -0.2236,
-               0.0170,         0,
-               0,    0.0170]
-
-    RVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    AVector = sunlineEKF.new_doubleArray(len(AMatrix))
-    for i in range(len(AMatrix)):
-        sunlineEKF.doubleArray_setitem(AVector, i, AMatrix[i])
-        sunlineEKF.doubleArray_setitem(RVector, i, 0.0)
-
-    sunlineEKF.ukfQRDJustR(AVector, 6, 2, RVector)
-    RMatrix = []
-    for i in range(2*2):
-        RMatrix.append(sunlineEKF.doubleArray_getitem(RVector, i))
-    RBaseNumpy = numpy.array(RMatrix).reshape(2,2)
-    AMatNumpy = numpy.array(AMatrix).reshape(6,2)
-    q,r = numpy.linalg.qr(AMatNumpy)
-    for i in range(r.shape[0]):
-        if r[i,i] < 0.0:
-            r[i,:] *= -1.0
-
-    if numpy.linalg.norm(r - RBaseNumpy) > 1.0E-15:
-        testFailCount += 1
-        testMessages.append("QR Decomposition accuracy failure")
-
-
-    LUSourceMat = [8,1,6,3,5,7,4,9,2]
-    LUSVector = sunlineEKF.new_doubleArray(len(LUSourceMat))
-    LVector = sunlineEKF.new_doubleArray(len(LUSourceMat))
-    UVector = sunlineEKF.new_doubleArray(len(LUSourceMat))
-    intSwapVector = sunlineEKF.new_intArray(3)
-    
-    for i in range(len(LUSourceMat)):
-        sunlineEKF.doubleArray_setitem(LUSVector, i, LUSourceMat[i])
-        sunlineEKF.doubleArray_setitem(UVector, i, 0.0)
-        sunlineEKF.doubleArray_setitem(LVector, i, 0.0)
-
-    exCount = sunlineEKF.ukfLUD(LUSVector, 3, 3, LVector, intSwapVector)
-    #sunlineEKF.ukfUInv(LUSVector, 3, 3, UVector)
-    LMatrix = []
-    UMatrix = []
-    #UMatrix = []
-    for i in range(3):
-        currRow = sunlineEKF.intArray_getitem(intSwapVector, i)
-        for j in range(3):
-            if(j<i):
-                LMatrix.append(sunlineEKF.doubleArray_getitem(LVector, i*3+j))
-                UMatrix.append(0.0)
-            elif(j>i):
-                LMatrix.append(0.0)
-                UMatrix.append(sunlineEKF.doubleArray_getitem(LVector, i*3+j))
-            else:
-                LMatrix.append(1.0)
-                UMatrix.append(sunlineEKF.doubleArray_getitem(LVector, i*3+j))
-    #    UMatrix.append(sunlineEKF.doubleArray_getitem(UVector, i))
-
-    LMatrix = numpy.array(LMatrix).reshape(3,3)
-    UMatrix = numpy.array(UMatrix).reshape(3,3)
-    outMat = numpy.dot(LMatrix, UMatrix)
-    outMatSwap = numpy.zeros((3,3)) 
-    for i in range(3):
-        currRow = sunlineEKF.intArray_getitem(intSwapVector, i)
-        outMatSwap[i,:] = outMat[currRow, :]
-        outMat[currRow,:] = outMat[i, :]
-    LuSourceArray = numpy.array(LUSourceMat).reshape(3,3)
-
-    if(numpy.linalg.norm(outMatSwap - LuSourceArray) > 1.0E-14):
-        testFailCount += 1
-        testMessages.append("LU Decomposition accuracy failure")
-
-    EqnSourceMat = [2.0, 1.0, 3.0, 2.0, 6.0, 8.0, 6.0, 8.0, 18.0]
-    BVector = [1.0, 3.0, 5.0]
-    EqnVector = sunlineEKF.new_doubleArray(len(EqnSourceMat))
-    EqnBVector = sunlineEKF.new_doubleArray(len(LUSourceMat)/3)
-    EqnOutVector = sunlineEKF.new_doubleArray(len(LUSourceMat)/3)
-
-    for i in range(len(EqnSourceMat)):
-        sunlineEKF.doubleArray_setitem(EqnVector, i, EqnSourceMat[i])
-        sunlineEKF.doubleArray_setitem(EqnBVector, i/3, BVector[i/3])
-        sunlineEKF.intArray_setitem(intSwapVector, i/3, 0)
-        sunlineEKF.doubleArray_setitem(LVector, i, 0.0)
-    
-    exCount = sunlineEKF.ukfLUD(EqnVector, 3, 3, LVector, intSwapVector)
-    
-    sunlineEKF.ukfLUBckSlv(LVector, 3, 3, intSwapVector, EqnBVector, EqnOutVector)
-    
-    expectedSol = [3.0/10.0, 4.0/10.0, 0.0]
-    errorVal = 0.0
-    for i in range(3):
-        errorVal += abs(sunlineEKF.doubleArray_getitem(EqnOutVector, i) -expectedSol[i])
-
-    if(errorVal > 1.0E-14):
-        testFailCount += 1
-        testMessages.append("LU Back-Solve accuracy failure")
-
-
-    InvSourceMat = [8,1,6,3,5,7,4,9,2]
-    SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
-    InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
-    for i in range(len(InvSourceMat)):
-        sunlineEKF.doubleArray_setitem(SourceVector, i, InvSourceMat[i])
-        sunlineEKF.doubleArray_setitem(InvVector, i, 0.0)
     nRow = int(math.sqrt(len(InvSourceMat)))
     sunlineEKF.ukfMatInv(SourceVector, nRow, nRow, InvVector)
 
@@ -252,60 +104,16 @@ def sunline_utilities_test(show_plots):
     for i in range(len(InvSourceMat)):
         InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
 
-    InvOut = numpy.array(InvOut).reshape(nRow, nRow)
-    expectIdent = numpy.dot(InvOut, numpy.array(InvSourceMat).reshape(3,3))
-    errorNorm = numpy.linalg.norm(expectIdent - numpy.identity(3))
+    InvOut = np.array(InvOut).reshape(nRow, nRow)
+    expectIdent = np.dot(InvOut, np.array(InvSourceMat).reshape(3,3))
+    errorNorm = np.linalg.norm(expectIdent - np.identity(3))
     if(errorNorm > 1.0E-14):
         testFailCount += 1
-        testMessages.append("LU Matrix Inverse accuracy failure")
-
-    
-    cholTestMat = [1.0, 0.0, 0.0, 0.0, 10.0, 5.0, 0.0, 5.0, 10.0]
-    SourceVector = sunlineEKF.new_doubleArray(len(cholTestMat))
-    CholVector = sunlineEKF.new_doubleArray(len(cholTestMat))
-    for i in range(len(cholTestMat)):
-        sunlineEKF.doubleArray_setitem(SourceVector, i, cholTestMat[i])
-        sunlineEKF.doubleArray_setitem(CholVector, i, 0.0)
-    nRow = int(math.sqrt(len(cholTestMat)))
-    sunlineEKF.ukfCholDecomp(SourceVector, nRow, nRow, CholVector)
-    cholOut = []
-    for i in range(len(cholTestMat)):
-        cholOut.append(sunlineEKF.doubleArray_getitem(CholVector, i))
-
-    cholOut = numpy.array(cholOut).reshape(nRow, nRow)
-    cholComp = numpy.linalg.cholesky(numpy.array(cholTestMat).reshape(nRow, nRow))
-    errorNorm = numpy.linalg.norm(cholOut - cholComp)
-    if(errorNorm > 1.0E-14):
-        testFailCount += 1
-        testMessages.append("Cholesky Matrix Decomposition accuracy failure")
+        testMessages.append("Filter Reset Failure")
 
 
-    InvSourceMat = [2.1950926119414667, 0.0, 0.0, 0.0,
-               1.0974804773131115, 1.9010439702743847, 0.0, 0.0,
-               0.0, 1.2672359635912551, 1.7923572711881284, 0.0,
-               1.0974804773131113, -0.63357997864171967, 1.7920348101787789, 0.033997451205364251]
-               
-    SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
-    InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
-    for i in range(len(InvSourceMat)):
-        sunlineEKF.doubleArray_setitem(SourceVector, i, InvSourceMat[i])
-        sunlineEKF.doubleArray_setitem(InvVector, i, 0.0)
-    nRow = int(math.sqrt(len(InvSourceMat)))
-    sunlineEKF.ukfLInv(SourceVector, nRow, nRow, InvVector)
 
-    InvOut = []
-    for i in range(len(InvSourceMat)):
-        InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
-
-    InvOut = numpy.array(InvOut).reshape(nRow, nRow)
-    expectIdent = numpy.dot(InvOut, numpy.array(InvSourceMat).reshape(nRow,nRow))
-    errorNorm = numpy.linalg.norm(expectIdent - numpy.identity(nRow))
-    if(errorNorm > 1.0E-12):
-        print errorNorm
-        testFailCount += 1
-        testMessages.append("L Matrix Inverse accuracy failure")
-
-    InvSourceMat = numpy.transpose(numpy.array(InvSourceMat).reshape(nRow, nRow)).reshape(nRow*nRow).tolist()
+    InvSourceMat = np.transpose(np.array(InvSourceMat).reshape(nRow, nRow)).reshape(nRow*nRow).tolist()
     SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
     InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
     for i in range(len(InvSourceMat)):
@@ -318,13 +126,77 @@ def sunline_utilities_test(show_plots):
     for i in range(len(InvSourceMat)):
         InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
 
-    InvOut = numpy.array(InvOut).reshape(nRow, nRow)
-    expectIdent = numpy.dot(InvOut, numpy.array(InvSourceMat).reshape(nRow,nRow))
-    errorNorm = numpy.linalg.norm(expectIdent - numpy.identity(nRow))
+    InvOut = np.array(InvOut).reshape(nRow, nRow)
+    expectIdent = np.dot(InvOut, np.array(InvSourceMat).reshape(nRow,nRow))
+    errorNorm = np.linalg.norm(expectIdent - np.identity(nRow))
     if(errorNorm > 1.0E-12):
         print errorNorm
         testFailCount += 1
-        testMessages.append("U Matrix Inverse accuracy failure")
+        testMessages.append("State Propagation Failure")
+
+
+    InvSourceMat = np.transpose(np.array(InvSourceMat).reshape(nRow, nRow)).reshape(nRow*nRow).tolist()
+    SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    for i in range(len(InvSourceMat)):
+        sunlineEKF.doubleArray_setitem(SourceVector, i, InvSourceMat[i])
+        sunlineEKF.doubleArray_setitem(InvVector, i, 0.0)
+    nRow = int(math.sqrt(len(InvSourceMat)))
+    sunlineEKF.ukfUInv(SourceVector, nRow, nRow, InvVector)
+
+    InvOut = []
+    for i in range(len(InvSourceMat)):
+        InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
+
+    InvOut = np.array(InvOut).reshape(nRow, nRow)
+    expectIdent = np.dot(InvOut, np.array(InvSourceMat).reshape(nRow,nRow))
+    errorNorm = np.linalg.norm(expectIdent - np.identity(nRow))
+    if(errorNorm > 1.0E-12):
+        print errorNorm
+        testFailCount += 1
+        testMessages.append("STM Propagation Failure")
+
+    InvSourceMat = np.transpose(np.array(InvSourceMat).reshape(nRow, nRow)).reshape(nRow*nRow).tolist()
+    SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    for i in range(len(InvSourceMat)):
+        sunlineEKF.doubleArray_setitem(SourceVector, i, InvSourceMat[i])
+        sunlineEKF.doubleArray_setitem(InvVector, i, 0.0)
+    nRow = int(math.sqrt(len(InvSourceMat)))
+    sunlineEKF.ukfUInv(SourceVector, nRow, nRow, InvVector)
+
+    InvOut = []
+    for i in range(len(InvSourceMat)):
+        InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
+
+    InvOut = np.array(InvOut).reshape(nRow, nRow)
+    expectIdent = np.dot(InvOut, np.array(InvSourceMat).reshape(nRow,nRow))
+    errorNorm = np.linalg.norm(expectIdent - np.identity(nRow))
+    if(errorNorm > 1.0E-12):
+        print errorNorm
+        testFailCount += 1
+        testMessages.append("Dynamics Matrix Failure")
+
+    InvSourceMat = np.transpose(np.array(InvSourceMat).reshape(nRow, nRow)).reshape(nRow*nRow).tolist()
+    SourceVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    InvVector = sunlineEKF.new_doubleArray(len(InvSourceMat))
+    for i in range(len(InvSourceMat)):
+        sunlineEKF.doubleArray_setitem(SourceVector, i, InvSourceMat[i])
+        sunlineEKF.doubleArray_setitem(InvVector, i, 0.0)
+    nRow = int(math.sqrt(len(InvSourceMat)))
+    sunlineEKF.ukfUInv(SourceVector, nRow, nRow, InvVector)
+
+    InvOut = []
+    for i in range(len(InvSourceMat)):
+        InvOut.append(sunlineEKF.doubleArray_getitem(InvVector, i))
+
+    InvOut = np.array(InvOut).reshape(nRow, nRow)
+    expectIdent = np.dot(InvOut, np.array(InvSourceMat).reshape(nRow,nRow))
+    errorNorm = np.linalg.norm(expectIdent - np.identity(nRow))
+    if(errorNorm > 1.0E-12):
+        print errorNorm
+        testFailCount += 1
+        testMessages.append("Time update Failure")
 
 
     # If the argument provided at commandline "--show_plots" evaluates as true,
@@ -334,7 +206,7 @@ def sunline_utilities_test(show_plots):
 
     # print out success message if no error were found
     if testFailCount == 0:
-        print "PASSED: " + " UKF utilities"
+        print "PASSED: " + " EKF utilities"
 
     # return fail count and join into a single string all messages in the list
     # testMessage
@@ -362,7 +234,7 @@ def testStateUpdateSunLine(show_plots):
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
     # Construct algorithm and associated C++ container
-    moduleConfig = sunlineEKF.SunlineEKFConfig()
+    moduleConfig = sunlineEKF.sunlineEKFConfig()
     moduleWrap = alg_contain.AlgContain(moduleConfig,
                                         sunlineEKF.Update_sunlineEKF,
                                         sunlineEKF.SelfInit_sunlineEKF,
@@ -421,11 +293,11 @@ def testStateUpdateSunLine(show_plots):
                                                 0,
                                                 vehicleConfigOut)
 
-    testVector = numpy.array([-0.7, 0.7, 0.0])
+    testVector = np.array([-0.7, 0.7, 0.0])
     inputData = cssComm.CSSArraySensorIntMsg()
     dotList = []
     for element in CSSOrientationList:
-        dotProd = numpy.dot(numpy.array(element), testVector)
+        dotProd = np.dot(np.array(element), testVector)
         dotList.append(dotProd)
     inputData.CosValue = dotList
     inputMessageSize = inputData.getStructSize()
@@ -463,11 +335,11 @@ def testStateUpdateSunLine(show_plots):
             testFailCount += 1
             testMessages.append("State update failure")
 
-    testVector = numpy.array([-0.8, -0.9, 0.0])
+    testVector = np.array([-0.8, -0.9, 0.0])
     inputData = cssComm.CSSArraySensorIntMsg()
     dotList = []
     for element in CSSOrientationList:
-        dotProd = numpy.dot(numpy.array(element), testVector)
+        dotProd = np.dot(np.array(element), testVector)
         dotList.append(dotProd)
     inputData.CosValue = dotList
         
