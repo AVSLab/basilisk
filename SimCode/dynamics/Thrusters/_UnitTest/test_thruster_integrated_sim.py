1d28167f7e96929fa0389a1c3c332a521aa47673
Cody Allard <cody.allard@colorado.edu>
2017-06-13 14:45:11 -0600

    [BSK-690] So thrusters were actually slightly broken because they would shut off a tilmestep shy of the requested on time due to the integTime being off by a tilmestep. Now all tests pass
    Former-commit-id: ea48df8fdc7124ba9639cddeabc960c2cfb2394b
    Former-commit-id: 24b7b7aa8da5b26f8e1a93d5c72b14d6b61d27ec

--- SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
+++ SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
@@ -104,7 +104,7 @@ def test_thrusterIntegratedTest(show_plots):
     # set thruster commands
     ThrustMessage = thrusterDynamicEffector.THRArrayOnTimeCmdIntMsg()
     msgSize = ThrustMessage.getStructSize()
-    ThrustMessage.OnTimeRequest = [10.0]
+    ThrustMessage.OnTimeRequest = [9.9]
     unitTestSim.TotalSim.CreateNewMessage(unitProcessName, thrusterCommandName, msgSize, 2)
     unitTestSim.TotalSim.WriteMessageData(thrusterCommandName, msgSize, 0, ThrustMessage)
 
