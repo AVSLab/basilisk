03f7fae499573a1be8001ed063e72553f6dd0ac2
Cody Allard <cody.allard@colorado.edu>
2017-06-16 14:38:16 -0600

    [BSK-726] Fixed issue with thruster integrated sim
    Former-commit-id: 960e61bc34a416dae8d6b087a927106660ac1fa9
    Former-commit-id: 6856068f083c4a890c375432675ef135065e85d7

--- SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
+++ SimCode/dynamics/Thrusters/_UnitTest/test_thruster_integrated_sim.py
@@ -80,8 +80,8 @@ def test_thrusterIntegratedTest(show_plots):
     simIncludeThruster.clearSetup()
     simIncludeThruster.create(
         'MOOG_Monarc_1',
-        [1,0,0],                # location in S frame
-        [0,1,0]                 # direction in S frame
+        [1,0,0],                # location in B frame
+        [0,1,0]                 # direction in B frame
     )
 
     # create thruster object container and tie to spacecraft object
