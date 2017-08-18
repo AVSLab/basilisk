[BSK-777] Updating the test to include rotation energy which would have found a bug!
Former-commit-id: f642e238d8ed3cc4127e2ef8821046805c006975
Former-commit-id: b5088212d0700209295dd603ee3947f464ef4318

SimCode/dynamics/reactionWheels/_UnitTest/test_reactionWheelStateEffector_integrated.py
@@ -33,10 +33,8 @@ import unitTestSupport  # general support file with common unit test functions
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import spacecraftPlus
-import sim_model
 import macros
 import gravityEffector
-import spice_interface
 import simIncludeRW
 import reactionWheelStateEffector
 
@@ -78,7 +76,7 @@ def reactionWheelIntegratedTest(show_plots,useFlag,testCase):
     unitTestSim.TotalSim.terminateSimulation()
 
     # Create test thread
-    testProcessRate = macros.sec2nano(0.001)  # update process rate update time
+    testProcessRate = macros.sec2nano(0.0001)  # update process rate update time
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
@@ -147,24 +145,11 @@ def reactionWheelIntegratedTest(show_plots,useFlag,testCase):
     unitTestSim.earthGravBody.isCentralBody = True
     unitTestSim.earthGravBody.useSphericalHarmParams = False
 
-    earthEphemData = spice_interface.SpicePlanetStateSimMsg()
-    earthEphemData.J2000Current = 0.0
-    earthEphemData.PositionVector = [0.0, 0.0, 0.0]
-    earthEphemData.VelocityVector = [0.0, 0.0, 0.0]
-    earthEphemData.J20002Pfix = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-    earthEphemData.J20002Pfix_dot = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
-    earthEphemData.PlanetName = "earth"
-
     scObject.gravField.gravBodies = spacecraftPlus.GravBodyVector([unitTestSim.earthGravBody])
 
     # log data
     unitTestSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, testProcessRate)
     unitTestSim.TotalSim.logThisMessage(rwStateEffector.OutputDataString, testProcessRate)
-
-    msgSize = earthEphemData.getStructSize()
-    unitTestSim.TotalSim.CreateNewMessage(unitProcessName,
-        unitTestSim.earthGravBody.bodyInMsgName, msgSize, 2)
-    unitTestSim.TotalSim.WriteMessageData(unitTestSim.earthGravBody.bodyInMsgName, msgSize, 0, earthEphemData)
 
     # Define initial conditions of the sim
     scObject.hub.mHub = 750.0
@@ -179,33 +164,32 @@ def reactionWheelIntegratedTest(show_plots,useFlag,testCase):
 
     unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbAngMomPntN_N", testProcessRate, 0, 2, 'double')
     unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotAngMomPntC_N", testProcessRate, 0, 2, 'double')
-
-    posRef = scObject.dynManager.getStateObject("hubPosition")
-    sigmaRef = scObject.dynManager.getStateObject("hubSigma")
-
-    scObject.hub.mHub = 750.0
-    scObject.hub.r_BcB_B = [[-0.0002], [0.0001], [0.1]]
-    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbEnergy", testProcessRate, 0, 0, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotEnergy", testProcessRate, 0, 0, 'double')
 
     stopTime = 5.
+    unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime/2))
+    unitTestSim.ExecuteSimulation()
+
+    cmdArray.motorTorque = [0.0, 0.0, 0.0] # [Nm]
+    unitTestSupport.setMessage(unitTestSim.TotalSim,
+                               unitProcessName,
+                               rwCommandName,
+                               cmdArray)
+
     unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
     unitTestSim.ExecuteSimulation()
 
     orbAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbAngMomPntN_N")
     rotAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotAngMomPntC_N")
+    rotEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotEnergy")
+    orbEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbEnergy")
 
-    wheelSpeeds = unitTestSim.pullMessageLogData(rwStateEffector.OutputDataString + "." + "wheelSpeeds",range(3))
+    posData = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.r_BN_N',range(3))
     sigmaData = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.sigma_BN',range(3))
-    omegaData = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.omega_BN_B',range(3))
 
-    # rotEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotEnergy")
-    # orbKinEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbKinEnergy")
-
-    dataPos = posRef.getState()
-    dataSigma = sigmaRef.getState()
-    dataPos = [[stopTime, dataPos[0][0], dataPos[1][0], dataPos[2][0]]]
-    dataSigma = [[stopTime, dataSigma[0][0], dataSigma[1][0], dataSigma[2][0]]]
-
+    dataPos = posData[-1]
+    dataSigma = sigmaData[-1]
 
     if testCase == 'BalancedWheels':
         truePos = [
@@ -251,73 +235,42 @@ def reactionWheelIntegratedTest(show_plots,useFlag,testCase):
                 [rotAngMom_N[-1,0], rotAngMom_N[-1,1], rotAngMom_N[-1,2], rotAngMom_N[-1,3]]
                  ]
 
-
-    # plt.figure(1)
-    # plt.plot(orbAngMom_N[:,0]*1e-9, orbAngMom_N[:,1] - orbAngMom_N[0,1], orbAngMom_N[:,0]*1e-9, orbAngMom_N[:,2] - orbAngMom_N[0,2], orbAngMom_N[:,0]*1e-9, orbAngMom_N[:,3] - orbAngMom_N[0,3])
-    # plt.title("Change in Orbital Angular Momentum")
-    #
-    # plt.figure(2)
-    # plt.plot(rotAngMom_N[:,0]*1e-9, rotAngMom_N[:,1] - rotAngMom_N[0,1], rotAngMom_N[:,0]*1e-9, rotAngMom_N[:,2] - rotAngMom_N[0,2], rotAngMom_N[:,0]*1e-9, rotAngMom_N[:,3] - rotAngMom_N[0,3])
-    # plt.title("Change in Rotational Angular Momentum")
-    #
-    # plt.figure(3)
-    # for i in range(1,4):
-    #     plt.subplot(4,1,i)
-    #     plt.plot(wheelSpeeds[:,0]*1.0E-9, wheelSpeeds[:,i] / (2.0 * math.pi) * 60, label='RWA' + str(i))
-    #     plt.xlabel('Time (s)')
-    #     plt.ylabel(r'RW' + str(i) + r' $\Omega$ (RPM)')
-    #
-    # plt.figure(4)
-    # for i in range(1,4):
-    #     plt.subplot(4,1,i)
-    #     plt.plot(sigmaData[:,0]*1.0E-9, sigmaData[:,i], label='MRP' + str(i))
-    #     plt.xlabel('Time (s)')
-    #     plt.ylabel(r'MRP b' + str(i))
-
-    thetaData = np.empty([len(sigmaData[:,0]),2])
-    thetaData[:,0] = sigmaData[:,0]
-    for i in range(0,len(sigmaData[:,0])):
-        thetaData[i,1] = 4*np.arctan(np.linalg.norm(sigmaData[i,1:]))
-    thetaFit = np.empty([len(sigmaData[:,0]),2])
-    thetaFit[:,0] = thetaData[:,0]
-    fitOrd = 2
-    p = np.polyfit(thetaData[:,0]*1e-9,thetaData[:,1],fitOrd)
-    thetaFit[:,1] = np.polyval(p,thetaFit[:,0]*1e-9)
-
-    # plt.figure(5)
-    # plt.plot(thetaData[:,0]*1e-9, thetaData[:,1])
-    # plt.plot(thetaFit[:,0]*1e-9, thetaFit[:,1], 'r--')
-    # plt.title("Principle Angle")
-    # plt.xlabel('Time (s)')
-    # plt.ylabel(r'$\theta$ (deg)')
-
-    plt.figure(6)
-    plt.plot(thetaData[:,0]*1e-9, thetaData[:,1]-thetaFit[:,1])
-    plt.title("Principle Angle Fit")
-    plt.xlabel('Time (s)')
-    plt.ylabel(r'$\theta$ (deg)')
-
-    # plt.figure(7)
-    # for i in range(1,4):
-    #     plt.subplot(4,1,i)
-    #     plt.plot(omegaData[:,0]*1.0E-9, omegaData[:,i] * 180/math.pi, label='omega' + str(i))
-    #     plt.xlabel('Time (s)')
-    #     plt.ylabel(r'b' + str(i) + r' $\omega$ (d/s)')
-
-    if show_plots == True:
-        plt.show()
-
+    plt.figure()
+    plt.clf()
+    plt.plot(orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,1] - orbAngMom_N[0,1])/orbAngMom_N[0,1], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,2] - orbAngMom_N[0,2])/orbAngMom_N[0,2], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,3] - orbAngMom_N[0,3])/orbAngMom_N[0,3])
+    plt.xlabel("Time (s)")
+    plt.ylabel("Relative Difference")
+    unitTestSupport.writeFigureLaTeX("ChangeInOrbitalAngularMomentum" + testCase, "Change in Orbital Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
+    plt.figure()
+    plt.clf()
+    plt.plot(orbEnergy[:,0]*1e-9, (orbEnergy[:,1] - orbEnergy[0,1])/orbEnergy[0,1])
+    plt.xlabel("Time (s)")
+    plt.ylabel("Relative Difference")
+    unitTestSupport.writeFigureLaTeX("ChangeInOrbitalEnergy" + testCase, "Change in Orbital Energy " + testCase, plt, "width=0.8\\textwidth", path)
+    plt.figure()
+    plt.clf()
+    plt.plot(rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,1] - rotAngMom_N[0,1])/rotAngMom_N[0,1], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,2] - rotAngMom_N[0,2])/rotAngMom_N[0,2], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,3] - rotAngMom_N[0,3])/rotAngMom_N[0,3])
+    plt.xlabel("Time (s)")
+    plt.ylabel("Relative Difference")
+    unitTestSupport.writeFigureLaTeX("ChangeInRotationalAngularMomentum" + testCase, "Change in Rotational Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
+    plt.figure()
+    plt.clf()
+    plt.plot(rotEnergy[int(len(rotEnergy)/2):,0]*1e-9, (rotEnergy[int(len(rotEnergy)/2):,1] - rotEnergy[int(len(rotEnergy)/2),1])/rotEnergy[int(len(rotEnergy)/2),1])
+    plt.xlabel("Time (s)")
+    plt.ylabel("Relative Difference")
+    unitTestSupport.writeFigureLaTeX("ChangeInRotationalEnergy" + testCase, "Change in Rotational Energy " + testCase, plt, "width=0.8\\textwidth", path)
+    plt.show(show_plots)
 
     accuracy = 1e-8
     for i in range(0,len(truePos)):
         # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(dataPos[i],truePos[i],3,accuracy):
+        if not unitTestSupport.isArrayEqualRelative(dataPos,truePos[i],3,accuracy):
             testFailCount += 1
             testMessages.append("FAILED: Reaction Wheel Integrated Test failed pos unit test")
 
     for i in range(0,len(trueSigma)):
         # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(dataSigma[i],trueSigma[i],3,accuracy):
+        if not unitTestSupport.isArrayEqualRelative(dataSigma,trueSigma[i],3,accuracy):
             testFailCount += 1
             testMessages.append("FAILED: Reaction Wheel Integrated Test failed attitude unit test")
 