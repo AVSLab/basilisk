01836977122c0ab72d768ac78e220ead72988226
Scott Carnahan <scottcarnahan@engr2-5-44-dhcp.int.colorado.edu>
2017-08-21 16:23:47 -0600

    [BSK-828] New multi-body test completed.
    Former-commit-id: d0345c5c433348e9187303913024ff01cbf21ca7
    Former-commit-id: 9586f8fdb2f4f82b244f1fade15f9ca177f3b941

--- SimCode/dynamics/gravityEffector/_UnitTest/test_gravityDynEffector.py
+++ SimCode/dynamics/gravityEffector/_UnitTest/test_gravityDynEffector.py
@@ -406,7 +406,7 @@ def singleGravityBody(show_plots):
     return [testFailCount, ''.join(testMessages)]
 
 def multiBodyGravity(show_plots):
-    testCase = 'multiBody'
+    testCase = 'multiBody' #for AutoTeX stuff
     # The __tracebackhide__ setting influences pytest showing of tracebacks:
     # the mrp_steering_tracking() function will not be shown unless the
     # --fulltrace command line option is specified.
@@ -421,139 +421,132 @@ def multiBodyGravity(show_plots):
     # DateSpice = "2015 February 10, 00:00:00.0 TDB"
     #
     # # Create a sim module as an empty container
-    TotalSim = SimulationBaseClass.SimBaseClass()
-    TotalSim.TotalSim.terminateSimulation()
+    multiSim = SimulationBaseClass.SimBaseClass()
+    multiSim.TotalSim.terminateSimulation()
     #
-    DynUnitTestProc = TotalSim.CreateNewProcess(unitProcessName)
+    DynUnitTestProc = multiSim.CreateNewProcess(unitProcessName)
     # # create the dynamics task and specify the integration update time
-    DynUnitTestProc.addTask(TotalSim.CreateNewTask(unitTaskName, macros.sec2nano(1000.0)))
-
-    localPlanetEditor = simMessages.SpicePlanetStateSimMsg()
-    localPlanetEditor.PositionVector = [om.AU, 0., 0.]
-    localPlanetEditor.VelocityVector = [0., 0., 0.]
-
-    #Grav Body 1 is twice the size of the other two
-    TotalSim.gravBody1 = gravityEffector.GravBodyData()
-    TotalSim.gravBody1.bodyInMsgName = "gravBody1_planet_data"
-    TotalSim.gravBody1.outputMsgName = "gravBody1_display_frame_data"
-    TotalSim.gravBody1.mu = 1000000.
-    TotalSim.gravBody1.radEquator = 6500.
-    TotalSim.gravBody1.isCentralBody = False
-    TotalSim.gravBody1.useSphericalHarmParams = False
-    TotalSim.gravBody1.localPlanet = localPlanetEditor
-
-    localPlanetEditor.PositionVector = [-om.AU, 0., 0.]
-    localPlanetEditor.VelocityVector = [0., 0., 0.]
-
-    #grav Body 2 and 3 are coincident with each other, half the mass of gravBody1 and are in the opposite direction of gravBody1
-    TotalSim.gravBody2 = gravityEffector.GravBodyData()
-    TotalSim.gravBody2.bodyInMsgName = "gravBody2_planet_data"
-    TotalSim.gravBody2.outputMsgName = "gravBody2_display_frame_data"
-    TotalSim.gravBody2.mu = 500000.
-    TotalSim.gravBody2.radEquator = 6500.
-    TotalSim.gravBody2.isCentralBody = False
-    TotalSim.gravBody2.useSphericalHarmParams = False
-    TotalSim.gravBody2.localPlanet = localPlanetEditor
-
-    # grav Body 2 and 3 are coincident with each other, half the mass of gravBody1 and are in the opposite direction of gravBody1
-    TotalSim.gravBody3 = gravityEffector.GravBodyData()
-    TotalSim.gravBody3.bodyInMsgName = "gravBody3_planet_data"
-    TotalSim.gravBody3.outputMsgName = "gravBody3_display_frame_data"
-    TotalSim.gravBody3.mu = 500000.
-    TotalSim.gravBody3.radEquator = 6500.
-    TotalSim.gravBody3.isCentralBody = False
-    TotalSim.gravBody3.useSphericalHarmParams = False
-    TotalSim.gravBody3.localPlanet = localPlanetEditor
+    DynUnitTestProc.addTask(multiSim.CreateNewTask(unitTaskName, macros.sec2nano(1000.0)))
 
     #Create dynParamManager to feed fake spacecraft data to so that the gravityEffector can access it.
-    TotalSim.newManager = stateArchitecture.DynParamManager()
+    #This places the spacecraft at the coordinate frame origin so that planets can be placed around it.
+    #velocity and attitude are just set to zero.
+    #center of mass and time are set to zero.
+    newManager = stateArchitecture.DynParamManager()
     positionName = "hubPosition"
     stateDim = [3, 1]
-    posState = TotalSim.newManager.registerState(stateDim[0], stateDim[1], positionName)
+    posState = newManager.registerState(stateDim[0], stateDim[1], positionName)
     posVelSig = [[0.],[0.],[0.]]
     posState.setState(posVelSig)
-    # TotalSim.newManager.setPropertyValue("systemTime", [[i * 1000 * 1e9], [i * 1000.0]])
     velocityName = "hubVelocity"
     stateDim = [3, 1]
-    velState = TotalSim.newManager.registerState(stateDim[0], stateDim[1], velocityName)
+    velState = newManager.registerState(stateDim[0], stateDim[1], velocityName)
     velState.setState(posVelSig)
     sigmaName = "hubSigma"
     stateDim = [3, 1]
-    sigmaState = TotalSim.newManager.registerState(stateDim[0], stateDim[1], sigmaName)
+    sigmaState = newManager.registerState(stateDim[0], stateDim[1], sigmaName)
     sigmaState.setState(posVelSig)
     initC_B = [[0.0], [0.0], [0.0]]
-    TotalSim.newManager.createProperty("centerOfMassSC", initC_B)
-    TotalSim.newManager.createProperty("systemTime", [[0], [0.0]])
+    newManager.createProperty("centerOfMassSC", initC_B)
+    newManager.createProperty("systemTime", [[0], [0.0]])
     #
-    #
+
+
+    #Create a message struct to place gravBody1 where it is wanted
+    localPlanetEditor = simMessages.SpicePlanetStateSimMsg()
+    localPlanetEditor.PositionVector = [om.AU/10., 0., 0.]
+    localPlanetEditor.VelocityVector = [0., 0., 0.]
+
+    #Grav Body 1 is twice the size of the other two
+    gravBody1 = gravityEffector.GravBodyData()
+    gravBody1.bodyInMsgName = "gravBody1_planet_data"
+    gravBody1.outputMsgName = "gravBody1_display_frame_data"
+    gravBody1.mu = 1000000.
+    gravBody1.radEquator = 6500.
+    gravBody1.isCentralBody = False
+    gravBody1.useSphericalHarmParams = False
+    gravBody1.localPlanet = localPlanetEditor
 
     #This is the gravityEffector which will actually compute the gravitational acceleration
     allGrav = gravityEffector.GravityEffector()
-    allGrav.gravBodies = gravityEffector.GravBodyVector([TotalSim.gravBody1, TotalSim.gravBody2, TotalSim.gravBody3])#, TotalSim.sunGravBody, TotalSim.marsGravBody])
-    allGrav.linkInStates(TotalSim.newManager)
-    allGrav.registerProperties(TotalSim.newManager)
-    TotalSim.AddModelToTask(unitTaskName, allGrav)
+    allGrav.gravBodies = gravityEffector.GravBodyVector([gravBody1])
+    allGrav.linkInStates(newManager)
+    allGrav.registerProperties(newManager)
+    multiSim.AddModelToTask(unitTaskName, allGrav)
+    allGrav.computeGravityField() #compute acceleration only considering the first body.
+    step1 = newManager.getPropertyReference("g_N") #retrieve total gravitational acceleration in inertial frame
 
+    #Create a message struct to place gravBody2&3 where they are wanted.
+    localPlanetEditor.PositionVector = [-om.AU/10., 0., 0.]
+    localPlanetEditor.VelocityVector = [0., 0., 0.]
 
-    TotalSim.InitializeSimulationAndDiscover()
+    #grav Body 2 and 3 are coincident with each other, half the mass of gravBody1 and are in the opposite direction of gravBody1
+    gravBody2 = gravityEffector.GravBodyData()
+    gravBody2.bodyInMsgName = "gravBody2_planet_data"
+    gravBody2.outputMsgName = "gravBody2_display_frame_data"
+    gravBody2.mu = gravBody1.mu/2.
+    gravBody2.radEquator = 6500.
+    gravBody2.isCentralBody = False
+    gravBody2.useSphericalHarmParams = False
+    gravBody2.localPlanet = localPlanetEditor
 
-    allGrav.computeGravityField()
-    print TotalSim.newManager.getPropertyReference("g_N")
-    #
-    # TotalSim.gravBody1.InertialPosition
-    #
-    #
-    # # pyswice.furnsh_c(splitPath[0] + '/External/EphemerisData/de430.bsp')
-    # # pyswice.furnsh_c(splitPath[0] + '/External/EphemerisData/naif0011.tls')
-    # # pyswice.furnsh_c(splitPath[0] + '/External/EphemerisData/de-403-masses.tpc')
-    # # pyswice.furnsh_c(splitPath[0] + '/External/EphemerisData/pck00010.tpc')
-    # # pyswice.furnsh_c(path + '/dawn_rec_081022_081109_090218_v1.bsp')
-    # #
-    # # SpiceObject.UTCCalInit = "2008-10-24T00:00:00"
-    # TotalSim.TotalSim.logThisMessage(TotalSim.gravBody1.outputMsgName, macros.sec2nano(1000.0))
-    # TotalSim.InitializeSimulationAndDiscover()
-    # TotalSim.ConfigureStopTime(macros.sec2nano(10000.0))
-    # TotalSim.ExecuteSimulation()
-    # earthPos = TotalSim.pullMessageLogData(TotalSim.gravBody1.outputMsgName+".PositionVector",range(3))
-    # print earthPos
-    # gravErrVec = []
-    #
-    #
-    #
-    # accuracy = 5.0e-4
-    #
-    # for gravMeas in gravErrVec:
-    #     if gravMeas > accuracy:
-    #         testFailCount += 1
-    #         testMessages.append("Gravity multi-body error too high for kernel comparison")
-    #         break
-    # snippetName = testCase + 'Accuracy'
-    # snippetContent = '{:1.1e}'.format(accuracy)  # write formatted LATEX string to file to be used by auto-documentation.
-    # unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path) #write formatted LATEX string to file to be used by auto-documentation.
-    #
-    # if testFailCount == 0:
-    #     passFailText = 'PASSED'
-    #     print "PASSED: " + "Multi-body with Spherical Harmonics"
-    #     colorText = 'ForestGreen'  # color to write auto-documented "PASSED" message in in LATEX
-    #     snippetName = testCase + 'FailMsg'
-    #     snippetContent = ""
-    #     unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path)  # write formatted LATEX string to file to be used by auto-documentation.
-    # else:
-    #     passFailText = 'FAILED'
-    #     colorText = 'Red'  # color to write auto-documented "FAILED" message in in LATEX
-    #     snippetName = testCase + 'FailMsg'
-    #     snippetContent = passFailText
-    #     for message in testMessages:
-    #         snippetContent += ". " + message
-    #     snippetContent += "."
-    #     unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path)  # write formatted LATEX string to file to be used by auto-documentation.
-    # snippetName = testCase + 'PassFail'  # name of file to be written for auto-documentation which specifies if this test was passed or failed.
-    # snippetContent = '\\textcolor{' + colorText + '}{' + passFailText + '}' #write formatted LATEX string to file to be used by auto-documentation.
-    # unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path) #write formatted LATEX string to file to be used by auto-documentation.
+    #This is the gravityEffector which will actually compute the gravitational acceleration
+    allGrav.gravBodies = gravityEffector.GravBodyVector([gravBody1, gravBody2])
+    allGrav.computeGravityField() #compute acceleration considering the first and second bodies.
+    step2 = newManager.getPropertyReference("g_N") #retrieve total gravitational acceleration in inertial frame
 
+    # grav Body 2 and 3 are coincident with each other, half the mass of gravBody1 and are in the opposite direction of gravBody1
+    gravBody3 = gravityEffector.GravBodyData()
+    gravBody3.bodyInMsgName = "gravBody3_planet_data"
+    gravBody3.outputMsgName = "gravBody3_display_frame_data"
+    gravBody3.mu = gravBody2.mu
+    gravBody3.radEquator = 6500.
+    gravBody3.isCentralBody = False
+    gravBody3.useSphericalHarmParams = False
+    gravBody3.localPlanet = localPlanetEditor
 
-    # return fail count and join into a single string all messages in the list
-    # testMessage
+    #This is the gravityEffector which will actually compute the gravitational acceleration
+    allGrav.gravBodies = gravityEffector.GravBodyVector([gravBody1, gravBody2, gravBody3])
+    allGrav.computeGravityField() #comput acceleration considering all three bodies
+    step3 = newManager.getPropertyReference("g_N") #retrieve total gravitational acceleration in inertial frame
+
+    step3 = [0., step3[0][0], step3[1][0], step3[2][0]] #add a first (time) column to use isArrayZero
+
+    #Test results for accuracy
+    accuracy = 1e-12
+    snippetName = testCase + 'Accuracy'
+    snippetContent = '{:1.1e}'.format(accuracy)  # write formatted LATEX string to file to be used by auto-documentation.
+    unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path) #write formatted LATEX string to file to be used by auto-documentation.
+    if not unitTestSupport.isDoubleEqualRelative(step2[0][0]/step1[0][0], 0.5, accuracy): #if the second grav body doesn't cancel exactly half of the first body's acceleration.
+        testFailCount += 1
+        passFailText = "Step 2 was not half of step 1"
+        testMessages.append(passFailText)
+    elif not unitTestSupport.isArrayZero(step3, 3, accuracy): #if the net acceleration is not now 0.
+        testFailCount += 1
+        passFailText = "Step 3 did not cause gravity to return to 0"
+        testMessages.append(passFailText)
+
+    #Record test results to LaTeX
+    if testFailCount == 0:
+        passFailText = 'PASSED'
+        print "PASSED: " + " Multi-Body"
+        colorText = 'ForestGreen'  # color to write auto-documented "PASSED" message in in LATEX
+        snippetName = testCase + 'FailMsg'
+        snippetContent = ""
+        unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path)  # write formatted LATEX string to file to be used by auto-documentation.
+    else:
+        passFailText = 'FAILED'
+        colorText = 'Red'  # color to write auto-documented "FAILED" message in in LATEX
+        snippetName = testCase + 'FailMsg'
+        snippetContent = passFailText
+        for message in testMessages:
+            snippetContent += ". " + message
+        snippetContent += "."
+        unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path)  # write formatted LATEX string to file to be used by auto-documentation.
+    snippetName = testCase + 'PassFail'  # name of file to be written for auto-documentation which specifies if this test was passed or failed.
+    snippetContent = '\\textcolor{' + colorText + '}{' + passFailText + '}' #write formatted LATEX string to file to be used by auto-documentation.
+    unitTestSupport.writeTeXSnippet(snippetName, snippetContent, path) #write formatted LATEX string to file to be used by auto-documentation.
+
     return [testFailCount, ''.join(testMessages)]
 
 if __name__ == "__main__":
