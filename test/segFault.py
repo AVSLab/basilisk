5d8819a490d8756469b8f12197f00421d2e5329c
Nathan Bellowe <nbellowe@gmail.com>
2017-10-05 16:54:35 -0600

    Added temporary not-working test
    Former-commit-id: 81c4cf3d839025ed533aeb98353c5322e8aab51b
    Former-commit-id: 08e691953356e6a388ba20b2acf10e7a81c56351

---
+++ test/segFault.py
@@ -0,0 +1,150 @@
+import pytest
+import sys, os, inspect
+import matplotlib
+import numpy as np
+import ctypes
+import math
+import csv
+import logging
+
+# import general simulation support files
+from Basilisk.utilities import SimulationBaseClass
+from Basilisk.utilities import unitTestSupport                  # general support file with common unit test functions
+import matplotlib.pyplot as plt
+from Basilisk.utilities import macros
+from Basilisk.utilities import orbitalMotion
+
+# import simulation related support
+from Basilisk.modules import spacecraftPlus
+from Basilisk.utilities import simIncludeGravBody
+
+
+def createAndExecute():
+    # Create simulation variable names
+    simTaskName = "simTask"
+    simProcessName = "simProcess"
+
+    scSim = SimulationBaseClass.SimBaseClass()
+    scSim.TotalSim.terminateSimulation()
+
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+
+    simulationTimeStep = macros.sec2nano(10.)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
+
+    scObject = spacecraftPlus.SpacecraftPlus()
+    scObject.ModelTag = "spacecraftBody"
+    scObject.hub.useTranslation = True
+    scObject.hub.useRotation = False
+
+    # add spacecraftPlus object to the simulation process
+    scSim.AddModelToTask(simTaskName, scObject)
+
+    # setup Gravity Body
+    gravFactory = simIncludeGravBody.gravBodyFactory()
+    planet = gravFactory.createEarth()
+    planet.isCentralBody = True          # ensure this is the central gravitational body
+    mu = planet.mu
+
+    scObject.gravField.gravBodies = spacecraftPlus.GravBodyVector(gravFactory.gravBodies.values())
+
+    oe = orbitalMotion.ClassicElements()
+    rLEO = 7000.*1000      # meters
+    rGEO = 42000.*1000     # meters
+    oe.a     = rLEO
+    oe.e     = 0.0001
+    oe.i     = 33.3*macros.D2R
+    oe.Omega = 48.2*macros.D2R
+    oe.omega = 347.8*macros.D2R
+    oe.f     = 85.3*macros.D2R
+    rN, vN = orbitalMotion.elem2rv(mu, oe)
+    oe = orbitalMotion.rv2elem(mu, rN, vN)      # this stores consistent initial orbit elements
+                                                # with circular or equatorial orbit, some angles are
+                                                # arbitrary
+    scObject.hub.r_CN_NInit = unitTestSupport.np2EigenVectorXd(rN)  # m   - r_BN_N
+    scObject.hub.v_CN_NInit = unitTestSupport.np2EigenVectorXd(vN)  # m/s - v_BN_N
+
+
+    n = np.sqrt(mu/oe.a/oe.a/oe.a)
+    P = 2.*np.pi/n
+    simulationTime = macros.sec2nano(0.75*P)
+
+    numDataPoints = 100
+    samplingTime = simulationTime / (numDataPoints-1)
+    scSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, samplingTime)
+
+    scSim.InitializeSimulationAndDiscover()
+    scSim.ConfigureStopTime(simulationTime)
+    scSim.ExecuteSimulation()
+
+def create():
+    simTaskName = "simTask"
+    simProcessName = "simProcess"
+
+    scSim = SimulationBaseClass.SimBaseClass()
+    scSim.TotalSim.terminateSimulation()
+
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+
+    simulationTimeStep = macros.sec2nano(10.)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
+
+    scObject = spacecraftPlus.SpacecraftPlus()
+    scObject.ModelTag = "spacecraftBody"
+    scObject.hub.useTranslation = True
+    scObject.hub.useRotation = False
+
+    # add spacecraftPlus object to the simulation process
+    scSim.AddModelToTask(simTaskName, scObject)
+
+    # setup Gravity Body
+    gravFactory = simIncludeGravBody.gravBodyFactory()
+    planet = gravFactory.createEarth()
+    planet.isCentralBody = True          # ensure this is the central gravitational body
+    mu = planet.mu
+
+    scObject.gravField.gravBodies = spacecraftPlus.GravBodyVector(gravFactory.gravBodies.values())
+
+    oe = orbitalMotion.ClassicElements()
+    rLEO = 7000.*1000      # meters
+    rGEO = 42000.*1000     # meters
+    oe.a     = rLEO
+    oe.e     = 0.0001
+    oe.i     = 33.3*macros.D2R
+    oe.Omega = 48.2*macros.D2R
+    oe.omega = 347.8*macros.D2R
+    oe.f     = 85.3*macros.D2R
+    rN, vN = orbitalMotion.elem2rv(mu, oe)
+    oe = orbitalMotion.rv2elem(mu, rN, vN)      # this stores consistent initial orbit elements
+                                                # with circular or equatorial orbit, some angles are
+                                                # arbitrary
+    scObject.hub.r_CN_NInit = unitTestSupport.np2EigenVectorXd(rN)  # m   - r_BN_N
+    scObject.hub.v_CN_NInit = unitTestSupport.np2EigenVectorXd(vN)  # m/s - v_BN_N
+
+
+    n = np.sqrt(mu/oe.a/oe.a/oe.a)
+    P = 2.*np.pi/n
+    simulationTime = macros.sec2nano(0.75*P)
+
+    numDataPoints = 100
+    samplingTime = simulationTime / (numDataPoints-1)
+    scSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, samplingTime)
+    scSim.InitializeSimulationAndDiscover()
+    scSim.ConfigureStopTime(simulationTime)
+
+    return scSim
+
+def execute(scSim):
+    scSim.ExecuteSimulation()
+
+
+if __name__ == "__main__":
+    print "The createAndExecute function should be completely identical to execute(create())"
+    print "First trying to createAndExecute function"
+    createAndExecute()
+
+    print "... successful"
+
+    print "Now trying execute(create())"
+    execute(create())
+    print "... successful"
