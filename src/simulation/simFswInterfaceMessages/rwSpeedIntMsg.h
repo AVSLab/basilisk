b6317d84876398dc35f1b792133d1f3a8cc6cf9d
Cody Allard <cody.allard@colorado.edu>
2017-11-27 12:28:18 -0700

    [BSK-925] Added thetas to fswInterfaceMessages
    Former-commit-id: a15566c97ec2899568f1cc2f6acdea3d78c2dcc4
    Former-commit-id: c27387381118750b0109dfa033584c19c247f306

--- src/simulation/simFswInterfaceMessages/rwSpeedIntMsg.h
+++ src/simulation/simFswInterfaceMessages/rwSpeedIntMsg.h
@@ -25,6 +25,7 @@
 /*! @brief Structure used to define the output definition for reaction wheel speeds*/
 typedef struct {
     double wheelSpeeds[MAX_EFF_CNT];                //!< r/s The current angular velocities of the RW wheel
+    double wheelThetas[MAX_EFF_CNT];                //!< r/s The current angle of the RW if jitter is enabled
 }RWSpeedIntMsg;
 
 #endif
