a611c72b57ddf1b0789be299a5bd5f58e0d6f11d
Cody Allard <cody.allard@colorado.edu>
2017-11-27 13:54:22 -0700

    [HOT-FIX] Added all of the hinged rigid body code over to the new form
    Former-commit-id: 1ff6ef436962d1f33fde119b0546c3d14610ed0f
    Former-commit-id: ea0840733ace3bcaef5e349dca134e7275c234f5

---
+++ src/simulation/dynamics/dualHingedRigidBodies/UnitTest/dualhingedRigidBodyStateEffector.py
@@ -0,0 +1,169 @@
+''' '''
+'''
+ ISC License
+
+ Copyright (c) 2016-2017, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+'''
+import sys, os, inspect
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy
+import pytest
+import math
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+splitPath = path.split('SimCode')
+sys.path.append(splitPath[0] + '/modules')
+sys.path.append(splitPath[0] + '/PythonModules')
+
+import SimulationBaseClass
+import unitTestSupport  # general support file with common unit test functions
+import macros
+import spacecraftPlus
+import hingedRigidBodyStateEffector
+import sim_model
+import macros
+import ctypes
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+# uncomment this line if this test has an expected failure, adjust message as needed
+# @pytest.mark.xfail() # need to update how the RW states are defined
+# provide a unique test method name, starting with test_
+def spacecraftPlusAllTest(show_plots):
+    [testResults, testMessage] = test_hubPropagate(show_plots)
+    assert testResults < 1, testMessage
+
+def test_hubPropagate(show_plots):
+    # The __tracebackhide__ setting influences pytest showing of tracebacks:
+    # the mrp_steering_tracking() function will not be shown unless the
+    # --fulltrace command line option is specified.
+    __tracebackhide__ = True
+
+    testFailCount = 0  # zero unit test result counter
+    testMessages = []  # create empty list to store test log messages
+    
+    scObject = spacecraftPlus.SpacecraftPlus()
+    scObject.ModelTag = "spacecraftBody"
+    
+    unitTaskName = "unitTask"  # arbitrary name (don't change)
+    unitProcessName = "TestProcess"  # arbitrary name (don't change)
+    
+    #   Create a sim module as an empty container
+    unitTestSim = SimulationBaseClass.SimBaseClass()
+    unitTestSim.TotalSim.terminateSimulation()
+    
+    # Create test thread
+    testProcessRate = macros.sec2nano(0.1)  # update process rate update time
+    testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+
+    unitTestSim.panel1 = hingedRigidBodyStateEffector.HingedRigidBodyStateEffector()
+    unitTestSim.panel2 = hingedRigidBodyStateEffector.HingedRigidBodyStateEffector()
+
+    # Define Variable for panel 1
+    unitTestSim.panel1.mass = 100.0
+    unitTestSim.panel1.IPntS_S = [[100.0, 0.0, 0.0], [0.0, 50.0, 0.0], [0.0, 0.0, 50.0]]
+    unitTestSim.panel1.d = 1.5
+    unitTestSim.panel1.k = 100.0
+    unitTestSim.panel1.c = 0.0
+    unitTestSim.panel1.rHB_B = [[0.5], [0.0], [1.0]]
+    unitTestSim.panel1.dcmHB = [[-1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0]]
+    unitTestSim.panel1.nameOfThetaState = "hingedRigidBodyTheta1"
+    unitTestSim.panel1.nameOfThetaDotState = "hingedRigidBodyThetaDot1"
+
+    # Define Variables for panel 2
+    unitTestSim.panel2.mass = 100.0
+    unitTestSim.panel2.IPntS_S = [[100.0, 0.0, 0.0], [0.0, 50.0, 0.0], [0.0, 0.0, 50.0]]
+    unitTestSim.panel2.d = 1.5
+    unitTestSim.panel2.k = 100.0
+    unitTestSim.panel2.c = 0.0
+    unitTestSim.panel2.rHB_B = [[-0.5], [0.0], [1.0]]
+    unitTestSim.panel2.dcmHB = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    unitTestSim.panel2.nameOfThetaState = "hingedRigidBodyTheta2"
+    unitTestSim.panel2.nameOfThetaDotState = "hingedRigidBodyThetaDot2"
+
+    # Add panels to spaceCraft
+    # this next line is not working
+    scObject.addStateEffector(unitTestSim.panel1)
+    scObject.addStateEffector(unitTestSim.panel2)
+    
+    # Add test module to runtime call list
+    unitTestSim.AddModelToTask(unitTaskName, scObject)
+    
+    unitTestSim.InitializeSimulation()
+
+    posRef = scObject.dynManager.getStateObject("hubPosition")
+    velRef = scObject.dynManager.getStateObject("hubVelocity")
+    sigmaRef = scObject.dynManager.getStateObject("hubSigma")
+    omegaRef = scObject.dynManager.getStateObject("hubOmega")
+    theta1Ref = scObject.dynManager.getStateObject("hingedRigidBodyTheta1")
+    thetaDot1Ref = scObject.dynManager.getStateObject("hingedRigidBodyThetaDot1")
+    theta2Ref = scObject.dynManager.getStateObject("hingedRigidBodyTheta2")
+    thetaDot2Ref = scObject.dynManager.getStateObject("hingedRigidBodyThetaDot2")
+
+    posRef.setState([[0.0], [0.0], [0.0]])
+    velRef.setState([[0.0], [0.0], [0.0]])
+    sigmaRef.setState([[0.0], [0.0], [0.0]])
+    omegaRef.setState([[0.1], [-0.1], [0.1]])
+    theta1Ref.setState([[5*numpy.pi/180.0]])
+    thetaDot1Ref.setState([[0.0]])
+    theta2Ref.setState([[0.0]])
+    thetaDot2Ref.setState([[0.0]])
+
+    scObject.hub.mHub = 750.0
+    scObject.hub.rBcB_B = [[0.0], [0.0], [0.0]]
+    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
+
+    stopTime = 60.0*10.0
+    unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
+    unitTestSim.ExecuteSimulation()
+
+    dataPos = posRef.getState()
+    dataPos = [[stopTime, dataPos[0][0], dataPos[1][0], dataPos[2][0]]]
+    dataSigma =  sigmaRef.getState()
+    dataSigma = [[stopTime, dataSigma[0][0], dataSigma[1][0], dataSigma[2][0]]]
+
+    truePos = [
+                [-13.3842058040891, -13.2968874812058, 0.155873769585104]
+                ]
+    trueSigma = [
+                  [0.228198675962671, -0.329880460528557,  0.266599868549938]
+                  ]
+
+    accuracy = 1e-8
+    for i in range(0,len(truePos)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqualRelative(dataPos[i],truePos[i],3,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED:  Dynamics Mode failed pos unit test at t=" + str(dataPos[i,0]*macros.NANO2SEC) + "sec\n")
+
+    for i in range(0,len(trueSigma)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqualRelative(dataSigma[i],trueSigma[i],3,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED:  Dynamics Mode failed attitude unit test at t=" + str(dataSigma[i,0]*macros.NANO2SEC) + "sec\n")
+
+    if testFailCount == 0:
+        print "PASSED: " + " Hub Propagate"
+    # return fail count and join into a single string all messages in the list
+    # testMessage
+    return [testFailCount, ''.join(testMessages)]
+
+if __name__ == "__main__":
+    spacecraftPlusAllTest(False)
---
+++ src/simulation/dynamics/dualHingedRigidBodies/dualHingedRigidBodyStateEffector.cpp
@@ -0,0 +1,299 @@
+/*
+ ISC License
+
+ Copyright (c) 2016-2017, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+
+#include "dualHingedRigidBodyStateEffector.h"
+
+DualHingedRigidBodyStateEffector::DualHingedRigidBodyStateEffector()
+{
+    //! - zero the mass props and mass prop rates contributions
+    this->effProps.mEff = 0.0;
+    this->effProps.mEffDot = 0.0;
+    this->effProps.rEff_CB_B.setZero();
+    this->effProps.IEffPntB_B.setZero();
+    this->effProps.rEffPrime_CB_B.setZero();
+    this->effProps.IEffPrimePntB_B.setZero();
+    this->matrixFDHRB.resize(2, 3);
+    this->matrixGDHRB.resize(2, 3);
+
+    //! - Initialize the variables to working values
+    this->mass1 = 0.0;
+    this->d1 = 1.0;
+    this->k1 = 1.0;
+    this->c1 = 0.0;
+    this->mass2 = 0.0;
+    this->d2 = 1.0;
+    this->k2 = 1.0;
+    this->c2 = 0.0;
+    this->theta1Init = 0.0;
+    this->theta1DotInit = 0.0;
+    this->theta2Init = 0.0;
+    this->theta2DotInit = 0.0;
+    this->IPntS1_S1.setIdentity();
+    this->IPntS2_S2.setIdentity();
+    this->rH1B_B.setZero();
+    this->dcmH1B.setIdentity();
+    this->thetaH2S1 = 0.0;
+    this->nameOfTheta1State = "hingedRigidBodyTheta1";
+    this->nameOfTheta1DotState = "hingedRigidBodyTheta1Dot";
+    this->nameOfTheta2State = "hingedRigidBodyTheta2";
+    this->nameOfTheta2DotState = "hingedRigidBodyTheta2Dot";
+
+    return;
+}
+
+
+DualHingedRigidBodyStateEffector::~DualHingedRigidBodyStateEffector()
+{
+    return;
+}
+
+void DualHingedRigidBodyStateEffector::linkInStates(DynParamManager& statesIn)
+{
+    //! - Get access to the hubs sigma, omegaBN_B and velocity needed for dynamic coupling
+    this->hubVelocity = statesIn.getStateObject("hubVelocity");
+    this->hubSigma = statesIn.getStateObject("hubSigma");
+    this->hubOmega = statesIn.getStateObject("hubOmega");
+    this->g_N = statesIn.getPropertyReference("g_N");
+
+    return;
+}
+
+void DualHingedRigidBodyStateEffector::registerStates(DynParamManager& states)
+{
+    //! - Register the states associated with hinged rigid bodies - theta and thetaDot
+    this->theta1State = states.registerState(1, 1, this->nameOfTheta1State);
+    this->theta1DotState = states.registerState(1, 1, this->nameOfTheta1DotState);
+    this->theta2State = states.registerState(1, 1, this->nameOfTheta2State);
+    this->theta2DotState = states.registerState(1, 1, this->nameOfTheta2DotState);
+
+    //! - Add this code to allow for non-zero initial conditions, as well hingedRigidBody
+    Eigen::MatrixXd theta1InitMatrix(1,1);
+    theta1InitMatrix(0,0) = this->theta1Init;
+    this->theta1State->setState(theta1InitMatrix);
+    Eigen::MatrixXd theta1DotInitMatrix(1,1);
+    theta1DotInitMatrix(0,0) = this->theta1DotInit;
+    this->theta1DotState->setState(theta1DotInitMatrix);
+    Eigen::MatrixXd theta2InitMatrix(1,1);
+    theta2InitMatrix(0,0) = this->theta2Init;
+    this->theta2State->setState(theta2InitMatrix);
+    Eigen::MatrixXd theta2DotInitMatrix(1,1);
+    theta2DotInitMatrix(0,0) = this->theta2DotInit;
+    this->theta2DotState->setState(theta2DotInitMatrix);
+
+    return;
+}
+
+void DualHingedRigidBodyStateEffector::updateEffectorMassProps(double integTime)
+{
+    //! - Give the mass of the hinged rigid body to the effProps mass
+    this->effProps.mEff = this->mass1 + this->mass2;
+
+    //! - find hinged rigid bodies' position with respect to point B
+    //! - First need to grab current states
+    this->theta1 = this->theta1State->getState()(0, 0);
+    this->theta1Dot = this->theta1DotState->getState()(0, 0);
+    this->theta2 = this->theta2State->getState()(0, 0);
+    this->theta2Dot = this->theta2DotState->getState()(0, 0);
+    //! - Next find the sHat unit vectors
+    Eigen::Matrix3d dcmS1H1;
+    dcmS1H1 = eigenM2(this->theta1);
+    this->dcmS1B = dcmS1H1*this->dcmH1B;
+    Eigen::Matrix3d dcmH2S1;
+    dcmH2S1 = eigenM2(this->thetaH2S1);
+    Eigen::Matrix3d dcmH2B;
+    dcmH2B = dcmH2S1*this->dcmS1B;
+    Eigen::Matrix3d dcmS2H2;
+    dcmS2H2 = eigenM2(this->theta2);
+    this->dcmS2B = dcmS2H2 * dcmH2B;
+    this->sHat11_B = this->dcmS1B.row(0);
+    this->sHat12_B = this->dcmS1B.row(1);
+    this->sHat13_B = this->dcmS1B.row(2);
+    this->sHat21_B = this->dcmS2B.row(0);
+    this->sHat22_B = this->dcmS2B.row(1);
+    this->sHat23_B = this->dcmS2B.row(2);
+    this->rS1B_B = this->rH1B_B - this->d1*this->sHat11_B;
+    this->rS2B_B = this->rH1B_B - this->l1*this->sHat11_B - this->d2*this->sHat21_B;
+    this->effProps.rEff_CB_B = 1.0/this->effProps.mEff*(this->mass1*this->rS1B_B + this->mass2*this->rS2B_B);
+
+    //! - Find the inertia of the hinged rigid body about point B
+    //! - Define rTildeSB_B
+    this->rTildeS1B_B = eigenTilde(this->rS1B_B);
+    this->rTildeS2B_B = eigenTilde(this->rS2B_B);
+    this->effProps.IEffPntB_B = this->dcmS1B.transpose()*this->IPntS1_S1*this->dcmS1B + this->mass1*this->rTildeS1B_B*this->rTildeS1B_B.transpose() + this->dcmS2B.transpose()*this->IPntS2_S2*this->dcmS2B + this->mass2*this->rTildeS2B_B*this->rTildeS2B_B.transpose();
+
+    //! First, find the rPrimeSB_B
+    this->rPrimeS1B_B = this->d1*this->theta1Dot*this->sHat13_B;
+    this->rPrimeS2B_B = this->l1*this->theta1Dot*this->sHat13_B + this->d2*(this->theta1Dot + this->theta2Dot)*this->sHat23_B;
+    this->effProps.rEffPrime_CB_B = 1.0/this->effProps.mEff*(this->mass1*this->rPrimeS1B_B + this->mass2*this->rPrimeS2B_B);
+
+    //! - Next find the body time derivative of the inertia about point B
+    //! - Define tilde matrix of rPrimeSB_B
+    this->rPrimeTildeS1B_B = eigenTilde(this->rPrimeS1B_B);
+    this->rPrimeTildeS2B_B = eigenTilde(this->rPrimeS2B_B);
+    //! - Find body time derivative of IPntS_B
+    this->IS1PrimePntS1_B = this->theta1Dot*(this->IPntS1_S1(2,2) - this->IPntS1_S1(0,0))*(this->sHat11_B*this->sHat13_B.transpose() + this->sHat13_B*this->sHat11_B.transpose());
+    this->IS2PrimePntS2_B = (this->theta1Dot+this->theta2Dot)*(this->IPntS2_S2(2,2) - this->IPntS2_S2(0,0))*(this->sHat21_B*this->sHat23_B.transpose() + this->sHat23_B*this->sHat21_B.transpose());
+    //! - Find body time derivative of IPntB_B
+    this->effProps.IEffPrimePntB_B = this->IS1PrimePntS1_B - this->mass1*(this->rPrimeTildeS1B_B*this->rTildeS1B_B + this->rTildeS1B_B*this->rPrimeTildeS1B_B) + this->IS2PrimePntS2_B - this->mass2*(this->rPrimeTildeS2B_B*this->rTildeS2B_B + this->rTildeS2B_B*this->rPrimeTildeS2B_B);
+
+    return;
+}
+
+void DualHingedRigidBodyStateEffector::updateContributions(double integTime, Eigen::Matrix3d & matrixAcontr, Eigen::Matrix3d & matrixBcontr, Eigen::Matrix3d & matrixCcontr, Eigen::Matrix3d & matrixDcontr, Eigen::Vector3d & vecTranscontr, Eigen::Vector3d & vecRotcontr)
+{
+    Eigen::MRPd sigmaBNLocal;
+    Eigen::Matrix3d dcmBN;                        /* direction cosine matrix from N to B */
+    Eigen::Matrix3d dcmNB;                        /* direction cosine matrix from B to N */
+    Eigen::Vector3d gravityTorquePntH1_B;          /* torque of gravity on HRB about Pnt H */
+    Eigen::Vector3d gravityTorquePntH2_B;          /* torque of gravity on HRB about Pnt H */
+    Eigen::Vector3d gLocal_N;                          /* gravitational acceleration in N frame */
+    Eigen::Vector3d g_B;                          /* gravitational acceleration in B frame */
+    gLocal_N = *this->g_N;
+
+    //! - Find dcmBN
+    sigmaBNLocal = (Eigen::Vector3d )this->hubSigma->getState();
+    dcmNB = sigmaBNLocal.toRotationMatrix();
+    dcmBN = dcmNB.transpose();
+    //! - Map gravity to body frame
+    g_B = dcmBN*gLocal_N;
+
+    //! - Define omegaBN_S
+    this->omegaBNLoc_B = this->hubOmega->getState();
+    this->omegaBN_S1 = this->dcmS1B*this->omegaBNLoc_B;
+    this->omegaBN_S2 = this->dcmS2B*this->omegaBNLoc_B;
+    //! - Define omegaTildeBNLoc_B
+    this->omegaTildeBNLoc_B = eigenTilde(this->omegaBNLoc_B);
+    //! - Define matrices needed for back substitution
+    //gravityTorquePntH1_B = -this->d1*this->sHat11_B.cross(this->mass1*g_B); //Need to review these equations and implement them - SJKC
+    //gravityTorquePntH2_B = -this->d2*this->sHat21_B.cross(this->mass2*g_B); //Need to review these equations and implement them - SJKC
+    this->matrixADHRB(0,0) = this->IPntS1_S1(1,1) + this->mass1*this->d1*this->d1 + this->mass2*this->l1*this->l1 + this->mass2*this->l1*this->d2*this->sHat13_B.transpose()*(this->sHat23_B);
+    this->matrixADHRB(0,1) = this->mass2*this->l1*this->d2*this->sHat13_B.transpose()*(this->sHat23_B);
+    this->matrixADHRB(1,0) = IPntS2_S2(1,1) + this->mass2*this->d2*this->d2 + this->mass2*this->l1*this->d2*this->sHat23_B.transpose()*this->sHat13_B;
+    this->matrixADHRB(1,1) = this->IPntS2_S2(1,1) + this->mass2*this->d2*this->d2;
+    this->matrixEDHRB = this->matrixADHRB.inverse();
+    this->matrixFDHRB.row(0) = -(this->mass2*this->l1 + this->mass1*this->d1)*this->sHat13_B.transpose();
+    this->matrixFDHRB.row(1) = -this->mass2*this->d2*this->sHat23_B.transpose();
+
+    this->matrixGDHRB.row(0) = -(this->IPntS1_S1(1,1)*this->sHat12_B.transpose() - this->mass1*this->d1*this->sHat13_B.transpose()*this->rTildeS1B_B - this->mass2*this->l1*this->sHat13_B.transpose()*this->rTildeS2B_B);
+    this->matrixGDHRB.row(1) = -(this->IPntS2_S2(1,1)*this->sHat22_B.transpose() + this->mass2*this->d2*this->sHat23_B.transpose()*this->rTildeS2B_B);
+
+    this->vectorVDHRB(0) =  -(this->IPntS1_S1(0,0) - this->IPntS1_S1(2,2))*this->omegaBN_S1(2)*this->omegaBN_S1(0) - this->k1*this->theta1 - this->c1*this->theta1Dot + this->k2*this->theta2 + this->c2*this->theta2Dot
+                            - this->mass1*this->d1*this->sHat13_B.transpose()*(2*this->omegaTildeBNLoc_B*this->rPrimeS1B_B + this->omegaTildeBNLoc_B*this->omegaTildeBNLoc_B*this->rS1B_B)
+                            - this->mass2*this->l1*this->sHat13_B.transpose()*(2*this->omegaTildeBNLoc_B*this->rPrimeS2B_B + this->omegaTildeBNLoc_B*this->omegaTildeBNLoc_B*this->rS2B_B + this->l1*this->theta1Dot*this->theta1Dot*this->sHat11_B + this->d2*(this->theta1Dot + this->theta2Dot)*(this->theta1Dot + this->theta2Dot)*this->sHat21_B); //still missing torque and force terms - SJKC
+
+    this->vectorVDHRB(1) =  -(this->IPntS2_S2(0,0) - this->IPntS2_S2(2,2))*this->omegaBN_S2(2)*this->omegaBN_S2(0)
+                            - this->k2*this->theta2 - this->c2*this->theta2Dot - this->mass2*this->d2*this->sHat23_B.transpose()*(2*this->omegaTildeBNLoc_B*this->rPrimeS2B_B + this->omegaTildeBNLoc_B*this->omegaTildeBNLoc_B*this->rS2B_B + this->l1*this->theta1Dot*this->theta1Dot*this->sHat11_B); // still missing torque term. - SJKC
+
+    //! - Start defining them good old contributions - start with translation
+    //! - For documentation on contributions see Allard, Diaz, Schaub flex/slosh paper
+    matrixAcontr = (this->mass1*this->d1*this->sHat13_B + this->mass2*this->l1*this->sHat13_B + this->mass2*this->d2*this->sHat23_B)*matrixEDHRB.row(0)*this->matrixFDHRB + this->mass2*this->d2*this->sHat23_B*this->matrixEDHRB.row(1)*this->matrixFDHRB;
+    matrixBcontr = (this->mass1*this->d1*this->sHat13_B + this->mass2*this->l1*this->sHat13_B + this->mass2*this->d2*this->sHat23_B)*this->matrixEDHRB.row(0)*(matrixGDHRB) + this->mass2*this->d2*this->sHat23_B*this->matrixEDHRB.row(1)*(matrixGDHRB);
+    vecTranscontr = -(this->mass1*this->d1*this->theta1Dot*this->theta1Dot*this->sHat11_B + this->mass2*(this->l1*this->theta1Dot*this->theta1Dot*this->sHat11_B + this->d2*(this->theta1Dot+this->theta2Dot)*(this->theta1Dot+this->theta2Dot)*this->sHat21_B)
+                    + (this->mass1*this->d1*this->sHat13_B + this->mass2*this->l1*this->sHat13_B + this->mass2*this->d2*this->sHat23_B)*this->matrixEDHRB.row(0)*this->vectorVDHRB + this->mass2*this->d2*this->sHat23_B*this->matrixEDHRB.row(1)*this->vectorVDHRB);
+
+    //! - Define rotational matrice contributions (Eq 96 in paper)
+    
+    matrixCcontr = (this->IPntS1_S1(1,1)*this->sHat12_B + this->mass1*this->d1*this->rTildeS1B_B*this->sHat13_B + this->IPntS2_S2(1,1)*this->sHat22_B + this->mass2*this->l1*this->rTildeS2B_B*this->sHat13_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(0)*this->matrixFDHRB
+                    + (this->IPntS2_S2(1,1)*this->sHat22_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(1)*this->matrixFDHRB;
+    
+    matrixDcontr = (this->IPntS1_S1(1,1)*this->sHat12_B + this->mass1*this->d1*this->rTildeS1B_B*this->sHat13_B + this->IPntS2_S2(1,1)*this->sHat22_B + this->mass2*this->l1*this->rTildeS2B_B*this->sHat13_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(0)*this->matrixGDHRB
+                    +(this->IPntS2_S2(1,1)*this->sHat22_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(1)*this->matrixGDHRB;
+    
+    vecRotcontr = -(this->theta1Dot*this->IPntS1_S1(1,1)*this->omegaTildeBNLoc_B*this->sHat12_B
+                    + this->mass1*this->omegaTildeBNLoc_B*this->rTildeS1B_B*this->rPrimeS1B_B + this->mass1*this->d1*this->theta1Dot*this->theta1Dot*this->rTildeS1B_B*this->sHat11_B + (this->theta1Dot+this->theta2Dot)*this->IPntS2_S2(1,1)*this->omegaTildeBNLoc_B*this->sHat22_B + this->mass2*this->omegaTildeBNLoc_B*this->rTildeS2B_B*this->rPrimeS2B_B
+                    + this->mass2*this->rTildeS2B_B*(this->l1*this->theta1Dot*this->theta1Dot*this->sHat11_B + this->d2*(this->theta1Dot+this->theta2Dot)*(this->theta1Dot+this->theta2Dot)*this->sHat21_B) + (this->IPntS1_S1(1,1)*this->sHat12_B + this->mass1*this->d1*this->rTildeS1B_B*this->sHat13_B + this->IPntS2_S2(1,1)*this->sHat22_B
+                    + this->mass2*this->l1*this->rTildeS2B_B*this->sHat13_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(0)*this->vectorVDHRB + (this->IPntS2_S2(1,1)*this->sHat22_B + this->mass2*this->d2*this->rTildeS2B_B*this->sHat23_B)*this->matrixEDHRB.row(1)*this->vectorVDHRB);
+
+    return;
+}
+
+void DualHingedRigidBodyStateEffector::computeDerivatives(double integTime)
+{
+    //! - Define necessarry variables
+    Eigen::MRPd sigmaBNLocal;
+    Eigen::Matrix3d dcmBN;                        /* direction cosine matrix from N to B */
+    Eigen::Matrix3d dcmNB;                        /* direction cosine matrix from B to N */
+    Eigen::MatrixXd theta1DDot(1,1);               /* thetaDDot variable to send to state manager */
+    Eigen::MatrixXd theta2DDot(1,1);               /* thetaDDot variable to send to state manager */
+    Eigen::Vector3d rDDotBNLoc_N;                 /* second time derivative of rBN in N frame */
+    Eigen::Vector3d rDDotBNLoc_B;                 /* second time derivative of rBN in B frame */
+    Eigen::Vector3d omegaDotBNLoc_B;              /* time derivative of omegaBN in B frame */
+
+    //! Grab necessarry values from manager (these have been previously computed in hubEffector)
+    rDDotBNLoc_N = this->hubVelocity->getStateDeriv();
+    sigmaBNLocal = (Eigen::Vector3d )this->hubSigma->getState();
+    omegaDotBNLoc_B = this->hubOmega->getStateDeriv();
+    dcmNB = sigmaBNLocal.toRotationMatrix();
+    dcmBN = dcmNB.transpose();
+    rDDotBNLoc_B = dcmBN*rDDotBNLoc_N;
+
+    //! - Compute Derivatives
+    //! - First is trivial
+    this->theta1State->setDerivative(theta1DotState->getState());
+    //! - Second, a little more involved - see Allard, Diaz, Schaub flex/slosh paper
+    theta1DDot(0,0) = this->matrixEDHRB.row(0).dot(this->matrixFDHRB*rDDotBNLoc_B) + this->matrixEDHRB.row(0)*this->matrixGDHRB*omegaDotBNLoc_B + this->matrixEDHRB.row(0)*this->vectorVDHRB;
+    this->theta1DotState->setDerivative(theta1DDot);
+    this->theta2State->setDerivative(theta2DotState->getState());
+    theta2DDot(0,0) = this->matrixEDHRB.row(1)*(this->matrixFDHRB*rDDotBNLoc_B) + this->matrixEDHRB.row(1).dot(this->matrixGDHRB*omegaDotBNLoc_B) + this->matrixEDHRB.row(1)*this->vectorVDHRB;
+    this->theta2DotState->setDerivative(theta2DDot);
+
+    return;
+}
+/*! This method is for calculating the contributions of the DHRB state effector to the energy and momentum of the s/c */
+void DualHingedRigidBodyStateEffector::updateEnergyMomContributions(double integTime, Eigen::Vector3d &
+                                                                rotAngMomPntCContr_B, double & rotEnergyContr)
+{
+    // - Get the current omega state
+    Eigen::Vector3d omegaLocal_BN_B;
+    omegaLocal_BN_B = hubOmega->getState();
+    
+    // - Find rotational angular momentum contribution from hub
+    Eigen::Vector3d omega_S1B_B;
+    Eigen::Vector3d omega_S2B_B;
+    Eigen::Vector3d omega_S1N_B;
+    Eigen::Vector3d omega_S2N_B;
+    Eigen::Matrix3d IPntS1_B;
+    Eigen::Matrix3d IPntS2_B;
+    Eigen::Vector3d rDot_S1B_B;
+    Eigen::Vector3d rDot_S2B_B;
+    omega_S1B_B = this->theta1Dot*this->sHat12_B;
+    omega_S2B_B = (this->theta1Dot + this->theta2Dot)*this->sHat22_B;
+    omega_S1N_B = omega_S1B_B + omegaLocal_BN_B;
+    omega_S2N_B = omega_S2B_B + omegaLocal_BN_B;
+    IPntS1_B = this->dcmS1B.transpose()*this->IPntS1_S1*this->dcmS1B;
+    IPntS2_B = this->dcmS2B.transpose()*this->IPntS2_S2*this->dcmS2B;
+    rDot_S1B_B = this->rPrimeS1B_B + omegaLocal_BN_B.cross(this->rS1B_B);
+    rDot_S2B_B = this->rPrimeS2B_B + omegaLocal_BN_B.cross(this->rS2B_B);
+    rotAngMomPntCContr_B = IPntS1_B*omega_S1N_B + this->mass1*this->rS1B_B.cross(rDot_S1B_B)
+                            + IPntS2_B*omega_S2N_B + this->mass2*this->rS2B_B.cross(rDot_S2B_B);
+    
+    // - Find rotational energy contribution from the hub
+    double rotEnergyContrS1;
+    double rotEnergyContrS2;
+    rotEnergyContrS1 =  0.5*omega_S1N_B.dot(IPntS1_B*omega_S1N_B)
+                        + 0.5*this->mass1*rDot_S1B_B.dot(rDot_S1B_B)
+                        + 0.5*this->k1*this->theta1*this->theta1;
+    rotEnergyContrS2 =  0.5*omega_S2N_B.dot(IPntS2_B*omega_S2N_B)
+                        + 0.5*this->mass2*rDot_S2B_B.dot(rDot_S2B_B)
+                        + 0.5*this->k2*this->theta2*this->theta2;
+    rotEnergyContr = rotEnergyContrS1 + rotEnergyContrS2;
+    
+    return;
+}
