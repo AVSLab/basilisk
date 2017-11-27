cb2fe8be262628589077d4f3b5cc3ffc53cfd40d
Thibaud Teil <thibaud.teil@colorado.edu>
2017-11-27 12:59:06 -0700

    [BSK-893] Mass prop changes in uKF and EKF
    Former-commit-id: fb9d8d6a051c96416a67b498beb778c9d6497d09
    Former-commit-id: e2e08e66e46c20132e11de63459979c569c7aa0d

--- src/fswAlgorithms/attDetermination/sunlineUKF/sunlineUKF.h
+++ src/fswAlgorithms/attDetermination/sunlineUKF/sunlineUKF.h
@@ -83,7 +83,6 @@ typedef struct {
     int32_t navStateOutMsgId;     /*!< -- ID for the outgoing body estimate message*/
     int32_t filtDataOutMsgId;   /*!< [-] ID for the filter data output message*/
     int32_t cssDataInMsgId;      /*!< -- ID for the incoming CSS sensor message*/
-    int32_t massPropsInMsgId;    /*!< [-] ID for the incoming mass properties message*/
     int32_t cssConfInMsgId;   /*!< [-] ID associated with the CSS configuration data*/
 }SunlineUKFConfig;
 
