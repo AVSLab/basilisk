42ba48896ec7ec8efbf5e7fbd50d8c82c8849228
Cody Allard <cody.allard@colorado.edu>
2017-12-06 13:49:09 -0700

    [BSK-757] Added plotting and setting variables from python
    Former-commit-id: 52c7bd00d8d9da4901668347b4d697964fb9a28a
    Former-commit-id: f6ad15cef19e8ea03debd3781f16ef149079d968

--- src/simulation/dynamics/dualHingedRigidBodies/UnitTest/dualhingedRigidBodyStateEffector.py
+++ src/simulation/dynamics/dualHingedRigidBodies/UnitTest/dualhingedRigidBodyStateEffector.py
@@ -30,9 +30,8 @@ path = os.path.dirname(os.path.abspath(filename))
 splitPath = path.split('simulation')
 
 from Basilisk.utilities import SimulationBaseClass
-from Basilisk.utilities import unitTestSupport  # general support file with common unit test functions
 from Basilisk.simulation import spacecraftPlus
-from Basilisk.simulation import hingedRigidBodyStateEffector
+from Basilisk.simulation import dualHingedRigidBodyStateEffector
 from Basilisk.utilities import macros
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
@@ -64,95 +63,140 @@ def test_hubPropagate(show_plots):
     unitTestSim.TotalSim.terminateSimulation()
     
     # Create test thread
-    testProcessRate = macros.sec2nano(0.1)  # update process rate update time
+    testProcessRate = macros.sec2nano(0.0001)  # update process rate update time
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
-    unitTestSim.panel1 = hingedRigidBodyStateEffector.HingedRigidBodyStateEffector()
-    unitTestSim.panel2 = hingedRigidBodyStateEffector.HingedRigidBodyStateEffector()
+    unitTestSim.panel1 = dualHingedRigidBodyStateEffector.DualHingedRigidBodyStateEffector()
+    unitTestSim.panel2 = dualHingedRigidBodyStateEffector.DualHingedRigidBodyStateEffector()
 
     # Define Variable for panel 1
-    unitTestSim.panel1.mass = 100.0
-    unitTestSim.panel1.IPntS_S = [[100.0, 0.0, 0.0], [0.0, 50.0, 0.0], [0.0, 0.0, 50.0]]
-    unitTestSim.panel1.d = 1.5
-    unitTestSim.panel1.k = 100.0
-    unitTestSim.panel1.c = 0.0
-    unitTestSim.panel1.rHB_B = [[0.5], [0.0], [1.0]]
-    unitTestSim.panel1.dcmHB = [[-1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0]]
-    unitTestSim.panel1.nameOfThetaState = "hingedRigidBodyTheta1"
-    unitTestSim.panel1.nameOfThetaDotState = "hingedRigidBodyThetaDot1"
+    unitTestSim.panel1.mass1 = 50.0
+    unitTestSim.panel1.IPntS1_S1 = [[50.0, 0.0, 0.0], [0.0, 25.0, 0.0], [0.0, 0.0, 25.0]]
+    unitTestSim.panel1.d1 = 0.75
+    unitTestSim.panel1.l1 = 1.5
+    unitTestSim.panel1.k1 = 100.0
+    unitTestSim.panel1.c1 = 0.0
+    unitTestSim.panel1.rH1B_B = [[0.5], [0.0], [1.0]]
+    unitTestSim.panel1.dcmH1B = [[-1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0]]
+    unitTestSim.panel1.nameOfTheta1State = "dualHingedRigidBody1Theta1"
+    unitTestSim.panel1.nameOfTheta1DotState = "dualHingedRigidBody1ThetaDot1"
+    unitTestSim.panel1.mass2 = 50.0
+    unitTestSim.panel1.IPntS2_S2 = [[50.0, 0.0, 0.0], [0.0, 25.0, 0.0], [0.0, 0.0, 25.0]]
+    unitTestSim.panel1.d2 = 0.75
+    unitTestSim.panel1.l2 = 1.5
+    unitTestSim.panel1.k2 = 100.0
+    unitTestSim.panel1.c2 = 0.0
+    unitTestSim.panel1.nameOfTheta2State = "dualHingedRigidBody1Theta2"
+    unitTestSim.panel1.nameOfTheta2DotState = "dualHingedRigidBody1ThetaDot2"
+    unitTestSim.panel1.theta1Init = 5*numpy.pi/180.0
+    unitTestSim.panel1.theta1DotInit = 0.0
+    unitTestSim.panel1.theta2Init = 0.0
+    unitTestSim.panel1.theta2DotInit = 0.0
 
     # Define Variables for panel 2
-    unitTestSim.panel2.mass = 100.0
-    unitTestSim.panel2.IPntS_S = [[100.0, 0.0, 0.0], [0.0, 50.0, 0.0], [0.0, 0.0, 50.0]]
-    unitTestSim.panel2.d = 1.5
-    unitTestSim.panel2.k = 100.0
-    unitTestSim.panel2.c = 0.0
-    unitTestSim.panel2.rHB_B = [[-0.5], [0.0], [1.0]]
-    unitTestSim.panel2.dcmHB = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-    unitTestSim.panel2.nameOfThetaState = "hingedRigidBodyTheta2"
-    unitTestSim.panel2.nameOfThetaDotState = "hingedRigidBodyThetaDot2"
+    unitTestSim.panel2.mass1 = 50.0
+    unitTestSim.panel2.IPntS1_S1 = [[50.0, 0.0, 0.0], [0.0, 25.0, 0.0], [0.0, 0.0, 25.0]]
+    unitTestSim.panel2.d1 = 0.75
+    unitTestSim.panel2.l1 = 1.5
+    unitTestSim.panel2.k1 = 100.0
+    unitTestSim.panel2.c1 = 0.0
+    unitTestSim.panel2.rH1B_B = [[-0.5], [0.0], [1.0]]
+    unitTestSim.panel2.dcmH1B = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    unitTestSim.panel2.nameOfTheta1State = "dualHingedRigidBody2Theta1"
+    unitTestSim.panel2.nameOfTheta1DotState = "dualHingedRigidBody2ThetaDot1"
+    unitTestSim.panel2.mass2 = 50.0
+    unitTestSim.panel2.IPntS2_S2 = [[50.0, 0.0, 0.0], [0.0, 25.0, 0.0], [0.0, 0.0, 25.0]]
+    unitTestSim.panel2.d2 = 0.75
+    unitTestSim.panel2.l2 = 1.5
+    unitTestSim.panel2.k2 = 100.0
+    unitTestSim.panel2.c2 = 0.0
+    unitTestSim.panel2.nameOfTheta2State = "dualHingedRigidBody2Theta2"
+    unitTestSim.panel2.nameOfTheta2DotState = "dualHingedRigidBody2ThetaDot2"
+    unitTestSim.panel2.theta1Init = 5*numpy.pi/180.0
+    unitTestSim.panel2.theta1DotInit = 0.0
+    unitTestSim.panel2.theta2Init = 0.0
+    unitTestSim.panel2.theta2DotInit = 0.0
 
     # Add panels to spaceCraft
     # this next line is not working
     scObject.addStateEffector(unitTestSim.panel1)
     scObject.addStateEffector(unitTestSim.panel2)
-    
+
+    scObject.hub.mHub = 750.0
+    scObject.hub.r_BcB_B = [[0.0], [0.0], [0.0]]
+    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
+
+    # Set the initial values for the states
+    scObject.hub.r_CN_NInit = [[0.1], [-0.4], [0.3]]
+    scObject.hub.v_CN_NInit = [[-0.2], [0.5], [0.1]]
+    scObject.hub.sigma_BNInit = [[0.0], [0.0], [0.0]]
+    scObject.hub.omega_BN_BInit = [[0.1], [-0.1], [0.1]]
+
     # Add test module to runtime call list
     unitTestSim.AddModelToTask(unitTaskName, scObject)
+
+    unitTestSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, testProcessRate)
     
     unitTestSim.InitializeSimulation()
 
-    posRef = scObject.dynManager.getStateObject("hubPosition")
-    velRef = scObject.dynManager.getStateObject("hubVelocity")
-    sigmaRef = scObject.dynManager.getStateObject("hubSigma")
-    omegaRef = scObject.dynManager.getStateObject("hubOmega")
-    theta1Ref = scObject.dynManager.getStateObject("hingedRigidBodyTheta1")
-    thetaDot1Ref = scObject.dynManager.getStateObject("hingedRigidBodyThetaDot1")
-    theta2Ref = scObject.dynManager.getStateObject("hingedRigidBodyTheta2")
-    thetaDot2Ref = scObject.dynManager.getStateObject("hingedRigidBodyThetaDot2")
+    # Add energy and momentum variables to log
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbEnergy", testProcessRate, 0, 0, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbAngMomPntN_N", testProcessRate, 0, 2, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotAngMomPntC_N", testProcessRate, 0, 2, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotEnergy", testProcessRate, 0, 0, 'double')
 
-    posRef.setState([[0.0], [0.0], [0.0]])
-    velRef.setState([[0.0], [0.0], [0.0]])
-    sigmaRef.setState([[0.0], [0.0], [0.0]])
-    omegaRef.setState([[0.1], [-0.1], [0.1]])
-    theta1Ref.setState([[5*numpy.pi/180.0]])
-    thetaDot1Ref.setState([[0.0]])
-    theta2Ref.setState([[0.0]])
-    thetaDot2Ref.setState([[0.0]])
-
-    scObject.hub.mHub = 750.0
-    scObject.hub.rBcB_B = [[0.0], [0.0], [0.0]]
-    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
-
-    stopTime = 60.0*10.0
+    stopTime = 10.0
     unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
     unitTestSim.ExecuteSimulation()
 
-    dataPos = posRef.getState()
-    dataPos = [[stopTime, dataPos[0][0], dataPos[1][0], dataPos[2][0]]]
-    dataSigma =  sigmaRef.getState()
-    dataSigma = [[stopTime, dataSigma[0][0], dataSigma[1][0], dataSigma[2][0]]]
+    orbEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbEnergy")
+    orbAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbAngMomPntN_N")
+    rotAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotAngMomPntC_N")
+    rotEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotEnergy")
 
-    truePos = [
-                [-13.3842058040891, -13.2968874812058, 0.155873769585104]
-                ]
-    trueSigma = [
-                  [0.228198675962671, -0.329880460528557,  0.266599868549938]
-                  ]
+    sigmaOut = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.sigma_BN',range(3))
+    rOut_BN_N = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.r_BN_N',range(3))
+    vOut_CN_N = unitTestSim.pullMessageLogData(scObject.scStateOutMsgName+'.v_CN_N',range(3))
 
-    accuracy = 1e-8
-    for i in range(0,len(truePos)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(dataPos[i],truePos[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED:  Dynamics Mode failed pos unit test at t=" + str(dataPos[i,0]*macros.NANO2SEC) + "sec\n")
+    plt.figure()
+    plt.clf()
+    plt.plot(orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,1] - orbAngMom_N[0,1])/orbAngMom_N[0,1], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,2] - orbAngMom_N[0,2])/orbAngMom_N[0,2], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,3] - orbAngMom_N[0,3])/orbAngMom_N[0,3])
+    plt.xlabel('time (s)')
+    plt.ylabel('Relative Difference')
 
-    for i in range(0,len(trueSigma)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(dataSigma[i],trueSigma[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED:  Dynamics Mode failed attitude unit test at t=" + str(dataSigma[i,0]*macros.NANO2SEC) + "sec\n")
+    plt.figure()
+    plt.clf()
+    plt.plot(orbEnergy[:,0]*1e-9, (orbEnergy[:,1] - orbEnergy[0,1])/orbEnergy[0,1])
+    plt.xlabel('time (s)')
+    plt.ylabel('Relative Difference')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,1] - rotAngMom_N[0,1])/rotAngMom_N[0,1], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,2] - rotAngMom_N[0,2])/rotAngMom_N[0,2], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,3] - rotAngMom_N[0,3])/rotAngMom_N[0,3])
+    plt.xlabel('time (s)')
+    plt.ylabel('Relative Difference')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(rotEnergy[:,0]*1e-9, (rotEnergy[:,1] - rotEnergy[0,1])/rotEnergy[0,1])
+    plt.xlabel('time (s)')
+    plt.ylabel('Relative Difference')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(vOut_CN_N[:,0]*1e-9, vOut_CN_N[:,1], vOut_CN_N[:,0]*1e-9, vOut_CN_N[:,2], vOut_CN_N[:,0]*1e-9, vOut_CN_N[:,3])
+    plt.xlabel('time (s)')
+    plt.ylabel('m/s')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(vOut_CN_N[:,0]*1e-9, (vOut_CN_N[:,1] - vOut_CN_N[0,1])/vOut_CN_N[0,1], vOut_CN_N[:,0]*1e-9, (vOut_CN_N[:,2] - vOut_CN_N[0,2])/vOut_CN_N[0,2], vOut_CN_N[:,0]*1e-9, (vOut_CN_N[:,3] - vOut_CN_N[0,3])/vOut_CN_N[0,3])
+    plt.xlabel('time (s)')
+    plt.ylabel('Relative Difference')
+
+    plt.show(show_plots)
+    plt.close("all")
 
     if testFailCount == 0:
         print "PASSED: " + " Hub Propagate"
@@ -161,4 +205,4 @@ def test_hubPropagate(show_plots):
     return [testFailCount, ''.join(testMessages)]
 
 if __name__ == "__main__":
-    spacecraftPlusAllTest(False)
+    spacecraftPlusAllTest(True)
