f7d58fac56f222837ee9e36084e8dceba89a475a
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2018-04-25 15:28:01 -0600

    Updated the BSK version number
    Former-commit-id: 23a1701e6a22fbcb81a0f00ab261116d98dee9b4
    Former-commit-id: d7c86ea31226974b1c85b035a5b4535a6a78652c

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ class BuildDocsCommand(Command):
 
 setup(
     name='Basilisk',
-    version='0.2.0',
+    version='0.2.1',
     description="Astrodynamic Simulation Library",
     packages=['Basilisk', ],
     license=open('./LICENSE').read(),
