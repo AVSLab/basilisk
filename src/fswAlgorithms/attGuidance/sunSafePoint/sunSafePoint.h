f8d87e35838d0b4226ad4a74c41ff418bbdd6fa5
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2018-04-27 09:34:50 -0600

    [BSK-1009] fixed some variable definition comments
    Former-commit-id: 4276f44f7fa80b8ccbeb72d68efb1433eb3312ef
    Former-commit-id: 8539c659004ce59d51a6d63ec13240a8c66e20b4

--- src/fswAlgorithms/attGuidance/sunSafePoint/sunSafePoint.h
+++ src/fswAlgorithms/attGuidance/sunSafePoint/sunSafePoint.h
@@ -36,8 +36,8 @@ typedef struct {
     char outputDataName[MAX_STAT_MSG_LENGTH]; /*!< The name of the output message*/
     char inputSunVecName[MAX_STAT_MSG_LENGTH]; /*!< The name of the Input message*/
     char inputIMUDataName[MAX_STAT_MSG_LENGTH]; /*!< The name of the incoming IMU message*/
-    double minUnitMag;       /*!< -- The minimally acceptable sun body unit vector*/
-    double sunAngleErr;      /*!< r  The current error between cmd and obs sun angle*/
+    double minUnitMag;       /*!< -- The minimally acceptable norm of sun body vector*/
+    double sunAngleErr;      /*!< rad The current error between cmd and obs sun angle*/
     double sunMnvrVec[3];    /*!< -- The eigen axis that we want to rotate on to get sun*/
     double sHatBdyCmd[3];    /*!< -- Desired body vector to point at the sun*/
     int32_t outputMsgID;     /*!< -- ID for the outgoing body estimate message*/
