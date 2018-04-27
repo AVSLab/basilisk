37b28415eda8f2958118c3c8473f1ea09f13d0f9
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2018-04-27 17:30:51 -0600

    [BSK-1009] create unit test of the existing sunSafePoint module functionality
    Former-commit-id: 64ae70133904b7296d0df59deab2d906d979ebf5
    Former-commit-id: 33d38b549610a5c49a157ec6973e4fcd10c31009

---
+++ src/fswAlgorithms/attGuidance/sunSafePoint/_UnitTest/test_sunSafePoint.py
@@ -0,0 +1,237 @@
+''' '''
+'''
+ ISC License
+
+ Copyright (c) 2016-2018, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+'''
+#
+#   Unit Test Script
+#   Module Name:        sunSafePoint
+#   Author:             Hanspeter Schaub
+#   Creation Date:      April 25, 2018
+#
+
+import pytest
+import sys, os, inspect
+import numpy as np
+# import packages as needed e.g. 'numpy', 'ctypes, 'math' etc.
+
+
+
+
+
+
+
+# Import all of the modules that we are going to be called in this simulation
+from Basilisk.utilities import SimulationBaseClass
+from Basilisk.utilities import unitTestSupport                  # general support file with common unit test functions
+import matplotlib.pyplot as plt
+from Basilisk.fswAlgorithms import sunSafePoint                   # import the module that is to be tested
+from Basilisk.fswAlgorithms import fswMessages
+from Basilisk.simulation import simFswInterfaceMessages
+from Basilisk.utilities import macros as mc
+
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+# uncomment this line if this test has an expected failure, adjust message as needed
+#@pytest.mark.xfail(conditionstring)
+# provide a unique test method name, starting with test_
+
+# The following 'parametrize' function decorator provides the parameters and expected results for each
+#   of the multiple test runs for this test.
+@pytest.mark.parametrize("case", [
+     ("SUN_VISIBLE")
+    ,("SUN_HIDDEN")
+])
+
+def test_module(show_plots, case):
+    # each test method requires a single assert method to be called
+    [testResults, testMessage] = sunSafePointTestFunction(show_plots, case)
+    assert testResults < 1, testMessage
+
+
+def sunSafePointTestFunction(show_plots, case):
+    testFailCount = 0                       # zero unit test result counter
+    testMessages = []                       # create empty array to store test log messages
+    unitTaskName = "unitTask"               # arbitrary name (don't change)
+    unitProcessName = "TestProcess"         # arbitrary name (don't change)
+
+    # Create a sim module as an empty container
+    unitTestSim = SimulationBaseClass.SimBaseClass()
+    # terminateSimulation() is needed if multiple unit test scripts are run
+    # that run a simulation for the test. This creates a fresh and
+    # consistent simulation environment for each test run.
+    unitTestSim.TotalSim.terminateSimulation()
+
+    # Create test thread
+    testProcessRate = mc.sec2nano(0.5)     # update process rate update time
+    testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+
+
+    # Construct algorithm and associated C++ container
+    moduleConfig = sunSafePoint.sunSafePointConfig()
+    moduleWrap = unitTestSim.setModelDataWrap(moduleConfig)
+    moduleWrap.ModelTag = "sunSafePoint"
+
+    # Add test module to runtime call list
+    unitTestSim.AddModelToTask(unitTaskName, moduleWrap, moduleConfig)
+
+    # Initialize the test module configuration data
+    moduleConfig.outputDataName = "outputName"
+    moduleConfig.inputSunVecName = "inputSunVecName"
+    moduleConfig.inputIMUDataName = "inputIMUDataName"
+    sHat_Cmd_B = [0,0,1]
+    moduleConfig.sHatBdyCmd = sHat_Cmd_B
+    moduleConfig.minUnitMag = 0.1
+
+    # Create input messages
+    #
+    inputSunVecData = simFswInterfaceMessages.NavAttIntMsg()  # Create a structure for the input message
+    sunVec_B = [1.0, 0.0, 0.0]
+    inputSunVecData.vehSunPntBdy = sunVec_B
+    unitTestSupport.setMessage(unitTestSim.TotalSim,
+                               unitProcessName,
+                               moduleConfig.inputSunVecName,
+                               inputSunVecData)
+
+    inputIMUData = fswMessages.IMUSensorBodyFswMsg()  # Create a structure for the input message
+    omega_BN_B = [0.01, 0.50, -0.2]
+    inputIMUData.AngVelBody = omega_BN_B
+    unitTestSupport.setMessage(unitTestSim.TotalSim,
+                               unitProcessName,
+                               moduleConfig.inputIMUDataName,
+                               inputIMUData)
+
+
+
+    # Setup logging on the test module output message so that we get all the writes to it
+    unitTestSim.TotalSim.logThisMessage(moduleConfig.outputDataName, testProcessRate)
+
+    # Need to call the self-init and cross-init methods
+    unitTestSim.InitializeSimulation()
+
+    # Set the simulation time.
+    # NOTE: the total simulation time may be longer than this value. The
+    # simulation is stopped at the next logging event on or after the
+    # simulation end time.
+    unitTestSim.ConfigureStopTime(mc.sec2nano(1.))        # seconds to stop simulation
+
+    # Begin the simulation time run set above
+    unitTestSim.ExecuteSimulation()
+
+    # This pulls the actual data log from the simulation run.
+    # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
+    #
+    # check sigma_BR
+    #
+    moduleOutputName = "sigma_BR"
+    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
+                                                  range(3))
+    # set the filtered output truth states
+    eHat = np.cross(sunVec_B,sHat_Cmd_B)
+    eHat = eHat / np.linalg.norm(eHat)
+    Phi = np.arccos(np.dot(sunVec_B,sHat_Cmd_B))
+    sigmaTrue = eHat * np.tan(Phi/4.0)
+    trueVector = [
+                sigmaTrue.tolist(),
+                sigmaTrue.tolist(),
+                sigmaTrue.tolist()
+               ]
+
+    # compare the module results to the truth values
+    accuracy = 1e-12
+    for i in range(0,len(trueVector)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
+                                moduleOutputName + " unit test at t=" +
+                                str(moduleOutput[i,0] * mc.NANO2SEC) +
+                                "sec\n")
+
+
+    #
+    # check omega_RN_B
+    #
+    moduleOutputName = "omega_RN_B"
+    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
+                                                  range(3))
+    # set the filtered output truth states
+    trueVector = [
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0]
+    ]
+    # compare the module results to the truth values
+    accuracy = 1e-12
+    for i in range(0,len(trueVector)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
+                                moduleOutputName + " unit test at t=" +
+                                str(moduleOutput[i,0] * mc.NANO2SEC) +
+                                "sec\n")
+
+    #
+    # check domega_RN_B
+    #
+    moduleOutputName = "domega_RN_B"
+    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
+                                                  range(3))
+    # set the filtered output truth states
+    trueVector = [
+               [0.0, 0.0, 0.0],
+               [0.0, 0.0, 0.0],
+               [0.0, 0.0, 0.0]
+               ]
+
+    # compare the module results to the truth values
+    accuracy = 1e-12
+    for i in range(0,len(trueVector)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqual(moduleOutput[i],trueVector[i],3,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
+                                moduleOutputName + " unit test at t=" +
+                                str(moduleOutput[i,0] * mc.NANO2SEC) +
+                                "sec\n")
+
+
+    # If the argument provided at commandline "--show_plots" evaluates as true,
+    # plot all figures
+#    if show_plots:
+#        # plot a sample variable.
+#        plt.figure(1)
+#        plt.plot(variableState[:,0]*macros.NANO2SEC, variableState[:,1], label='Sample Variable')
+#        plt.legend(loc='upper left')
+#        plt.xlabel('Time [s]')
+#        plt.ylabel('Variable Description [unit]')
+#        plt.show()
+
+    # each test method requires a single assert method to be called
+    # this check below just makes sure no sub-test failures were found
+    return [testFailCount, ''.join(testMessages)]
+
+
+#
+# This statement below ensures that the unitTestScript can be run as a
+# stand-along python script
+#
+if __name__ == "__main__":
+    sunSafePointTestFunction(False, "SUN_VISIBLE")
