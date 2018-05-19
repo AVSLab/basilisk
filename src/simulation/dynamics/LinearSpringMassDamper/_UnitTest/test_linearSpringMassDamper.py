6bdf0c2692d36f9fe024e2a51783e9f8e3ef5932
Cody Allard <cody.allard@colorado.edu>
2018-05-19 12:44:19 -0600

    [BSK-1025] Added test in linearSpringMassDamper that tests that linearSpringMassDamper can be added to a tank to exhibit fuel slosh and deplete the tank mass in a ratio based way
    Former-commit-id: f6dcaadda137cd7328e51ee66f3269d27af29ed8
    Former-commit-id: 3d44b99a26283ad45f01cfce008418facfc95b48

--- src/simulation/dynamics/LinearSpringMassDamper/_UnitTest/test_linearSpringMassDamper.py
+++ src/simulation/dynamics/LinearSpringMassDamper/_UnitTest/test_linearSpringMassDamper.py
@@ -31,11 +31,15 @@ from Basilisk.simulation import spacecraftPlus
 from Basilisk.simulation import linearSpringMassDamper
 from Basilisk.simulation import gravityEffector
 from Basilisk.utilities import macros
+from Basilisk.simulation import fuelTank
+from Basilisk.simulation import thrusterDynamicEffector
+from Basilisk.utilities import simIncludeThruster
 
 @pytest.mark.parametrize("useFlag, testCase", [
     (False,'NoGravity'),
     (False,'Gravity'),
-    (False,'Damping')
+    (False,'Damping'),
+    (False,'MassDepletion')
 ])
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
@@ -111,6 +115,50 @@ def fuelSloshTest(show_plots,useFlag,testCase):
     unitTestSim.particle3.rhoDotInit = 0.0
     unitTestSim.particle3.massInit = 15.0
 
+    if testCase == 'MassDepletion':
+        thrusterCommandName = "acs_thruster_cmds"
+        # add thruster devices
+        # The clearThrusterSetup() is critical if the script is to run multiple times
+        simIncludeThruster.clearSetup()
+        simIncludeThruster.create('MOOG_Monarc_445',
+                                  [1,0,0],                # location in S frame
+                                  [0,1,0]                 # direction in S frame
+                                  )
+
+        # create thruster object container and tie to spacecraft object
+        thrustersDynamicEffector = thrusterDynamicEffector.ThrusterDynamicEffector()
+
+        unitTestSim.fuelTankStateEffector = fuelTank.FuelTank()
+        unitTestSim.fuelTankStateEffector.setTankModel(fuelTank.TANK_MODEL_CONSTANT_VOLUME)
+        tankModel = fuelTank.cvar.FuelTankModelConstantVolume
+        tankModel.propMassInit = 40.0
+        tankModel.r_TcT_TInit = [[0.0],[0.0],[0.0]]
+        unitTestSim.fuelTankStateEffector.r_TB_B = [[0.0],[0.0],[0.0]]
+        tankModel.radiusTankInit = 46.0 / 2.0 / 3.2808399 / 12.0
+
+        # Add tank and thruster
+        scObject.addStateEffector(unitTestSim.fuelTankStateEffector)
+        simIncludeThruster.addToSpacecraft(  "Thrusters",
+                                         thrustersDynamicEffector,
+                                         scObject, unitTestSim.fuelTankStateEffector)
+
+        # set thruster commands
+        ThrustMessage = thrusterDynamicEffector.THRArrayOnTimeCmdIntMsg()
+        msgSize = ThrustMessage.getStructSize()
+        ThrustMessage.OnTimeRequest = [5.0]
+        unitTestSim.TotalSim.CreateNewMessage(unitProcessName, thrusterCommandName, msgSize, 2)
+        unitTestSim.TotalSim.WriteMessageData(thrusterCommandName, msgSize, 0, ThrustMessage)
+
+        # Add test module to runtime call list
+        unitTestSim.AddModelToTask(unitTaskName, unitTestSim.fuelTankStateEffector)
+        unitTestSim.AddModelToTask(unitTaskName, thrustersDynamicEffector)
+        unitTestSim.TotalSim.logThisMessage(unitTestSim.fuelTankStateEffector.FuelTankOutMsgName, testProcessRate)
+
+        # Add particles to tank to activate mass depletion
+        unitTestSim.fuelTankStateEffector.pushFuelSloshParticle(unitTestSim.particle1)
+        unitTestSim.fuelTankStateEffector.pushFuelSloshParticle(unitTestSim.particle2)
+        unitTestSim.fuelTankStateEffector.pushFuelSloshParticle(unitTestSim.particle3)
+
     # Add particles to spacecraft
     scObject.addStateEffector(unitTestSim.particle1)
     scObject.addStateEffector(unitTestSim.particle2)
@@ -151,13 +199,36 @@ def fuelSloshTest(show_plots,useFlag,testCase):
     unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbAngMomPntN_N", testProcessRate, 0, 2, 'double')
     unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotAngMomPntC_N", testProcessRate, 0, 2, 'double')
     unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotEnergy", testProcessRate, 0, 0, 'double')
+    if testCase == 'MassDepletion':
+        unitTestSim.AddVariableForLogging(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass1').getState()", testProcessRate, 0, 0, 'double')
+        unitTestSim.AddVariableForLogging(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass2').getState()", testProcessRate, 0, 0, 'double')
+        unitTestSim.AddVariableForLogging(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass3').getState()", testProcessRate, 0, 0, 'double')
+
 
     posRef = scObject.dynManager.getStateObject("hubPosition")
     sigmaRef = scObject.dynManager.getStateObject("hubSigma")
 
     stopTime = 2.5
+    if testCase == 'MassDepletion':
+        stopTime = 10.0
+
     unitTestSim.ConfigureStopTime(macros.sec2nano(stopTime))
     unitTestSim.ExecuteSimulation()
+
+    if testCase == 'MassDepletion':
+        fuelMass = unitTestSim.pullMessageLogData(unitTestSim.fuelTankStateEffector.FuelTankOutMsgName + '.fuelMass',
+                                                  range(1))
+        fuelMassDot = unitTestSim.pullMessageLogData(unitTestSim.fuelTankStateEffector.FuelTankOutMsgName + '.fuelMassDot',
+                                                  range(1))
+        mass1Out = unitTestSim.GetLogVariableData(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass1').getState()")
+        mass2Out = unitTestSim.GetLogVariableData(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass2').getState()")
+        mass3Out = unitTestSim.GetLogVariableData(
+            "spacecraftBody.dynManager.getStateObject('linearSpringMassDamperMass3').getState()")
 
     orbEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbEnergy")
     orbAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbAngMomPntN_N")
@@ -196,24 +267,25 @@ def fuelSloshTest(show_plots,useFlag,testCase):
                 [rotEnergy[-1,0], rotEnergy[-1,1]]
                  ]
 
-    plt.figure()
-    plt.clf()
-    plt.plot(orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,1] - orbAngMom_N[0,1])/orbAngMom_N[0,1], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,2] - orbAngMom_N[0,2])/orbAngMom_N[0,2], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,3] - orbAngMom_N[0,3])/orbAngMom_N[0,3])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Relative Difference")
-    unitTestSupport.writeFigureLaTeX("ChangeInOrbitalAngularMomentum" + testCase, "Change in Orbital Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
-    plt.figure()
-    plt.clf()
-    plt.plot(orbEnergy[:,0]*1e-9, (orbEnergy[:,1] - orbEnergy[0,1])/orbEnergy[0,1])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Relative Difference")
-    unitTestSupport.writeFigureLaTeX("ChangeInOrbitalEnergy" + testCase, "Change in Orbital Energy " + testCase, plt, "width=0.8\\textwidth", path)
-    plt.figure()
-    plt.clf()
-    plt.plot(rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,1] - rotAngMom_N[0,1])/rotAngMom_N[0,1], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,2] - rotAngMom_N[0,2])/rotAngMom_N[0,2], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,3] - rotAngMom_N[0,3])/rotAngMom_N[0,3])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Relative Difference")
-    unitTestSupport.writeFigureLaTeX("ChangeInRotationalAngularMomentum" + testCase, "Change in Rotational Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
+    if testCase != 'MassDepletion':
+        plt.figure()
+        plt.clf()
+        plt.plot(orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,1] - orbAngMom_N[0,1])/orbAngMom_N[0,1], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,2] - orbAngMom_N[0,2])/orbAngMom_N[0,2], orbAngMom_N[:,0]*1e-9, (orbAngMom_N[:,3] - orbAngMom_N[0,3])/orbAngMom_N[0,3])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Relative Difference")
+        unitTestSupport.writeFigureLaTeX("ChangeInOrbitalAngularMomentum" + testCase, "Change in Orbital Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
+        plt.figure()
+        plt.clf()
+        plt.plot(orbEnergy[:,0]*1e-9, (orbEnergy[:,1] - orbEnergy[0,1])/orbEnergy[0,1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Relative Difference")
+        unitTestSupport.writeFigureLaTeX("ChangeInOrbitalEnergy" + testCase, "Change in Orbital Energy " + testCase, plt, "width=0.8\\textwidth", path)
+        plt.figure()
+        plt.clf()
+        plt.plot(rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,1] - rotAngMom_N[0,1])/rotAngMom_N[0,1], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,2] - rotAngMom_N[0,2])/rotAngMom_N[0,2], rotAngMom_N[:,0]*1e-9, (rotAngMom_N[:,3] - rotAngMom_N[0,3])/rotAngMom_N[0,3])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Relative Difference")
+        unitTestSupport.writeFigureLaTeX("ChangeInRotationalAngularMomentum" + testCase, "Change in Rotational Angular Momentum " + testCase, plt, "width=0.8\\textwidth", path)
     if testCase == 'Gravity' or testCase == 'NoGravity':
         plt.figure()
         plt.clf()
@@ -221,37 +293,73 @@ def fuelSloshTest(show_plots,useFlag,testCase):
         plt.xlabel("Time (s)")
         plt.ylabel("Relative Difference")
         unitTestSupport.writeFigureLaTeX("ChangeInRotationalEnergy" + testCase, "Change in Rotational Energy " + testCase, plt, "width=0.8\\textwidth", path)
+    if testCase == 'MassDepletion':
+        plt.figure()
+        plt.plot(fuelMass[:,0]*1e-9, fuelMass[:,1])
+        plt.title("Tank Fuel Mass")
+        plt.figure()
+        plt.plot(fuelMassDot[:,0]*1e-9, fuelMassDot[:,1])
+        plt.title("Tank Fuel Mass Dot")
+        plt.figure()
+        plt.plot(mass1Out[:,0]*1e-9, mass1Out[:,1])
+        plt.title("Fuel Particle 1 Mass")
+        plt.figure()
+        plt.plot(mass2Out[:,0]*1e-9, mass2Out[:,1])
+        plt.title("Fuel Particle 2 Mass")
+        plt.figure()
+        plt.plot(mass3Out[:,0]*1e-9, mass3Out[:,1])
+        plt.title("Fuel Particle 3 Mass")
+        mDotFuel = -0.19392039093
+        mDotParicle1True = mDotFuel*(10./85.)
+        mDotParicle2True = mDotFuel*(20./85.)
+        mDotParicle3True = mDotFuel*(15./85.)
+        mDotParicle1Data = [0,(mass1Out[2,1] - mass1Out[1,1])/((mass1Out[2,0] - mass1Out[1,0])*1e-9)]
+        mDotParicle2Data = [0,(mass2Out[2,1] - mass2Out[1,1])/((mass2Out[2,0] - mass2Out[1,0])*1e-9)]
+        mDotParicle3Data = [0,(mass3Out[2,1] - mass3Out[1,1])/((mass3Out[2,0] - mass3Out[1,0])*1e-9)]
 
     plt.show(show_plots)
 
-    accuracy = 1e-10
-    for i in range(0,len(initialOrbAngMom_N)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(finalOrbAngMom[i],initialOrbAngMom_N[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: Fuel Slosh unit test failed orbital angular momentum unit test")
-
-    for i in range(0,len(initialRotAngMom_N)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(finalRotAngMom[i],initialRotAngMom_N[i],3,accuracy):
-            testFailCount += 1
-            testMessages.append("FAILED: Fuel Slosh unit test failed rotational angular momentum unit test")
-
-    if testCase == 'Gravity' or testCase == 'NoGravity':
-        for i in range(0,len(initialRotEnergy)):
+    if testCase != 'MassDepletion':
+        accuracy = 1e-10
+        for i in range(0,len(initialOrbAngMom_N)):
             # check a vector values
-            if not unitTestSupport.isArrayEqualRelative(finalRotEnergy[i],initialRotEnergy[i],1,accuracy):
+            if not unitTestSupport.isArrayEqualRelative(finalOrbAngMom[i],initialOrbAngMom_N[i],3,accuracy):
                 testFailCount += 1
-                testMessages.append("FAILED: Fuel Slosh unit test failed rotational energy unit test")
+                testMessages.append("FAILED: Linear Spring Mass Damper unit test failed orbital angular momentum unit test")
 
-    for i in range(0,len(initialOrbEnergy)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqualRelative(finalOrbEnergy[i],initialOrbEnergy[i],1,accuracy):
+        for i in range(0,len(initialRotAngMom_N)):
+            # check a vector values
+            if not unitTestSupport.isArrayEqualRelative(finalRotAngMom[i],initialRotAngMom_N[i],3,accuracy):
+                testFailCount += 1
+                testMessages.append("FAILED: Linear Spring Mass Damper unit test failed rotational angular momentum unit test")
+
+        if testCase == 'Gravity' or testCase == 'NoGravity':
+            for i in range(0,len(initialRotEnergy)):
+                # check a vector values
+                if not unitTestSupport.isArrayEqualRelative(finalRotEnergy[i],initialRotEnergy[i],1,accuracy):
+                    testFailCount += 1
+                    testMessages.append("FAILED: Linear Spring Mass Damper unit test failed rotational energy unit test")
+
+        for i in range(0,len(initialOrbEnergy)):
+            # check a vector values
+            if not unitTestSupport.isArrayEqualRelative(finalOrbEnergy[i],initialOrbEnergy[i],1,accuracy):
+                testFailCount += 1
+                testMessages.append("FAILED: Linear Spring Mass Damper unit test failed orbital energy unit test")
+
+    if testCase == 'MassDepletion':
+        accuracy = 1e-4
+        if not unitTestSupport.isDoubleEqual(mDotParicle1Data,mDotParicle1True,accuracy):
             testFailCount += 1
-            testMessages.append("FAILED: Fuel Slosh unit test failed orbital energy unit test")
+            testMessages.append("FAILED: Linear Spring Mass Damper unit test failed mass 1 dot test")
+        if not unitTestSupport.isDoubleEqual(mDotParicle2Data,mDotParicle2True,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: Linear Spring Mass Damper unit test failed mass 2 dot test")
+        if not unitTestSupport.isDoubleEqual(mDotParicle3Data,mDotParicle3True,accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: Linear Spring Mass Damper unit test failed mass 3 dot test")
 
     if testFailCount == 0:
-        print "PASSED: " + " Fuel Slosh Test"
+        print "PASSED: " + " Linear Spring Mass Damper Test"
 
     assert testFailCount < 1, testMessages
     # return fail count and join into a single string all messages in the list
@@ -259,4 +367,4 @@ def fuelSloshTest(show_plots,useFlag,testCase):
     return [testFailCount, ''.join(testMessages)]
 
 if __name__ == "__main__":
-    fuelSloshTest(True,False,'Gravity')
+    fuelSloshTest(True,False,'MassDepletion')
