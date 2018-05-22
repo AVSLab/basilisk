[HOT-FIX] made the addMRP and subMRP functions properly handle the 360 deg result.  Also made sure that these routines always return the short rotation MRP representation
Former-commit-id: 7409ab4644b2cac650108e59317fd0ad624db8ff
Former-commit-id: f3ddfdc45796d0c57274a9c78b97dd6cd411e60f

src/utilities/RigidBodyKinematics.py
@@ -597,11 +597,18 @@ def addMRP(q1, q2):
     	rotations Q1 and Q2.
     """
 
-    num = (1 - np.dot(q1, q1)) * q2 + (1 - np.dot(q2, q2)) * q1 + 2 * np.cross(q1, q2)
     den = 1 + np.dot(q1, q1) * np.dot(q2, q2) - 2 * np.dot(q1, q2)
+
     if np.abs(den) < 1e-5:
-        return subMRP(q1, -q2)
+        q2 = -q2/np.dot(q2,q2)
+        den = 1 + np.dot(q1, q1) * np.dot(q2, q2) - 2 * np.dot(q1, q2)
+    num = (1 - np.dot(q1, q1)) * q2 + (1 - np.dot(q2, q2)) * q1 + 2 * np.cross(q1, q2)
+
     q = num / den
+
+    if np.dot(q,q) > 1:
+            q = - q/np.dot(q,q)
+
     return q
 
 
@@ -2762,11 +2769,16 @@ def subMRP(q1, q2):
     q2m = np.linalg.norm(q2)
     q1m = np.linalg.norm(q1)
 
-    num = (1 - q2m * q2m) * q1 - (1 - q1m * q1m) * q2 + 2 * np.cross(q1, q2)
     den = 1 + (q1m * q1m) * (q2m * q2m) + 2 * np.dot(q1, q2)
     if den < 1e-5:
-        return addMRP(q1, -q2)
+        q2 = -q2/np.dot(q2,q2)
+        den = 1 + (q1m * q1m) * (q2m * q2m) + 2 * np.dot(q1, q2)
+    num = (1 - q2m * q2m) * q1 - (1 - q1m * q1m) * q2 + 2 * np.cross(q1, q2)
+
     q = num / den
+    if np.dot(q,q)>1:
+        q = -q/np.dot(q,q)
+
     return q
 
 