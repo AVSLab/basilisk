18e12663cdd8f95eff5a99c0d81355c627163940
Cody Allard <cody.allard@colorado.edu>
2018-06-06 13:48:55 -0600

    [BSK-1040] The last commit message was wrong that message should have been for this committ
    Former-commit-id: 490e3e539cf533e4dd0de39e506cbe5f21c729c2
    Former-commit-id: 467a8a2ded312a3f110ff5ab8b1e4a0ae5cb7c4a

--- src/simulation/simMessages/scEnergyMomentumSimMsg.h
+++ src/simulation/simMessages/scEnergyMomentumSimMsg.h
@@ -17,24 +17,17 @@
 
  */
 
-#ifndef SC_STATE_MESSAGE_H
-#define SC_STATE_MESSAGE_H
+#ifndef SC_ENERGY_MOMENTUM_MESSAGE_H
+#define SC_ENERGY_MOMENTUM_MESSAGE_H
 
 /*! @brief This structure is used in the messaging system to communicate what the
  state of the vehicle is currently.*/
 typedef struct {
-    double r_BN_N[3];                 //!< m  Current position vector (inertial)
-    double v_BN_N[3];                 //!< m/s Current velocity vector (inertial)
-    double r_CN_N[3];                 //!< m  Current position of CoM vector (inertial)
-    double v_CN_N[3];                 //!< m/s Current velocity of CoM vector (inertial)
-    double sigma_BN[3];               //!< -- Current MRPs (inertial)
-    double omega_BN_B[3];             //!< r/s Current angular velocity
-    double omegaDot_BN_B[3];          //!< r/s/s Current angular acceleration
-    double TotalAccumDVBdy[3];        //!< m/s Accumulated DV of center of mass in body frame coordinates
-    double TotalAccumDV_BN_B[3];      //!< m/s Accumulated DV of body frame in body frame coordinates
-    double nonConservativeAccelpntB_B[3];//!< m/s/s Current Spacecraft non-conservative body frame accel
-    uint64_t MRPSwitchCount;          //!< -- Number of times that MRPs have switched
-}SCStatesSimMsg;
+    double spacecraftOrbEnergy;                 //!< [J] Total orbital kinetic energy
+    double spacecraftRotEnergy;                 //!< [J] Total rotational energy
+    double spacecraftOrbAngMomPntN_N[3];  //!< [kg m^2/s] Total orbital angular momentum about N in N frame compenents
+    double spacecraftRotAngMomPntC_N[3];  //!< [kg m^2/s] Total rotational angular momentum about C in N frame compenents
+}SCEnergyMomentumSimMsg;
 
 
 
