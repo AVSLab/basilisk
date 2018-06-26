dbb03c5b5cc755b358b6851fb550325fa3bc2951
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2018-06-26 10:47:00 +0900

    [BSK-1049] THE BSK message type is now always prepended to the message.
    Former-commit-id: 48be1f6370c693362b9f666d54df55731d5d6524
    Former-commit-id: e28345d1f93475b4321bd55adceae9c6550df7ce

--- src/simulation/utilities/bsk_Print.h
+++ src/simulation/utilities/bsk_Print.h
@@ -36,8 +36,8 @@ typedef enum {
 
 #ifdef _WIN32
 //#define BSK_PRINT(X, _fmt, ...) if (EXPAND(X) <= MSG_LEVEL) {BSK_MESSAGE(EXPAND(_fmt) , EXPAND(__VA_ARGS__))}
-#define BSK_PRINT(X, _fmt, ...) if (EXPAND(X) <= MSG_LEVEL) {printf(_fmt, __VA_ARGS__);}
-#define BSK_PRINT_NOWHERE(X, _fmt, ...) if (EXPAND(X) <= MSG_LEVEL) {printf(_fmt, __VA_ARGS__);}
+#define BSK_PRINT(X, _fmt, ...) if (EXPAND(X) <= MSG_LEVEL) {printf(#X ": " _fmt, __VA_ARGS__);}
+#define BSK_PRINT_NOWHERE(X, _fmt, ...) if (EXPAND(X) <= MSG_LEVEL) {printf(#X ": " _fmt, __VA_ARGS__);}
 
 //#define BSK_PRINT(X, _fmt, ...) if(X <= MSG_LEVEL) \
 									BSK_MESSAGE(_fmt ,##__VA_ARGS__)
@@ -47,9 +47,9 @@ typedef enum {
 #define WHERESTR "[FILE : %s, FUNC : %s, LINE : %d]:\n"
 #define WHEREARG __FILE__,__func__,__LINE__
 #define BSK_PRINT(X, _fmt, ...) if(X <= MSG_LEVEL) \
-                                   BSK_MESSAGE(WHERESTR _fmt, WHEREARG,##__VA_ARGS__)
+                                BSK_MESSAGE(WHERESTR #X ": " _fmt, WHEREARG, ##__VA_ARGS__)
 #define BSK_PRINT_NOWHERE(X, _fmt, ...) if(X <= MSG_LEVEL) \
-                                   BSK_MESSAGE("" _fmt, ##__VA_ARGS__)
+                                BSK_MESSAGE(#X ": " _fmt, ##__VA_ARGS__)
 
 #endif
 
