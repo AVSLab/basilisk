3f65bb2cb75b3c87c6cf417e886fa3bb76928d0d
Thibaud Teil <thibaud.teil@colorado.edu>
2018-10-24 11:46:45 -0600

    [BSK-1122] Camera messages written with all the camera components named as they should be in Unity
    Former-commit-id: a8a6fdb79cbd90411001918fac2a12d046a3ff46
    Former-commit-id: f43da3cc8acd3d66c23d745bddd4770b53fa5fb2

---
+++ src/simulation/simFswInterfaceMessages/cameraConfigMsg.h
@@ -0,0 +1,30 @@
+/*
+ ISC License
+
+ Copyright (c) 2016-2018, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+/*! @brief Structure used to define the output definition for attitude guidance*/
+typedef struct {
+    int64_t cameraID;          /*!< [-]   ID of the camera that took the snapshot*/
+    double fieldOfView;        /*!< [-]   Camera Field of View*/
+    double resolution[2];      /*!< [-] Camera resolution, width/height in pixels (pixelWidth/pixelHeight in Unity)*/
+    double focalLength;        /*!< [-]   Camera Focal Length*/
+    double sensorSize[2];      /*!< [-]   Size of the camera sensor-paired with resolution gives you pixel size*/
+    double cameraPos_B[3];     /*!< [-] Camera position in body frame */
+    double cameraDir_B[3];     /*!< [-] Rotation to the camera frame MRP*/
+}CameraConfigMsg;
+
