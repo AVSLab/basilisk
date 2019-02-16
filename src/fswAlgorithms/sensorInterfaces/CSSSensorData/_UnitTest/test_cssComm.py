a881a2b227c6e2502debd46986c1349ce68d9a6e
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-02-16 08:46:27 -0700

    [BSK-1240] quick tweak to how the last unit test is run based on simFswInterfaceMessages.MAX_NUM_CSS_SENSORS
    Former-commit-id: fdf5c423792d71320aedffaa9a954432f9de2a5f
    Former-commit-id: 7f2cfe409dbd880ac3e6331df98fa5a83b78eb1f

--- src/fswAlgorithms/sensorInterfaces/CSSSensorData/_UnitTest/test_cssComm.py
+++ src/fswAlgorithms/sensorInterfaces/CSSSensorData/_UnitTest/test_cssComm.py
@@ -20,7 +20,7 @@ path = os.path.dirname(os.path.abspath(filename))
 @pytest.mark.parametrize("numSensors, sensorData", [
     (4, [-100e-6, 200e-6, 600e-6, 300e-6, 200e-6]),  # Five data inputs used despite four sensors to ensure all reset conditions are tested.
     pytest.param(0, [-100e-6, 200e-6, 600e-6, 300e-6]), # Zero sensor number to ensure all reset conditions are tested
-    pytest.param(33, [200e-6]*32)  # Zero sensor number to ensure all reset conditions are tested
+    pytest.param(simFswInterfaceMessages.MAX_NUM_CSS_SENSORS+1, [200e-6]*simFswInterfaceMessages.MAX_NUM_CSS_SENSORS)  # Indicate more sensor devices than is allowed.  The output should be clipped to the allowed length
 ])
 
 
@@ -103,7 +103,7 @@ def cssCommTestFunction(numSensors, sensorData):
     trueCssList= [0]*MAX_NUM_CSS_SENSORS
     if numSensors==4:
         trueCssList[0:4] = [0.0, 0.45791653042, 0.0, 0.615444781018]
-    if numSensors==33:
+    if numSensors==MAX_NUM_CSS_SENSORS+1:
         trueCssList = [0.45791653042]*32
 
     # Create the true array
