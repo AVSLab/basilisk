7abe2f5a203cbe0f7dbbef80aa30f6b1a7d59de7
Thibaud Teil <thibaud.teil@colorado.edu>
2019-02-27 14:43:05 -0700

    [BSK-1275] Polishes in .c
    Former-commit-id: 5e23a9c84babafc5c1ab7c3d36d099ceccd963fd
    Former-commit-id: 122444cd8c814305eb02bbc0fc5fce42abed7bea

--- src/fswAlgorithms/attGuidance/attTrackingError/attTrackingError.c
+++ src/fswAlgorithms/attGuidance/attTrackingError/attTrackingError.c
@@ -16,24 +16,24 @@
  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "attGuidance/attTrackingError/attTrackingError.h"
 #include <string.h>
+#include "attGuidance/attTrackingError/attTrackingError.h"
 #include "fswUtilities/fswDefinitions.h"
 #include "simFswInterfaceMessages/macroDefinitions.h"
 #include "simulation/utilities/linearAlgebra.h"
 #include "simulation/utilities/rigidBodyKinematics.h"
 
 
-/*! This method initializes the ConfigData for this module.
+/*! This method initializes the configData for this module.
  It checks to ensure that the inputs are sane and then creates the
  output message
  @return void
- @param ConfigData The configuration data associated with the attitude tracking error module
+ @param configData The configuration data associated with the attitude tracking error module
  */
-void SelfInit_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t moduleID)
+void SelfInit_attTrackingError(attTrackingErrorConfig *configData, uint64_t moduleID)
 {
         /*! Create output message for module */
-    ConfigData->outputMsgID = CreateNewMessage(ConfigData->outputDataName,
+    configData->outputMsgID = CreateNewMessage(configData->outputDataName,
                                                sizeof(AttGuidFswMsg),
                                                "AttGuidFswMsg",
                                                moduleID);
@@ -42,15 +42,15 @@ void SelfInit_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t modu
 /*! This method performs the second stage of initialization for this module.
  It's primary function is to link the input messages that were created elsewhere.
  @return void
- @param ConfigData The configuration data associated with the attitude tracking error module
+ @param configData The configuration data associated with the attitude tracking error module
  */
-void CrossInit_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t moduleID)
+void CrossInit_attTrackingError(attTrackingErrorConfig *configData, uint64_t moduleID)
 {
     /*! - Get the reference and navigation data message ID*/
-    ConfigData->inputRefID = subscribeToMessage(ConfigData->inputRefName,
+    configData->inputRefID = subscribeToMessage(configData->inputRefName,
                                                 sizeof(AttRefFswMsg),
                                                 moduleID);
-    ConfigData->inputNavID = subscribeToMessage(ConfigData->inputNavName,
+    configData->inputNavID = subscribeToMessage(configData->inputNavName,
                                                 sizeof(NavAttIntMsg),
                                                 moduleID);
 
@@ -58,20 +58,20 @@ void CrossInit_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t mod
 
 /*! This method performs a complete reset of the module. Local module variables that retain time varying states between function calls are reset to their default values.
  @return void
- @param ConfigData The configuration data associated with the attitude tracking error module
+ @param configData The configuration data associated with the attitude tracking error module
  */
-void Reset_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t callTime, uint64_t moduleID)
+void Reset_attTrackingError(attTrackingErrorConfig *configData, uint64_t callTime, uint64_t moduleID)
 {
-
+    return;
 }
 
 /*! The Update method performs reads the Navigation message (containing the spacecraft attitude information), and the Reference message (containing the desired attitude). It computes the attitude error and writes it in the Guidance message.
  @return void
- @param ConfigData The configuration data associated with the attitude tracking error module
+ @param configData The configuration data associated with the attitude tracking error module
  @param callTime The clock time at which the function was called (nanoseconds)
  @param moduleID The Basilisk module identifier
  */
-void Update_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t callTime, uint64_t moduleID)
+void Update_attTrackingError(attTrackingErrorConfig *configData, uint64_t callTime, uint64_t moduleID)
 {
     uint64_t    timeOfMsgWritten;
     uint32_t    sizeOfMsgWritten;
@@ -81,15 +81,15 @@ void Update_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t callTi
     /*! - Read the input messages */
     memset(&ref, 0x0, sizeof(AttRefFswMsg));
     memset(&nav, 0x0, sizeof(NavAttIntMsg));
-    ReadMessage(ConfigData->inputRefID, &timeOfMsgWritten, &sizeOfMsgWritten,
+    ReadMessage(configData->inputRefID, &timeOfMsgWritten, &sizeOfMsgWritten,
                 sizeof(AttRefFswMsg), (void*) &(ref), moduleID);
-    ReadMessage(ConfigData->inputNavID, &timeOfMsgWritten, &sizeOfMsgWritten,
+    ReadMessage(configData->inputNavID, &timeOfMsgWritten, &sizeOfMsgWritten,
                 sizeof(NavAttIntMsg), (void*) &(nav), moduleID);
 
-    computeAttitudeError(nav, ref, ConfigData);
+    computeAttitudeError(nav, ref, configData);
 
-    WriteMessage(ConfigData->outputMsgID, callTime, sizeof(AttGuidFswMsg),   /*! - Write guidance message */
-                 (void*) &(ConfigData->attGuidOut), moduleID);
+    WriteMessage(configData->outputMsgID, callTime, sizeof(AttGuidFswMsg),   /*! - Write guidance message */
+                 (void*) &(configData->attGuidOut), moduleID);
 
     return;
 }
@@ -98,25 +98,25 @@ void Update_attTrackingError(attTrackingErrorConfig *ConfigData, uint64_t callTi
  @return void
  @param nav The spacecraft attitude information
  @param ref The reference attitude
- @param ConfigData The configuration data associated with the attitude tracking error module
+ @param configData The configuration data associated with the attitude tracking error module
  */
-void computeAttitudeError(NavAttIntMsg nav, AttRefFswMsg ref, attTrackingErrorConfig *ConfigData){
+void computeAttitudeError(NavAttIntMsg nav, AttRefFswMsg ref, attTrackingErrorConfig *configData){
     double      sigma_RR0[3];               /* MRP from the original reference frame R0 to the corrected reference frame R */
     double      sigma_RN[3];                /* MRP from inertial to updated reference frame */
     double      dcm_BN[3][3];               /* DCM from inertial to body frame */
     
     /*! - compute the initial reference frame orientation that takes the corrected body frame into account */
-    v3Scale(-1.0, ConfigData->sigma_R0R, sigma_RR0);
+    v3Scale(-1.0, configData->sigma_R0R, sigma_RR0);
     addMRP(ref.sigma_RN, sigma_RR0, sigma_RN);
     
-    subMRP(nav.sigma_BN, sigma_RN, ConfigData->attGuidOut.sigma_BR);               /*! - compute attitude error */
+    subMRP(nav.sigma_BN, sigma_RN, configData->attGuidOut.sigma_BR);               /*! - compute attitude error */
     
     MRP2C(nav.sigma_BN, dcm_BN);                                /* [BN] */
-    m33MultV3(dcm_BN, ref.omega_RN_N, ConfigData->attGuidOut.omega_RN_B);              /*! - compute reference omega in body frame components */
+    m33MultV3(dcm_BN, ref.omega_RN_N, configData->attGuidOut.omega_RN_B);              /*! - compute reference omega in body frame components */
     
-    v3Subtract(nav.omega_BN_B, ConfigData->attGuidOut.omega_RN_B, ConfigData->attGuidOut.omega_BR_B);     /*! - delta_omega = omega_B - [BR].omega.r */
+    v3Subtract(nav.omega_BN_B, configData->attGuidOut.omega_RN_B, configData->attGuidOut.omega_BR_B);     /*! - delta_omega = omega_B - [BR].omega.r */
     
-    m33MultV3(dcm_BN, ref.domega_RN_N, ConfigData->attGuidOut.domega_RN_B);            /*! - compute reference d(omega)/dt in body frame components */
+    m33MultV3(dcm_BN, ref.domega_RN_N, configData->attGuidOut.domega_RN_B);            /*! - compute reference d(omega)/dt in body frame components */
     
 }
 
