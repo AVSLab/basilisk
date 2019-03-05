c7290ab23ad8891157037233c4159937eb46a63a
Thibaud Teil <thibaud.teil@colorado.edu>
2019-03-05 09:21:56 -0700

    [BSK-1092] Added protobuffers to utilities and trying to generate them during cmake configure
    Former-commit-id: d894b358d1e0facf45cdc2ac21b91bc665ea7e9a
    Former-commit-id: c5b245c3e8e62a0113693cf1e0034bbe93fbb9c1

--- src/simulation/viz_interface/vizInterface.h
+++ src/simulation/viz_interface/vizInterface.h
@@ -19,7 +19,7 @@
 #ifndef VIZ_INTERFACE_H
 #define VIZ_INTERFACE_H
 
-#include "vizProtoBuffer/vizMessage.pb.h"
+#include "vizProtobuffer/vizMessage.pb.h"
 #include <vector>
 #include <fstream>
 #include <map>
