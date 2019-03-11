b2f2db0b3995d77a8e13e67f540dd426eacb099a
Thibaud Teil <thibaud.teil@colorado.edu>
2019-03-11 11:46:29 -0600

    [BSK-1300] Removed the results file, and added computation in the true test file.
    Former-commit-id: 515e901e01b056af02138964fd7f6fe782574b2a
    Former-commit-id: 56f7e90fb4fc83fc768f93f56364342728b99aeb

--- src/fswAlgorithms/attGuidance/celestialTwoBodyPoint/_UnitTest/test_celestialTwoBodyPoint.py
+++ src/fswAlgorithms/attGuidance/celestialTwoBodyPoint/_UnitTest/test_celestialTwoBodyPoint.py
@@ -28,15 +28,16 @@
 import pytest
 import sys, os, inspect
 import numpy as np
+from numpy import linalg as la
 # Import all of the modules that we are going to be called in this simulation
 from Basilisk.utilities import SimulationBaseClass
 from Basilisk.simulation import alg_contain
 from Basilisk.utilities import unitTestSupport  # general support file with common unit test functions
-import matplotlib.pyplot as plt
 from Basilisk.fswAlgorithms import celestialTwoBodyPoint  # module that is to be tested
 from Basilisk.fswAlgorithms import cheby_pos_ephem  # module that creates needed input
 from Basilisk.utilities import macros
 from Basilisk.utilities import astroFunctions as af
+from Basilisk.utilities import RigidBodyKinematics as rbk
 
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
@@ -44,6 +45,54 @@ from Basilisk.utilities import astroFunctions as af
 # uncomment this line if this test has an expected failure, adjust message as needed
 # @pytest.mark.xfail(conditionstring)
 # provide a unique test method name, starting with test_
+
+def computeCelestialTwoBodyPoint(R_P1, v_P1, a_P1, R_P2, v_P2, a_P2):
+
+    # Beforehand computations
+    R_n = np.cross(R_P1, R_P2)
+    v_n = np.cross(v_P1, R_P2) + np.cross(R_P1, v_P2)
+    a_n = np.cross(a_P1, R_P2) + np.cross(R_P1, a_P2) + 2 * np.cross(v_P1, v_P2)
+
+    # Reference Frame generation
+    r1_hat = R_P1/la.norm(R_P1)
+    r3_hat = R_n/la.norm(R_n)
+    r2_hat = np.cross(r3_hat, r1_hat)
+    RN = np.array([r1_hat, r2_hat, r3_hat])
+    sigma_RN = rbk.C2MRP(RN)
+
+    # Reference base-vectors first time-derivative
+    I_33 = np.identity(3)
+    C1 = I_33 - np.outer(r1_hat, r1_hat)
+    dr1_hat = 1.0 / la.norm(R_P1) * np.dot(C1, v_P1)
+    C3 = I_33 - np.outer(r3_hat, r3_hat)
+    dr3_hat = 1.0 / la.norm(R_n) * np.dot(C3, v_n)
+    dr2_hat = np.cross(dr3_hat, r1_hat) + np.cross(r3_hat, dr1_hat)
+
+    # Angular Velocity computation
+    omega_RN_R = np.array([
+        np.dot(r3_hat, dr2_hat),
+        np.dot(r1_hat, dr3_hat),
+        np.dot(r2_hat, dr1_hat)
+    ])
+    omega_RN_N = np.dot(RN.T, omega_RN_R)
+
+    # Reference base-vectors second time-derivative
+    temp33_1 = 2 * np.outer(dr1_hat, r1_hat) + np.outer(r1_hat, dr1_hat)
+    ddr1_hat = 1.0 / la.norm(R_P1) * (np.dot(C1, a_P1) - np.dot(temp33_1, v_P1))
+    temp33_3 = 2 * np.outer(dr3_hat, r3_hat) + np.outer(r3_hat, dr3_hat)
+    ddr3_hat = 1.0 / la.norm(R_n) * (np.dot(C3, a_n) - np.dot(temp33_3, v_n))
+    ddr2_hat = np.cross(ddr3_hat, r1_hat) + np.cross(ddr1_hat, r3_hat) + 2 * np.cross(dr3_hat, dr1_hat)
+
+    # Angular Acceleration computation
+    domega_RN_R = np.array([
+        np.dot(dr3_hat, dr2_hat) + np.dot(r3_hat, ddr2_hat) - np.dot(omega_RN_R, dr1_hat),
+        np.dot(dr1_hat, dr3_hat) + np.dot(r1_hat, ddr3_hat) - np.dot(omega_RN_R, dr2_hat),
+        np.dot(dr2_hat, dr1_hat) + np.dot(r2_hat, ddr1_hat) - np.dot(omega_RN_R, dr3_hat)
+
+    ])
+    domega_RN_N = np.dot(RN.T, domega_RN_R)
+
+    return sigma_RN, omega_RN_N, domega_RN_N
 
 def test_celestialTwoBodyPoint(show_plots):
 
@@ -141,18 +190,6 @@ def celestialTwoBodyPointTestFunction(show_plots):
                                           inputSpiceMessageSize,
                                           0, CelBodyData)
 
-    #   Spice Input Message of Secondary Body
-    # unitTestSim.TotalSim.CreateNewMessage(unitProcessName,
-    #                                       moduleConfig.inputSecMessName,
-    #                                       inputSpiceMessageSize, 2)
-    # SecBodyData = spice_interface.SpicePlanetStateSimMsg()
-    # secPositionVec = [500., 500., 500.]
-    # SecBodyData.PositionVector = secPositionVec
-    # secVelocityVec = [0., 0., 0.]
-    # SecBodyData.VelocityVector = secVelocityVec
-    # unitTestSim.TotalSim.WriteMessageData(moduleConfig.inputSecMessName,
-    #                                       inputSpiceMessageSize,
-    #                                       0, SecBodyData)
 
     # Setup logging on the test module output message so that we get all the writes to it
     unitTestSim.TotalSim.logThisMessage(moduleConfig.outputDataName, testProcessRate)
@@ -169,6 +206,29 @@ def celestialTwoBodyPointTestFunction(show_plots):
     # Begin the simulation time run set above
     unitTestSim.ExecuteSimulation()
 
+    ## Set truth values
+    a = af.E_radius * 2.8
+    e = 0.0
+    i = 0.0
+    Omega = 0.0
+    omega = 0.0
+    f = 60 * af.D2R
+    (r, v) = af.OE2RV(af.mu_E, a, e, i, Omega, omega, f)
+    r_BN_N = np.array([0., 0., 0.])
+    v_BN_N = np.array([0., 0., 0.])
+    celPositionVec = r
+    celVelocityVec = v
+
+    # Begin Method
+    R_P1 = celPositionVec - r_BN_N
+    v_P1 = celVelocityVec - v_BN_N
+    a_P1 = np.array([0., 0., 0.])
+    R_P2 = np.cross(R_P1, v_P1)
+    v_P2 = np.cross(R_P1, a_P1)
+    a_P2 = np.cross(v_P1, a_P1)
+
+    sigma_RN, omega_RN_N, domega_RN_N = computeCelestialTwoBodyPoint(R_P1, v_P1, a_P1, R_P2, v_P2, a_P2)
+
     # This pulls the actual data log from the simulation run.
     # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
     # check sigma_RN
@@ -176,19 +236,12 @@ def celestialTwoBodyPointTestFunction(show_plots):
 
     moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
                                                   range(3))
-    # set the filtered output truth states
-    trueVector = [
-        [0.379795897113, 0.219275263435, 0.219275263435],
-        [0.379795897113, 0.219275263435, 0.219275263435],
-        [0.379795897113, 0.219275263435, 0.219275263435]
-    ]
 
     # compare the module results to the truth values
-
     accuracy = 1e-12
-    for i in range(0, len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+    # check a vector values
+    for i in range(0, len(moduleOutput)):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], sigma_RN, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
@@ -200,17 +253,10 @@ def celestialTwoBodyPointTestFunction(show_plots):
     moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
                                                   range(3))
 
-    # set the filtered output truth states
-    trueVector = [
-        [0.0, 0.0, 0.000264539877],
-        [0.0, 0.0, 0.000264539877],
-        [0.0, 0.0, 0.000264539877]
-    ]
     # compare the module results to the truth values
-    accuracy = 1e-12
-    for i in range(0, len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+    # check a vector values
+    for i in range(0, len(moduleOutput)):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], omega_RN_N, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
@@ -222,43 +268,15 @@ def celestialTwoBodyPointTestFunction(show_plots):
     moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
                                                   range(3))
 
-    # set the filtered output truth states
-    trueVector = [
-        [-6.998134641547e-08, -6.506313905857e-24, 0.0],
-        [-6.998134641547e-08, -6.506313905857e-24, 0.0],
-        [-6.998134641547e-08, -6.506313905857e-24, 0.0]
-    ]
-
     # compare the module results to the truth values
-    accuracy = 1e-12
-    for i in range(0, len(trueVector)):
-        # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+    # check a vector values
+    for i in range(0, len(moduleOutput)):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], domega_RN_N, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
                                 str(moduleOutput[i, 0] * macros.NANO2SEC) +
                                 "sec\n")
-
-    # Note that we can continue to step the simulation however we feel like.
-    # Just because we stop and query data does not mean everything has to stop for good
-    unitTestSim.ConfigureStopTime(macros.sec2nano(0.6))  # run an additional 0.6 seconds
-    unitTestSim.ExecuteSimulation()
-
-    # If the argument provided at commandline "--show_plots" evaluates as true,
-    # plot all figures
-    #    if show_plots:
-    #        # plot a sample variable.
-    #        plt.figure(1)
-    #        plt.plot(variableState[:,0]*macros.NANO2SEC, variableState[:,1], label='Sample Variable')
-    #        plt.legend(loc='upper left')
-    #        plt.xlabel('Time [s]')
-    #        plt.ylabel('Variable Description [unit]')
-    #        plt.show()
-
-
-    # each test method requires a single assert method to be called
-    # this check below just makes sure no sub-test failures were found
 
     return [testFailCount, ''.join(testMessages)]
 
@@ -302,9 +320,7 @@ def secBodyCelestialTwoBodyPointTestFunction(show_plots):
 
     #moduleConfig.inputSecMessName = "inputSecMessName"
     moduleConfig.outputDataName = "outputName"
-
     moduleConfig.singularityThresh = 1.0 * af.D2R
-
 
     # Previous Computation of Initial Conditions for the test
     a = af.E_radius * 2.8
@@ -387,19 +403,14 @@ def secBodyCelestialTwoBodyPointTestFunction(show_plots):
     moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.outputDataName + '.' + moduleOutputName,
                                                   range(3))
     # set the filtered output truth states
-    trueVector = [
-        [0.474475084038,  0.273938317493,  0.191443718765],
-        [0.474475084038,  0.273938317493,  0.191443718765],
-        [0.474475084038,  0.273938317493,  0.191443718765]
-    ]
+    trueVector = [0.474475084038,  0.273938317493,  0.191443718765]
+
 
     # compare the module results to the truth values
-
     accuracy = 1e-12
-    for i in range(0, len(trueVector)):
+    for i in range(0, len(moduleOutput)):
         # check a vector values
-        # print("{:.12}, {:.12}, {:.12}".format(moduleOutput[i,1], moduleOutput[i,2], moduleOutput[i,3]))
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
@@ -412,17 +423,11 @@ def secBodyCelestialTwoBodyPointTestFunction(show_plots):
                                                   range(3))
 
     # set the filtered output truth states
-    trueVector = [
-        [1.59336987e-04,   2.75979758e-04,   2.64539877e-04],
-        [1.59336987e-04,   2.75979758e-04,   2.64539877e-04],
-        [1.59336987e-04,   2.75979758e-04,   2.64539877e-04]
-    ]
+    trueVector = [1.59336987e-04,   2.75979758e-04,   2.64539877e-04]
     # compare the module results to the truth values
-    accuracy = 1e-12
-    for i in range(0, len(trueVector)):
-        # print("{:.12}, {:.12}, {:.12}".format(moduleOutput[i, 1], moduleOutput[i, 2], moduleOutput[i, 3]))
+    for i in range(0, len(moduleOutput)):
         # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
@@ -435,18 +440,12 @@ def secBodyCelestialTwoBodyPointTestFunction(show_plots):
                                                   range(3))
 
     # set the filtered output truth states
-    trueVector = [
-        [-2.12284893e-07,   5.69968291e-08,  -4.83648052e-08],
-        [-2.12284893e-07,   5.69968291e-08,  -4.83648052e-08],
-        [-2.12284893e-07,   5.69968291e-08,  -4.83648052e-08]
-    ]
+    trueVector = [-2.12284893e-07,   5.69968291e-08,  -4.83648052e-08]
 
     # compare the module results to the truth values
-    accuracy = 1e-12
-    for i in range(0, len(trueVector)):
-        print(moduleOutput[i])
+    for i in range(0, len(moduleOutput)):
         # check a vector values
-        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector[i], 3, accuracy):
+        if not unitTestSupport.isArrayEqual(moduleOutput[i], trueVector, 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + moduleWrap.ModelTag + " Module failed " +
                                 moduleOutputName + " unit test at t=" +
@@ -458,21 +457,8 @@ def secBodyCelestialTwoBodyPointTestFunction(show_plots):
     unitTestSim.ConfigureStopTime(macros.sec2nano(0.6))  # run an additional 0.6 seconds
     unitTestSim.ExecuteSimulation()
 
-    # If the argument provided at commandline "--show_plots" evaluates as true,
-    # plot all figures
-    #    if show_plots:
-    #        # plot a sample variable.
-    #        plt.figure(1)
-    #        plt.plot(variableState[:,0]*macros.NANO2SEC, variableState[:,1], label='Sample Variable')
-    #        plt.legend(loc='upper left')
-    #        plt.xlabel('Time [s]')
-    #        plt.ylabel('Variable Description [unit]')
-    #        plt.show()
-
-
     # each test method requires a single assert method to be called
     # this check below just makes sure no sub-test failures were found
-
     return [testFailCount, ''.join(testMessages)]
 
 
