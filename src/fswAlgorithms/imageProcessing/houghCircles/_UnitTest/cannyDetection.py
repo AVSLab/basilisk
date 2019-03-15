84060f99983d45e2d76fa30f8a4333ed928e14df
Thibaud Teil <thibaud.teil@colorado.edu>
2019-03-14 18:40:23 -0600

    [BSK-965] Python Hough Test is finding circles on test image. Ready to be compared to the module outputs
    Former-commit-id: 109f0663a1ef394e913474ac54d90f6e245c1d39
    Former-commit-id: c95c7ee480468f7e20d5cd03981384bcf5435252

---
+++ src/fswAlgorithms/imageProcessing/houghCircles/_UnitTest/cannyDetection.py
@@ -0,0 +1,116 @@
+from math import sqrt, atan2, pi
+import numpy as np
+
+def canny_edge_detector(input_image):
+    input_pixels = input_image.load()
+    width = input_image.width
+    height = input_image.height
+
+    # Transform the image to grayscale
+    grayscaled = compute_grayscale(input_pixels, width, height)
+
+    # Blur it to remove noise
+    blurred = compute_blur(grayscaled, width, height)
+
+    # Compute the gradient
+    gradient, direction = compute_gradient(blurred, width, height)
+
+    # Non-maximum suppression
+    filter_out_non_maximum(gradient, direction, width, height)
+
+    # Filter out some edges
+    keep = filter_strong_edges(gradient, width, height, 10, 30)
+
+    return keep
+
+
+def compute_grayscale(input_pixels, width, height):
+    grayscale = np.empty((width, height))
+    for x in range(width):
+        for y in range(height):
+            pixel = input_pixels[x, y]
+            grayscale[x, y] = (pixel[0] + pixel[1] + pixel[2]) / 3.
+    return grayscale
+
+
+def compute_blur(input_pixels, width, height):
+    # Keep coordinate inside image
+    clip = lambda x, l, u: l if x < l else u if x > u else x
+
+    blursize = 5
+    # Gaussian kernel
+    kernel = 1./blursize**2.*np.ones([blursize, blursize])
+
+    # Middle of the kernel
+    offset = len(kernel) // 2
+
+    # Compute the blurred image
+    blurred = np.empty((width, height))
+    for x in range(width):
+        for y in range(height):
+            acc = 0
+            for a in range(len(kernel)):
+                for b in range(len(kernel)):
+                    xn = clip(x + a - offset, 0, width - 1)
+                    yn = clip(y + b - offset, 0, height - 1)
+                    acc += input_pixels[xn, yn] * kernel[a, b]
+            blurred[x, y] = int(acc)
+    return blurred
+
+
+def compute_gradient(input_pixels, width, height):
+    gradient = np.zeros((width, height))
+    direction = np.zeros((width, height))
+    for x in range(width):
+        for y in range(height):
+            if 0 < x < width - 1 and 0 < y < height - 1:
+                magx = input_pixels[x + 1, y] - input_pixels[x - 1, y]
+                magy = input_pixels[x, y + 1] - input_pixels[x, y - 1]
+                gradient[x, y] = sqrt(magx**2 + magy**2)
+                direction[x, y] = atan2(magy, magx)
+    return gradient, direction
+
+
+def filter_out_non_maximum(gradient, direction, width, height):
+    for x in range(1, width - 1):
+        for y in range(1, height - 1):
+            angle = direction[x, y] if direction[x, y] >= 0 else direction[x, y] + pi
+            rangle = round(angle / (pi / 4))
+            mag = gradient[x, y]
+            if ((rangle == 0 or rangle == 4) and (gradient[x - 1, y] > mag or gradient[x + 1, y] > mag)
+                    or (rangle == 1 and (gradient[x - 1, y - 1] > mag or gradient[x + 1, y + 1] > mag))
+                    or (rangle == 2 and (gradient[x, y - 1] > mag or gradient[x, y + 1] > mag))
+                    or (rangle == 3 and (gradient[x + 1, y - 1] > mag or gradient[x - 1, y + 1] > mag))):
+                gradient[x, y] = 0
+
+
+def filter_strong_edges(gradient, width, height, low, high):
+    # Keep strong edges
+    keep = set()
+    for x in range(width):
+        for y in range(height):
+            if gradient[x, y] > high:
+                keep.add((x, y))
+
+    # Keep weak edges next to a pixel to keep
+    lastiter = keep
+    while lastiter:
+        newkeep = set()
+        for x, y in lastiter:
+            for a, b in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+                if gradient[x + a, y + b] > low and (x+a, y+b) not in keep:
+                    newkeep.add((x+a, y+b))
+        keep.update(newkeep)
+        lastiter = newkeep
+
+    return list(keep)
+
+
+if __name__ == "__main__":
+    from PIL import Image, ImageDraw
+    input_image = Image.open("mars.png")
+    output_image = Image.new("RGB", input_image.size)
+    draw = ImageDraw.Draw(output_image)
+    for x, y in canny_edge_detector(input_image):
+        draw.point((x, y), (255, 255, 255))
+    output_image.save("canny.png")
--- src/fswAlgorithms/imageProcessing/houghCircles/_UnitTest/houghCirlces.py
+++ src/fswAlgorithms/imageProcessing/houghCircles/_UnitTest/houghCirlces.py
@@ -23,6 +23,10 @@
 #   Author:             Thibaud Teil
 #   Creation Date:      March 13, 2019
 #
+from PIL import Image, ImageDraw
+from math import sqrt, pi, cos, sin
+from cannyDetection import canny_edge_detector
+from collections import defaultdict
 
 import pytest
 import sys, os, inspect
@@ -67,196 +71,106 @@ def houghCircles(show_plots):
     unitProcessName = "TestProcess"         # arbitrary name (don't change)
 
     # Create a sim module as an empty container
-    unitTestSim = SimulationBaseClass.SimBaseClass()
-    # terminateSimulation() is needed if multiple unit test scripts are run
-    # that run a simulation for the test. This creates a fresh and
-    # consistent simulation environment for each test run.
-    unitTestSim.TotalSim.terminateSimulation()
+    # unitTestSim = SimulationBaseClass.SimBaseClass()
+    # # terminateSimulation() is needed if multiple unit test scripts are run
+    # # that run a simulation for the test. This creates a fresh and
+    # # consistent simulation environment for each test run.
+    # unitTestSim.TotalSim.terminateSimulation()
+    #
+    # # Create test thread
+    # testProcessRate = macros.sec2nano(0.5)     # update process rate update time
+    # testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    # testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+    #
+    #
+    # # Construct algorithm and associated C++ container
+    # moduleConfig = houghCircles.HoughCircles()
+    # moduleConfig.ModelTag = "houghCircles"
+    #
+    # # Add test module to runtime call list
+    # unitTestSim.AddModelToTask(unitTaskName, moduleConfig)
+    #
+    # # Initialize the test module configuration data
+    # moduleConfig.navRateOutMsgName = "sampleOutput"
+    # moduleConfig.imuRateInMsgName = "sampleInput"
+    #
+    # # Create input message and size it because the regular creator of that message
+    # # is not part of the test.
+    # inputMessageData = houghCircles.CameraImageMsg()
+    # inputMessageData.AngVelBody = [-0.1, 0.2, -0.3]
+    # unitTestSupport.setMessage(unitTestSim.TotalSim,
+    #                            unitProcessName,
+    #                            moduleConfig.imuRateInMsgName,
+    #                            inputMessageData)
+    #
+    # # Setup logging on the test module output message so that we get all the writes to it
+    # unitTestSim.TotalSim.logThisMessage(moduleConfig.navRateOutMsgName, testProcessRate)
+    #
+    # # Need to call the self-init and cross-init methods
+    # unitTestSim.InitializeSimulation()
+    #
+    # # Set the simulation time.
+    # # NOTE: the total simulation time may be longer than this value. The
+    # # simulation is stopped at the next logging event on or after the
+    # # simulation end time.
+    # unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))        # seconds to stop simulation
+    #
+    # # Begin the simulation time run set above
+    # unitTestSim.ExecuteSimulation()
 
-    # Create test thread
-    testProcessRate = macros.sec2nano(0.5)     # update process rate update time
-    testProc = unitTestSim.CreateNewProcess(unitProcessName)
-    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+    #################################################
+    # Truth values from python
+    # Load image:
+    input_image = Image.open("circles.png")
 
+    # Output image:
+    output_image = Image.new("RGB", input_image.size)
+    output_image.paste(input_image)
+    draw_result = ImageDraw.Draw(output_image)
 
-    # Construct algorithm and associated C++ container
-    moduleConfig = houghCircles.HoughCircles()
-    moduleConfig.ModelTag = "houghCircles"
+    # Find circles
+    rmin = 5
+    rmax = int(input_image.size[0]/2)
+    steps = 100
+    threshold = 0.4
 
-    # Add test module to runtime call list
-    unitTestSim.AddModelToTask(unitTaskName, moduleConfig)
+    points = []
+    for r in range(rmin, rmax + 1):
+        for t in range(steps):
+            points.append((r, int(r * cos(2 * pi * t / steps)), int(r * sin(2 * pi * t / steps))))
 
-    # Initialize the test module configuration data
-    moduleConfig.navRateOutMsgName = "sampleOutput"
-    moduleConfig.imuRateInMsgName = "sampleInput"
+    acc = defaultdict(int)
+    for x, y in canny_edge_detector(input_image):
+        for r, dx, dy in points:
+            a = x - dx
+            b = y - dy
+            acc[(a, b, r)] += 1
 
-    # Create input message and size it because the regular creator of that message
-    # is not part of the test.
-    inputMessageData = houghCircles.CameraImageMsg()
-    inputMessageData.AngVelBody = [-0.1, 0.2, -0.3]
-    unitTestSupport.setMessage(unitTestSim.TotalSim,
-                               unitProcessName,
-                               moduleConfig.imuRateInMsgName,
-                               inputMessageData)
+    circles = []
+    for k, v in sorted(acc.items(), key=lambda i: -i[1]):
+        x, y, r = k
+        if float(v) / steps >= threshold and all((x - xc) ** 2 + (y - yc) ** 2 > rc ** 2 for xc, yc, rc in circles):
+            print(float(v) / steps, x, y, r)
+            circles.append((x, y, r))
 
-    # Setup logging on the test module output message so that we get all the writes to it
-    unitTestSim.TotalSim.logThisMessage(moduleConfig.navRateOutMsgName, testProcessRate)
+    for x, y, r in circles:
+        draw_result.ellipse((x - r, y - r, x + r, y + r), outline=(255, 0, 0, 0))
 
-    # Need to call the self-init and cross-init methods
-    unitTestSim.InitializeSimulation()
-
-    # Set the simulation time.
-    # NOTE: the total simulation time may be longer than this value. The
-    # simulation is stopped at the next logging event on or after the
-    # simulation end time.
-    unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))        # seconds to stop simulation
-
-    # Begin the simulation time run set above
-    unitTestSim.ExecuteSimulation()
-
-    # compare the module results to the truth values
-    accuracy = 1e-12
-    unitTestSupport.writeTeXSnippet("toleranceValue", str(accuracy), path)
-
-    # This pulls the actual data log from the simulation run.
-    # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
-    moduleOutputName = "omega_BN_B"
-    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.navRateOutMsgName + '.' + moduleOutputName,
-                                                  range(3))
-
-    # set the filtered output truth states
-    trueVector = [
-        [-0.1, 0.2, -0.3],
-        [-0.1, 0.2, -0.3],
-        [-0.1, 0.2, -0.3]
-    ]
-    testFailCount, testMessages = unitTestSupport.compareArray(trueVector, moduleOutput,
-                                                               accuracy, "Output Vector",
-                                                               testFailCount, testMessages)
-
-    # write TeX Tables for documentation
-    resultTable = moduleOutput
-    resultTable[:,0] = macros.NANO2SEC*resultTable[:,0]
-    diff = np.delete(moduleOutput,0,1) - trueVector
-    resultTable = np.insert(resultTable,range(2,2+len(diff.transpose())), diff, axis=1)
-
-    tableName = "testRateMsgConverter"       # make this a unique name
-    tableHeaders = ["time [s]", "Output 1", "Error", "Output 2", "Error", "Output 3 $\\bm r$", "Error"]
-    caption = 'Unit test output table.'
-    unitTestSupport.writeTableLaTeX(
-        tableName,
-        tableHeaders,
-        caption,
-        resultTable,
-        path)
-
-
-    moduleOutputName = "omega_BN_B"
-    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.navRateOutMsgName + '.' + moduleOutputName,
-                                                  range(3))
-
-    # set the filtered output truth states
-    trueVector = [
-        [-0.1, 0.2, -0.3],
-        [-0.1, 0.2, -0.3],
-        [-0.1, 0.2, -0.3]
-    ]
-    testFailCount, testMessages = unitTestSupport.compareArray(trueVector, moduleOutput,
-                                                               accuracy, "Output Rate Vector",
-                                                               testFailCount, testMessages)
-
-    # write TeX Tables for documentation
-    resultTable = moduleOutput
-    resultTable[:,0] = macros.NANO2SEC*resultTable[:,0]
-    diff = np.delete(moduleOutput,0,1) - trueVector
-    resultTable = np.insert(resultTable,range(2,2+len(diff.transpose())), diff, axis=1)
-
-    tableName = "testRate"       # make this a unique name
-    tableHeaders = ["time [s]", "Output 1", "Error", "Output 2", "Error", "Output 3 $\\bm \\omega_{B/N}$", "Error"]
-    caption = 'Unit test output table for rate values.'
-    unitTestSupport.writeTableLaTeX(
-        tableName,
-        tableHeaders,
-        caption,
-        resultTable,
-        path)
-
-
-    moduleOutputName = "sigma_BN"
-    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.navRateOutMsgName + '.' + moduleOutputName,
-                                                  range(3))
-
-    # set the filtered output truth states
-    trueVector = [
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0]
-    ]
-    testFailCount, testMessages = unitTestSupport.compareArray(trueVector, moduleOutput,
-                                                               accuracy, "Output MRP Vector",
-                                                               testFailCount, testMessages)
-
-    # write TeX Tables for documentation
-    resultTable = moduleOutput
-    resultTable[:,0] = macros.NANO2SEC*resultTable[:,0]
-    diff = np.delete(moduleOutput,0,1) - trueVector
-    resultTable = np.insert(resultTable,range(2,2+len(diff.transpose())), diff, axis=1)
-
-    tableName = "testMRP"       # make this a unique name
-    tableHeaders = ["time [s]", "Output 1", "Error", "Output 2", "Error", "Output 3 $\\bm \\sigma$", "Error"]
-    caption = 'Unit test output table for MRPs.'
-    unitTestSupport.writeTableLaTeX(
-        tableName,
-        tableHeaders,
-        caption,
-        resultTable,
-        path)
-
-    moduleOutputName = "vehSunPntBdy"
-    moduleOutput = unitTestSim.pullMessageLogData(moduleConfig.navRateOutMsgName + '.' + moduleOutputName,
-                                                  range(3))
-
-    # set the filtered output truth states
-    trueVector = [
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0]
-    ]
-    testFailCount, testMessages = unitTestSupport.compareArray(trueVector, moduleOutput,
-                                                               accuracy, "Output sun heading Vector",
-                                                               testFailCount, testMessages)
-
-    # write TeX Tables for documentation
-    resultTable = moduleOutput
-    resultTable[:,0] = macros.NANO2SEC*resultTable[:,0]
-    diff = np.delete(moduleOutput,0,1) - trueVector
-    resultTable = np.insert(resultTable,range(2,2+len(diff.transpose())), diff, axis=1)
-
-    tableName = "testSunHeading"       # make this a unique name
-    tableHeaders = ["time [s]", "Output 1", "Error", "Output 2", "Error", "Output 3 $\\bm d$", "Error"]
-    caption = 'Unit test output table for sun heading vector.'
-    unitTestSupport.writeTableLaTeX(
-        tableName,
-        tableHeaders,
-        caption,
-        resultTable,
-        path)
-
-
-
+    # Save output image
+    output_image.save("result.png")
 
 
     #   print out success message if no error were found
-    snippentName = "passFail"
-    if testFailCount == 0:
-        colorText = 'ForestGreen'
-        print "PASSED: " + moduleWrap.ModelTag
-        passedText = '\\textcolor{' + colorText + '}{' + "PASSED" + '}'
-    else:
-        colorText = 'Red'
-        print "Failed: " + moduleWrap.ModelTag
-        passedText = '\\textcolor{' + colorText + '}{' + "Failed" + '}'
-    unitTestSupport.writeTeXSnippet(snippentName, passedText, path)
+    # snippentName = "passFail"
+    # if testFailCount == 0:
+    #     colorText = 'ForestGreen'
+    #     print "PASSED: " + moduleWrap.ModelTag
+    #     passedText = '\\textcolor{' + colorText + '}{' + "PASSED" + '}'
+    # else:
+    #     colorText = 'Red'
+    #     print "Failed: " + moduleWrap.ModelTag
+    #     passedText = '\\textcolor{' + colorText + '}{' + "Failed" + '}'
+    # unitTestSupport.writeTeXSnippet(snippentName, passedText, path)
 
 
     # each test method requires a single assert method to be called
