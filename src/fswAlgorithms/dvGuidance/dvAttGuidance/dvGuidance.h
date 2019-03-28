bf04548e0a3772fe9dcdcfdad2a648eaaf8f7998
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-03-28 09:10:29 -0600

    [BSK-1310] added a doxygen module description
    Former-commit-id: 63eabde0ef2b5d9d6ae8c5372937907c6a64cbbe
    Former-commit-id: 0ec1a92b902c5831acf924715e05413dd4f32344

--- src/fswAlgorithms/dvGuidance/dvAttGuidance/dvGuidance.h
+++ src/fswAlgorithms/dvGuidance/dvAttGuidance/dvGuidance.h
@@ -26,6 +26,9 @@
 #include <stdint.h>
 
 /*! \defgroup  dvAttGuidance
+    @brief This module creates a time varying attitude reference frame message that allows the orbit correction burn direction to rotate at a constant rate.
+
+ A message is read in containing the base \f$\Delta\mathbf{v}\f$ direction, the burn duration, as well as a nominal rotation axis.  A base burn frame is created relative to which a constant rotation about the 3rd frame axis is performed.  The output message contains the full reference frame states including the constant angular velocity vector and a zero angular acceleration vector. More information can be found in the [PDF Description](Basilisk-dvGuidance-2019-03-28.pdf).
  * @{
  */
 
