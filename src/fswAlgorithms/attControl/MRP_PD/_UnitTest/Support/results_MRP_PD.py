b04c12e611217f78d430434ec775048c86adcd5a
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-03-29 16:38:59 -0600

    [BSK-1320] updated the unit test to use a non-zero known external torque, compute the true answer live in python, and do general BSK script updates to make it current
    Former-commit-id: 1684b085e1cf33e00bc67ff9e51ed5834067f4ff
    Former-commit-id: a922729fade30b970c2c123be267629baee0e56b

--- src/fswAlgorithms/attControl/MRP_PD/_UnitTest/Support/results_MRP_PD.py
+++ src/fswAlgorithms/attControl/MRP_PD/_UnitTest/Support/results_MRP_PD.py
@@ -42,11 +42,12 @@ K = 0.15
 P = 150.0
 
 # Begin Method
-L = np.array([0., 0., 0.])
+L = np.array([0.1, 0.2, 0.3])
 omega_BN_B = omega_BR_B + omega_RN_B
 temp1 = np.dot(I, omega_BN_B)
 temp2 = domega_RN_B - np.cross(omega_BN_B, omega_RN_B)
 Lr = K * sigma_BR + P * omega_BR_B - np.cross(omega_RN_B, temp1) - np.dot(I, temp2)
+Lr += L
 Lr *= -1.0
 
 # Print Results
