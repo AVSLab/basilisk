cf582f06ecf1da66f6dc3817ba060511b882f491
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-04-26 15:16:33 -0600

    [BSK-1311] added PDF documentation to module Doxygen documentation
    Former-commit-id: a872c2153de45e9b8c46e33b104c341a264a3c15
    Former-commit-id: 32f91cdf365b48abd654efb8d743ee53902a5bf0

--- src/fswAlgorithms/transDetermination/oeStateEphem/oeStateEphem.h
+++ src/fswAlgorithms/transDetermination/oeStateEphem/oeStateEphem.h
@@ -30,9 +30,12 @@
 
 /*! \defgroup oeStateEphem
  @brief This module takes the TDB time, current object time and computes the state of the object
- * using the time corrected by TDB and the stored Chebyshev coefficients.  If the time provided
+ * using the time corrected by TDB and the stored Chebyshev coefficients.
+ *
+ * If the time provided
  * is outside the specified range for which the stored Chebyshev coefficients are valid then
- * the position vectors rail high/low appropriately.
+ * the position vectors rail high/low appropriately. 
+ * More information can be found in the [PDF Description](Basilisk-oeStateEphem-20190426.pdf).
  *  @{
 */
 
