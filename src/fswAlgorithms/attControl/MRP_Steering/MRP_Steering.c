20b43d7d09261db9e79630cb6013eaa4133d61a0
Kieran <kieran.wilson@colorado.edu>
2019-04-28 09:29:45 -0600

    Removed "else" condition for omega_ast_p computation; now zeroed by default and then computed if not ignoreOuterLoopFeedforward
    Former-commit-id: c8739b298748a5e44cab84a7efea279d19fac84f
    Former-commit-id: 150d8018961cc962f3010744a3927bfc188d1a85

--- src/fswAlgorithms/attControl/MRP_Steering/MRP_Steering.c
+++ src/fswAlgorithms/attControl/MRP_Steering/MRP_Steering.c
@@ -29,16 +29,16 @@
 #include <string.h>
 #include <math.h>
 
-/*! This method initializes the ConfigData for this module.
+/*! This method initializes the configData for this module.
  It checks to ensure that the inputs are sane and then creates the
  output message
  @return void
- @param ConfigData The configuration data associated with this module
+ @param configData The configuration data associated with this module
  */
-void SelfInit_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t moduleID)
+void SelfInit_MRP_Steering(MRP_SteeringConfig *configData, uint64_t moduleID)
 {
         /*! - Create output message for module */
-    ConfigData->outputMsgID = CreateNewMessage(ConfigData->outputDataName,
+    configData->outputMsgID = CreateNewMessage(configData->outputDataName,
         sizeof(RateCmdFswMsg), "RateCmdFswMsg", moduleID);
 
 }
@@ -46,21 +46,21 @@ void SelfInit_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t moduleID)
 /*! This method performs the second stage of initialization for this module.
  Its primary function is to link the input messages that were created elsewhere.
  @return void
- @param ConfigData The configuration data associated with this module
+ @param configData The configuration data associated with this module
  */
-void CrossInit_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t moduleID)
+void CrossInit_MRP_Steering(MRP_SteeringConfig *configData, uint64_t moduleID)
 {
     /*! - Get the control data message IDs*/
-    ConfigData->inputGuidID = subscribeToMessage(ConfigData->inputGuidName,
+    configData->inputGuidID = subscribeToMessage(configData->inputGuidName,
                                                  sizeof(AttGuidFswMsg), moduleID);
 }
 
 /*! This method performs a complete reset of the module.  Local module variables that retain
  time varying states between function calls are reset to their default values.
  @return void
- @param ConfigData The configuration data associated with the MRP steering control
+ @param configData The configuration data associated with the MRP steering control
  */
-void Reset_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t callTime, uint64_t moduleID)
+void Reset_MRP_Steering(MRP_SteeringConfig *configData, uint64_t callTime, uint64_t moduleID)
 {
     return;
 }
@@ -68,10 +68,10 @@ void Reset_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t callTime, uint6
 /*! This method takes the attitude and rate errors relative to the Reference frame, as well as
     the reference frame angular rates and acceleration, and computes the required control torque Lr.
  @return void
- @param ConfigData The configuration data associated with the MRP Steering attitude control
+ @param configData The configuration data associated with the MRP Steering attitude control
  @param callTime The clock time at which the function was called (nanoseconds)
  */
-void Update_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t callTime,
+void Update_MRP_Steering(MRP_SteeringConfig *configData, uint64_t callTime,
     uint64_t moduleID)
 {
     AttGuidFswMsg       guidCmd;            /*!< Guidance Message */
@@ -80,30 +80,29 @@ void Update_MRP_Steering(MRP_SteeringConfig *ConfigData, uint64_t callTime,
     
     memset(&guidCmd, 0x0, sizeof(AttGuidFswMsg));
     /*! - Read the dynamic input messages */
-    ReadMessage(ConfigData->inputGuidID, &timeOfMsgWritten, &sizeOfMsgWritten,
+    ReadMessage(configData->inputGuidID, &timeOfMsgWritten, &sizeOfMsgWritten,
                 sizeof(AttGuidFswMsg), (void*) &(guidCmd), moduleID);
 
     /* evalute MRP kinematic steering law */
-    MRPSteeringLaw(ConfigData, guidCmd.sigma_BR, ConfigData->outMsg.omega_BastR_B, ConfigData->outMsg.omegap_BastR_B);
+    MRPSteeringLaw(configData, guidCmd.sigma_BR, configData->outMsg.omega_BastR_B, configData->outMsg.omegap_BastR_B);
 
     /* Store the output message and pass it to the message bus */
-    WriteMessage(ConfigData->outputMsgID, callTime, sizeof(RateCmdFswMsg),
-                 (void*) &(ConfigData->outMsg), moduleID);
+    WriteMessage(configData->outputMsgID, callTime, sizeof(RateCmdFswMsg),
+                 (void*) &(configData->outMsg), moduleID);
     
     return;
 }
-
 
 /*! This method computes the MRP Steering law.  A commanded body rate is returned given the MRP
  attitude error measure of the body relative to a reference frame.  The function returns the commanded
  body rate, as well as the body frame derivative of this rate command.
  @return void
- @param ConfigData  The configuration data associated with this module
+ @param configData  The configuration data associated with this module
  @param sigma_BR    MRP attitude error of B relative to R
  @param omega_ast   Commanded body rates
  @param omega_ast_p Body frame derivative of the commanded body rates
  */
-void MRPSteeringLaw(MRP_SteeringConfig *ConfigData, double sigma_BR[3], double omega_ast[3], double omega_ast_p[3])
+void MRPSteeringLaw(MRP_SteeringConfig *configData, double sigma_BR[3], double omega_ast[3], double omega_ast_p[3])
 {
     double  sigma_i;        /*!< ith component of sigma_B/R */
     double  B[3][3];        /*!< B-matrix of MRP differential kinematic equations */
@@ -111,24 +110,23 @@ void MRPSteeringLaw(MRP_SteeringConfig *ConfigData, double sigma_BR[3], double o
     double  value;
     int     i;
 
-    /* Determine the desired steering rates  */
+    /* Equation (18): Determine the desired steering rates  */
     for (i=0;i<3;i++) {
         sigma_i      = sigma_BR[i];
-        value        = atan(M_PI_2/ConfigData->omega_max*(ConfigData->K1*sigma_i
-                       + ConfigData->K3*sigma_i*sigma_i*sigma_i))/M_PI_2*ConfigData->omega_max;
+        value        = atan(M_PI_2/configData->omega_max*(configData->K1*sigma_i
+                       + configData->K3*sigma_i*sigma_i*sigma_i))/M_PI_2*configData->omega_max;
         omega_ast[i] = -value;
 
     }
-    if (ConfigData->ignoreOuterLoopFeedforward) {
-        v3SetZero(omega_ast_p);
-    } else {
-        /* Determine the body frame derivative of the steering rates */
+    v3SetZero(omega_ast_p);
+    if (!configData->ignoreOuterLoopFeedforward) {
+        /* Equation (21): Determine the body frame derivative of the steering rates */
         BmatMRP(sigma_BR, B);
         m33MultV3(B, omega_ast, sigma_p);
         v3Scale(0.25, sigma_p, sigma_p);
         for (i=0;i<3;i++) {
             sigma_i        = sigma_BR[i];
-            value          = (3*ConfigData->K3*sigma_i*sigma_i + ConfigData->K1)/(pow(M_PI_2/ConfigData->omega_max*(ConfigData->K1*sigma_i + ConfigData->K3*sigma_i*sigma_i*sigma_i),2) + 1);
+            value          = (3*configData->K3*sigma_i*sigma_i + configData->K1)/(pow(M_PI_2/configData->omega_max*(configData->K1*sigma_i + configData->K3*sigma_i*sigma_i*sigma_i),2) + 1);
             omega_ast_p[i] = - value*sigma_p[i];
         }
     }
