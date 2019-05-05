5314257994d5b5aad75a974ed775f4e167398f2a
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-05-04 20:57:45 -0600

    [BSK-1337] include the PDF documentation in the doxygen HTML documentation
    Former-commit-id: 87db2daa013c8e215f57fc1131ffaf66ce0fcf74
    Former-commit-id: c23d8468f06e38f87e12454eeef887d78d8185b9

--- src/simulation/environment/planetEphemeris/planetEphemeris.h
+++ src/simulation/environment/planetEphemeris/planetEphemeris.h
@@ -33,10 +33,9 @@
 
 
 /*! @brief The planetEphemeris module uses classical heliocentric orbit elements to specify a planet's
-    position and velocity vectors.
+ position and velocity vectors.
 
- The module [PDF Description]()
- contains further information on this module's function, how to run it, as well as testing.
+ Optionally, the planet's orientation can also be specified through a right ascension angle, a declination angle, and a location sidereal time at epoch.  The planet rotation about its third (polar) axis can be specified as well.  If the planet attitude information is not complete or missing then an inertially fixed zero planet orientation is modeled.  The module is able to receive a stack or vector of classical orbit elements and orientation information to output a series of planet ephemeris messages. The module [PDF Description](Basilisk-planetEphemeris-20190422.pdf) contains further information on this module's function, how to run it, as well as testing.
 */
 
 class PlanetEphemeris: public SysModel {
