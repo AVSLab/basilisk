4758606986713bcb5a3c626dbb66211cbd749861
Scott Carnahan <scott.carnahan@colorado.edu>
2019-05-15 13:52:25 -0400

    [BSK-1247] comment sys interface
    Former-commit-id: ff6fe9b1ff39b791a40faf6acb6a57385746efd9
    Former-commit-id: fecb50726d82c292da0e5740f319758c1cdcc431

--- src/simulation/_GeneralModuleFiles/sys_interface.h
+++ src/simulation/_GeneralModuleFiles/sys_interface.h
@@ -27,33 +27,50 @@
  * @{
  */
 
+/*!
+ * This struct is dual purpose. The comments in-line describe the use as it is in
+ * the processData variable. Here, the block comment describes its used as it is in
+ * messageTraffic:
+ * std::string messageSource Name of message in source buffer
+ * std::string messageDest Name of message in dest buffer (left blank i.e. same name)
+ * int64_t source the ID for the message in its source buffer
+ * int64_t destination The ID for the message in the destination buffer
+ * uint64_t updateCounter the number of times a message had been written last time it was routed
+ */
 typedef struct {
-    std::string messageSource;           //!< (-) source name used to find ID
-    std::string messageDest;             //!< (-) dest name used to find ID
-    int64_t source;       //!< The buffer to copy messages from (negative indicates not ready)
-    int64_t destination;         //!< The buffer to copy messages to (negative indicates not ready)
-    uint64_t updateCounter;  //!< (-) The previous update counter that was routed
-}MessageInterfaceMatch;
+    std::string messageSource;  //!< (-) name of process to get message from
+    std::string messageDest;  //!< (-) name of process to put message in
+    int64_t source;  //!< The ID for the buffer to copy messages from (negative indicates not ready)
+    int64_t destination;  //!< The ID for the buffer to copy messages to (negative indicates not ready)
+    uint64_t updateCounter;  //!< (-) The number of times a message had been written last time it was routed.
+}MessageInterfaceMatch;  //!< -- Routing information for a specific message
 
+/*!
+ * This class encapsulates the interface from a single source to a single destination.
+ * A SysInterface has a list of these.
+ */
 class  InterfaceDataExchange : public SysModel{
 public:
     InterfaceDataExchange();
     virtual ~InterfaceDataExchange();
-    virtual bool linkProcesses();
-    virtual bool linkMessages();
-    virtual void discoverMessages();
-    virtual void routeMessages();
+    virtual bool linkProcesses();  //!<  uses the input process names to
+    virtual bool linkMessages();  //!< get read/write permission in source/destination buffers
+    virtual void discoverMessages();  //!< looks for unpublished messages in the buffer and adds them to the list
+    virtual void routeMessages();  //!< copy message data from source to destination
 public:
-    bool exchangeActive;                //!< (-) Flag indicating that exchange is active
+    bool exchangeActive;  //!< (-) Flag indicating that exchange is active
     MessageInterfaceMatch processData;  //!< Definition of process movement
-    std::vector<MessageInterfaceMatch> messageTraffic; //!< Message movement
-    bool needDelete;
+    std::vector<MessageInterfaceMatch> messageTraffic;  //!< Message movement
+    bool needDelete;  //!< Used by destructor to clear memory for this exchange
 private:
-    uint64_t msgBufferSize;
+    uint64_t msgBufferSize;  //!< size of message in bytes currently being routed
     uint8_t *msgBuffer;
 };
 
-//! Class used to pass messages between one or more processes
+/*!
+ * This class is used to organize and manage all of the interfaces for a specific process. Like an interface manager.
+ * It maintains a list of InterfaceDataExchange. For instance,
+ */
 class SysInterface
 {
     
@@ -70,8 +87,8 @@ public:
     void connectInterfaces();
     
 public:
-    std::vector<InterfaceDataExchange *> interfaceDef; //!< Definition of messages to move
-    bool interfaceActive;                           //!< -- Flag indicate whether Task has been disabled
+    std::vector<InterfaceDataExchange *> interfaceDef; //!< List of interfaces
+    bool interfaceActive;                           //!< -- Flag indicate whether interface has been disabled
     bool interfacesLinked;                          //!< (-) Flag indicating ints have all been linked
 private:
     InterfaceDataExchange *currentInterface;        //!< (-) allows user to get/set msgs for single int
