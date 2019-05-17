1ffae7e520bfd0439b86b4eb2ccc5b0d0a186c55
Andrew <anha5550@colorado.edu>
2019-05-17 10:28:54 -0600

    Documentation/Unit Test commit, ready for review.
    Former-commit-id: 19a0513400d2499dccffe6a0d647c0973ef0597e
    Former-commit-id: e100c0f91b7b769d0139853a07b4f31bf5597af3

--- src/simulation/dynamics/facetDragEffector/_UnitTest/test_unitFacetDrag.py
+++ src/simulation/dynamics/facetDragEffector/_UnitTest/test_unitFacetDrag.py
@@ -19,12 +19,13 @@
 '''
 
 #
-# Basilisk Scenario Script and Integrated Testeccomending treatment or not.
 #
-# Purpose:  Test the validity of a simple exponential atmosphere model.
+#
+# Purpose:  Test the facetDrag module.
 # Author:   Andrew Harris
-# Creation Date:  Jan 18, 2017
+# Creation Date:  May 16 2019
 #
+
 
 import sys, os, inspect
 import matplotlib
@@ -34,6 +35,12 @@ import math
 import csv
 import logging
 import pytest
+
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+bskName = 'Basilisk'
+splitPath = path.split(bskName)
 
 
 # import general simulation support files
@@ -72,8 +79,24 @@ def test_unitFacetDrag():
     testMessage.append(dragMsg)
     testResults.append(dragRes)
 
+    shadowRes, shadowMsg = TestShadowCalculation()
+    testMessage.append(shadowMsg)
+    testResults.append(shadowRes)
 
     testSum = sum(testResults)
+
+    snippetName = "unitTestPassFail"
+
+    if testSum == 0:
+        colorText = 'ForestGreen'
+        print "PASSED"
+        passedText = '\\textcolor{' + colorText + '}{' + "PASSED" + '}'
+    else:
+        colorText = 'Red'
+        print "Failed"
+        passedText = '\\textcolor{' + colorText + '}{' + "Failed" + '}'
+    unitTestSupport.writeTeXSnippet(snippetName, passedText, path)
+
     assert testSum < 1, testMessage
 
 def SetDensityMsgTest(dragEffector):
@@ -165,7 +188,6 @@ def TestDragCalculation():
     rN = np.array([r_eq+200.0e3,0,0])
     vN = np.array([0,7.788e3,0])
     sig_BN = np.array([0,0,0])
-
     #   initialize Spacecraft States with the initialization variables
     scObject.hub.r_CN_NInit = unitTestSupport.np2EigenVectorXd(rN)  # m - r_CN_N
     scObject.hub.v_CN_NInit = unitTestSupport.np2EigenVectorXd(vN)  # m - v_CN_N
@@ -238,6 +260,147 @@ def TestDragCalculation():
 
     return testFailCount, testMessages
 
+
+def TestShadowCalculation():
+
+    #   Init test support variables
+    showVal = False
+    testFailCount = 0
+    testResults = []
+    testMessages = []
+
+    ##   Simulation initialization
+    simTaskName = "simTask"
+    simProcessName = "simProcess"
+    scSim = SimulationBaseClass.SimBaseClass()
+    scSim.TotalSim.terminateSimulation()
+
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+    simulationTimeStep = macros.sec2nano(10.)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
+
+    # initialize spacecraftPlus object and set properties
+    scObject = spacecraftPlus.SpacecraftPlus()
+    scObject.ModelTag = "spacecraftBody"
+    ##   Initialize new atmosphere and drag model, add them to task
+    newAtmo = exponentialAtmosphere.ExponentialAtmosphere()
+    newAtmo.ModelTag = "ExpAtmo"
+    newAtmo.addSpacecraftToModel(scObject.scStateOutMsgName)
+
+    newDrag = facetDragDynamicEffector.FacetDragDynamicEffector()
+    newDrag.setDensityMessage(newAtmo.envOutMsgNames[-1])
+    newDrag.ModelTag = "FacetDrag"
+
+    scObject.addDynamicEffector(newDrag)
+
+    try:
+        scAreas = [1.0, 1.0]
+        scCoeff = np.array([2.0, 2.0])
+        B_normals = [np.array([0, 0, -1]), np.array([0, -1, 0])]
+        B_locations = [np.array([0,0,0.1]), np.array([0,0.1,0])]
+
+        for ind in range(0,len(scAreas)):
+            newDrag.addFacet(scAreas[ind], scCoeff[ind], B_normals[ind], B_locations[ind])
+    except:
+        testFailCount += 1
+        testMessage.append("ERROR: FacetDrag unit test failed while setting facet parameters.")
+        return testFailCount, testMessage
+
+    # clear prior gravitational body and SPICE setup definitions
+    gravFactory = simIncludeGravBody.gravBodyFactory()
+    planet = gravFactory.createEarth()
+
+    planet.isCentralBody = True          # ensure this is the central gravitational body
+    mu = planet.mu
+    # attach gravity model to spaceCraftPlus
+    scObject.gravField.gravBodies = spacecraftPlus.GravBodyVector(gravFactory.gravBodies.values())
+
+    #
+    #   setup orbit and simulation time
+    oe = orbitalMotion.ClassicElements()
+
+    r_eq = 6371*1000.0
+    refBaseDens = 1.217
+    refScaleHeight = 8500.0
+
+    #   Set base density, equitorial radius, scale height in Atmosphere
+    newAtmo.baseDensity = refBaseDens
+    newAtmo.scaleHeight = refScaleHeight
+    newAtmo.planetRadius = r_eq
+
+    rN = np.array([r_eq+200.0e3,0,0])
+    vN = np.array([0,7.788e3,0])
+    sig_BN = np.array([0,0,0])
+
+    #   initialize Spacecraft States with the initialization variables
+    scObject.hub.r_CN_NInit = unitTestSupport.np2EigenVectorXd(rN)  # m - r_CN_N
+    scObject.hub.v_CN_NInit = unitTestSupport.np2EigenVectorXd(vN)  # m - v_CN_N
+    scObject.hub.sigma_BNInit =  unitTestSupport.np2EigenVectorXd(sig_BN)
+
+    simulationTime = macros.sec2nano(10.)
+    #
+    #   Setup data logging before the simulation is initialized
+    #
+    numDataPoints = 10
+
+    # add BSK objects to the simulation process
+    scSim.AddModelToTask(simTaskName, scObject)
+    scSim.AddModelToTask(simTaskName, newAtmo)
+    scSim.AddModelToTask(simTaskName, newDrag)
+
+    #
+    #   initialize Simulation
+    #
+    scSim.InitializeSimulation()
+
+    samplingTime = simulationTimeStep
+    scSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, samplingTime)
+    scSim.TotalSim.logThisMessage(newAtmo.ModelTag+"_0_data", samplingTime)
+
+    scSim.AddVariableForLogging(newDrag.ModelTag + ".forceExternal_N",
+                                      simulationTimeStep, 0, 2, 'double')
+    scSim.AddVariableForLogging(newDrag.ModelTag + ".torqueExternalPntB_B",
+                                      simulationTimeStep, 0, 2, 'double')
+
+    #   configure a simulation stop time time and execute the simulation run
+    #
+    scSim.ConfigureStopTime(simulationTime)
+    scSim.ExecuteSimulation()
+
+    #   Retrieve logged data
+    #dragDataForce_B = scSim.GetLogVariableData(newDrag.ModelTag + ".forceExternal_B")
+    dragDataForce_N = scSim.GetLogVariableData(newDrag.ModelTag + ".forceExternal_N")
+    dragTorqueData = scSim.GetLogVariableData(newDrag.ModelTag + ".torqueExternalPntB_B")
+    posData = scSim.pullMessageLogData(scObject.scStateOutMsgName+'.r_BN_N',range(3))
+    velData = scSim.pullMessageLogData(scObject.scStateOutMsgName + '.v_BN_N', range(3))
+    attData = scSim.pullMessageLogData(scObject.scStateOutMsgName + '.sigma_BN', range(3))
+    densData = scSim.pullMessageLogData(newAtmo.ModelTag+"_0_data.neutralDensity")
+    np.set_printoptions(precision=16)
+
+    def checkFacetDragForce(dens, area, coeff, facet_dir, sigma_BN, inertial_vel):
+        vel_dir = inertial_vel / np.linalg.norm(inertial_vel)
+        dcm = rbk.MRP2C(sigma_BN)
+        drag_force = -0.5 * dens * area * (facet_dir.dot(dcm.dot(vel_dir))) *  coeff * np.linalg.norm(inertial_vel)**2.0 * vel_dir
+        return drag_force
+
+
+    #   Compare to expected values
+    accuracy = 1e-9
+    #unitTestSupport.writeTeXSnippet("toleranceValue", str(accuracy), path)
+
+    if len(densData) > 0:
+        for ind in range(1,len(densData)):
+            if not unitTestSupport.isArrayZero(dragDataForce_N[ind,:], 3,accuracy):
+                testFailCount += 1
+                testMessages.append(
+                    "FAILED:  FacetDragEffector failed shadow unit test at t=" + str(dragDataForce_N[ind,0]* macros.NANO2SEC) + "sec with a value difference of "+str(dragDataForce_N[ind,1:]))
+
+
+    else:
+        testFailCount += 1
+        testMessages.append("FAILED:  ExpAtmo failed to pull any logged data")
+
+    return testFailCount, testMessages
 
 if __name__=="__main__":
     test_unitFacetDrag()
