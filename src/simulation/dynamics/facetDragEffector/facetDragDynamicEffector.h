fe605d6a13044979eba080988ddb6a8a4030deb3
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-06-11 16:04:04 -0600

    [HOT-FIX] make sure the facetDrag doxygen documentation links back to the module PDF documentation as well.
    Former-commit-id: 44e2aedaa4bd15c54a8c768f0f6754e39128a4d6
    Former-commit-id: 5be1a58044183be2fc177d544597089f47e199aa

--- src/simulation/dynamics/facetDragEffector/facetDragDynamicEffector.h
+++ src/simulation/dynamics/facetDragEffector/facetDragDynamicEffector.h
@@ -48,7 +48,9 @@ typedef struct {
 //! @brief Drag dynamics class used to compute drag effects on spacecraft bodies
 /*! This class is used to implement drag dynamic effects on spacecraft using a variety of simple or complex models, which will include
 cannonball (attitude-independent) drag, single flat-plate drag, faceted drag models, and an interface to full-CAD GPU-accellerated
-drag models. */
+drag models.
+For more information see the [PDF Description](Basilisk-facet_drag-20190515.pdf).
+ */
 
 class FacetDragDynamicEffector: public SysModel, public DynamicEffector {
 public:
