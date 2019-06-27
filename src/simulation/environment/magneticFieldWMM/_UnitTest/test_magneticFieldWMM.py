6addc94b2344b913161de92f8774138f42396613
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-06-26 19:56:53 -0600

    [BSK-1376] wrapped up the unit test using the WMM provided test cases.  These are included in a Support folder as a PDF.
    Former-commit-id: 9efa659117dcf927240b38049b1b21d628e1d6fb
    Former-commit-id: 3d37591264a3d025adab3e8dbcec4f28c448db7e

--- src/simulation/environment/magneticFieldWMM/_UnitTest/test_magneticFieldWMM.py
+++ src/simulation/environment/magneticFieldWMM/_UnitTest/test_magneticFieldWMM.py
@@ -37,8 +37,6 @@ splitPath = path.split(bskName)
 
 
 
-
-
 # Import all of the modules that we are going to be called in this simulation
 from Basilisk.utilities import SimulationBaseClass
 from Basilisk.utilities import unitTestSupport                  # general support file with common unit test functions
@@ -46,7 +44,7 @@ from Basilisk.simulation import magneticFieldWMM
 from Basilisk.simulation import simMessages
 from Basilisk.utilities import macros
 from Basilisk.utilities import orbitalMotion
-from Basilisk.utilities import simSetPlanetEnvironment
+from Basilisk.utilities import RigidBodyKinematics as rbk
 
 
 # Uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed.
@@ -56,20 +54,38 @@ from Basilisk.utilities import simSetPlanetEnvironment
 # Provide a unique test method name, starting with 'test_'.
 # The following 'parametrize' function decorator provides the parameters and expected results for each
 #   of the multiple test runs for this test.
-@pytest.mark.parametrize("useDefault", [ True, False])
+@pytest.mark.parametrize("decimalYear, Height, Lat, Lon, BxTrue, ByTrue, BzTrue", [
+      (2015,   0,  80,   0,  6636.6,   -451.9,  54408.9)
+    , (2015,   0,   0, 120, 39521.1,    377.7, -11228.8)
+    , (2015,   0, -80, 240,  5796.3,  15759.1, -52927.1)
+    , (2015, 100,  80,   0,  6323.4,   -477.6,  52249.1)
+    , (2015, 100,   0, 120, 37538.1,    351.1, -10751.1)
+    , (2015, 100, -80, 240,  5612.2,  14789.3, -50385.8)
+    , (2017.5, 0, 80, 0, 6605.2, -298.7, 54506.3)
+    , (2017.5, 0, 0, 120, 39569.4, 252.3, -11067.9)
+    , (2017.5, 0, -80, 240, 5864.6, 15764.1, -52706.1)
+    , (2017.5, 100, 80, 0, 6294.3, -331.1, 52337.8)
+    , (2017.5, 100, 0, 120, 37584.4, 235.7, -10600.5)
+    , (2017.5, 100, -80, 240, 5674.9, 14793.1, -50179.5)
+])
+@pytest.mark.parametrize("useDefault, useMsg", [
+    (False, False)
+    , (False, True)
+    , (True, True)
+])
 @pytest.mark.parametrize("useMinReach", [ True, False])
 @pytest.mark.parametrize("useMaxReach", [ True, False])
 @pytest.mark.parametrize("usePlanetEphemeris", [ True, False])
 
 
 # update "module" in this function name to reflect the module name
-def test_module(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
+def test_module(show_plots, decimalYear, Height, Lat, Lon, BxTrue, ByTrue, BzTrue, useDefault, useMsg, useMinReach, useMaxReach, usePlanetEphemeris):
     # each test method requires a single assert method to be called
-    [testResults, testMessage] = run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris)
+    [testResults, testMessage] = run(show_plots, decimalYear, Height, Lat, Lon, BxTrue, ByTrue, BzTrue, useDefault, useMsg, useMinReach, useMaxReach, usePlanetEphemeris)
     assert testResults < 1, testMessage
 
 
-def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
+def run(show_plots, decimalYear, Height, Lat, Lon, BxTrue, ByTrue, BzTrue, useDefault, useMsg, useMinReach, useMaxReach, usePlanetEphemeris):
     testFailCount = 0                       # zero unit test result counter
     testMessages = []                       # create empty array to store test log messages
     unitTaskName = "unitTask"               # arbitrary name (don't change)
@@ -87,19 +103,38 @@ def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
     testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
 
-
     # Construct algorithm and associated C++ container
     testModule = magneticFieldWMM.MagneticFieldWMM()
     testModule.ModelTag = "WMM"
+    testModule.dataPath = splitPath[0] + 'Basilisk/supportData/MagneticField/'
 
+    if not useDefault:
+        testModule.epochDate = decimalYear
+
+    if useMsg:
+        testModule.epochInMsgName = "simEpoch"
+        epochMsg = simMessages.EpochSimMsg()
+        dt = unitTestSupport.decimalYearToDateTime(decimalYear)
+        epochMsg.year = dt.year
+        epochMsg.month = dt.month
+        epochMsg.day = dt.day
+        epochMsg.hours = dt.hour
+        epochMsg.minutes = dt.minute
+        epochMsg.seconds = dt.second
+        unitTestSupport.setMessage(unitTestSim.TotalSim,
+                                  unitProcessName,
+                                  testModule.epochInMsgName,
+                                  epochMsg)
+        if not useDefault:
+            testModule.epochDate = decimalYear + 1.0
 
     minReach = -1.0
     if useMinReach:
-        minReach = 200*1000.0     # meters
+        minReach = (orbitalMotion.REQ_EARTH+200.)*1000.0     # meters
         testModule.envMinReach = minReach
     maxReach = -1.0
     if useMaxReach:
-        maxReach = 200*1000.0     # meters
+        maxReach = (orbitalMotion.REQ_EARTH-200.)*1000.0     # meters
         testModule.envMaxReach = maxReach
     planetPosition = np.array([0.0, 0.0, 0.0])
     refPlanetDCM = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)))
@@ -128,22 +163,11 @@ def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
 
 
     # define the spacecraft locations
-    r0 = 6571 * 1000.0  # meters
-    r1 = 6600 * 1000.0  # meters
-    #
-    #   setup orbit and simulation time
-    oe = orbitalMotion.ClassicElements()
-    mu = 0.3986004415E+15  # meters^3/s^2
-    oe.a = r0
-    oe.e = 0.0
-    oe.i = 45.0 * macros.D2R
-    oe.Omega = 30.0 * macros.D2R
-    oe.omega = 120.0 * macros.D2R
-    oe.f = 0.0 * macros.D2R
-    r0N, v0N = orbitalMotion.elem2rv(mu, oe)
-    oe.a = r1
-    r1N, v1N = orbitalMotion.elem2rv(mu, oe)
-
+    r0 = (orbitalMotion.REQ_EARTH + Height) * 1000.0  # meters
+    phi = Lat * macros.D2R
+    long = Lon * macros.D2R
+    r0P = np.array([np.cos(phi)*np.cos(long),np.cos(phi)*np.sin(long),np.sin(phi)])*r0
+    r0N = np.dot(refPlanetDCM.transpose(),r0P)
 
     # create the input messages
     sc0StateMsg = simMessages.SCPlusStatesSimMsg()  # Create a structure for the input message
@@ -153,7 +177,7 @@ def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
                                sc0StateMsgName,
                                sc0StateMsg)
     sc1StateMsg = simMessages.SCPlusStatesSimMsg()  # Create a structure for the input message
-    sc1StateMsg.r_BN_N = np.array(r1N) + np.array(planetPosition)
+    sc1StateMsg.r_BN_N = np.array(r0N) + np.array(planetPosition)
     unitTestSupport.setMessage(unitTestSim.TotalSim,
                                unitProcessName,
                                sc1StateMsgName,
@@ -166,57 +190,63 @@ def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
     # Need to call the self-init and cross-init methods
     unitTestSim.InitializeSimulation()
 
+    unitTestSim.TotalSim.SingleStepProcesses()
+
     # Set the simulation time.
     # NOTE: the total simulation time may be longer than this value. The
     # simulation is stopped at the next logging event on or after the
     # simulation end time.
-    unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))        # seconds to stop simulation
+    # unitTestSim.ConfigureStopTime(macros.sec2nano(1.0))        # seconds to stop simulation
 
     # Begin the simulation time run set above
-    unitTestSim.ExecuteSimulation()
+    # unitTestSim.ExecuteSimulation()
 
 
     # This pulls the actual data log from the simulation run.
     # Note that range(3) will provide [0, 1, 2]  Those are the elements you get from the vector (all of them)
-    mag0Data = unitTestSim.pullMessageLogData(testModule.envOutMsgNames[0] + ".magField_N", range(3))
-    mag1Data = unitTestSim.pullMessageLogData(testModule.envOutMsgNames[1] + ".magField_N", range(3))
+    mag0Data = unitTestSim.pullMessageLogData(testModule.envOutMsgNames[0] + ".magField_N", range(3))*1e9
+    mag1Data = unitTestSim.pullMessageLogData(testModule.envOutMsgNames[1] + ".magField_N", range(3))*1e9
+    print "BSK:" + str(mag0Data)
 
-
-    def centeredDipole(pos_N, X, refPlanetRadius, refPlanetDCM, minReach, maxReach):
+    def wmmInertial(pos_N, Bx, By, Bz, phi, long, refPlanetDCM, minReach, maxReach):
         radius = np.linalg.norm(pos_N)
-        planetPos_E = refPlanetDCM.dot(pos_N)
-        rHat_E = planetPos_E/radius
+        B_M = np.array([Bx, By, Bz])
+        M2 = rbk.euler2(phi + np.pi/2.0)
+        M3 = rbk.euler3(-long)
+        PM = np.dot(M3,M2)
+        NM = np.dot(refPlanetDCM.transpose(), PM)
+        print NM
+        print B_M
+        magField_N = [np.dot(NM, B_M).tolist()]
 
-        magField_E = (refPlanetRadius/radius)**3 * (3*rHat_E*np.dot(rHat_E, X)-X)
-
-        magField_N = [((refPlanetDCM.transpose()).dot(magField_E)).tolist()]*3
 
         if radius < minReach:
-            magField_N = [[0.0, 0.0, 0.0]]*3
+            magField_N = [[0.0, 0.0, 0.0]]
         if radius > maxReach and maxReach > 0:
-            magField_N = [[0.0, 0.0, 0.0]]*3
+            magField_N = [[0.0, 0.0, 0.0]]
         return magField_N
 
 
     # compare the module results to the truth values
-    accuracy = 1e-5
+    accuracy = 1e-1
     unitTestSupport.writeTeXSnippet("unitTestToleranceValue", str(accuracy), path)
 
 
     # check the exponential atmosphere results
     #
     # check spacecraft 0 neutral density results
-    # if len(mag0Data) > 0:
-    #     trueMagField = centeredDipole(r0N, np.array([refg11, refh11, refg10]), refPlanetRadius, refPlanetDCM, minReach, maxReach)
-    #     testFailCount, testMessages = unitTestSupport.compareArrayRelative(
-    #         trueMagField, mag0Data, accuracy, "SC0 mag vector",
-    #         testFailCount, testMessages)
-    #
-    # if len(mag1Data) > 0:
-    #     trueMagField = centeredDipole(r1N, np.array([refg11, refh11, refg10]), refPlanetRadius, refPlanetDCM, minReach, maxReach)
-    #     testFailCount, testMessages = unitTestSupport.compareArrayRelative(
-    #         trueMagField, mag1Data, accuracy, "SC1 mag vector",
-    #         testFailCount, testMessages)
+    if len(mag0Data) > 0:
+        trueMagField = wmmInertial(r0N, BxTrue, ByTrue, BzTrue, phi, long, refPlanetDCM, minReach, maxReach)
+
+        print "B(true):" + str(trueMagField)
+        testFailCount, testMessages = unitTestSupport.compareArray(
+            trueMagField, mag0Data, accuracy, "SC0 mag vector",
+            testFailCount, testMessages)
+
+    if len(mag1Data) > 0:
+        testFailCount, testMessages = unitTestSupport.compareArrayRelative(
+            trueMagField, mag1Data, accuracy, "SC1 mag vector",
+            testFailCount, testMessages)
 
 
     #   print out success or failure message
@@ -244,8 +274,16 @@ def run(show_plots, useDefault, useMinReach, useMaxReach, usePlanetEphemeris):
 if __name__ == "__main__":
     test_module(              # update "module" in function name
                  False,         # showplots
-                 False,          # useDefault
+                 2017.5,        # decimalYear
+                 100,             # Height (km)
+                 0,            # latitude (deg)
+                 120,             # longitude (deg)
+                 37584.4,        # BxTrue (nT)
+                 235.7,        # ByTrue (nT)
+                 -10600.5,       # BzTrue (nT)
+                 True,          # useDefault
+                 True,         # useMsg
                  False,         # useMinReach
                  False,         # useMaxReach
-                 True          # usePlanetEphemeris
+                 False          # usePlanetEphemeris
                )
