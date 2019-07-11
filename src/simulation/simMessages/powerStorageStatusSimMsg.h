79220c5ee1914c0759c84ea21effa39c1a2234a3
Andrew <anha5550@colorado.edu>
2019-07-11 08:37:49 -0600

    Initial commit of base power storage class / messages
    Former-commit-id: c4459e40a4c1c7c5b1c48732bf628bd267ca5097
    Former-commit-id: aac367d39e22ce3c4701900d19b48a798c55ae5e

---
+++ src/simulation/simMessages/powerStorageStatusSimMsg.h
@@ -0,0 +1,14 @@
+//
+// Created by andrew on 7/9/19.
+//
+
+#ifndef BASILISK_POWERSTORAGESTATUSSIMMSG_H
+#define BASILISK_POWERSTORAGESTATUSSIMMSG_H
+
+
+typedef struct{
+    double storageLevel;
+    double storageCapacity;
+}PowerStorageStatusSimMsg;
+
+#endif //BASILISK_POWERSTORAGESTATUSSIMMSG_H
