d3e6831ba188ad4ce0c7d66810cd0a603a87223b
Andrew <anha5550@colorado.edu>
2019-07-12 12:21:09 -0600

    simPowerStorageBase, simPowerNode base class / support messages done.
    Former-commit-id: 5758851fc1437905439edb9ba668c60e021b5e73
    Former-commit-id: 4b9d98ab61f435b56c7e18349eda2f7d10b485ca

---
+++ src/simulation/power/simplePowerMonitor/simplePowerMonitor.h
@@ -0,0 +1,14 @@
+//
+// Created by andrew on 7/12/19.
+//
+
+#ifndef BASILISK_SIMPLEPOWERMONITOR_H
+#define BASILISK_SIMPLEPOWERMONITOR_H
+
+
+class simplePowerMonitor {
+
+};
+
+
+#endif //BASILISK_SIMPLEPOWERMONITOR_H
