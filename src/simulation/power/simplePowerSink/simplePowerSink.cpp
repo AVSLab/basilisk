f6e390f1562a5c46433c47d6e21384eb493408b3
Andrew <anha5550@colorado.edu>
2019-07-12 13:24:52 -0600

    Initial commit of simplePowerSink module.
    Former-commit-id: 5ebba70800fa15557ce84c627f4fadb03b92688c
    Former-commit-id: bd5e9fa1f6c0b1e5c1a5325e9d6e81acf8fca859

--- src/simulation/power/simplePowerSink/simplePowerSink.cpp
+++ src/simulation/power/simplePowerSink/simplePowerSink.cpp
@@ -1,45 +1,25 @@
-/*
- ISC License
-
- Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
-
- Permission to use, copy, modify, and/or distribute this software for any
- purpose with or without fee is hereby granted, provided that the above
- copyright notice and this permission notice appear in all copies.
-
- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
- */
+//
+// Created by andrew on 7/12/19.
+//
 
 #include "simplePowerSink.h"
-#include "../../simMessages/powerNodeUsageSimMsg.h"
 
-/*! Constructor, which sets the default nodePowerOut to zero.
-*/
-SimplePowerSink::SimplePowerSink(){
+void SimplePowerSink::SimplePowerSink(){
 
     this->nodePowerOut = 0.0;
     return;
 
 }
 
-SimplePowerSink::~SimplePowerSink(){
+void SimplePowerSink::~SimplePowerSink(){
 
     return;
 }
 
-/*! Loads the nodePowerOut attribute into the powerUsageSimMessage instance.
-*/
 void SimplePowerSink::evaluatePowerModel(PowerNodeUsageSimMsg *powerUsageSimMsg){
 
 
-    powerUsageSimMsg->netPower_W = this->nodePowerOut;
+    powerUsageSimMsg.netPower_W = this->nodePowerOut;
 
     return;
 }
