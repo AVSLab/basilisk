f7450b2a518c17c6ed138307cde3261c48281e25
Andrew <anha5550@colorado.edu>
2019-07-12 12:21:09 -0600

    simPowerStorageBase, simPowerNode base class / support messages done.
    Former-commit-id: f6ca7108840cc2a1737a47eaabfa0426f62633e1
    Former-commit-id: 564f630ba3bf8a6c1f1b4c30bf3cb3535c94572a

--- src/simulation/power/simplePowerMonitor/simplePowerMonitor.cpp
+++ src/simulation/power/simplePowerMonitor/simplePowerMonitor.cpp
@@ -3,24 +3,3 @@
 //
 
 #include "simplePowerMonitor.h"
-
-
-void SimplePowerMonitor::SimplePowerMonitor(){
-
-    this->storageCapacity = -1;
-    return;
-}
-
-void SimplePowerMonitor::~SimplePowerMonitor(){
-
-    return;
-}
-
-void SimplePowerMonitor::evaluateBatteryModel(PowerStorageStatusSimMsg *msg,double currentTime) {
-
-    this->storedCharge = this->storedCharge + this->currentPowerSum * (currentTime - this->previousTime);
-    msg.storageCapacity = this->storageCapacity;
-    msg.chargeLevel = this->storedCharge;
-
-    return;
-}
