2e249117b5c7fe6feef6eaf74daaa01dbefcd392
Andrew <anha5550@colorado.edu>
2019-07-19 09:04:11 -0600

    SimpleBattery initial commit. Limits stored charge based on capacity
    Former-commit-id: 0b8ea02d28bef97b1fb7df29b35c163441d5709c
    Former-commit-id: 8f9f32d7631a8dff88c83739133df792ba26d13e

--- src/simulation/power/simpleBattery/simpleBattery.cpp
+++ src/simulation/power/simpleBattery/simpleBattery.cpp
@@ -1,24 +1,9 @@
-/*
- ISC License
+//
+// Created by andrew on 7/18/19.
+//
 
- Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
-
- Permission to use, copy, modify, and/or distribute this software for any
- purpose with or without fee is hereby granted, provided that the above
- copyright notice and this permission notice appear in all copies.
-
- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
- */
 #include "simpleBattery.h"
 
-/*! The constructor creates a SimpleBattery instance with zero stored charge and a capacity of -1.*/
 SimpleBattery::SimpleBattery(){
 
     this->storageCapacity = -1;
@@ -31,16 +16,11 @@ SimpleBattery::~SimpleBattery(){
     return;
 }
 
-/*! This method integrates the current net power, and checks to see whether the integrated power falls between 0 and the battery's storageCapacity.
- @param *msg:  pointer to a PowerStorageStatusSimMsg instance
- @return void
- */
-void SimpleBattery::evaluateBatteryModel(PowerStorageStatusSimMsg *msg) {
+void SimpleBattery::evaluateBatteryModel(PowerStorageStatusSimMsg *msg,double currentTime) {
 
-    this->storedCharge = this->storedCharge + this->currentPowerSum * (this->currentTimestep*SEC2HOUR); //integrate over hours
+    this->storedCharge = this->storedCharge + this->currentPowerSum * (this->currentTimestep);
 
-
-    if(this->storedCharge > this->storageCapacity) {
+    if (this->storedCharge > this->storageCapacity) {
         this->storedCharge = this->storageCapacity;
     }
 
@@ -50,8 +30,8 @@ void SimpleBattery::evaluateBatteryModel(PowerStorageStatusSimMsg *msg) {
     }
 
     msg->storageCapacity = this->storageCapacity;
-    msg->storageLevel = this->storedCharge;
-    msg->currentNetPower = this->currentPowerSum;
+    msg->storageLevel = this->storedCharge / 60.0; //Convert into W-Hr
+    msg->currentNet2Power = this->currentPowerSum;
 
     return;
 }
--- src/simulation/power/simpleBattery/simpleBattery.h
+++ src/simulation/power/simpleBattery/simpleBattery.h
@@ -1,42 +1,10 @@
-/*
- ISC License
-
- Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
-
- Permission to use, copy, modify, and/or distribute this software for any
- purpose with or without fee is hereby granted, provided that the above
- copyright notice and this permission notice appear in all copies.
-
- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
- */
+//
+// Created by andrew on 7/18/19.
+//
 
 #ifndef BASILISK_SIMPLEBATTERY_H
 #define BASILISK_SIMPLEBATTERY_H
 
-
-#include "../_GeneralModuleFiles/simPowerStorageBase.h"
-#include "../../simMessages/powerStorageStatusSimMsg.h"
-#include "simFswInterfaceMessages/macroDefinitions.h"
-
-/*! \addtogroup SimModelGroup
- * @{
- */
-
-/*! @brief Power storage module that considers net power up to some capacity.
-
- The SimpleBattery class is a minimal model of battery functionality that considers:
-1. Integrated net input power of the attached modules
-2. The battery's maximum storage capacity as defined by the storageCapacity attribute.
-Integration of the net input power is performed with a simple Euler method. 
-
- */
 
 class SimpleBattery: public PowerStorageBase {
 
@@ -45,10 +13,10 @@ public:
     ~SimpleBattery();
 
 private:
-    void evaluateBatteryModel(PowerStorageStatusSimMsg *msg);
+    void evaluateBatteryModel(PowerStorageStatusSimMsg *msg, double currentTime);
 
 public:
-    double storageCapacity; //!< [W-hr] Battery capacity in Watt-Hours.
+    double storageCapacity;
 
 };
 
