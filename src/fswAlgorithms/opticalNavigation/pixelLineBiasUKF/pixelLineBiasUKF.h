b6f0c0cc2f09b37e76b61d6630188d76a536e82b
Thibaud Teil <thibaud.teil@colorado.edu>
2019-08-28 16:30:56 -0600

    [BSK-1403] Added more verbose description in .tex and .h, notably when it comes to the difference between the relativeOD and pixelLineFilter.
    Former-commit-id: 2933bc4b3d9b1f47906da37ce68cbdfe2d46862e
    Former-commit-id: 7cce000f8a5782d3476761e72f2cfe77b5493ee0

--- src/fswAlgorithms/opticalNavigation/pixelLineBiasUKF/pixelLineBiasUKF.h
+++ src/fswAlgorithms/opticalNavigation/pixelLineBiasUKF/pixelLineBiasUKF.h
@@ -37,7 +37,7 @@
 
 
 /*! \defgroup PixelLineBiasUKF
- @brief This module filters position measurements that have been processed from planet images in order to estimate spacecraft relative position to an observed body in the inertial frame. The filter used is an unscented Kalman filter, and the images are first processed by houghCricles and pixelLineConverter in order to produce this filter's measurements.
+ @brief This module filters position measurements that have been processed from planet images in order to estimate spacecraft relative position to an observed body in the inertial frame. It is similar to the relativeOD filter except that it estimates measurement bias in pixels and therefore integrates the pixel and line transformation in the measurement model. This means this module reads in circle data directly.
 
  The module [PDF Description](Basilisk-PixelLineBiasUKF-20190620.pdf) contains further information on this module's function,
  how to run it, as well as testing.
