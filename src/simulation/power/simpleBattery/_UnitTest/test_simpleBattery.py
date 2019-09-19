b29c8ee6be8b4725a98eae0cbf7722cdc54bbcad
Andrew <anha5550@colorado.edu>
2019-09-19 09:14:19 -0600

    Added a unit test for simpleBattery, and a docstring for the setMessage function in unitTestSupport
    Former-commit-id: cd6aab427f5a3b3e382f1a60928a7eea9cdab33e
    Former-commit-id: 84944b071ea6886c571a52cf2998feaeb7f8437b

---
+++ src/simulation/power/simpleBattery/_UnitTest/test_simpleBattery.py
@@ -0,0 +1,144 @@
+''' '''
+'''
+ ISC License
+
+ Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+'''
+
+import pytest
+import os, inspect
+import numpy as np
+import math
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+bskName = 'Basilisk'
+splitPath = path.split(bskName)
+
+# Import all of the modules that we are going to be called in this simulation
+from Basilisk.utilities import SimulationBaseClass
+from Basilisk.utilities import unitTestSupport                  # general support file with common unit test functions
+from Basilisk.simulation import simMessages
+from Basilisk.simulation import simpleBattery
+from Basilisk.utilities import macros
+
+
+
+# Uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed.
+# @pytest.mark.skipif(conditionstring)
+# Uncomment this line if this test has an expected failure, adjust message as needed.
+# @pytest.mark.xfail(conditionstring)
+# Provide a unique test method name, starting with 'test_'.
+# The following 'parametrize' function decorator provides the parameters and expected results for each
+#   of the multiple test runs for this test.
+#@pytest.mark.parametrize("useDefault", [ True, False])
+#@pytest.mark.parametrize("useMinReach", [ True, False])
+#@pytest.mark.parametrize("useMaxReach", [ True, False])
+#@pytest.mark.parametrize("usePlanetEphemeris", [ True, False])
+
+
+# update "module" in this function name to reflect the module name
+def test_module(show_plots):
+    # each test method requires a single assert method to be called
+    [testResults, testMessage] = test_storage_limits(show_plots)
+    assert testResults < 1, testMessage
+
+
+def test_storage_limits(show_plots):
+    """
+    Tests:
+    1. Whether the simpleBattery can add multiple nodes (core base class functionality);
+    2. That the battery correctly evaluates how much stored power it should have given a pair of five-watt input messages.
+    :param show_plots: Not used; no plots to be shown.
+    :return:
+    """
+
+    testFailCount = 0                       # zero unit test result counter
+    testMessages = []                       # create empty array to store test log messages
+    unitTaskName = "unitTask"               # arbitrary name (don't change)
+    unitProcessName = "TestProcess"         # arbitrary name (don't change)
+
+    # Create a sim module as an empty container
+    unitTestSim = SimulationBaseClass.SimBaseClass()
+    unitTestSim.TotalSim.terminateSimulation()
+
+    # Create test thread
+    testProcessRate = macros.sec2nano(0.1)     # update process rate update time
+    testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+
+    test_battery = simpleBattery.SimpleBattery()
+    test_battery.storedCharge = -5.0
+    test_battery.storageCapacity = 10./3600. #   10 W-s capacity.
+
+
+    powerMsg1 = simMessages.PowerNodeUsageSimMsg()
+    powerMsg1.netPower_W = 5.0
+    powerMsg2 = simMessages.PowerNodeUsageSimMsg()
+    powerMsg2.netPower_W = 5.0
+
+    unitTestSupport.setMessage(unitTestSim.TotalSim,
+                               unitProcessName,
+                               "node_1_msg",
+                               powerMsg1)
+
+    unitTestSupport.setMessage(unitTestSim.TotalSim,
+                               unitProcessName,
+                               "node_2_msg",
+                               powerMsg2)
+
+    #Test the addNodeToStorage method:
+    test_battery.addPowerNodeToModel("node_1_msg")
+    test_battery.addPowerNodeToModel("node_2_msg")
+    test_battery.batPowerOutMsgName = 'test_battery_status'
+
+    unitTestSim.AddModelToTask(unitTaskName, test_battery)
+
+    logFreq = testProcessRate
+    unitTestSim.TotalSim.logThisMessage(test_battery.batPowerOutMsgName,logFreq)
+
+    unitTestSim.InitializeSimulationAndDiscover()
+    unitTestSim.ConfigureStopTime(macros.sec2nano(5.0))
+
+    unitTestSim.ExecuteSimulation()
+
+    storedChargeLog = unitTestSim.pullMessageLogData(test_battery.batPowerOutMsgName+".storageLevel")
+    capacityLog = unitTestSim.pullMessageLogData(test_battery.batPowerOutMsgName+".storageCapacity")
+    netPowerLog= unitTestSim.pullMessageLogData(test_battery.batPowerOutMsgName+".currentNetPower")
+
+    #   Check 1 - is net power equal to 10.?
+    for ind in range(0,len(netPowerLog)):
+        currentPower = netPowerLog[ind,1]
+        if currentPower < 10.:
+            testFailCount +=1
+            testMessages.append("FAILED: SimpleBattery did not correctly log the net power.")
+
+    for ind in range(0,len(storedChargeLog)):
+        if storedChargeLog[ind,1] > capacityLog[ind,1]:
+            testFailCount +=1
+            testMessages.append("FAILED: SimpleBattery's stored charge exceeded its capacity.")
+
+        if storedChargeLog[ind,1] < 0.:
+            testFailCount +=1
+            testMessages.append("FAILED: SimpleBattery's stored charge was negative.")
+
+
+    # each test method requires a single assert method to be called
+    # this check below just makes sure no sub-test failures were found
+    return [testFailCount, ''.join(testMessages)]
+
+if __name__ == "__main__":
+    print(test_module(False))
