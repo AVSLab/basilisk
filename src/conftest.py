0d251ec816247d8124762fd27f0e6d448c7d7314
Scott Carnahan <scott.carnahan@colorado.edu>
2019-10-08 12:59:41 -0400

    [BSK-1500] Include figures in report
    Former-commit-id: 8b2adc21e3ddc4c0c7da118adf58429e89f80b66
    Former-commit-id: 52ec420dfe6c44123178935149c86db2a91edde4

--- src/conftest.py
+++ src/conftest.py
@@ -18,6 +18,15 @@
 
 '''
 import pytest
+import matplotlib.pyplot as plt
+import os
+import shutil
+testFigsDir = './assets/testFigs/'
+if os.path.exists(testFigsDir):
+    shutil.rmtree(testFigsDir)
+    os.makedirs(testFigsDir)
+else:
+    os.makedirs(testFigsDir)
 
 def pytest_addoption(parser):
     parser.addoption("--show_plots", action="store_true",
@@ -28,13 +37,46 @@ def pytest_addoption(parser):
 def show_plots(request):
     return request.config.getoption("--show_plots")
 
+def get_test_name(item):
+    ugly_name = str(item.function)
+    less_ugly_name = ugly_name.strip('<function')
+    beautiful_name = less_ugly_name.split(' at')[0]
+    return beautiful_name
+
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
+    """
+        We use this function to do two things:
+        1) append the docstrings to the test log
+        3) print test plots to the test log
+    """
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
+    
+    # add the doc string
     docstring = '<tt>' + str(item.function.__doc__).replace('\n', '</br>', ) + '</tt>'
     extra.append(pytest_html.extras.html(docstring))
+    
+    # save the figures
+    dir_name = testFigsDir + get_test_name(item)
+    i = 0
+    if len(plt.get_fignums()) > 0:
+        while i > -1:
+            dir_name_num = dir_name + '_' + str(i) + '/'
+            if not os.path.exists(dir_name_num):
+                os.mkdir(dir_name_num)
+                break
+            else:
+                i += 1
+                continue
+    for f in plt.get_fignums():
+        filename = dir_name_num + 'figure_' + str(f) + '.png'
+        plt.figure(f).savefig(filename)
+        plt.close(f)
+        extra.append(pytest_html.extras.png(filename))
     report.extra = extra
+
 
