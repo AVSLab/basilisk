f39ce8c16c1bef005d37e858c347eec39e09af15
Scott Carnahan <scott.carnahan@colorado.edu>
2019-10-11 10:06:34 -0400

    [BSK-1500] now pytest still works if you don't have pytest-html
    it checks if you have the package installed before it defines the functions that write the test data out. will still fail if you try to ask it for a report, but that's on the user.
    
    Former-commit-id: 7f3b82abfebccc5392cdab65313faa990d1fe869
    Former-commit-id: 24572befd776f80f1ee2369f031313979c5923d4

--- src/conftest.py
+++ src/conftest.py
@@ -23,13 +23,17 @@ import matplotlib.pyplot as plt
 import os
 import shutil
 import sys
+import subprocess
 import time
 
-
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+make_report = False
+if 'pytest-html' in installed_packages:
+    make_report = ('--report' in sys.argv)
 report_dir = 'tests/report/'  # relative to src/
 rel_fig_dir = 'assets/testFigs/'  # relative to report/
 testFigsDir = report_dir + rel_fig_dir  # can assume this is run from basilisk/src/
-make_report = ('--report' in sys.argv)
 
 # remove the old report because we don't want stale data around
 if os.path.exists(report_dir):
@@ -41,6 +45,11 @@ if make_report:
 def pytest_cmdline_preparse(config, args):
     # this is a pytest function we can use to adjust args
     # we want to convert --report into --html=<pathToReport>
+    # we don't want the user to be able to call --html=<pathToReport> directly,
+    # because we have hardcoded some paths to save figures etc and --report is cleaner
+    if '--html' in args:
+        print('Please use --report with no arguments instead of --html and try again')
+        quit()
     if '--report' in args:
         args.remove('--report')
         html_file = report_dir + 'BasiliskTestReport.html'
@@ -51,7 +60,7 @@ def pytest_cmdline_preparse(config, args):
 def pytest_addoption(parser):
     parser.addoption("--show_plots", action="store_true",
                      help="test(s) shall display plots")
-    parser.addoption("--report", action="store_true",  # --report is easier and more controlled than --html=etc.
+    parser.addoption("--report", action="store_true",  # --report is easier, more controlled than --html=<pathToReport>
                      help="whether or not to gen a pytest-html report. "
                           "The report is saved in src/tests/report")
 
@@ -76,63 +85,64 @@ def do_report():  # this is necessary to get make_report into the pytest_runtest
     return make_report
 
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    """
-        We (Basilisk) use this function to do two things:
-        1) append the docstrings to the test log
-        2) print test plots to the test log
+if make_report:  # only do this if the report is requested AND pytest-html is installed.
+    @pytest.hookimpl(hookwrapper=True)
+    def pytest_runtest_makereport(item, call):
+        """
+            We (Basilisk) use this function to do two things:
+            1) append the docstrings to the test log extras
+            2) print test plots to the test log extras
 
-        This is kept neat by inserting a table into the extras to separate the two things.
-    """
-    pytest_html = item.config.pluginmanager.getplugin('html')
-    outcome = yield
-    if do_report:  # don't save pictures etc. if not making a report
-        report = outcome.get_result()
-        extra = getattr(report, 'extra', [])
+            This is kept neat by inserting a table into the extras to separate the two things.
+        """
+        pytest_html = item.config.pluginmanager.getplugin('html')
+        outcome = yield
+        if do_report:  # don't save pictures etc. if not making a report
+            report = outcome.get_result()
+            extra = getattr(report, 'extra', [])
 
-        # make the table *within* the extras cell with a diff row per docstring/figures
-        # the extra <div> throughout this function are to stop pytest_html from making everything its own div
-        extra.append(pytest_html.extras.html('</div><table><tr><td><div>'))
-        # add the doc string
-        extra.append(pytest_html.extras.html(get_docstring(item)))
-        extra.append(pytest_html.extras.html('</div></td></tr><tr><div>'))
+            # make the table *within* the extras cell with a diff row per docstring/figures
+            # the extra <div> throughout this function are to stop pytest_html from making everything its own div
+            extra.append(pytest_html.extras.html('</div><table><tr><td><div>'))
+            # add the doc string
+            extra.append(pytest_html.extras.html(get_docstring(item)))
+            extra.append(pytest_html.extras.html('</div></td></tr><tr><div>'))
 
-        # save the figures
-        dir_name = testFigsDir + get_test_name(item)
-        i = 0
-        if len(plt.get_fignums()) > 0:
-            while i > -1:  # this loops makes a numbered directory per run of the same test to avoid overwrite
-                dir_name_num = dir_name + '_' + str(i) + '/'
-                if not os.path.exists(dir_name_num):
-                    os.makedirs(dir_name_num)
-                    break
-                else:
-                    i += 1
-                    continue
+            # save the figures
+            dir_name = testFigsDir + get_test_name(item)
+            i = 0
+            if len(plt.get_fignums()) > 0:
+                while i > -1:  # this loops makes a numbered directory per run of the same test to avoid overwrite
+                    dir_name_num = dir_name + '_' + str(i) + '/'
+                    if not os.path.exists(dir_name_num):
+                        os.makedirs(dir_name_num)
+                        break
+                    else:
+                        i += 1
+                        continue
 
-            for f in plt.get_fignums():
-                if not os.path.exists(dir_name_num):
-                    time.sleep(.02)
-                filename = dir_name_num + 'figure_' + str(f) + '.png'  # expect cwd to be src/
-                plt.figure(f).savefig(filename)
-                plt.close(f)  # close figures so test writers don't have to
-                img_src = 'assets' + filename.split('assets')[1]  # just want a relative (to report) path here
-                extra.append(pytest_html.extras.html('</div><td><div class="image"><a href="' + img_src +
-                                                     '"><img src="' + img_src + '"/></a></div></td><div>'))
-        else:
-            extra.append(pytest_html.extras.html('<tt> This test has no images.</br></tt>'))
-        extra.append(pytest_html.extras.html('</div></tr></table><div>'))  # this finishes off our fancy html table
-        report.extra = extra
-
-
-def pytest_html_results_table_header(cells):
-    # remove the "links" column from the report
-    cells.pop()
+                for f in plt.get_fignums():
+                    if not os.path.exists(dir_name_num):
+                        time.sleep(.02)
+                    filename = dir_name_num + 'figure_' + str(f) + '.png'
+                    plt.figure(f).savefig(filename)
+                    plt.close(f)  # close figures so test writers don't have to. prevents saving same image multiple times
+                    img_src = 'assets' + filename.split('assets')[1]  # just want a relative (to report) path here
+                    extra.append(pytest_html.extras.html('</div><td><div class="image"><a href="' + img_src +
+                                                         '"><img src="' + img_src + '"/></a></div></td><div>'))
+            else:
+                extra.append(pytest_html.extras.html('<tt> This test has no images.</br></tt>'))
+            extra.append(pytest_html.extras.html('</div></tr></table><div>'))  # this finishes off our fancy html table
+            report.extra = extra
 
 
-def pytest_html_results_table_row(report, cells):
-    # remove the "links column from the report
-    cells.pop()
+    def pytest_html_results_table_header(cells):
+        # remove the "links" column from the report
+        cells.pop()
+
+
+    def pytest_html_results_table_row(report, cells):
+        # remove the "links column from the report
+        cells.pop()
 
 
