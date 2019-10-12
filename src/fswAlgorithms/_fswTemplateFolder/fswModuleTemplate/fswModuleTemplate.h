590475dded3dc5e3d656d46b749eec3bd979e781
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-10-12 13:29:14 -0600

    [BSK-1505] included all the module documentation guidelines from the module PDF to the module *.h file doxygen description
    Former-commit-id: 82f329347e54b2f92827037cc8b1906b2a96fb3e
    Former-commit-id: bd696d67c032a711bc322dab60fdc7f051957ba0

--- src/fswAlgorithms/_fswTemplateFolder/fswModuleTemplate/fswModuleTemplate.h
+++ src/fswAlgorithms/_fswTemplateFolder/fswModuleTemplate/fswModuleTemplate.h
@@ -27,10 +27,89 @@
 
 
 /*! \defgroup fswModuleTemplate
- * @brief This is a template module for FSW algorithms in ANSI-C.  Make a copy of this module and change the
- * file names to match the new module you are creating.  More information can be found
- * in the [PDF Description](Basilisk-MODULENAME-yyyymmdd.pdf).
- * @{
+  @brief This is a template module for FSW algorithms in ANSI-C.
+
+ # Module Purpose
+ ## Executive Summary
+    Provide a brief introduction to purpose and intent of this module.
+
+ ## Module Assumptions and Limitations
+    This section should describe the assumptions used in formulating the mathematical model and how those assumptions limit the usefulness of the module.
+
+ ## Message Connection Descriptions
+    Input Message | Msg Type | Description
+    --------------|----------|-------------
+    dataInMsgName|FswModuleTemplateFswMsg | Input message description.  Note here if this message is optional, and what the default behavior is if this message is not provided.
+
+    Output Message | Msg Type | Description
+    --------------|----------|-------------
+    dataOutMsgName |  FswModuleTemplateFswMsg | Output message description.
+
+
+ # Detailed Module Description
+    Provide a brief introduction to the material being discussed in this report.  For example, include what the motivation is, maybe provide a supportive figure such as shown below, reference earlier work if needed in a literature review web links. Describe the module including mathematics, implementation, etc.
+
+ ## Equations
+    Equations can be provided with LaTeX as well.  For example, the code
+
+    `\f$ a = b^{2} \f$`
+
+    produces this equation
+
+    \f$ a = b^{2} \f$
+
+    If the module description requires extensive math discussion, this can be TeX'd up inside a module `_Documentation` folder and a link to the PDF can be provided.  Don't forget to update `docs/DoxyData` to include the path to this PDF file.
+
+ ## Citations
+    If you want to cite other papers or text, provide a web link to a paper.  For example,
+
+    `[The link text](http://example.net/)`
+
+    creates [The link text](http://example.net/).
+
+ ## Figures
+    To include figures, you must copy the web compatible image (jpg, png, svg) to the `docs/Images/doc` folder, preferably into a module specific sub-folder.  This keeps the modules images grouped within this sub-folder. For example, you can use code such as
+
+    `\image html Images/doc/fswTemplateModule/Fig1.jpg "Sample Figure Illustration" width=500px`
+
+    to generate the image such as
+
+    \image html Images/doc/fswTemplateModule/Fig1.jpg "Sample Figure Illustration" width=500px
+
+    Note that with pixelated figures providing a width value that is at least half that of the actual images the figure will look good even on a high resolution display.
+
+ ## Tables
+    The standard Doxygen table formating can be used to generate tables.  More info on tables is found [here](http://www.doxygen.nl/manual/markdown.html#md_tables).  For example, the code
+
+    ~~~~~~~
+    Animal | Description | Price ($)
+    -------|-------------|----------
+    Gnat   | per gram | 13.65
+    Gnu    | stuffed  | 92.50
+    Emu    | stuffed  | 33.33
+    Armadillo | frozen  |  8.99
+    ~~~~~~~
+
+    generates this table
+
+    Animal | Description | Price ($)
+    -------|-------------|----------
+    Gnat   | per gram | 13.65
+    Gnu    | stuffed  | 92.50
+    Emu    | stuffed  | 33.33
+    Armadillo | frozen  |  8.99
+
+ # User Guide
+    This section contains information directed specifically to users. It contains clear descriptions of what inputs are needed and what effect they have. It should also help the user be able to use the model for the first time.
+
+    Add sample code as needed.  For example, to specify that the module variables `dummy` and `dumVector` must be setup first, you can include python formatted code using:
+    ~~~~~~~{.py}
+    moduleConfig.dummy = 1
+    moduleConfig.dumVector = [1., 2., 3.]
+    ~~~~~~~
+
+    In the user guide, provide sub-sections as need to help explain how to use this module, list what variables must be set, discuss variables that might have default values if not specified by the user, etc.
+  @{
  */
 
 /*! @brief Top level structure for the sub-module routines. */
