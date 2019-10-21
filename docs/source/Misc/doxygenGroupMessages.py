f232dee3049d9ed2430b4f66014e4920d804df6e
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-10-21 18:07:54 -0400

    [BSK-1480] removed doxygen related support files, no longer needed
    Former-commit-id: dd8c27357ee9e26d8c0bb7a94f546b3db59e391e
    Former-commit-id: ac6ba862932a057553360f7bf3e250bce3f681c3

--- docs/source/Misc/doxygenGroupMessages.py
+++
@@ -1,9 +0,0 @@
-## \defgroup Messages  Message Definitions
-# @brief Folder with Basilisk message definitions.
-## @{
-#  \defgroup fswMessages fswMessages
-#  \defgroup simFswInterfaceMessages simFswInterfaceMessages
-#  \defgroup simMessages simMessages
-#
-## @}
-
