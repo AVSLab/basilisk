4b76ba0b3f8187ffd4c5dc716f1991d25bfa18de
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-10-21 09:01:05 -0400

    [BSK-1481] apply Demet's patch 2
    Former-commit-id: e5e510524c34e4c040da2a935c7d231f6c33e02b
    Former-commit-id: 16ceb9ca0fb856ce9e755b5338263db042540d9a

--- src/tests/scenarios/scenarioTAM.py
+++ src/tests/scenarios/scenarioTAM.py
@@ -277,7 +277,7 @@ def run(show_plots, orbitCase, planetCase, useBias, useBounds):
     TAM.scaleFactor = 1.0
     TAM.tamDataOutMsgName = "TAM_output"
     TAM.stateIntMsgName = scObject.scStateOutMsgName
-    TAM.senNoiseStd = 0
+    TAM.senNoiseStd = 100e-9
     if useBias:
         TAM.senBias = [0, 0, -1e-6]  # Tesla
     if useBounds:
