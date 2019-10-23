41999606ac4488f26a4b4958e9ab4e74fadd358b
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-10-23 18:31:11 -0400

    [HOT-FIX] some quick typo fixes
    Former-commit-id: ab1a1470d6cf798a311b0324742d4fe4262a3190
    Former-commit-id: 4609922568360f1c419d73eca8560c3a7dafc73d

--- src/fswAlgorithms/opticalNavigation/relativeODuKF/relativeODuKF.h
+++ src/fswAlgorithms/opticalNavigation/relativeODuKF/relativeODuKF.h
@@ -43,14 +43,14 @@
  * @{
  */
 
-/*! @breif Structure to gather the OpNav messages and content */
+/*! @brief Structure to gather the OpNav messages and content */
 typedef struct {
     char opNavInMsgName[MAX_STAT_MSG_LENGTH];    //!< [-] Input message buffer from opNav measurement method
     int32_t opNavInMsgId;                        //!< [-] Input message Id from opNav measurement method
     double noise[3*3];                        //!< [-] Per axis noise on the measurement
 }OpNavMeas;
 
-/*! @breif Structure to gather the OpNav messages and content */
+/*! @brief Structure to gather the OpNav messages and content */
 typedef struct {
     int numMethods;                                  //!< Number of opNav measurement methods
     OpNavMeas OpNav[MAX_ST_VEH_COUNT];     //!< [-] Decoded data for both measurement methods
