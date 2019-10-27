ed3436439db0636490c2ddbb8f11aba8f3966a40
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-10-27 08:55:32 -0600

    [BSK-1514] fixed headers in the doxygen documentation
    Former-commit-id: 1a5acd9bee8291c4f98e43c79332e88c3b1dcc6b
    Former-commit-id: 5267dec00e554aa5121f9b74bd50eee308f25e8f

--- src/simulation/environment/eclipseEffect/eclipseEffect.h
+++ src/simulation/environment/eclipseEffect/eclipseEffect.h
@@ -30,32 +30,32 @@
 
 /*! @brief Apply the input eclipse shadow factor
 
- # Module Purpose
- ## Executive Summary
+ ## Module Purpose
+ ### Executive Summary
     This module takes in a solar flux message and an eclipse message. It modifies the flux message data and outputs the
     modified values to the same flux message.
 
- ## Module Assumptions and Limitations
+ ### Module Assumptions and Limitations
     This model is limited to a single pair of inputs and single output.
     It is assumed that the specific solar flux and eclipse messages are for the same spacecraft
     at the same time and position.
 
- ## Message Connection Descriptions
+ ### Message Connection Descriptions
     The following table lists all the module input and output messages.  The module msg variable name is set by the user from python.  The msg type contains a link to the message structure definition, while the description provides information on what this message is used for.
     Msg Variable Name | Msg Type | Description
     ------------------|----------|-------------
     solarFluxInOutMsgName| SolarFluxSimMsg | This message is read to get the non-eclipse-modified solar flux value. the modified flux value is written to the same message
     eclpseInMsgName |  EclipseSimMsg | This message is used to get the eclipse message which contains a shadow factor to modify the solar flux.
 
- # Detailed Module Description
+ ## Detailed Module Description
    Use of this module should be preferred over other modules individually applying eclipse conditions.
 
- ## Equations
+ ### Equations
     The distance is the norm of the vector from the spacecraft to the sun
 
     \f$ F_{out} = F_{in} * f_{eclipse} \f$
 
- # User Guide
+ ## User Guide
     The user can only instantiate this module, change the i/o names, and add it to a task.
     The names below are only special in that they are useful defaults.
     ~~~~~~~{.py}
