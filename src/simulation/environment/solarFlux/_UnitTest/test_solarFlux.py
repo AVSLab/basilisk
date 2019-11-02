4fbb15dc81ef6e33fa7fd0a841e00d8778129734
Scott Carnahan <scott.carnahan@colorado.edu>
2019-11-02 13:34:41 -0400

    [BSK-1522] fixing merge issues
    Former-commit-id: f2e61071bb5f68ece4aa6eb619d2f287d0b2e558
    Former-commit-id: 001ac833245eedaddcc54ea3c0ab384a1f58a3ee

--- src/simulation/environment/solarFlux/_UnitTest/test_solarFlux.py
+++ src/simulation/environment/solarFlux/_UnitTest/test_solarFlux.py
@@ -22,9 +22,6 @@
 import numpy as np
 import pytest
 
-import numpy as np
-import pytest
-
 from Basilisk.simulation import solarFlux
 from Basilisk.simulation.simMessages import SpicePlanetStateSimMsg
 from Basilisk.simulation.simMessages import SCPlusStatesSimMsg
