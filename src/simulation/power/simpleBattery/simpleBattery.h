03e550a445fcdb0ef99d2698f476729ce7c96109
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-11-02 21:05:02 -0600

    [BSK-1480] update simplePower documentation
    Former-commit-id: 6d12f49a0e4cc891ff65e966d4972b5831eb948a
    Former-commit-id: ad584ec8902307bf3b8aaae9b256f64d546bbc8d

--- src/simulation/power/simpleBattery/simpleBattery.h
+++ src/simulation/power/simpleBattery/simpleBattery.h
@@ -25,50 +25,46 @@
 #include "../../simMessages/powerStorageStatusSimMsg.h"
 #include "simFswInterfaceMessages/macroDefinitions.h"
 
-/*! \addtogroup SimModelGroup
- * @{
- */
+/*!
+\verbatim embed:rst
 
-/*! @brief Power storage module that considers net power up to some capacity.
-
- ## Module Purpose
- ### Executive Summary
+ Executive Summary
     The SimpleBattery class is a minimal model of battery functionality that considers:
     1. Integrated net input power of the attached modules
-    2. The battery's maximum storage capacity as defined by the `storageCapacity` attribute.
+    2. The battery's maximum storage capacity as defined by the ``storageCapacity`` attribute.
     Integration of the net input power is performed with a simple Euler method.
-    \f[
-         W_{stored} = \dot{W}_{net} (t_{current} - t_{previous})
-    \f]
 
- ### Module Assumptions and Limitations
-    See PowerStorageBase class for inherited assumption and limitations.  The SimpleBattery class assumes that the net energy storage amount is a fixed value.
+         :math:`W_{stored} = \dot{W}_{net} (t_{current} - t_{previous})`
 
- ### Message Connection Descriptions
-    This module only uses the input and output messages of the PowerStorageBase base class.
 
- ## User Guide
-    To set up this module users must create a simpleBattery instance.
-    ~~~~~~~~~~{.py}
-    battery = simpleBattery.SimpleBattery()
-    battery.ModelTag = "batteryModel"
-    ~~~~~~~~~~
+ Module Assumptions and Limitations
+    See :ref:`PowerStorageBase` class for inherited assumption and limitations.  The SimpleBattery class assumes that the net energy storage amount is a fixed value.
 
-    In addition to the variables that must be set for the PowerNodeBase base class, this module requires the `storageCapacity` attribute to be specified.  The total power stored in the battery will be limited to not exceed this capacity value.
-    ~~~~~~~~~~{.py}
-    battery.storageCapacity = 10.0 # Given in Joules or Watt-seconds
-    ~~~~~~~~~~
-    The next step is to attach one or more PowerNodeUsageSimMsg instances to it using the `addNodeToStorage()` method.
-    ~~~~~~~~~~{.py}
-    battery.addPowerNodeToModel("msg name")
-    ~~~~~~~~~~
-    The final step is to specify the output message name.
-    ~~~~~~~~~~{.py}
-    battery.batPowerOutMsgName= "outMsgName"
-    ~~~~~~~~~~
+ Message Connection Descriptions
+    This module only uses the input and output messages of the :ref:`PowerStorageBase` base class.
 
-    For more information on how to set up and use this module, see the simple power system example: @ref scenarioSimplePowerDemo
+ User Guide
+    To set up this module users must create a SimpleBattery instance::
 
+        battery = simpleBattery.SimpleBattery()
+        battery.ModelTag = "batteryModel"
+
+    In addition to the variables that must be set for the :ref:`PowerStorageBase` base class, this module requires the ``storageCapacity`` attribute to be specified.  The total power stored in the battery will be limited to not exceed this capacity value::
+
+        battery.storageCapacity = 10.0 # Given in Joules or Watt-seconds
+
+    The next step is to attach one or more :ref:`PowerNodeUsageSimMsg` instances to it using the ``addNodeToStorage()`` method::
+
+        battery.addPowerNodeToModel("msg name")
+
+    The final step is to specify the output message name::
+
+        battery.batPowerOutMsgName= "outMsgName"
+
+
+    For more information on how to set up and use this module, see the simple power system example :ref:`scenarioSimplePowerDemo`.
+
+ \endverbatim
  */
 
 class SimpleBattery: public PowerStorageBase {
