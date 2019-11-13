dd2297ae591baf82046c132e11150cddddf146a3
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-11-13 05:32:15 -0700

    [BSK-1480] added other build paths for convenience in documentation development and debugging
    Former-commit-id: 5ac1dc8bdbdd9ad8b1c20006bb565080b2aa6642
    Former-commit-id: 70eac3552c35f5cd7bbb244e0221a0f29a5d99f4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -455,8 +455,11 @@ if rebuild:
     if os.path.exists(officialDoc):
         shutil.rmtree(officialDoc)
     # adjust the fileCrawler path to a local folder to just build a sub-system
-    breathe_projects_source = fileCrawler.run("../../src/") # When I run this, scenario finds the wrong doc_string
-    # breathe_projects_source = fileCrawler.run("../../src/simulation/power") # When I run this, scenario finds the wrong doc_string
+    breathe_projects_source = fileCrawler.run(officialSrc)
+    # breathe_projects_source = fileCrawler.run(officialSrc+"/fswAlgorithms/transDetermination")
+    # breathe_projects_source = fileCrawler.run(officialSrc+"/simulation/viz_interface")
+    # breathe_projects_source = fileCrawler.run(officialSrc+"/examples")
+    # breathe_projects_source = fileCrawler.run(officialSrc+"/utilities")
     with open("breathe.data", 'wb') as f:
         pickle.dump(breathe_projects_source, f)
 else:
