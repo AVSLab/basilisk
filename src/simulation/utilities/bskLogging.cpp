5be5ff005e375c63ea83e3551c34f4dc246d7ec9
Max Schwarz <max.schwarz@colorado.edu>
2019-11-30 17:26:37 -0700

    Changed name of bsklogging file
    Former-commit-id: 255fd4925642157b23d333e549a52480dbb2c425
    Former-commit-id: 72ccc07257a37f144703a0077b59e0af2c7e93d5

---
+++ src/simulation/utilities/bskLogging.cpp
@@ -0,0 +1,97 @@
+/*
+ ISC License
+
+ Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "utilities/bskLogging.h"
+
+logLevel_t LogLevel = DEBUG;
+
+logLevel_t getLogLevel()
+{
+  return LogLevel;
+}
+
+/*! The constructor initialies the logger for a module and uses default verbostiy level for logging */
+BSKLogger::BSKLogger()
+{
+  //Default print verbosity
+  this->_logLevel = getLogLevel();
+}
+
+/*! The constructor initialies the logger for a module and uses a user defined verbostiy level for logging */
+BSKLogger::BSKLogger(logLevel_t logLevel)
+{
+  this->_logLevel = logLevel;
+}
+
+/*! This method changes the logging verbosity after construction */
+void BSKLogger::setLogLevel(logLevel_t logLevel)
+{
+  this->_logLevel = logLevel;
+}
+
+/*! This method reads the current logging verbosity */
+void BSKLogger::printLogLevel()
+{
+  const char* currLevelStr = this->logLevelMap[this->_logLevel];
+  printf("Current Message Level: %s\n", currLevelStr);
+}
+
+/*! This method logs information. The current behavior is to simply print out the message and the targeted logging level.
+    This should be the main method called in user code.
+*/
+void BSKLogger::bskLog(logLevel_t targetLevel, const char* info, ...)
+{
+  if(targetLevel >= this->_logLevel)
+  {
+    const char* targetLevelStr = this->logLevelMap[targetLevel];
+    char formatMessage[MAX_LOGGING_LENGTH];
+    va_list args;
+    va_start (args, info);
+    vsnprintf(formatMessage, sizeof(formatMessage), info, args);
+    printf("Message Level: %s, Message: %s\n", targetLevelStr, formatMessage);
+  }
+}
+
+/*! Section contains C interfacing to C++ object */
+EXTERN BSKLogger* _BSKLogger(void)
+{
+    return new BSKLogger();
+}
+
+EXTERN void _BSKLogger_d(BSKLogger* bskLogger)
+{
+    delete bskLogger;
+}
+
+EXTERN void _readLogLevel(BSKLogger* bskLogger)
+{
+    bskLogger->readLogLevel();
+}
+
+EXTERN void _setLogLevel(BSKLogger* bskLogger, logLevel_t logLevel)
+{
+    bskLogger->setLogLevel(logLevel);
+}
+
+EXTERN void _bskLog(BSKLogger* bskLogger, logLevel_t logLevel, const char* info)
+{
+    bskLogger->bskLog(logLevel, info);
+}
---
+++ src/simulation/utilities/bskLogging.h
@@ -0,0 +1,82 @@
+/*
+ ISC License
+
+ Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef _BSK_LOG_
+#define _BSK_LOG_
+
+//maximum length of info to log in a reference to BSKLogging in C, not relevant in C++
+#define MAX_LOGGING_LENGTH = 255;
+
+typedef enum {
+    DEBUG,
+    INFORMATION,
+    WARNING,
+    ERROR,
+    SILENT          // the coder should never use this flag when using BSK_PRINT().  It is used to turn off all BSK_PRINT()
+} logLevel_t;
+
+extern logLevel_t LogLevel;
+
+#ifdef __cplusplus
+#include <map>
+#include <string>
+
+logLevel_t getLogLevel();
+
+class BSKLogger
+{
+  public:
+    BSKLogger();
+    BSKLogger(logLevel_t logLevel);
+    virtual ~BSKLogger() = default;
+    void setLogLevel(logLevel_t logLevel);
+    void printLogLevel();
+    void bskLog(logLevel_t targetLevel, const char* info, ...);
+
+  //Provides a mapping from log level enum to str
+  public:
+    std::map<int, const char*> logLevelMap
+    {
+      {0, "DEBUG"},
+      {1, "INFORMATION"},
+      {2, "WARNING"},
+      {3, "ERROR"},
+      {4, "SILENT"}
+    };
+
+  private:
+    logLevel_t _logLevel;
+};
+
+#else
+typedef struct BSKLogger BSKLogger;
+#endif
+
+#ifdef __cplusplus
+    #define EXTERN extern "C"
+#else
+    #define EXTERN
+#endif
+
+EXTERN BSKLogger* _BSKLogger(void);
+EXTERN void _BSKLogger_d(BSKLogger*);
+EXTERN void _readLogLevel(BSKLogger*);
+EXTERN void _setLogLevel(BSKLogger*, logLevel_t);
+EXTERN void _bskLog(BSKLogger*, logLevel_t, const char*);
+#endif
