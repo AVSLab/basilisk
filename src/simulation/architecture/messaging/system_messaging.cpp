255a99e195c04f14c6cdf8a246359118754f058b
andrew <andrew.t.harris@colorado.edu>
2019-12-02 16:35:19 -0700

    SystemMessaging error message enhancements.
    Former-commit-id: 772e24b87545f51c40d62252672ced4e6e170b3e
    Former-commit-id: f1e906eef1ea463997648f4a26958665f5fd20d4

--- src/simulation/architecture/messaging/system_messaging.cpp
+++ src/simulation/architecture/messaging/system_messaging.cpp
@@ -450,7 +450,7 @@ bool SystemMessaging::WriteMessage(int64_t MessageID, uint64_t ClockTimeNanos,
     // Check the message size
     if(MsgSize != MsgHdr->MaxMessageSize)
     {
-        BSK_PRINT_BRIEF(MSG_ERROR, "Received a write request from module %" PRId64" that was incorrect size for: %s. ( %" PRId64" instead of %" PRId64".)sYou get nothing.",
+        BSK_PRINT_BRIEF(MSG_ERROR, "Received a write request from module %d that was incorrect size for: %s. ( %d instead of %d.)sYou get nothing.",
                   moduleID, MsgHdr->MessageName, MsgSize, MsgHdr->MaxMessageSize);
         this->WriteFails++;
         return(false);
