94f55c2e717eed817b093666ac3b079ba56ad543
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2019-12-09 09:07:14 -0700

    [BSK-1246] added a test function for scenarioBskLog
    Former-commit-id: 35e52149f57dff6844c0811a68c8e043288ec9ce
    Former-commit-id: 25441c6b417a7fe0e007f6ee36e22def8c58d54b

---
+++ src/tests/test_scenarioBskLog.py
@@ -0,0 +1,94 @@
+
+# ISC License
+#
+# Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+
+
+#
+# Basilisk Scenario Script and Integrated Test
+#
+# Purpose:  Integrated test of the spacecraftPlus() and gravity modules.  Illustrates
+#           a 3-DOV spacecraft on a range of orbit types.
+# Author:   Hanspeter Schaub
+# Creation Date:  Nov. 26, 2016
+#
+
+import sys, os, inspect
+import pytest
+from Basilisk.utilities import unitTestSupport
+
+# Get current file path
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+sys.path.append(path + '/../examples')
+import scenarioBskLog
+
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+
+# uncomment this line if this test has an expected failure, adjust message as needed
+# @pytest.mark.xfail(True, reason="Previously set sim parameters are not consistent with new formulation\n")
+
+
+# The following 'parametrize' function decorator provides the parameters and expected results for each
+#   of the multiple test runs for this test.
+@pytest.mark.parametrize("case", [
+    (0),
+    (1),
+    (2)
+])
+@pytest.mark.scenarioTest
+
+def test_scenarioBasicOrbit(show_plots, case):
+    '''This function is called by the py.test environment.'''
+    # each test method requires a single assert method to be called
+    # provide a unique test method name, starting with test_
+
+    testFailCount = 0                       # zero unit test result counter
+    testMessages = []                       # create empty array to store test log messages
+
+    level = scenarioBskLog.run(case)
+
+    # setup truth data for unit test
+    if case == 0:
+        trueLevel = 0
+    elif case == 1:
+        trueLevel = 2
+    elif case == 2:
+        trueLevel = 3
+    else:
+        print("Incorrect case being tested")
+        exit(1)
+
+    if level != trueLevel:
+        testFailCount += 1
+        testMessages += "bskLog test returned an incorrect value"
+
+    #   print out success message if no error were found
+    if testFailCount == 0:
+        print("PASSED ")
+    else:
+        print(testFailCount)
+        print(testMessages)
+
+    # each test method requires a single assert method to be called
+    # this check below just makes sure no sub-test failures were found
+
+    assert testFailCount < 1, testMessages
+
