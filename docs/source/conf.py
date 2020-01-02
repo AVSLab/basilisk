bfdc11b0b4cc08c0585c71ed92e63e66c8244cac
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-01-01 18:29:36 -0700

    [BSK-1569] only make the "Module: " prefix appear in front of actual BSK modules, not utilities, msg definitions, etc.
    Former-commit-id: 046fee3223aae48e1860572fc917a4c1a4a5c1aa
    Former-commit-id: 336bfdc3b07e73fe80608d243a798801f4c43edd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -370,7 +370,13 @@ class fileCrawler():
                 module_files_temp = []
                 lines = ""
                 lines += ".. _" + c_file_basename + ":\n\n"
-                lines += "Module: " + c_file_basename + "\n" + "=" * (len(c_file_basename) + 8) + "\n\n"
+                if "fswMessages" in src_path \
+                        or "simFswInterfaceMessages" in src_path \
+                        or "simMessages" in src_path\
+                        or "utilities" in src_path:
+                    lines += c_file_basename + "\n" + "=" * (len(c_file_basename) + 8) + "\n\n"
+                else:
+                    lines += "Module: " + c_file_basename + "\n" + "=" * (len(c_file_basename) + 8) + "\n\n"
 
                 # pull in the module documentation file if it exists
                 docFileName = os.path.join(src_path, c_file_basename + '.rst')
