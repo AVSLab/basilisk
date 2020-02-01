041ee2037bea13e5c2fdbf9251177835732beb14
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-01-31 21:10:28 -0700

    [BSK-1590] update thruster attitude scenario to run the enableUnityViz setting (without saving to file) to cover more code in this test
    Former-commit-id: ddd250aa3dbea3343bdfcc80292c1591ae1d04bf
    Former-commit-id: a7a8e5ed54d0fc4cd56549b3bb8788c13ea7057b

--- src/examples/scenarioAttitudeFeedback2T_TH.py
+++ src/examples/scenarioAttitudeFeedback2T_TH.py
@@ -671,7 +671,14 @@ def run(show_plots, useDVThrusters):
     scObject.hub.omega_BN_BInit = [[0.001], [-0.01], [0.03]]  # rad/s - omega_BN_B
 
     # if this scenario is to interface with the BSK Viz, uncomment the following lines
-    # vizSupport.enableUnityVisualization(scSim, dynTaskName,  dynProcessName, gravBodies = gravFactory, saveFile=fileName, thrDevices=[(thFactory.getNumOfDevices(), thrModelTag)])
+    viz = vizSupport.enableUnityVisualization(scSim, dynTaskName,  dynProcessName,
+                                              gravBodies = gravFactory,
+                                              # saveFile=fileName,
+                                              thrDevices=[(thFactory.getNumOfDevices(), thrModelTag)],
+                                              scName=[scObject.ModelTag]
+                                              )
+    vizSupport.setActuatorGuiSetting(viz, showThrusterLabels=True)
+
 
     #
     #   initialize Simulation
