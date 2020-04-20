607aec9b641d848fe71e078d7183c5be9332fe31
John Martin <john.r.martin@colorado.edu>
2020-04-20 14:02:08 -0600

    changed the conanfile.py optional argument names, removed rpath issue from linux build
    Former-commit-id: 5e7317ae72b05b19c4a26cddc3a1735d1fb70681
    Former-commit-id: e5d5918b482704dd2d81be106226154ca1e1918a

--- conanfile.py
+++ conanfile.py
@@ -6,23 +6,23 @@ class BasiliskConan(ConanFile):
     generators = "cmake_find_package_multi"
     requires = "eigen/3.3.7@conan/stable"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"with_opnav": [True, False], "with_vizInterface": [True, False]}
-    default_options = {"with_opnav": False, "with_vizInterface": True}
+    options = {"opnav_packages": [True, False], "vizInterface_packages": [True, False]}
+    default_options = {"opnav_packages": False, "vizInterface_packages": True}
 
     def requirements(self):
-        if self.options.with_opnav: 
-            self.options.with_vizInterface = True
+        if self.options.opnav_packages: 
+            self.options.vizInterface_packages = True
             self.requires.add("opencv/4.1.1@conan/stable")
             self.requires.add("zlib/1.2.11@conan/stable")
             self.requires.add("bzip2/1.0.8@conan/stable")
 
-        if self.options.with_vizInterface: 
+        if self.options.vizInterface_packages: 
             self.requires.add("libsodium/1.0.18@bincrafters/stable")
             self.requires.add("protobuf/3.5.2@bincrafters/stable")
             self.requires.add("cppzmq/4.3.0@bincrafters/stable")
 
     def configure(self):
-        if self.options.with_opnav:
+        if self.options.opnav_packages:
             self.options['opencv'].contrib = True
 
 
