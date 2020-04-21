6ead1bf308523f0a57933794b02c04b067be4822
John Martin <john.r.martin@colorado.edu>
2020-04-21 15:51:42 -0600

    conanfile updates
    Former-commit-id: c71832b6eb76e178cd3d623132a6141f52e74e0a
    Former-commit-id: 83d9e331d6916dfe12de9bd33d97208ebf1491be

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,26 @@
-from conans import ConanFile, CMake
+import os
+from datetime import datetime
+from conans import ConanFile, CMake, tools
 
 class BasiliskConan(ConanFile):
+    root = os.path.abspath(os.path.curdir)
     name = "Basilisk"
-    homepage = "http://hanspeterschaub.info/basilisk/index.html"
+    homepage = "http://hanspeterschaub.info/basilisk"
+    version = '1.7.2'
     generators = "cmake_find_package_multi"
     requires = "eigen/3.3.7@conan/stable"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"opnav_packages": [True, False], "vizInterface_packages": [True, False]}
-    default_options = {"opnav_packages": False, "vizInterface_packages": True}
+    build_policy = 'missing'
+
+    options = { "xcodeProject": [True, False], 
+                "opnav_packages": [True, False], 
+                "vizInterface_packages": [True, False]}
+
+    default_options = { "xcodeProject": False,
+                        "opnav_packages": False, 
+                        "vizInterface_packages": True}
+
+    generator=None # Autoselect fastest generator
 
     def requirements(self):
         if self.options.opnav_packages: 
@@ -19,17 +32,33 @@ class BasiliskConan(ConanFile):
         if self.options.vizInterface_packages: 
             self.requires.add("libsodium/1.0.18@bincrafters/stable")
             self.requires.add("protobuf/3.5.2@bincrafters/stable")
-            self.requires.add("cppzmq/4.3.0@bincrafters/stable")
+            self.requires.add("cppzmq/4.3.0@bincrafters/stable")            
+
 
     def configure(self):
+        if self.settings.build_type == "Debug":
+            print("Build type is set to Debug. Performance will be significantly lower.")
+        
         if self.options.opnav_packages:
             self.options['opencv'].contrib = True
 
 
-#    def build(self):
-#        cmake = CMake(self)
-#        cmake.definitions[""]
-#        cmake.configure()
-#        cmake.build()
-#
-#        
+    def build(self):
+        if self.options.xcodeProject == True:
+            self.generator="Xcode"
+
+        cmake = CMake(self,set_cmake_flags=True, generator=self.generator)    
+        cmake.definitions["BUILD_OPNAV"] = self.options.opnav_packages
+        cmake.definitions["BUILD_VIZINTERFACE"] = self.options.vizInterface_packages
+        cmake.configure()
+
+        if self.options.xcodeProject == False: # Need to build through xcode to support multithreading. 
+            start = datetime.now()
+            print("CPU Count: " +str(tools.cpu_count()))
+            cmake.build()
+            print("Total Build Time: "+ str(datetime.now()-start))
+        
+        #self.run("python3 -m pip3 install --user matplotlib")
+        #self.run("python3 -m pip3 install --user numpy")
+        #self.run("python3 -m pip3 install --user pandas")
+
