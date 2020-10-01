a98d0f67716872a96a98983e54c1066cf20f34b5
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-10-01 08:24:25 -0600

    [HOT-FIX] correct wrong variable description
    Former-commit-id: 38ba1091e565240efd5bb95440850cb420faaac3
    Former-commit-id: 3f9f3ec1f97c54754bf1dc1401af78028897b4ba

--- src/fswAlgorithms/fswMessages/attRefFswMsg.h
+++ src/fswAlgorithms/fswMessages/attRefFswMsg.h
@@ -24,7 +24,7 @@
 
 /*! @brief Structure used to define the output definition for attitude reference generation */
 typedef struct {
-    double sigma_RN[3];         //!<        Current attitude error estimate (MRPs) of B relative to R
+    double sigma_RN[3];         //!<        MRP Reference attitude of R relative to N
     double omega_RN_N[3];       //!< [r/s]  Reference frame rate vector of the of R relative to N in N frame components
     double domega_RN_N[3];      //!< [r/s2] Reference frame inertial acceleration of  R relative to N in N frame components
 }AttRefFswMsg;
