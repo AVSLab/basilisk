6bccf49c4ea4cdb2cfd7036aff3b2ff66dc87151
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-11-08 14:03:42 -0700

    [BSK-1630] simplified how the C++ interface to the C msg is created from Python.  It is now done via a single support function that is embedded in messages2.py
    Former-commit-id: 15b8c6acb84d5a4efd541e4bdb672a6d63d5be6f
    Former-commit-id: 14fcb9f30e0b52c2ff1e0c35c16457c97c3105d7

--- src/examples/scenarioAttitudeFeedback.py
+++ src/examples/scenarioAttitudeFeedback.py
@@ -291,11 +291,10 @@ def run(show_plots, useUnmodeledTorque, useIntGain, useKnownTorque):
     # The MRP Feedback algorithm requires the vehicle configuration structure. This defines various spacecraft
     # related states such as the inertia tensor and the position vector between the primary Body-fixed frame
     # B origin and the center of mass (defaulted to zero).  This message is set through
-    configDataMsg = messaging2.VehicleConfigMsgClass()  # a c++ message class instance with a configData payload, which is private
-    configDataWriter = configDataMsg.addAuthor()  # a WriteFunctor which is connected to the configDataMsg payload
-    configData = messaging2.VehicleConfigMsg()  # a configData message struct (payload)
-    configData.ISCPntB_B = I  # can be edited directly like any other swigged struct
-    configDataWriter(configData, 0)  # ask the WriteFunctor to overwrite configDataMsg payload with the argument provided.
+    configData = messaging2.VehicleConfigMsg()
+    configData.ISCPntB_B = I
+    configDataMsg = messaging2.writeCMsgCppInterface(configData)
+
 
     #
     # Setup data logging before the simulation is initialized
