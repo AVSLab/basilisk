759abd895407b6918949c25a2ef2192196c6df90
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-11-11 15:26:13 -0700

    [BSK-1630] removed the add-on 'Class' to the C++ interface such as SomeMsgClass().  This was required before because the message payload used to be called `SomeMsg`.  However, now that the payload structure is called `SomeMsgPayload` we can just call the SimMessage class `SomeMsg()`. Updated documentation
    Former-commit-id: e9e2cfde7b83b99c113d3d4e18d79572e6860399
    Former-commit-id: cf613aaa4724190df978a2b5b52989ee6fa42a2b

--- src/examples/scenarioAttitudeFeedback.py
+++ src/examples/scenarioAttitudeFeedback.py
@@ -292,7 +292,7 @@ def run(show_plots, useUnmodeledTorque, useIntGain, useKnownTorque):
     # B origin and the center of mass (defaulted to zero).  This message is set through
     configData = messaging2.VehicleConfigMsgPayload()
     configData.ISCPntB_B = I
-    configDataMsg = messaging2.VehicleConfigMsgClass().write(configData)
+    configDataMsg = messaging2.VehicleConfigMsg().write(configData)
 
 
     #
