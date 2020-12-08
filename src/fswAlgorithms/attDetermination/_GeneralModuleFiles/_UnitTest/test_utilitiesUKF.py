37986694e81954a339c42a2e5c425927b60ad341
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-12-08 15:12:40 -0700

    [BSK-1722] found 2 more hidden unit tests that were trying to read in the now removed `fswMessages` python modules.  All FSW tests pass again.
    Former-commit-id: dd3460e6fa732c206e68f90f226a7eee2679629c
    Former-commit-id: 05090913d33aa831a90341586132720be284d323

--- src/fswAlgorithms/attDetermination/_GeneralModuleFiles/_UnitTest/test_utilitiesUKF.py
+++ src/fswAlgorithms/attDetermination/_GeneralModuleFiles/_UnitTest/test_utilitiesUKF.py
@@ -1,34 +1,27 @@
-''' '''
-'''
- ISC License
 
- Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+# ISC License
+#
+# Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
- Permission to use, copy, modify, and/or distribute this software for any
- purpose with or without fee is hereby granted, provided that the above
- copyright notice and this permission notice appear in all copies.
 
- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-'''
-import sys, os, inspect
 import numpy
 import pytest
 import math
 
-from Basilisk.utilities import SimulationBaseClass, unitTestSupport, macros
-import matplotlib.pyplot as plt
 from Basilisk.fswAlgorithms import sunlineSuKF
 from Basilisk.fswAlgorithms import inertialUKF
-from Basilisk.fswAlgorithms import fswMessages
-from Basilisk.fswAlgorithms import cssComm
-from Basilisk.simulation import coarse_sun_sensor
 
 # uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
 # @pytest.mark.skipif(conditionstring)
