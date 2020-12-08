3fc47e9520c36bbb2038e7e25f90982d2ea992f6
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-12-08 14:48:43 -0700

    [BSK-1722] renamed `cMsgTemplate` to `msgTempateInputs` as it contains the auto-mated templating code for both C and C++ messages
    Former-commit-id: ef2bed31cbba4c452af3eb43ef648ce1e10120e6
    Former-commit-id: 21d3b07377184833f87c28b6fadfc20a504e72f9

--- conanfile.py
+++ conanfile.py
@@ -95,7 +95,7 @@ class BasiliskConan(ConanFile):
     # auto-generate C message definition files
     print(statusColor + "Auto-generating message definitions:" + endColor, end=" ")
     bskPath = os.getcwd()
-    os.chdir(os.path.join(bskPath, "src/architecture/messaging2/cMsgTemplate"))
+    os.chdir(os.path.join(bskPath, "src/architecture/messaging2/msgTemplateInputs"))
     cmdString = list()
     if platform.system() == "Windows":
         cmdString.append("py")
