b4ab156e79e67d2fe8cf839d34101940964efdc3
Andrew Harris <andrew.t.harris@colorado.edu>
2020-12-11 15:50:15 -0700

    Initial commit of simple hillStateConverter unit test.
    Former-commit-id: 580de610e98a7add892a2cfb52cd699d8c055002
    Former-commit-id: 95d3af5fc1a50077b057cf9efc08115dbd2dea9d

---
+++ src/fswAlgorithms/formationFlying/hillStateConverter/_UnitTest/test_hillStateConverter.py
@@ -0,0 +1,72 @@
+
+#   3rd party / std lib imports
+import pytest
+import os, inspect
+import numpy as np
+
+#   Utilities/macros
+from Basilisk.utilities import SimulationBaseClass as sbc 
+from Basilisk.utilities import macros, unitTestSupport
+from Basilisk.utilities import RigidBodyKinematics as rbk 
+from Basilisk.utilities import orbitalMotion as om 
+
+#   Modules to test
+from Basilisk.fswAlgorithms import hillStateConverter
+from Basilisk.simulation import simFswInterfaceMessages
+
+def test_hillStateConverter(show_plots):
+
+
+    sim = sbc.SimBaseClass()
+    procName = 'process'
+    taskName = 'task'
+    proc = sim.CreateNewProcess(procName)
+    task =  sim.CreateNewTask(taskName, macros.sec2nano(1.0))
+    proc.addTask(task)
+
+    #   Set up two spacecraft position messages
+    chief_r = [7100,0,0]
+    chief_v = [0,7.000,0]
+    dep_r = [7101, 0, 0]
+    dep_v = [0,7.010,0]
+
+    chiefNavMsg = simFswInterfaceMessages.NavTransIntMsg()
+    chiefNavMsg.r_BN_N = chief_r
+    chiefNavMsg.v_BN_N = chief_v
+    chiefMsgName = "chief_nav"
+    unitTestSupport.setMessage(sim.TotalSim, procName, chiefMsgName, chiefNavMsg, "NavTransIntMsg")
+
+    depNavMsg = simFswInterfaceMessages.NavTransIntMsg()
+    depNavMsg.r_BN_N = dep_r 
+    depNavMsg.v_BN_N = dep_v
+    depMsgName = "dep_nav"
+    unitTestSupport.setMessage(sim.TotalSim, procName, depMsgName, depNavMsg, "NavTransIntMsg")
+
+    #   Set up the hillStateConverter
+    hillStateNavData = hillStateConverter.HillStateConverterConfig()
+    hillStateNavWrap = sim.setModelDataWrap(hillStateNavData)
+    hillStateNavWrap.ModelTag = "dep_hillStateNav"
+    hillStateNavData.chiefStateInMsgName = chiefMsgName
+    hillStateNavData.depStateInMsgName = depMsgName
+    hillStateNavData.hillStateOutMsgName = 'dep_hill_nav'
+    
+    sim.AddModelToTask(taskName, hillStateNavWrap, hillStateNavData)
+    sim.TotalSim.logThisMessage(hillStateNavData.hillStateOutMsgName, macros.sec2nano(1.0))
+
+    sim.ConfigureStopTime(macros.sec2nano(1.0))
+    sim.InitializeSimulationAndDiscover()
+    sim.ExecuteSimulation()
+    hill_positions = sim.pullMessageLogData(hillStateNavData.hillStateOutMsgName+".r_DC_H",list(range(3)))
+    hill_velocities = sim.pullMessageLogData(hillStateNavData.hillStateOutMsgName+".v_DC_H",list(range(3)))
+
+    ref_pos = [1,0,0]
+    ref_vel = [0,0.00901408,0]
+    #   Test the position calculation:
+    for val1, val2 in zip(hill_positions[1,1:4], ref_pos):
+        assert pytest.approx(val1, val2)
+
+    for val1, val2  in zip(hill_velocities[1,1:4], ref_vel):
+        assert pytest.approx(val1, val2)
+
+if __name__=="__main__":
+    test_hillStateConverter(False)
