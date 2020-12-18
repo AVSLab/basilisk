fd4b914b9adb295480151256e35d966ee12e880d
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2020-12-18 07:44:02 -0700

    [BSK-1722] updated integrator unit test to pass with BSK2
    Former-commit-id: d58c850f25659eeeaf54ef6f3c224e0f5c7e0743
    Former-commit-id: 49df1a3b0ad4ef03e54d43a6f73efa0ea6a2ccb4

--- src/simulation/dynamics/Integrators/_UnitTest/test_Integrators.py
+++ src/simulation/dynamics/Integrators/_UnitTest/test_Integrators.py
@@ -1,22 +1,20 @@
-''' '''
-'''
- ISC License
 
- Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+# ISC License
+#
+# Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
- Permission to use, copy, modify, and/or distribute this software for any
- purpose with or without fee is hereby granted, provided that the above
- copyright notice and this permission notice appear in all copies.
-
- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-'''
 
 
 #
@@ -42,6 +40,7 @@ from Basilisk.utilities import orbitalMotion
 from Basilisk.simulation import spacecraftPlus
 from Basilisk.utilities import simIncludeGravBody
 from Basilisk.simulation import svIntegrators
+from Basilisk.architecture import messaging2
 
 # @cond DOXYGEN_IGNORE
 filename = inspect.getframeinfo(inspect.currentframe()).filename
@@ -146,8 +145,10 @@ def run(doUnitTests, show_plots, integratorCase):
     #
     numDataPoints = 100
     samplingTime = simulationTime // numDataPoints
-    scSim.TotalSim.logThisMessage(scObject.scStateOutMsgName, samplingTime)
-
+    dataLog = scObject.scStateOutMsg.log()
+    logTaskName = "loggingTask"
+    dynProcess.addTask(scSim.CreateNewTask(logTaskName, samplingTime))
+    scSim.AddModelToTask(logTaskName, dataLog)
 
     #
     #   initialize Simulation
@@ -163,8 +164,8 @@ def run(doUnitTests, show_plots, integratorCase):
     #
     #   retrieve the logged data
     #
-    posData = scSim.pullMessageLogData(scObject.scStateOutMsgName+'.r_BN_N', list(range(3)))
-    velData = scSim.pullMessageLogData(scObject.scStateOutMsgName+'.v_BN_N', list(range(3)))
+    posData = dataLog.r_BN_N
+    velData = dataLog.v_BN_N
 
     #
     #   plot the results
@@ -191,7 +192,7 @@ def run(doUnitTests, show_plots, integratorCase):
     fData = []
     labelStrings = ("rk4", "euler", "rk2")
     for idx in range(0, len(posData)):
-        oeData = orbitalMotion.rv2elem(mu, posData[idx, 1:4], velData[idx, 1:4])
+        oeData = orbitalMotion.rv2elem(mu, posData[idx], velData[idx])
         rData.append(oeData.rmag)
         fData.append(oeData.f + oeData.omega - oe.omega)
     plt.plot(rData*np.cos(fData)/1000, rData*np.sin(fData)/1000
