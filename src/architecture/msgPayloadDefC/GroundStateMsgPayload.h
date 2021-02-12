c2b1d4805b6b5cc48a0616c17a0f4514d0772719
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2021-02-12 15:09:48 -0700

    [BSK-1734] make sure that highlighted search text is readable in HTML dark mode
    Former-commit-id: 38f483f87629311d53ab0c8257465512edf27440
    Former-commit-id: c82aef2ad76b7001d6498d371f1e294f915bdfef

--- src/architecture/msgPayloadDefC/GroundStateMsgPayload.h
+++ src/architecture/msgPayloadDefC/GroundStateMsgPayload.h
@@ -25,7 +25,7 @@
 /*! @brief Message that defines the inertial location of a groundLocation at the current time.
  */
 typedef struct {
-    double r_LN_N[3]; //!< Position vector of the planet w.r.t. the inertial origin in the inertial frame
+    double r_LN_N[3]; //!< Position vector of the location w.r.t. the inertial origin in the inertial frame
     double r_LP_N[3]; //!< Position vector of the location with respect to the planet center in the inertial frame
 }GroundStateMsgPayload;
 
