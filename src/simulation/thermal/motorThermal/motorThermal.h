ddf287dccd554b4aacac2f85b76f882564dd7b95
João Vaz Carneiro <joao.carneiro@colorado.edu>
2021-03-08 20:18:00 -0700

    [BSK-1740] Finished the .cpp, .h and .i files for the motor temperature module.
    Former-commit-id: d6275a66f1c0bb6e7f59c6fc8887394e32ce80be
    Former-commit-id: d86e7e94e06ae2fe7e85e25804fdf9bb30becdb0

---
+++ src/simulation/thermal/motorThermal/motorThermal.h
@@ -0,0 +1,60 @@
+/*
+ ISC License
+
+ Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef MOTOR_THERMAL_H
+#define MOTOR_THERMAL_H
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/msgPayloadDefC/TemperatureMsgPayload.h"
+#include "architecture/msgPayloadDefC/RWConfigLogMsgPayload.h"
+#include "architecture/utilities/bskLogging.h"
+#include "architecture/messaging/messaging.h"
+
+/*! @brief Motor temperature module */
+class MotorThermal: public SysModel {
+public:
+    MotorThermal();
+    ~MotorThermal();
+
+    void Reset(uint64_t CurrentSimNanos);
+    void UpdateState(uint64_t CurrentSimNanos);
+    void readInputMessages();
+    void writeOutputMessages(uint64_t CurrentClock);
+    void computeTemperature(uint64_t CurrentSimNanos);
+
+public:
+    Message<TemperatureMsgPayload> temperatureOutMsg;   //!< [ºC] temperature output message
+    ReadFunctor<RWConfigLogMsgPayload> rwStateInMsg;   //!< reaction wheel state input message
+    double efficiency;                  //!< efficiency factor to convert power into mechanical power
+    double currentTemperature;          //!< [ºC] stored temperature
+    double ambientTemperature;          //!< [ºC] ambient temperature for heat dissipation
+    double ambientThermalResistance;    //!< [W/ºC] ambient thermal resistance to convert heat into temperature
+    double motorHeatCapacity;           //!< [J/ºC] motor heat caapcity to convert heat into temperature
+    BSKLogger bskLogger;                //!< -- BSK Logging
+
+private:
+    TemperatureMsgPayload temperatureBuffer;    //!< temperature buffer for internal calculations
+    RWConfigLogMsgPayload rwStateBuffer;        //!< reaction wheel state buffer
+
+    uint64_t prevTime;                 //!< -- Previous simulation time observed
+
+};
+
+
+#endif
