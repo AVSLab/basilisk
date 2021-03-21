74759c43c18f64a9a662b4f7b6d05482a66914b7
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2021-03-20 18:09:22 -0600

    removed the last sphinx duplicate label error message
    Former-commit-id: 3c6421c23eecd6463c9e9677ded3c10bf553dfe8
    Former-commit-id: 1035ef9c1fda9cdac686d8712c849ff74dccf46f

--- src/architecture/utilities/astroConstants.h
+++ src/architecture/utilities/astroConstants.h
@@ -43,26 +43,26 @@
 
 
 /* common conversions */
-#ifndef M_PI
-#define M_PI            3.141592653589793
+#ifndef MPI
+#define MPI            3.141592653589793
 #endif
-#ifndef M_PI_2
-#define M_PI_2          1.5707963267948966
+#ifndef MPI_2
+#define MPI_2          1.5707963267948966
 #endif
 #ifndef M_E
 #define M_E             2.718281828459045
 #endif
 #ifndef D2R
-#define D2R             (M_PI/180.)
+#define D2R             (MPI/180.)
 #endif
 #ifndef R2D
-#define R2D             180./M_PI
+#define R2D             180./MPI
 #endif
 #ifndef NAN
 #define NAN             sqrt((float)-1)
 #endif
 #ifndef RPM
-#define RPM             (2.*M_PI/60.)
+#define RPM             (2.*MPI/60.)
 #endif
 #ifndef SEC2DAY
 #define SEC2DAY 1.0 / (60.0 * 60.0 * 24.0);
@@ -100,7 +100,7 @@
 #define SMA_MERCURY       0.38709893*AU
 #define I_MERCURY         7.00487*D2R
 #define E_MERCURY         0.20563069
-#define OMEGA_MERCURY    (2*M_PI/1407.6/3600.)     /* Mercury's planetary rotation rate, rad/sec */
+#define OMEGA_MERCURY    (2*MPI/1407.6/3600.)     /* Mercury's planetary rotation rate, rad/sec */
 
 /* Venus */
 #define REQ_VENUS      6051.8               /* km */
@@ -108,7 +108,7 @@
 #define SMA_VENUS         0.72333199*AU
 #define I_VENUS           3.39471*D2R
 #define E_VENUS           0.00677323
-#define OMEGA_VENUS      (2*M_PI/(-5832.6)/3600.)     /* Venus' planetary rotation rate, rad/sec */
+#define OMEGA_VENUS      (2*MPI/(-5832.6)/3600.)     /* Venus' planetary rotation rate, rad/sec */
 
 /* Earth */
 #define REQ_EARTH      6378.1366            /* km, from SPICE */
@@ -121,14 +121,14 @@
 #define SMA_EARTH         1.00000011*AU
 #define I_EARTH           0.00005*D2R
 #define E_EARTH           0.01671022
-#define OMEGA_EARTH       (2*M_PI/23.9345/3600.)     /* Earth's planetary rotation rate, rad/sec */
+#define OMEGA_EARTH       (2*MPI/23.9345/3600.)     /* Earth's planetary rotation rate, rad/sec */
 
 /* Moon */
 #define REQ_MOON       1737.4
 #define J2_MOON         202.7e-6
 #define SMA_MOON          0.3844e6
 #define E_MOON            0.0549
-#define OMEGA_MOON       (2*M_PI/655.728/3600.)     /* Moon's rotation rate, rad/sec */
+#define OMEGA_MOON       (2*MPI/655.728/3600.)     /* Moon's rotation rate, rad/sec */
 
 /* Mars */
 #define REQ_MARS       3396.19              /* km */
@@ -137,7 +137,7 @@
 #define SMA_MARS          1.52366231*AU
 #define I_MARS            1.85061*D2R
 #define E_MARS            0.09341233
-#define OMEGA_MARS        (2*M_PI/24.6229/3600.)     /* Mars' polar rotation rate, rad/sec */
+#define OMEGA_MARS        (2*MPI/24.6229/3600.)     /* Mars' polar rotation rate, rad/sec */
 
 /* Phobos */
 #define REQ_PHOBOS     11.2               /* km */
@@ -151,7 +151,7 @@
 #define SMA_JUPITER       5.20336301*AU
 #define I_JUPITER         1.30530*D2R
 #define E_JUPITER         0.04839266
-#define OMEGA_JUPITER    (2*M_PI/9.9250/3600.)     /* Jupiter's polar rotation rate, rad/sec */
+#define OMEGA_JUPITER    (2*MPI/9.9250/3600.)     /* Jupiter's polar rotation rate, rad/sec */
 
 /* Saturn */
 #define REQ_SATURN    60268.
@@ -159,7 +159,7 @@
 #define SMA_SATURN        9.53707032*AU
 #define I_SATURN          2.48446*D2R
 #define E_SATURN          0.05415060
-#define OMEGA_SATURN     (2*M_PI/10.656/3600.)     /* Saturn's polar rotation rate, rad/sec */
+#define OMEGA_SATURN     (2*MPI/10.656/3600.)     /* Saturn's polar rotation rate, rad/sec */
 
 /* Uranus */
 #define REQ_URANUS    25559.
@@ -167,7 +167,7 @@
 #define SMA_URANUS       19.19126393*AU
 #define I_URANUS          0.76986*D2R
 #define E_URANUS          0.04716771
-#define OMEGA_URANUS     (2*M_PI/(-17.24)/3600.)     /* Uranus' polar rotation rate, rad/sec */
+#define OMEGA_URANUS     (2*MPI/(-17.24)/3600.)     /* Uranus' polar rotation rate, rad/sec */
 
 /* Neptune */
 #define REQ_NEPTUNE   24746.
@@ -175,13 +175,13 @@
 #define SMA_NEPTUNE      30.06896348*AU
 #define I_NEPTUNE         1.76917*D2R
 #define E_NEPTUNE         0.00858587
-#define OMEGA_NEPTUNE    (2*M_PI/16.11/3600.)     /* Neptune's polar rotation rate, rad/sec */
+#define OMEGA_NEPTUNE    (2*MPI/16.11/3600.)     /* Neptune's polar rotation rate, rad/sec */
 
 /* Pluto */
 #define REQ_PLUTO      1137.
 #define SMA_PLUTO        39.48168677*AU
 #define I_PLUTO          17.14175*D2R
 #define E_PLUTO           0.24880766
-#define OMEGA_PLUTO     (2*M_PI/(-153.2928)/3600.)     /* Pluto's polar rotation rate, rad/sec */
+#define OMEGA_PLUTO     (2*MPI/(-153.2928)/3600.)     /* Pluto's polar rotation rate, rad/sec */
 
 #endif
