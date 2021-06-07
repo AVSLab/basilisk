51cabda94aac706d6ae15a02b503bca8268b6394
João Vaz Carneiro <joao.carneiro@colorado.edu>
2021-06-07 18:37:25 +0100

    Added a #define to celestialTwoBodyPoint to make it work on Windows.
    Former-commit-id: ec9d73645ad21bf8158686a3ca218f4e66128a89
    Former-commit-id: 1543103119aed92f8a30ea675cd74c72efef3304

--- src/fswAlgorithms/attGuidance/celestialTwoBodyPoint/celestialTwoBodyPoint.c
+++ src/fswAlgorithms/attGuidance/celestialTwoBodyPoint/celestialTwoBodyPoint.c
@@ -17,6 +17,11 @@
 
  */
 #include <string.h>
+
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
 #include <math.h>
 #include "fswAlgorithms/attGuidance/celestialTwoBodyPoint/celestialTwoBodyPoint.h"
 #include "architecture/utilities/linearAlgebra.h"
