b8c24794daba4ccab00ead64c81c7184f4dda5d2
João Vaz Carneiro <joao.carneiro@colorado.edu>
2021-07-09 00:25:49 +0100

    [BSK-1793] New module that computes the barycenter of a swarm of spacecraft. Can compute the mean cartesian position and velocity or can use the mean orbital elements of all spacecraft. Will be useful for formation flying where the chief is the barycenter of the formation.
    Former-commit-id: cda31159f6560bc23d097c821ba2b1286a22f955
    Former-commit-id: 2f66de6c939dc573f91137a44e45c760bca343cb

---
+++ src/fswAlgorithms/formationFlying/formationBarycenter/formationBarycenter.cpp
@@ -0,0 +1,171 @@
+/*
+ ISC License
+
+ Copyright (c) 2021, Autonomous Vehicle Systems Lab, University of Colorado Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+
+#include "fswAlgorithms/formationFlying/formationBarycenter/formationBarycenter.h"
+#include "architecture/utilities/orbitalMotion.h"
+#include "architecture/msgPayloadDefC/ClassicElementsMsgPayload.h"
+
+
+/*! This is the constructor for the module class.  It sets default variable
+    values and initializes the various parts of the model */
+FormationBarycenter::FormationBarycenter() {
+    this->useOrbitalElements = false;
+    this->mu = 0;
+}
+
+/*! Module Destructor */
+FormationBarycenter::~FormationBarycenter() {
+}
+
+/*! This method is used to reset the module and checks that required input messages are connected.
+*/
+void FormationBarycenter::Reset(uint64_t CurrentSimNanos) {
+    // check that required input messages are connected
+    if (this->scNavInMsgs.size() == 0 || this->scPayloadInMsgs.size() == 0) {
+        bskLogger.bskLog(BSK_ERROR, "FormationBarycenter module must have at least one spacecraft added through `addSpacecraftToModel`");
+    }
+
+    // check if the gravitational parameter is set if using orbital elements averaging
+    if (this->mu == 0 && this->useOrbitalElements) {
+        bskLogger.bskLog(BSK_ERROR, "FormationBarycenter module requires defining a gravitational parameter if using orbital elements.");
+    }
+
+    // zero the output message buffer
+    this->transOutBuffer = this->transOutMsg.zeroMsgPayload;
+
+}
+
+/*! Adds a scNav and scPayload messages name to the vector of names to be subscribed to.
+*/
+void FormationBarycenter::addSpacecraftToModel(Message<NavTransMsgPayload>* tmpScNavMsg, Message<VehicleConfigMsgPayload>* tmpScPayloadMsg) {
+    this->scNavInMsgs.push_back(tmpScNavMsg->addSubscriber());
+    this->scPayloadInMsgs.push_back(tmpScPayloadMsg->addSubscriber());
+
+    // expand the buffer vector
+    NavTransMsgPayload scNavMsg;
+    this->scNavBuffer.push_back(scNavMsg);
+    VehicleConfigMsgPayload scPayloadMsg;
+    this->scPayloadBuffer.push_back(scPayloadMsg);
+}
+
+/*! Reads the input messages
+*/
+void FormationBarycenter::ReadInputMessages() {
+
+    NavTransMsgPayload scNavMsg;
+    VehicleConfigMsgPayload scPayloadMsg;
+
+    // clear out the vector of spacecraft navigation and mass messages.  This is created freshly below.
+    this->scNavBuffer.clear();
+    this->scPayloadBuffer.clear();
+
+    // read in the spacecraft state messages
+    for (long unsigned int c = 0; c < this->scNavInMsgs.size(); c++) {
+        scNavMsg = this->scNavInMsgs.at(c)();
+        scPayloadMsg = this->scPayloadInMsgs.at(c)();
+        this->scNavBuffer.push_back(scNavMsg);
+        this->scPayloadBuffer.push_back(scPayloadMsg);
+    }
+
+}
+
+/*! Does the barycenter calculations
+*/
+void FormationBarycenter::computeBaricenter() {
+    //create temporarary variables
+    double barycenter[] {0, 0, 0};
+    double barycenterVelocity[] {0, 0, 0};
+    double totalMass {0};
+
+    // check which averaging to use
+    if (!this->useOrbitalElements) {
+        // compute the cartesian barycenter
+        for (long unsigned int c = 0; c < this->scNavInMsgs.size(); c++) {
+            for (int n = 0; n < 3; n++) {
+                barycenter[n] += this->scPayloadBuffer.at(c).massSC * this->scNavBuffer.at(c).r_BN_N[n];
+                barycenterVelocity[n] += this->scPayloadBuffer.at(c).massSC * this->scNavBuffer.at(c).v_BN_N[n];
+            }
+            totalMass += this->scPayloadBuffer.at(c).massSC;
+        }
+
+        for (int n = 0; n < 3; n++) {
+            barycenter[n] /= totalMass;
+            barycenterVelocity[n] /= totalMass;
+        }
+    } else {
+        classicElements orbitElements, tempElements;
+
+        // zero the orbit elements first
+        orbitElements.a = 0;
+        orbitElements.e = 0;
+        orbitElements.i = 0;
+        orbitElements.Omega = 0;
+        orbitElements.omega = 0;
+        orbitElements.f = 0;
+
+        // compute the orbital elements barycenter
+        for (long unsigned int c = 0; c < this->scNavInMsgs.size(); c++) {
+            // convert the position and velocity vectors into orbital elements
+            rv2elem(this->mu, this->scNavBuffer.at(c).r_BN_N, this->scNavBuffer.at(c).v_BN_N, &tempElements);
+
+            orbitElements.a += this->scPayloadBuffer.at(c).massSC * tempElements.a;
+            orbitElements.e += this->scPayloadBuffer.at(c).massSC * tempElements.e;
+            orbitElements.i += this->scPayloadBuffer.at(c).massSC * tempElements.i;
+            orbitElements.Omega += this->scPayloadBuffer.at(c).massSC * tempElements.Omega;
+            orbitElements.omega += this->scPayloadBuffer.at(c).massSC * tempElements.omega;
+            orbitElements.f += this->scPayloadBuffer.at(c).massSC * tempElements.f;
+
+            totalMass += this->scPayloadBuffer.at(c).massSC;
+
+        }
+
+        orbitElements.a /= totalMass;
+        orbitElements.e /= totalMass;
+        orbitElements.i /= totalMass;
+        orbitElements.Omega /= totalMass;
+        orbitElements.omega /= totalMass;
+        orbitElements.f /= totalMass;
+
+        // convert orbital elements into position and velocity vectors
+        elem2rv(this->mu, &orbitElements, barycenter, barycenterVelocity);
+    }
+
+    for (int n = 0; n < 3; n++) {
+        this->transOutBuffer.r_BN_N[n] = barycenter[n];
+        this->transOutBuffer.v_BN_N[n] = barycenterVelocity[n];
+    }
+}
+
+/*! writes the output messages
+*/
+void FormationBarycenter::WriteOutputMessage(uint64_t CurrentClock) {
+    //! - write barycenter output message
+    this->transOutMsg.write(&this->transOutBuffer, this->moduleID, CurrentClock);
+}
+
+/*! This is the main method that gets called every time the module is updated.
+*/
+void FormationBarycenter::UpdateState(uint64_t CurrentSimNanos)
+{
+    this->ReadInputMessages();
+    this->computeBaricenter();
+    this->WriteOutputMessage(CurrentSimNanos);
+}
+
