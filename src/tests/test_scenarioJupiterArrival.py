ae02ce2235eaa28e8deb6cf5807d5a486443a5b9
Leah Kiner <leahannkiner@gmail.com>
2021-10-04 08:36:47 -0600

    pytest file
    Former-commit-id: 84ee10ee844188d2cd09aa4e30224749c8cd2269
    Former-commit-id: c1b67f23a43778349093644ec24696066a3e771b

---
+++ src/tests/test_scenarioJupiterArrival.py
@@ -0,0 +1,90 @@
+
+# ISC License
+#
+# Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+
+
+#
+# Basilisk Scenario Script and Integrated Test
+#
+# Purpose:  Integrated test of the spacecraft() and gravity modules.  Illustrates
+#           a 3-DOV spacecraft on a range of orbit types.
+# Author:   Leah Kiner
+# Creation Date:  Sep. 30, 2021
+#
+
+import sys, os, inspect
+import pytest
+from Basilisk.utilities import unitTestSupport
+
+# Get current file path
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+sys.path.append(path + '/../../examples')
+import scenarioJupiterArrival
+
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+
+# uncomment this line if this test has an expected failure, adjust message as needed
+# @pytest.mark.xfail(True, reason="Previously set sim parameters are not consistent with new formulation\n")
+
+@pytest.mark.scenarioTest
+
+def test_scenarioBasicOrbit(show_plots):
+    """This function is called by the py.test environment."""
+    # each test method requires a single assert method to be called
+    # provide a unique test method name, starting with test_
+
+    testFailCount = 0                       # zero unit test result counter
+    testMessages = []                       # create empty array to store test log messages
+
+    try:
+        posData, figureList = scenarioJupiterArrival.run(show_plots)
+        # save the figures to the Doxygen scenario images folder
+        for pltName, plt in list(figureList.items()):
+            unitTestSupport.saveScenarioFigure(pltName, plt, path)
+
+    except OSError as err:
+        testFailCount += 1
+        testMessages.append("scenarioJupiterArrival  test are failed.")
+
+    # compare the results to the truth values
+    accuracy = 3.0  # meters
+
+    if posData > accuracy:
+        testFailCount += 1
+        testMessages += "FAILED: scenarioJupiterArrival r_BN_N Norm test"
+
+    #   print out success message if no error were found
+    if testFailCount == 0:
+        print("PASSED ")
+    else:
+        print(testFailCount)
+        print(testMessages)
+
+    # each test method requires a single assert method to be called
+    # this check below just makes sure no sub-test failures were found
+
+    assert testFailCount < 1, testMessages
+
+if __name__ == "__main__":
+    test_scenarioJupiterArrival(
+        False,        # show_plots
+    )
