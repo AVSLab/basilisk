e4af33fc4187046a0c73f361b7a3a21a42588f20
Leah Kiner <leahannkiner@gmail.com>
2021-11-16 16:26:04 -0700

    [BSK_1848]  DentonFluxModule preliminary commit - succeeded build
    Former-commit-id: e5904d9ca527f9863d1f0e4cfc5f6f3ca731e770
    Former-commit-id: 41894a6bfc7dad8d358cb853522cc8060fa6208e

---
+++ src/simulation/environment/dentonFluxModel/dentonFluxModel.h
@@ -0,0 +1,66 @@
+/*
+ ISC License
+
+ Copyright (c) 2021, Autonomous Vehicle Systems Lab, University of Colorado Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+
+#ifndef DENTONFLUXMODEL_H
+#define DENTONFLUXMODEL_H
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/msgPayloadDefC/SCStatesMsgPayload.h"
+//#include "architecture/msgPayloadDefC/EphemerisMsgPayload.h"
+#include "architecture/msgPayloadDefC/SpicePlanetStateMsgPayload.h"
+#include "architecture/msgPayloadDefC/FluxMsgPayload.h"
+#include "architecture/utilities/bskLogging.h"
+#include "architecture/messaging/messaging.h"
+
+/*! @brief This is an auto-created sample C++ module.  The description is included with the module class definition
+ */
+class DentonFluxModel: public SysModel {
+public:
+    DentonFluxModel();
+    ~DentonFluxModel();
+
+    void Reset(uint64_t CurrentSimNanos);
+    void UpdateState(uint64_t CurrentSimNanos);
+    
+    // Other Methods
+    double calcLocalTime(double v1[3], double v2[3]);
+    long double bilinear(int, int, long double, long double, double, long double, long double, long double, long double, long double);
+    
+public:
+    
+    int choose_kp;
+    long double choose_energy;
+    double choose_local_time;
+    
+    ReadFunctor<SCStatesMsgPayload> satStateInMsg;  //!< input spacecraft states
+    //ReadFunctor<EphemerisMsgPayload> earthStateInMsg;  //!< input Earth states
+    //ReadFunctor<EphemerisMsgPayload> sunStateInMsg;  //!< input sun states
+
+    //ReadFunctor<SpicePlanetStateMsgPayload> earthStateInputMsg;  //!< input planet states
+    ReadFunctor<SpicePlanetStateMsgPayload> sunStateInputMsg;  //!< input sun states
+
+    Message<FluxMsgPayload> fluxOutMsg;  //!< output ion and electron fluxes
+
+    BSKLogger bskLogger;              //!< -- BSK Logging
+
+};
+
+
+#endif
