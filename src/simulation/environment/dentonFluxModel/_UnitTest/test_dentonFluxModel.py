dcb05be98a8ee85ffa63aa241c7c5234e219ec67
Julian Hammerl <>
2021-12-20 23:17:16 +0100

    Completed unitTest including necessary support data, and finished documentation
    Former-commit-id: fff10e5500dafd646f723e34056570e9222bcbc4
    Former-commit-id: e75c1582ad4df1ce9da2feed14b7aa362196ecb6

--- src/simulation/environment/dentonFluxModel/_UnitTest/test_dentonFluxModel.py
+++ src/simulation/environment/dentonFluxModel/_UnitTest/test_dentonFluxModel.py
@@ -14,11 +14,23 @@
 #  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 #  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# 
-# 
-
+#
+#
+#   Unit Test Script
+#   Module Name:        dentonFluxModel
+#   Author:             Julian Hammerl
+#   Creation Date:      December 12, 2021
+#
+import math
+import matplotlib.pyplot as plt
+import numpy as np
 import pytest
+import os, inspect
 from Basilisk import __path__
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
 bskPath = __path__[0]
 
 from Basilisk.utilities import SimulationBaseClass
@@ -27,42 +39,54 @@ from Basilisk.architecture import messaging
 from Basilisk.utilities import macros
 from Basilisk.simulation import dentonFluxModel
 
-@pytest.mark.parametrize("accuracy", [1e-12])
-@pytest.mark.parametrize("param1, param2", [
-     (1, 1)
-    ,(1, 3)
+Kps = [0, 21]
+LTs = [0.00, 14.73]
+z_offsets = [0., 3500e3]
+r_EN_Ns = np.array([[0., 0., 0.], [400e3, 300e3, -200e3]])
+
+@pytest.mark.parametrize("accuracy", [1e-6])
+@pytest.mark.parametrize("param1_Kp, param2_LT, param3_z, param4_r_EN", [
+    (Kps[0], LTs[0], z_offsets[0], r_EN_Ns[0]),
+    (Kps[1], LTs[1], z_offsets[1], r_EN_Ns[0]),
+    (Kps[1], LTs[1], z_offsets[0], r_EN_Ns[1]),
+    (Kps[1], LTs[1], z_offsets[1], r_EN_Ns[1]),
+    (Kps[1], LTs[0], z_offsets[1], r_EN_Ns[1]),
 ])
 
-def test_dentonFluxModel(show_plots, param1, param2, accuracy):
+def test_dentonFluxModel(show_plots, param1_Kp, param2_LT, param3_z, param4_r_EN, accuracy):
     r"""
     **Validation Test Description**
 
-    Compose a general description of what is being tested in this unit test script.
+    The Denton Flux Module is tested for several different Kp indices, local times, and spacecraft/Sun/Earth positions
 
     **Test Parameters**
 
-    Discuss the test parameters used.
-
     Args:
-        param1 (int): Dummy test parameter for this parameterized unit test
-        param2 (int): Dummy test parameter for this parameterized unit test
+        show_plots (bool): specify if plots should be shown
+        param1_Kp (int): Kp Index
+        param2_LT (float): Local Time (use 2 decimals)
+        param3_z (float): z-offset to test spacecraft and Sun position with offset to equatorial plane
+        param4_r_EN (float np.array): r_EN_N position vector of Earth w.r.t. N frame, in N frame components
         accuracy (float): absolute accuracy value used in the validation tests
 
     **Description of Variables Being Tested**
 
-    Here discuss what variables and states are being checked. 
+    The electron and ion energies are compared to make sure the flux data is computed for the same energy. The main
+    part of the unitTest is to compare the electron and ion flux.
     """
-    [testResults, testMessage] = dentonFluxModelTestFunction(show_plots, param1, param2, accuracy)
+    [testResults, testMessage] = dentonFluxModelTestFunction(show_plots, param1_Kp, param2_LT, param3_z, param4_r_EN,
+                                                             accuracy)
     assert testResults < 1, testMessage
 
 
-def dentonFluxModelTestFunction(show_plots, param1, param2, accuracy):
+def dentonFluxModelTestFunction(show_plots, param1_Kp, param2_LT, param3_z, param4_r_EN, accuracy):
     """Test method"""
     testFailCount = 0
     testMessages = []
     unitTaskName = "unitTask"
     unitProcessName = "TestProcess"
 
+    # Create sim module and test thread
     unitTestSim = SimulationBaseClass.SimBaseClass()
     testProcessRate = macros.sec2nano(0.5)
     testProc = unitTestSim.CreateNewProcess(unitProcessName)
@@ -71,24 +95,34 @@ def dentonFluxModelTestFunction(show_plots, param1, param2, accuracy):
     # setup module to be tested
     module = dentonFluxModel.DentonFluxModel()
     module.ModelTag = "dentonFluxModule"
-    module.kpIndex = 2
-    module.numOutputEnergies = 30
+    module.kpIndex = param1_Kp
+    module.numOutputEnergies = 6
     module.dataPath = bskPath + '/supportData/DentonGEO/'
-    print(module.dataPath)
 
     unitTestSim.AddModelToTask(unitTaskName, module)
 
+    # Set up position vectors (param3_z is used to offset S/C and sun from equatorial plane)
+    LT = param2_LT
+    angle = LT * 360./24. * np.pi/180 - np.pi
+    OrbitRadius = 42000 * 1e3  # GEO orbit
+
+    r_BE_N = np.array([OrbitRadius*math.cos(angle), OrbitRadius*math.sin(angle), param3_z])
+    r_SE_N = np.array([149000000000.0, 0., -2.73*param3_z])
+    r_EN_N = param4_r_EN
+    r_BN_N = r_BE_N + r_EN_N
+    r_SN_N = r_SE_N + r_EN_N
+
     # Configure input messages
     scStateInMsgData = messaging.SCStatesMsgPayload()
-    scStateInMsgData.r_BN_N = [7000, 0, 0]
+    scStateInMsgData.r_BN_N = r_BN_N
     scStateInMsg = messaging.SCStatesMsg().write(scStateInMsgData)
 
     sunStateInMsgData = messaging.SpicePlanetStateMsgPayload()
-    sunStateInMsgData.PositionVector = [0, 149000000000.0, 0]
+    sunStateInMsgData.PositionVector = r_SN_N
     sunStateInMsg = messaging.SpicePlanetStateMsg().write(sunStateInMsgData)
 
     earthStateInMsgData = messaging.SpicePlanetStateMsgPayload()
-    earthStateInMsgData.PositionVector = [0, 0, 0]
+    earthStateInMsgData.PositionVector = r_EN_N
     earthStateInMsg = messaging.SpicePlanetStateMsg().write(earthStateInMsgData)
 
     # subscribe input messages to module
@@ -100,21 +134,55 @@ def dentonFluxModelTestFunction(show_plots, param1, param2, accuracy):
     fluxOutMsgRec = module.fluxOutMsg.recorder()
     unitTestSim.AddModelToTask(unitTaskName, fluxOutMsgRec)
 
+    # run simulation
     unitTestSim.InitializeSimulation()
-    unitTestSim.ConfigureStopTime(macros.sec2nano(0.5))
-    unitTestSim.ExecuteSimulation()
+    unitTestSim.TotalSim.SingleStepProcesses()
 
-    # pull module data and make sure it is correct
+    # pull module data
+    EnergyData = fluxOutMsgRec.energies[0][0:module.numOutputEnergies]
+    ElectronFluxData = np.log10(fluxOutMsgRec.meanElectronFlux[0][0:module.numOutputEnergies])
+    IonFluxData = np.log10(fluxOutMsgRec.meanIonFlux[0][0:module.numOutputEnergies])
 
+    # load true data from corresponding support file (note that Python indexing starts at 0 and Fortran indexing
+    # starts at 1, relevant for Kp index)
+    filename = 'FluxData_' + str(param1_Kp+1) + '_' + str("%.2f" % param2_LT) + '.txt'
+    filepath = path + '/Support/' + filename
+    with open(filepath, 'r') as file:
+        rows = np.loadtxt(file, delimiter=",", unpack=False)
+        trueEnergyData = rows[0]
+        trueElectronFluxData = rows[1]
+        trueIonFluxData = rows[2]
+
+    # make sure module output data is correct
+    ParamsString = ' for Kp-Index=' + str(param1_Kp) + ', LT=' + str(param2_LT)
+    testFailCount, testMessages = unitTestSupport.compareDoubleArray(
+        trueEnergyData, EnergyData, accuracy, ('electron and ion energies' + ParamsString),
+        testFailCount, testMessages)
+    testFailCount, testMessages = unitTestSupport.compareDoubleArray(
+        trueElectronFluxData, ElectronFluxData, accuracy, ('electron and ion energies' + ParamsString),
+        testFailCount, testMessages)
+    testFailCount, testMessages = unitTestSupport.compareDoubleArray(
+        trueIonFluxData, IonFluxData, accuracy, ('electron and ion energies' + ParamsString),
+        testFailCount, testMessages)
+
+    # print out success or failure message
     if testFailCount == 0:
         print("PASSED: " + module.ModelTag)
+        print("This test uses an accuracy value of " + str(accuracy) + " (true values don't have higher precision)")
     else:
+        print("FAILED " + module.ModelTag)
         print(testMessages)
 
-    return [testFailCount, "".join(testMessages)]
+    plt.figure(1)
+    fig = plt.gcf()
+    ax = fig.gca()
+    plt.plot(EnergyData, ElectronFluxData)
+
+    if show_plots:
+        plt.show()
+
+    return [testFailCount, ''.join(testMessages)]
 
 
 if __name__ == "__main__":
-    test_dentonFluxModel(False, 1, 1, 1e-12)
-
-
+    test_dentonFluxModel(False, Kps[1], LTs[0], z_offsets[1], r_EN_Ns[1], 1e-6)
--- src/simulation/environment/dentonFluxModel/dentonFluxModel.rst
+++ src/simulation/environment/dentonFluxModel/dentonFluxModel.rst
@@ -1,6 +1,10 @@
 Executive Summary
 -----------------
-This module provides the 10-year averaged GEO elecon and ion flux as discussed in the paper by Denton.
+This module provides the GEO electron and ion flux based on 82 satellite-years as discussed in `this paper <https://doi
+.org/10.1002/2015SW001168>`__ by M. H. Denton and on `this website <https://gemelli.spacescience.org/mdenton/>`__.
+A Fortran module can be downloaded from this website which computes the electron and ion flux for a given Kp index,
+local time and particle energy, and also includes the relevant flux data. The Basilisk module only uses the mean
+flux data.
 
 
 Message Connection Descriptions
@@ -9,6 +13,13 @@ The following table lists all the module input and output messages.
 The module msg connection is set by the user from python.  
 The msg type contains a link to the message structure definition, while the description 
 provides information on what this message is used for.
+
+.. _ModuleIO_Denton_Flux_Model:
+.. figure:: /../../src/simulation/environment/dentonFluxModel/_Documentation/Images/moduleDentonFluxModel.svg
+    :align: center
+
+    Figure 1: ``dentonFluxModel()`` Module I/O Illustration
+
 
 .. list-table:: Module I/O Messages
     :widths: 25 25 50
@@ -30,3 +41,37 @@ provides information on what this message is used for.
       - :ref:`PlasmaFluxMsgPayload`
       - output ion and electron fluxes
 
+Module Assumptions and Limitations
+----------------------------------
+This module assumes that the position vectors of the spacecraft, Sun and Earth are expressed in an Earth equatorial
+frame with the z-component perpendicular to the equatorial plane. The local time of the spacecraft is determined by
+projecting the spacecraft and Sun position vectors onto the equatorial plane and computing the angle between the
+resulting projected vectors. When both projected vectors point in the same direction, then the local time (LT) is 12
+hours.
+
+The particle energies are limited between 100 eV and 40 keV. The lower limit of 100 eV is recommended by M. H.
+Denton, because the fluxes of smaller energies are unreliable due to contamination with secondary electrons and
+photoelectrons. The flux data provided by Denton does not provide information above 40 keV.
+
+Since the flux data is only valid for the GEO regime, a BSK warning message is generated if the spacecraft location is
+more than 4,000 km away from Geostationary Orbit (equatorial orbit with radius of 42,000 km).
+
+User Guide
+----------
+The ETcontrol module is created using:
+
+.. code-block:: python
+    :linenos:
+
+    FluxModule = dentonFluxModel.DentonFluxModel()
+    FluxModule.ModelTag = "dentonFluxModule"
+    FluxModule.dataPath = bskPath + '/supportData/DentonGEO/'
+    scSim.AddModelToTask(dynTaskName, FluxModule)
+
+The Kp index (kpIndex) and number of output energies (numOutputEnergies) must be added to FluxModule as well, while
+the local time is computed within the module using the position vectors of the spacecraft, Sun and Earth.
+
+Note that the `Kp index <https://www.spaceweatherlive.com/en/help/the-kp-index.html>`__ (global geomagnetic activity
+index) ranges from 0 to 9, with sub-indices '-','o' and '+' and therefore a total number of 28 indices (Kp index 0-
+and 9+ do not exist). Instead of the actual Kp index, this module uses a number between 0 and 27 to specify the Kp
+index. For example, a Kp index value of 5 in this module corresponds to an actual Kp index of 2-.
