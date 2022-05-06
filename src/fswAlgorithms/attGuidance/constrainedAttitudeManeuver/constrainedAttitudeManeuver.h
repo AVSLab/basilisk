3c32089354adbfd14168f7cbecefa33329081240
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2022-05-05 21:09:35 -0600

    [HOT-FIX] separated out the variable definitions so the definition string is setup for each variable (avoids sphinx build warnings)
    Former-commit-id: 6508b7e40c9ba21525db9c05dd98fc123e01128f
    Former-commit-id: faf2dbed8e038dfcb3bc34e053f8ef78550f256b

--- src/fswAlgorithms/attGuidance/constrainedAttitudeManeuver/constrainedAttitudeManeuver.h
+++ src/fswAlgorithms/attGuidance/constrainedAttitudeManeuver/constrainedAttitudeManeuver.h
@@ -35,15 +35,19 @@
 //! @brief The constraintStruc structure is used to store the inertial direction of the keep-in and keep-out zones
 struct constraintStruct {
 
-    double keepOutDir_N[3], keepInDir_N[3];                         //!< Inertial direction of keepIn and keepOut celestial bodies
-    bool keepOut, keepIn;                                           //!< Flags to assess whether keepIn and keepOut constraints are being considered
+    double keepOutDir_N[3];                                         //!< Inertial direction of keepOut celestial bodies
+    double keepInDir_N[3];                                          //!< Inertial direction of keepIn celestial bodies
+    bool keepOut;                                                   //!< Flag to assess whether keepOut constraints are being considered
+    bool keepIn;                                                    //!< Flag to assess whether keepIn constraints are being considered
 };
 
 //! @brief The scBoresightStruc structure is used to store the body frame directions and fields of view of the instruments
 struct scBoresightStruct {
 
-    double keepOutBoresight_B[10][3], keepInBoresight_B[10][3];     //!< Unit vectors containing body frame directions of keepIn and keepOut instruments
-    double keepOutFov[10], keepInFov[10];                           //!< Fields of view of the keepIn and keepOut instruments
+    double keepOutBoresight_B[10][3];                               //!< Unit vectors containing body frame directions of keepOut instruments
+    double keepInBoresight_B[10][3];                                //!< Unit vectors containing body frame directions of keepIn instruments
+    double keepOutFov[10];                                          //!< Fields of view of the keepOut instruments
+    double keepInFov[10];                                           //!< Fields of view of the keepIn instruments
     int keepOutBoresightCount = 0;                                  //!< Number of keepOut sensitive instruments
     int keepInBoresightCount = 0;                                   //!< Number of keepIn instruments
 };
