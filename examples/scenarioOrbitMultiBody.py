4cc685123a2f5aa5c7c4c19f5b7ae2eeb6b60f9f
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2022-05-20 16:26:51 -0600

    tutorial on using multiple gravitational bodies
    Former-commit-id: f20c820e30a9bbc1bd7ab649057a72a9043cb251
    Former-commit-id: d48420e49462ea3e34e80dae6ae4dc1ac97d15ca

--- examples/scenarioOrbitMultiBody.py
+++ examples/scenarioOrbitMultiBody.py
@@ -17,6 +17,11 @@
 #
 
 r"""
+
+.. raw:: html
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/oHCyeM1-mKI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 Overview
 --------
 
