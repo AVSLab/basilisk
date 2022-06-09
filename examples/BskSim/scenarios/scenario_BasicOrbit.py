da29387d7ef42d10d31621f056047d60ed58a907
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2022-06-09 11:32:11 -0600

    add bskSim tutorial videos
    Former-commit-id: 2ed37d197d1ace480d7c869fd3c794662bd22e41
    Former-commit-id: 178b41b6429dc1b0e5f4e21e2a9455d775f79a90

--- examples/BskSim/scenarios/scenario_BasicOrbit.py
+++ examples/BskSim/scenarios/scenario_BasicOrbit.py
@@ -17,6 +17,11 @@
 #
 
 """
+
+.. raw:: html
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YjRM_3MOjAc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 Overview
 --------
 
