a63f555e515fd2aaa837ccf5f574b436dc3dc7e7
Mark Stephenson <mark2000stephenson@gmail.com>
2022-09-01 10:17:09 -0600

    Deorbit scenario unit test
    Former-commit-id: 6aba88a6b4c2f99024437439cf17263cf367f8c7
    Former-commit-id: 9b9c127b663f398a3428c3c23a6b57470aab34f2

---
+++ src/tests/test_scenarioDragDeorbit.py
@@ -0,0 +1,87 @@
+# ISC License
+#
+# Copyright (c) 2022, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+#
+# Basilisk Scenario Script and Integrated Test
+#
+# Purpose:  Demonstration of deorbit using exponentialAtmosphere and dragDynamicEffector modules.
+# Author:   Mark Stephenson
+# Creation Date:  Aug. 31, 2022
+#
+
+import sys, os, inspect
+import pytest
+from Basilisk.utilities import unitTestSupport
+
+# Get current file path
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+sys.path.append(path + '/../../examples')
+import scenarioDragDeorbit
+
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+
+# uncomment this line if this test has an expected failure, adjust message as needed
+# @pytest.mark.xfail(True, reason="Previously set sim parameters are not consistent with new formulation\n")
+
+
+# The following 'parametrize' function decorator provides the parameters and expected results for each
+#   of the multiple test runs for this test.
+@pytest.mark.parametrize("initial_alt, deorbit_alt", [
+    (250, 150)
+])
+@pytest.mark.scenarioTest
+def test_scenarioDragDeorbit(show_plots, initial_alt, deorbit_alt):
+    """This function is called by the py.test environment."""
+    # each test method requires a single assert method to be called
+    # provide a unique test method name, starting with test_
+
+    testFailCount = 0  # zero unit test result counter
+    testMessages = []  # create empty array to store test log messages
+
+    try:
+        figureList = scenarioDragDeorbit.run(show_plots, initial_alt, deorbit_alt)
+        # save the figures to the Doxygen scenario images folder
+        for pltName, plt in list(figureList.items()):
+            unitTestSupport.saveScenarioFigure(pltName, plt, path)
+
+    except OSError as err:
+        testFailCount += 1
+        testMessages.append("scenarioDragDeorbit test are failed.")
+
+    #   print out success message if no error were found
+    if testFailCount == 0:
+        print("PASSED ")
+    else:
+        print(testFailCount)
+        print(testMessages)
+
+    # each test method requires a single assert method to be called
+    # this check below just makes sure no sub-test failures were found
+
+    assert testFailCount < 1, testMessages
+
+
+if __name__ == "__main__":
+    test_scenarioDragDeorbit(
+        False,  # show_plots
+        initial_alt=250,
+        deorbit_alt=150
+    )
