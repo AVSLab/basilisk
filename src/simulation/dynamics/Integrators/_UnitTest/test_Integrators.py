489c3069270565d03fcb56a1f8a5ced5530d7aba
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2022-11-28 07:46:53 -0700

    [HOT-FIX] apparently the CI Windows build didn't like this newer matplotlib command.  Wasn't required, just a nice to have.  So, I'm removing these offending statements to make all CI builds pass.
    Former-commit-id: 3aaa717e7dc8121042637f45af6cd8606a102eac
    Former-commit-id: e53edc6ff2310ac9fea8b01f70acd1eafa3055e9

--- src/simulation/dynamics/Integrators/_UnitTest/test_Integrators.py
+++ src/simulation/dynamics/Integrators/_UnitTest/test_Integrators.py
@@ -185,7 +185,6 @@ def run(doUnitTests, show_plots, integratorCase):
     plt.axis(np.array([-oe.rApoap, oe.rPeriap, -b, b]) / 1000 * 1.25)
     # draw the planet
     fig = plt.gcf()
-    fig.set_layout_engine('tight')
     ax = fig.gca()
     planetColor = '#008800'
     planetRadius = earth.radEquator / 1000
