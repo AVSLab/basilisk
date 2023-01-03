fa82fe342a2a8c01b5202b68cd9413298c84e052
Patrick Kenneally <patrick.kenneally@gmail.com>
2023-01-03 15:19:30 -0700

    Format CMakeLists.txt and *.cmake files

--- src/cmake/usingOpenCV.cmake
+++ src/cmake/usingOpenCV.cmake
@@ -1,23 +1,22 @@
 find_package(opencv CONFIG REQUIRED)
 
-# The following code is added to work around conan issues with Xcode not seeing the release
-# version in the IDE and giving false errors
-get_property(INCLUDE_DIRECTORY TARGET opencv::opencv
-             PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+# The following code is added to work around conan issues with Xcode not seeing the release version in the IDE and
+# giving false errors
+get_property(
+  INCLUDE_DIRECTORY
+  TARGET opencv::opencv
+  PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
 if(DEFINED opencv_INCLUDE_DIRS_DEBUG)
-    set(DIRECTORIES "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_DEBUG}>"
-                                     "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_DEBUG}>"
-                                     "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_DEBUG}>"
-                                     "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_DEBUG}>"
-                                     )
+  set(DIRECTORIES
+      "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_DEBUG}>" "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_DEBUG}>"
+      "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_DEBUG}>" "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_DEBUG}>")
 else()
-    set(DIRECTORIES "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_RELEASE}>"
-                                     "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_RELEASE}>"
-                                     "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_RELEASE}>"
-                                     "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_RELEASE}>")
+  set(DIRECTORIES
+      "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_RELEASE}>" "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_RELEASE}>"
+      "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_RELEASE}>"
+      "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_RELEASE}>")
 endif()
 set_property(TARGET opencv::opencv PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${DIRECTORIES})
-
 
 list(APPEND CUSTOM_DEPENDENCIES opencv::opencv)
 
--- src/fswAlgorithms/imageProcessing/centerRadiusCNN/Custom.cmake
+++ src/fswAlgorithms/imageProcessing/centerRadiusCNN/Custom.cmake
@@ -5,8 +5,7 @@ if(BUILD_OPNAV)
     link_libraries("-march=haswell -mno-lzcnt")
   endif()
 
-
 else()
-  MESSAGE("SKIPPED: ${TARGET_NAME}")
+  message("SKIPPED: ${TARGET_NAME}")
   set(CUSTOM_DEPENDENCIES_HANDLED 1)
 endif()
