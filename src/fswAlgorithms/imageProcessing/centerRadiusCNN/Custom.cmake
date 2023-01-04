b66c5dd9e43fc028b5b5ce6f1df4e652f3bffcbf
Patrick Kenneally <patrick.kenneally@gmail.com>
2023-01-03 20:37:09 -0700

    Separate excluded targets and their dependencies
    Previously the custom.cmake file was executed with include() prior
    to the definition of the compilation target with swig_add_library(). Splitting
    the exclusion options from the custom.cmake executions, allows for the
    custom.cmake to have access to the compilation target and define includes
    and libraries for the target. As a result build dependencies and config can
    be isolated and localized to the module's custom.cmake, decreasing
    coupling and increasing compilation unit cohesion.

--- src/fswAlgorithms/imageProcessing/centerRadiusCNN/Custom.cmake
+++ src/fswAlgorithms/imageProcessing/centerRadiusCNN/Custom.cmake
@@ -1,11 +1,4 @@
-if(BUILD_OPNAV)
-  include(usingOpenCV)
-
-  if(APPLE)
-    link_libraries("-march=haswell -mno-lzcnt")
-  endif()
-
-else()
-  message("SKIPPED: ${TARGET_NAME}")
-  set(CUSTOM_DEPENDENCIES_HANDLED 1)
+include(usingOpenCV)
+if(APPLE)
+  link_libraries("-march=haswell -mno-lzcnt")
 endif()
--- src/simulation/sensors/camera/Custom.cmake
+++ src/simulation/sensors/camera/Custom.cmake
@@ -1,14 +1,9 @@
-if(BUILD_OPNAV)
-  #  find_package(opencv CONFIG REQUIRED)
-  #
-  #  set(CUSTOM_CMAKE_BUILD_TARGETS opencv::opencv)
-  #
-  #  set(CMAKE_INCLUDE_CURRENT_DIR TRUE)
-  #  if(APPLE)
-  #    link_libraries("-framework OpenCL")
-  #  endif()
-  include(usingOpenCV)
-else()
-  message("SKIPPED: ${TARGET_NAME}")
-  set(CUSTOM_DEPENDENCIES_HANDLED 1)
-endif()
+#  find_package(opencv CONFIG REQUIRED)
+#
+#  set(CUSTOM_CMAKE_BUILD_TARGETS opencv::opencv)
+#
+#  set(CMAKE_INCLUDE_CURRENT_DIR TRUE)
+#  if(APPLE)
+#    link_libraries("-framework OpenCL")
+#  endif()
+include(usingOpenCV)
