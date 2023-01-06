41745547d996961d3b7c6c9e62514de742f20d4f
Leah Kiner <leki4360@colorado.edu>
2023-01-05 20:12:16 -0700

    Write the updateEnergyMomContributions() method
    This method calculates the contributions of the
    effector to the energy and momentum of the spacecraft

--- src/simulation/dynamics/prescribedMotion/prescribedMotionStateEffector.cpp
+++ src/simulation/dynamics/prescribedMotion/prescribedMotionStateEffector.cpp
@@ -221,6 +221,20 @@ void PrescribedMotionStateEffector::computeDerivatives(double integTime, Eigen::
 /*! This method is for calculating the contributions of the effector to the energy and momentum of the spacecraft */
 void PrescribedMotionStateEffector::updateEnergyMomContributions(double integTime, Eigen::Vector3d & rotAngMomPntCContr_B, double & rotEnergyContr, Eigen::Vector3d omega_BN_B)
 {
+    // Update omega_BN_B and omega_FN_B
+    this->omega_BN_B = omega_BN_B;
+    this->omegaTilde_BN_B = eigenTilde(this->omega_BN_B);
+    this->omega_FN_B = this->omega_FB_B + this->omega_BN_B;
+
+    // Compute rDot_FcB_B
+    this->rDot_FcB_B = this->rPrime_FcB_B + this->omegaTilde_BN_B * this->r_FcB_B;
+
+    // Find the rotational angular momentum contribution from hub
+    rotAngMomPntCContr_B = this->IPntFc_B * this->omega_FN_B + this->mass * this->rTilde_FcB_B * this->rDot_FcB_B;
+
+    // Find the rotational energy contribution from the hub
+    rotEnergyContr = 0.5 * this->omega_FN_B.dot(this->IPntFc_B * this->omega_FN_B) + 0.5 * this->mass * this->rDot_FcB_B.dot(this->rDot_FcB_B);
+
     return;
 }
 
