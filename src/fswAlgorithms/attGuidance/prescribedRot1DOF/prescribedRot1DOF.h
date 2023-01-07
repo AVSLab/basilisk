51f92926460f0bddb81acdbbf8d9ffcdb549d056
Leah Kiner <leki4360@colorado.edu>
2023-01-07 13:17:29 -0700

    Add the module variables

--- src/fswAlgorithms/attGuidance/prescribedRot1DOF/prescribedRot1DOF.h
+++ src/fswAlgorithms/attGuidance/prescribedRot1DOF/prescribedRot1DOF.h
@@ -20,10 +20,33 @@
 #define _PRESCRIBEDROT1DOF_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "architecture/utilities/bskLogging.h"
 
 /*! @brief Top level structure for the sub-module routines. */
 typedef struct {
+
+    /* User configurable variables */
+    double thetaDDotMax;                                        //!< [rad/s^2] Maximum angular acceleration of spinning body
+    double rotAxis_M[3];                                        //!< Rotation axis for the maneuver in M frame components
+    double r_FM_M[3];                                           //!< [m] Position of the F frame origin with respect to the M frame origin in M frame components (fixed)
+    double rPrime_FM_M[3];                                      //!< [m/s] B frame time derivative of r_FM_M in M frame components (fixed)
+    double rPrimePrime_FM_M[3];                                 //!< [m/s^2] B frame time derivative of rPrime_FM_M in M frame components (fixed)
+    double omega_FM_F[3];                                       //!< [rad/s] Angular velocity of frame F wrt frame M in F frame components
+    double omegaPrime_FM_F[3];                                  //!< [rad/s^2] B frame time derivative of omega_FM_F in F frame components
+    double sigma_FM[3];                                         //!< MRP attitude of frame F with respect to frame M
+
+    /* Private variables */
+    bool convergence;                                           //!< Boolean variable is true when the maneuver is complete
+    double tInit;                                               //!< [s] Simulation time at the beginning of the maneuver
+    double thetaInit;                                           //!< [rad] Initial spinning body angle from frame M to frame F about rotAxis_M
+    double thetaDotInit;                                        //!< [rad/s] Initial spinning body angle rate between frame M to frame F
+    double thetaRef;                                            //!< [rad] Reference angle from frame M to frame F about rotAxis_M
+    double thetaDotRef;                                         //!< [rad/s] Reference angle rate between frame M to frame F
+    double ts;                                                  //!< [s] The simulation time halfway through the maneuver (switch time for ang accel)
+    double tf;                                                  //!< [s] Simulation time when the maneuver is finished
+    double a;                                                   //!< Parabolic constant for the first half of the maneuver
+    double b;                                                   //!< Parabolic constant for the second half of the maneuver
 
     BSKLogger *bskLogger;                                       //!< BSK Logging
 
