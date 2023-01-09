1f8233aa41cdda41d97a44aaaca771d4a1d753e0
Riccardo <riccardo.calaon@colorado.edu>
2023-01-09 11:11:40 +0100

    Created initial commit with module files. Module does nothing.

---
+++ src/fswAlgorithms/attGuidance/solarArrayReference/solarArrayReference.h
@@ -0,0 +1,57 @@
+/*
+ ISC License
+
+ Copyright (c) 2022, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef _SOLAR_ARRAY_REFERENCE_
+#define _SOLAR_ARRAY_REFERENCE_
+
+#include <stdint.h>
+#include "architecture/utilities/bskLogging.h"
+#include "cMsgCInterface/NavAttMsg_C.h"
+#include "cMsgCInterface/AttRefMsg_C.h"
+#include "cMsgCInterface/SpinningBodyMsg_C.h"
+
+#define EPS 1e-6                    //!< accuracy to check if a variable is zero
+
+/*! @brief Top level structure for the sub-module routines. */
+typedef struct {
+
+    /* declare module IO interfaces */
+    NavAttMsg_C         attNavInMsg;               //!< input msg measured attitude
+    AttRefMsg_C         attRefInMsg;               //!< input attitude reference message
+    SpinningBodyMsg_C   spinningBodyInMsg;         //!< input spinning body message
+    SpinningBodyMsg_C   spinningBodyRefOutMsg;     //!< output msg containing spinning body target angle and angle rate
+
+    BSKLogger *bskLogger;                         //!< BSK Logging
+
+}solarArrayReferenceConfig;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void SelfInit_solarArrayReference(solarArrayReferenceConfig *configData, int64_t moduleID);
+    void Reset_solarArrayReference(solarArrayReferenceConfig *configData, uint64_t callTime, int64_t moduleID);
+    void Update_solarArrayReference(solarArrayReferenceConfig *configData, uint64_t callTime, int64_t moduleID);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
