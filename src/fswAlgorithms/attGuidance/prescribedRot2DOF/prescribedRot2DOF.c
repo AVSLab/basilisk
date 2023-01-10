8c1466dad9b701ebf2f6fb9317ad1036b0537280
Leah Kiner <leki4360@colorado.edu>
2023-01-10 11:00:59 -0700

    Create the module files

---
+++ src/fswAlgorithms/attGuidance/prescribedRot2DOF/prescribedRot2DOF.c
@@ -0,0 +1,52 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+/* Include the module header file. */
+#include "prescribedRot2DOF.h"
+
+/*! This method initializes the output message for this module.
+ @return void
+ @param configData The configuration data associated with this module
+ @param moduleID The module identifier
+ */
+void SelfInit_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, int64_t moduleID)
+{
+}
+
+/*! This method performs a complete reset of the module.  Local module variables that retain
+ time varying states between function calls are reset to their default values.
+ @return void
+ @param configData The configuration data associated with the module
+ @param callTime [ns] Time the method is called
+ @param moduleID The module identifier
+*/
+void Reset_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, uint64_t callTime, int64_t moduleID)
+{
+}
+
+/*! This method profiles a 1DOF rotational trajectory given two rotation angles and rotation axes. The prescribed states
+are updated in this routine as a function of time and written to the prescribedMotion output message.
+ @return void
+ @param configData The configuration data associated with the module
+ @param callTime [ns] Time the method is called
+ @param moduleID The module identifier
+*/
+void Update_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, uint64_t callTime, int64_t moduleID)
+{
+}
---
+++ src/fswAlgorithms/attGuidance/prescribedRot2DOF/prescribedRot2DOF.h
@@ -0,0 +1,40 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _PRESCRIBEDROT2DOF_
+#define _PRESCRIBEDROT2DOF_
+
+/*! Include the required files. */
+#include <stdint.h>
+
+/*! @brief Top level structure for the sub-module routines. */
+typedef struct
+{
+}PrescribedRot2DOFConfig;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void SelfInit_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, int64_t moduleID);                         //<! Method for initializing the module
+    void Reset_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, uint64_t callTime, int64_t moduleID);         //<! Method for resetting the module
+    void Update_prescribedRot2DOF(PrescribedRot2DOFConfig *configData, uint64_t callTime, int64_t moduleID);        //<! Method for the updating the module
+#ifdef __cplusplus
+}
+#endif
+
+#endif
