1febe2467713e68380105fc111ad0b68f7271b1b
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2023-02-19 12:56:01 -0700

    Finally, we can remove the old dynPtr variable as it is no longer used in any module

--- src/simulation/dynamics/_GeneralModuleFiles/stateVecIntegrator.h
+++ src/simulation/dynamics/_GeneralModuleFiles/stateVecIntegrator.h
@@ -35,8 +35,6 @@ public:
     virtual void integrate(double currentTime, double timeStep) = 0; //!< class method
     std::vector<DynamicObject*> dynPtrs; //!< This is an object that contains the method equationsOfMotion(), also known as the F function.
 
-protected:
-    DynamicObject* dynPtr; //!< This is an object that contains the method equationsOfMotion(), also known as the F function.
 };
 
 
