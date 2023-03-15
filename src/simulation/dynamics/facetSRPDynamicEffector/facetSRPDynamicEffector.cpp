e16d13fdde9a60d4d192b3269750bc4391192f68
Leah Kiner <leki4360@colorado.edu>
2023-03-15 00:37:41 -0600

    Add the module files

---
+++ src/simulation/dynamics/facetSRPDynamicEffector/facetSRPDynamicEffector.cpp
@@ -0,0 +1,88 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#include "facetSRPDynamicEffector.h"
+
+/*! The constructor initializes the member variables to zero. */
+FacetSRPDynamicEffector::FacetSRPDynamicEffector()
+{
+}
+
+/*! The destructor. */
+FacetSRPDynamicEffector::~FacetSRPDynamicEffector()
+{
+}
+
+/*! The reset member function. This method checks to ensure the input message is linked.
+ @return void
+ @param currentSimNanos [ns]  Time the method is called
+*/
+void FacetSRPDynamicEffector::Reset(uint64_t currentSimNanos)
+{
+}
+
+/*! The SRP dynamic effector does not write any output messages.
+ @return void
+ @param currentClock [ns] Time the method is called
+*/
+void FacetSRPDynamicEffector::writeOutputMessages(uint64_t currentClock)
+{
+}
+
+/*! This member function populates the spacecraft geometry structure with user-input facet information.
+ @return void
+ @param area  [m^2] Facet area
+ @param specCoeff  Facet spectral reflection optical coefficient
+ @param diffCoeff  Facet diffuse reflection optical coefficient
+ @param normal_B  Facet normal expressed in B frame components
+ @param locationPntB_B  [m] Facet location wrt point B in B frame components
+*/
+void FacetSRPDynamicEffector::addFacet(double area,
+                                       double specCoeff,
+                                       double diffCoeff,
+                                       Eigen::Vector3d normal_B,
+                                       Eigen::Vector3d locationPntB_B)
+{
+}
+
+/*! This method is used to link the faceted SRP effector to the hub attitude and position,
+which are required for calculating SRP forces and torques.
+ @return void
+ @param states  Dynamic parameter states
+*/
+void FacetSRPDynamicEffector::linkInStates(DynParamManager& states)
+{
+}
+
+/*! This method computes the body forces and torques for the SRP effector.
+ @return void
+ @param integTime  [s] Time the method is called
+ @param timeStep  [s] Simulation time step
+*/
+void FacetSRPDynamicEffector::computeForceTorque(double integTime, double timeStep)
+{
+}
+
+/*! This is the UpdateState() method
+ @return void
+ @param currentSimNanos [ns] Time the method is called
+*/
+void FacetSRPDynamicEffector::UpdateState(uint64_t currentSimNanos)
+{
+}
---
+++ src/simulation/dynamics/facetSRPDynamicEffector/facetSRPDynamicEffector.h
@@ -0,0 +1,45 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+
+#ifndef FACET_SRP_DYNAMIC_EFFECTOR_H
+#define FACET_SRP_DYNAMIC_EFFECTOR_H
+
+#include <Eigen/Dense>
+#include <vector>
+#include "simulation/dynamics/_GeneralModuleFiles/dynamicEffector.h"
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+
+/*! @brief Faceted Solar Radiation Pressure Dynamic Effector */
+class FacetSRPDynamicEffector: public SysModel, public DynamicEffector
+{
+public:                                                             
+    FacetSRPDynamicEffector();                                      //!< The module constructor
+    ~FacetSRPDynamicEffector();                                     //!< The module destructor
+    void linkInStates(DynParamManager& states);                     //!< Method for giving the effector access to the hub states
+    void computeForceTorque(double integTime, double timeStep);     //!< Method for computing the SRP force and torque about point B
+    void Reset(uint64_t currentSimNanos) override;                  //!< Reset method
+    void UpdateState(uint64_t currentSimNanos) override;            //!< Method for updating the effector states
+    void writeOutputMessages(uint64_t currentClock);                //!< Method for writing the output messages
+    void addFacet(double area, double specCoeff, double diffCoeff, Eigen::Vector3d normal_B, Eigen::Vector3d locationPntB_B);  //!< Method for adding facets to the spacecraft geometry structure
+
+private:
+};
+
+#endif 
