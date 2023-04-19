f6d73b7d08c53b8f6ec6c417736e5718e28aa9e1
Thibaud Teil <thibaud.teil@colorado.edu>
2023-04-19 14:33:29 -0600

    Add optical flow module including custom cmake

---
+++ src/fswAlgorithms/imageProcessing/opticalFlow/opticalFlow.cpp
@@ -0,0 +1,191 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+#include "opticalFlow.h"
+
+OpticalFlow::OpticalFlow() = default;
+
+OpticalFlow::~OpticalFlow() = default;
+
+/*! This method performs a complete reset of the module.  Local module variables that retain time varying states
+ * between function calls are reset to their default values.
+ @return void
+ @param CurrentSimNanos The clock time at which the function was called (nanoseconds)
+ */
+void OpticalFlow::Reset(uint64_t CurrentSimNanos)
+{
+    // check that the required message has not been connected
+    if (!this->imageInMsg.isLinked()) {
+        bskLogger.bskLog(BSK_ERROR, "opticalFlow.imageInMsg wasn't connected.");
+    }
+}
+
+/*! This method reads the images and follows the following high level logic:
+ * If no image has been saved from previous time steps and a new image is present, save it and make a mask
+ * If an image was previously saved and a new image arrives, perform the feature matching
+ * Return the features and clear the memory of both previous images
+ @return void
+ @param CurrentSimNanos The clock time at which the function was called (nanoseconds)
+ */
+void OpticalFlow::UpdateState(uint64_t CurrentSimNanos)
+{
+    std::string dirName;
+    CameraImageMsgPayload imageBuffer;
+    PairedKeyPointsMsgPayload featurePayload;
+
+    imageBuffer = this->imageInMsg.zeroMsgPayload;
+    featurePayload = this->keyPointsMsg.zeroMsgPayload;
+
+    /*! - Read in the bitmap*/
+    imageBuffer = this->imageInMsg();
+
+    this->sensorTimeTag = 0;
+    /* Added for debugging purposes*/
+    if (!this->filename.empty()){
+        this->newImage = cv::imread(this->filename, cv::IMREAD_COLOR);
+        this->sensorTimeTag = CurrentSimNanos;
+        this->newImagePresent = true;
+        this->filename = "";
+    }
+    else if(imageBuffer.valid == 1 && imageBuffer.timeTag > this->oldTimeTag){
+        /*! - Recast image pointer to CV type*/
+        std::vector<unsigned char> vectorBuffer((char*)imageBuffer.imagePointer,
+                                                (char*)imageBuffer.imagePointer + imageBuffer.imageBufferLength);
+        this->newImage = cv::imdecode(vectorBuffer, cv::IMREAD_COLOR);
+
+        this->sensorTimeTag = imageBuffer.timeTag;
+        this->newImagePresent = true;
+    }
+
+    NavAttMsgPayload navAttBuffer;
+    navAttBuffer = this->attitudeMsg();
+    double dtBetweenImagesSeconds;
+    dtBetweenImagesSeconds = (double)(this->sensorTimeTag - this->oldTimeTag)*NANO2SEC;
+    /*! - If there is a new image and an old image, write the paired features message */
+    if (this->oldImagePresent && this->newImagePresent && dtBetweenImagesSeconds >= this->minTimeBetweenPairs){
+        cv::cvtColor(this->newImage, this->newImage, cv::COLOR_BGR2GRAY);
+
+        std::vector<uchar> status;
+        std::vector<float> err;
+        auto criteria = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS,
+                                                     this->criteriaMaxCount,
+                                                     this->criteriaEpsilon);
+        cv::calcOpticalFlowPyrLK(this->oldImage,
+                                 this->newImage,
+                                 this->oldFeatures,
+                                 this->newFeatures,
+                                 status,
+                                 err,
+                                 cv::Size(this->flowSearchSize,this->flowSearchSize),
+                                 this->flowMaxLevel,
+                                 criteria);
+
+        featurePayload.timeTagImageNew = this->sensorTimeTag;
+        featurePayload.timeTagImageOld = this->oldTimeTag;
+
+        v3Copy(this->oldAttitude, featurePayload.sigmaBNImageOld);
+        v3Copy(navAttBuffer.sigma_BN, featurePayload.sigmaBNImageNew);
+
+        for (int i=0; i<this->newFeatures.size(); i++){
+            featurePayload.keyPointsImageOld[2*i] = this->oldFeatures[i][0];
+            featurePayload.keyPointsImageOld[2*i+1] = this->oldFeatures[i][1];
+            featurePayload.keyPointsImageNew[2*i] = this->newFeatures[i][0];
+            featurePayload.keyPointsImageNew[2*i+1] = this->newFeatures[i][1];
+        }
+        featurePayload.cameraID = imageBuffer.cameraID;
+        featurePayload.keyPointsFound = (uint64_t)this->newFeatures.size();
+        featurePayload.valid = true;
+
+        this->keyPointsMsg.write(&featurePayload, this->moduleID, CurrentSimNanos);
+
+        this->newImagePresent = false;
+        this->oldFeatures = this->newFeatures;
+        /*! Then reset values for next pair of images */
+        if (this->slidingWindowImages){
+            this->oldImagePresent = true;
+            v3Copy(navAttBuffer.sigma_BN, this->oldAttitude);
+            this->oldTimeTag = this->sensorTimeTag;
+        }
+        else{
+            this->oldImagePresent = false;
+            v3SetZero(this->oldAttitude);
+            this->oldTimeTag = 0;
+        }
+    }
+    /*! - If there is a new image but now old image, populate the old image buffers */
+    else if(this->newImagePresent){
+        cv::cvtColor(this->newImage, this->oldImage, cv::COLOR_BGR2GRAY);
+        cv::Mat mask(this->oldImage.size(), CV_8UC1, cv::Scalar(255));
+
+        OpticalFlow::makeMask(this->oldImage, mask);
+        cv::goodFeaturesToTrack(this->oldImage,
+                                this->newFeatures,
+                                this->maxNumberFeatures,
+                                this->qualityLevel,
+                                this->minumumFeatureDistance,
+                                 mask,
+                                this->blockSize,
+                                false,
+                                0.04);
+
+        v3Copy(navAttBuffer.sigma_BN, this->oldAttitude);
+        this->oldTimeTag = this->sensorTimeTag;
+        this->oldFeatures = this->newFeatures;
+        this->oldImagePresent = true;
+        this->newImagePresent = false;
+    }
+    /*! - If no new image is present, write zeros in message and set valid to false*/
+    else{
+        featurePayload.valid = false;
+        this->keyPointsMsg.write(&featurePayload, this->moduleID, CurrentSimNanos);
+        }
+
+}
+
+/*! This method reads an black and white image and makes a mask in order to remove the limb of the target.
+ * First the contours are found and drawn on an image.
+ * Second the main contour is filled with black
+ * Third the image is blured and thresholded to remove roughness on the limb
+ * Finally the edge is dilated to increase the margin off the limb
+ @return void
+ @param inputBWImage cv::Mat of the input image
+ @param mask cv::Mat of the output mask (binary black and white image)
+ */
+void OpticalFlow::makeMask (cv::Mat const & inputBWImage, cv::Mat mask) const {
+
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+
+    cv::findContours(inputBWImage, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+    cv::drawContours(mask, contours, -1, 1);
+
+    for(auto const &contour: contours){
+        cv::fillConvexPoly(mask, contour, 0);
+    }
+
+    cv::Mat kernel;
+    kernel = cv::Mat::ones(cv::Size(this->dilutionMask, this->dilutionMask), CV_8UC1);
+
+    cv::Mat blur;
+    cv::blur(mask, blur, cv::Size(this->dilutionMask,this->dilutionMask));
+    cv::Mat thresh;
+    cv::threshold(blur, thresh, this->thresholdMask, 255, cv::THRESH_BINARY);
+    cv::dilate(thresh, blur, kernel, cv::Point(-1,-1), 2);
+    cv::bitwise_not(blur, mask);
+}
---
+++ src/fswAlgorithms/imageProcessing/opticalFlow/opticalFlow.h
@@ -0,0 +1,92 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Laboratory for Atmospheric and Space Physics, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+#ifndef _OPTICAL_FLOW_H_
+#define _OPTICAL_FLOW_H_
+
+#include <stdint.h>
+#include <Eigen/Dense>
+#include "architecture/messaging/messaging.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core/mat.hpp"
+#include "opencv2/imgcodecs.hpp"
+
+#include "architecture/msgPayloadDefC/CameraImageMsgPayload.h"
+#include "architecture/msgPayloadDefC/NavAttMsgPayload.h"
+#include "architecture/msgPayloadDefCpp/PairedKeyPointsMsgPayload.h"
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/utilities/avsEigenMRP.h"
+#include "architecture/utilities/avsEigenSupport.h"
+#include "architecture/utilities/rigidBodyKinematics.h"
+#include "architecture/utilities/linearAlgebra.h"
+#include "architecture/utilities/bskLogging.h"
+
+/*! @brief feature tracking module */
+class OpticalFlow: public SysModel {
+public:
+    OpticalFlow();
+    ~OpticalFlow();
+    
+    void UpdateState(uint64_t CurrentSimNanos);
+    void Reset(uint64_t CurrentSimNanos);
+    void makeMask(cv::Mat const & inputBWImage, cv::Mat mask) const;
+
+private:
+    cv::Mat oldImage;
+    cv::Mat newImage;
+    bool oldImagePresent = false;
+    bool newImagePresent = false;
+    double oldAttitude[3];
+    uint64_t oldTimeTag;
+    std::vector<cv::Vec2f> newFeatures;
+    std::vector<cv::Vec2f> oldFeatures;
+
+public:
+    std::string filename = "";  //!< Filename for module to read an image directly
+
+    Message<PairedKeyPointsMsgPayload> keyPointsMsg;  //!< The name of the output message containing key points
+    ReadFunctor<CameraImageMsgPayload> imageInMsg;  //!< The name of the camera output message containing images
+    ReadFunctor<NavAttMsgPayload> attitudeMsg;  //!< The name of the input attitude information
+    uint64_t sensorTimeTag; //!< [ns] Current time tag for sensor out
+
+    double minTimeBetweenPairs = 1; //!< [s] Minimum time between pairs of images
+    bool slidingWindowImages = false; //!< [bool] Minimum time between pairs of images
+
+    /*! OpenCV specific arguments needed for Shi-Tomasi "good features to track" */
+    int32_t maxNumberFeatures = 100;
+    double qualityLevel = 0.3;
+    int32_t minumumFeatureDistance = 5;
+    int32_t blockSize = 7;
+
+    /*! OpenCV specific arguments needed for OpticalFlow */
+    int32_t criteriaMaxCount = 10;
+    double criteriaEpsilon = 0.03;
+    int32_t flowSearchSize = 10;
+    int32_t flowMaxLevel = 2;
+
+    /*! OpenCV specific arguments needed for masking */
+    int32_t thresholdMask = 20;
+    int32_t dilutionMask = 20;
+
+    BSKLogger bskLogger;
+};
+
+#endif
