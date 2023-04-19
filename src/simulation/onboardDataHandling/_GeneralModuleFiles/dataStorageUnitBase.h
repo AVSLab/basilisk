8732e8a7ec06773471e010079b544aa5111ed2cb
ai-in-aerospace <ai.in.aerospace@gmail.com>
2023-04-19 15:52:59 -0600

    This commit adds the capability of indexing into the storage unit using the data name

--- src/simulation/onboardDataHandling/_GeneralModuleFiles/dataStorageUnitBase.h
+++ src/simulation/onboardDataHandling/_GeneralModuleFiles/dataStorageUnitBase.h
@@ -61,6 +61,7 @@ public:
     Message<DataStorageStatusMsgPayload> storageUnitDataOutMsg; //!< Vector of message names to be written out by the storage unit
     double storageCapacity; //!< Storage capacity of the storage unit
     BSKLogger bskLogger;    //!< logging variable
+    bool useNameIndex = false;  //!< Flag denoting whether or not to use the data name as the index
 
 protected:
     DataStorageStatusMsgPayload storageStatusMsg; //!< class variable
