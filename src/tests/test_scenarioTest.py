8a8ed37738006fa4d52ada4cc76f779525e05d80
Julian Hammerl <julian.hammerl@hotmail.com>
2023-05-08 14:39:51 -0600

    Add integrated scenario

--- src/tests/test_scenarioTest.py
+++ src/tests/test_scenarioTest.py
@@ -64,6 +64,7 @@ sys.path.append(path + '/../../examples/')
                                         , 'scenarioTwoChargedSC'
                                         , 'scenarioRendezVous'
                                         , 'scenarioSensorThermal'
+                                        , 'scenarioLambertSolver'
                                         ])
 @pytest.mark.scenarioTest
 def test_scenarioBskScenarios(show_plots, scenarioCase):
