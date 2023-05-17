75a43d61c7a6da4ebe9992026ab0f81c62f5acf2
Juan Garcia Bonilla <juan@garciabonilla.com>
2023-05-17 16:49:12 -0600

    Remove duplicate code and add sanity checks

--- src/simulation/dynamics/gravityEffector/gravCoeffOps.py
+++ src/simulation/dynamics/gravityEffector/gravCoeffOps.py
@@ -17,54 +17,56 @@
 #
 import csv
 
-from Basilisk.architecture import sim_model
+from Basilisk import __path__
 
+def loadGravFromFile(
+        fileName: str, 
+        spherHarm: "SphericalHarmonicsGravityModel", 
+        maxDeg: int = 2
+    ):
 
-def loadGravFromFile(fileName, spherHarm, maxDeg=2):
+    [clmList, slmList, mu, radEquator] = loadGravFromFileToList(fileName, maxDeg=2)
 
+    spherHarm.muBody = mu
+    spherHarm.radEquator = radEquator
+    spherHarm.cBar = clmList
+    spherHarm.sBar = slmList
+    spherHarm.maxDeg = maxDeg
+
+def loadGravFromFileToList(fileName: str, maxDeg: int = 2):
+    fileName = fileName.replace("%BSK_PATH%", list(__path__)[0])
     with open(fileName, 'r') as csvfile:
         gravReader = csv.reader(csvfile, delimiter=',')
         firstRow = next(gravReader)
         clmList = []
         slmList = []
+
         try:
-            valCurr = int(firstRow[0])
-        except ValueError:
-            spherHarm.muBody = float(firstRow[1])
-            spherHarm.radEquator = float(firstRow[0])
-
-        clmRow = []
-        slmRow = []
-        currDeg = 0
-        for gravRow in gravReader:
-           while int(gravRow[0]) > currDeg:
-               if( len(clmRow) < currDeg+1):
-                   clmRow.extend([0.0]*(currDeg+1 - len(clmRow)))
-                   slmRow.extend([0.0]*(currDeg+1 - len(slmRow)))
-               clmList.append(clmRow)
-               slmList.append(slmRow)
-               clmRow = []
-               slmRow = []
-               currDeg += 1
-           clmRow.append(float(gravRow[2]))
-           slmRow.append(float(gravRow[3]))
-
-        spherHarm.cBar = sim_model.MultiArray(clmList)
-        spherHarm.sBar = sim_model.MultiArray(slmList)
-        spherHarm.maxDeg = maxDeg
-
-
-def loadGravFromFileToList(fileName, maxDeg=2):
-    with open(fileName, 'r') as csvfile:
-        gravReader = csv.reader(csvfile, delimiter=',')
-        firstRow = next(gravReader)
-        clmList = []
-        slmList = []
-        try:
-            valCurr = int(firstRow[0])
-        except ValueError:
-            mu = float(firstRow[1])
             radEquator = float(firstRow[0])
+            mu = float(firstRow[1])
+            # firstRow[2] is uncertainty in mu, not needed for Basilisk
+            maxDegreeFile = int(firstRow[3])
+            maxOrderFile = int(firstRow[4])
+            coefficientsNormalized = int(firstRow[5]) == 1
+            refLong = float(firstRow[6])
+            refLat = float(firstRow[7])
+        except Exception as ex:
+            raise ValueError("File is not in the expected JPL format for "
+                             "spherical Harmonics", ex)
+
+        if maxDegreeFile < maxDeg or maxOrderFile < maxDeg:
+            raise ValueError(f"Requested using Spherical Harmonics of degree {maxDeg}"
+                             f", but file '{fileName}' has maximum degree/order of"
+                             f"{min(maxDegreeFile, maxOrderFile)}")
+        
+        if not coefficientsNormalized:
+            raise ValueError("Coefficients in given file are not normalized. This is "
+                            "not currently supported in Basilisk.")
+
+        if refLong != 0 or refLat != 0:
+            raise ValueError("Coefficients in given file use a reference longitude"
+                             " or latitude that is not zero. This is not currently "
+                             "supported in Basilisk.")
 
         clmRow = []
         slmRow = []
@@ -85,7 +87,13 @@ def loadGravFromFileToList(fileName, maxDeg=2):
         return [clmList, slmList, mu, radEquator]
         
         
-def loadPolyFromFile(fileName, poly):
+def loadPolyFromFile(fileName: str, poly: "PolyhedralGravityModel"):
+    [vertList, faceList, _, _] = loadPolyFromFileToList(fileName)
+    poly.xyzVertex = vertList
+    poly.orderFacet = faceList
+
+def loadPolyFromFileToList(fileName: str):
+    fileName = fileName.replace("%BSK_PATH%", list(__path__)[0])
     with open(fileName) as polyFile:
         if fileName.endswith('.tab'):
             nVertex, nFacet = [int(x) for x in next(polyFile).split()] # read first line
@@ -137,64 +145,8 @@ def loadPolyFromFile(fileName, poly):
                     faceList.append([int(arrtemp[0]),int(arrtemp[1]),int(arrtemp[2])])
 
                 contLines += 1
-
-        poly.nVertex = nVertex
-        poly.nFacet = nFacet
-        poly.xyzVertex = vertList
-        poly.orderFacet = faceList
-
-
-def loadPolyFromFileToList(fileName):
-    with open(fileName) as polyFile:
-        if fileName.endswith('.tab'):
-            nVertex, nFacet = [int(x) for x in next(polyFile).split()] # read first line
-            vertList = []
-            faceList = []
-
-            contLines = 0
-            for line in polyFile:
-                arrtemp = []
-
-                for x in line.split():
-                    arrtemp.append(float(x))
-
-                if contLines < nVertex:
-                    vertList.append([float(arrtemp[1]*1e3),float(arrtemp[2]*1e3),float(arrtemp[3]*1e3)])
-                else:
-                    faceList.append([int(arrtemp[1]),int(arrtemp[2]),int(arrtemp[3])])
-
-                contLines += 1
-        elif fileName.endswith('.obj'):
-            nVertex = 0
-            nFacet = 0
-            vertList = []
-            faceList = []
-            for line in polyFile:
-                arrtemp = line.split()
-                if arrtemp:
-                    if arrtemp[0] == 'v':
-                        nVertex += 1
-                        vertList.append([float(arrtemp[1])*1e3, float(arrtemp[2])*1e3, float(arrtemp[3])*1e3])
-                    elif arrtemp[0] == 'f':
-                        nFacet += 1
-                        faceList.append([int(arrtemp[1]), int(arrtemp[2]), int(arrtemp[3])])
-        elif fileName.endswith('.txt'):
-            nVertex, nFacet = [int(x) for x in next(polyFile).split()] # read first line
-            vertList = []
-            faceList = []
-
-            contLines = 0
-            for line in polyFile:
-                arrtemp = []
-
-                for x in line.split():
-                    arrtemp.append(float(x))
-
-                if contLines < nVertex:
-                    vertList.append([float(arrtemp[0]*1e3),float(arrtemp[1]*1e3),float(arrtemp[2]*1e3)])
-                else:
-                    faceList.append([int(arrtemp[0]),int(arrtemp[1]),int(arrtemp[2])])
-
-                contLines += 1
+        else:
+            raise ValueError("Unrecognized file extension. Valid extensions are "
+                             "'.tab', '.obj', and '.txt'")
 
         return [vertList, faceList, nVertex, nFacet]
