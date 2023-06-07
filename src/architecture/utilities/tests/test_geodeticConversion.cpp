f1a83020147b3979212d869f328dd132c1d34d84
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2023-06-07 09:35:13 +0930

    add unit test of the enhanced geodetic conversion routines

--- src/architecture/utilities/tests/test_geodeticConversion.cpp
+++ src/architecture/utilities/tests/test_geodeticConversion.cpp
@@ -53,3 +53,30 @@ TEST(GeodeticConversion, testPCPF2PCI) {
     expected << 1, -3, 2;
     EXPECT_TRUE(ans == expected);
 }
+
+TEST(GeodeticConversion, testLLA2PCPF) {
+    Eigen::Vector3d llaPosition;
+    llaPosition << 0.6935805104270613, 1.832425562445269, 1596.668;
+
+    Eigen::Vector3d ans = LLA2PCPF(llaPosition, 6378.1363E3,  6356.7523E3);
+
+    Eigen::Vector3d expected;
+    expected << -1270640.01, 4745322.63, 4056784.62;
+    EXPECT_LT((expected - ans).norm(), 100.0);
+}
+
+TEST(GeodeticConversion, testPCPF2LLA) {
+
+Eigen::Vector3d pcpfPosition;
+pcpfPosition << -1270640.01, 4745322.63, 4056784.62;
+
+Eigen::Vector3d ans = PCPF2LLA(pcpfPosition, 6378.1363E3,  6356.7523E3);
+
+Eigen::Vector3d llaPosition;
+llaPosition << 0.6935805104270613, 1.832425562445269, 1596.668;
+
+EXPECT_LT(std::abs(llaPosition[0] - ans[0]), 0.0001);
+EXPECT_LT(std::abs(llaPosition[1] - ans[1]), 0.0001);
+EXPECT_LT(std::abs(llaPosition[2] - ans[2]), 100);
+
+}
