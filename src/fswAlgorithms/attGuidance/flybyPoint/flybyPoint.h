10ac8cbbb188af47b695c7ea82386f30bbc25e60
Riccardo <riccardo.calaon@colorado.edu>
2023-06-14 11:41:25 -0600

    Added user-defined tolerance and internal flag variable

--- src/fswAlgorithms/attGuidance/flybyPoint/flybyPoint.h
+++ src/fswAlgorithms/attGuidance/flybyPoint/flybyPoint.h
@@ -34,6 +34,12 @@ typedef enum flybyModel{
     cwEquations = 1
 } FlybyModel;
 
+typedef enum singularityFlag{
+    minusInfinity = -1,
+    nonSingular   =  0,
+    plusInfinity  =  1
+} SingularityFlag;
+
 
 /*! @brief A class to perform flyby pointing */
 class FlybyPoint: public SysModel {
@@ -45,6 +51,7 @@ public:
     void UpdateState(uint64_t CurrentSimNanos);
     
     double     dtFilterData = 0;       //!< time between two subsequent reads of the filter information
+    double     epsilon = 0;            //!< tolerance for singular conditions when position and velocity are collinear
     FlybyModel flybyModel;             //!< flag to indicate which flyby model is being used
 
     ReadFunctor<NavTransMsgPayload>  filterInMsg;               //!< input msg relative position w.r.t. asteroid
@@ -53,11 +60,12 @@ public:
     AttRefMsg_C attRefOutMsgC = {};                             //!< C-wrapped attitude reference output message
 
 private:
-    bool     firstRead;
-    double   f0;                       //!< ratio between relative velocity and position norms at time of read [Hz]
-    double   gamma0;                   //!< flight path angle of the spacecraft at time of read [rad]
-    double   R0N[3][3];                //!< inertial-to-reference DCM at time of read
-    uint64_t lastFilterReadTime;       //!< time of last filter read
+    bool            firstRead;           //!< variable to attest if this is the first read after a Reset
+    double          f0;                  //!< ratio between relative velocity and position norms at time of read [Hz]
+    double          gamma0;              //!< flight path angle of the spacecraft at time of read [rad]
+    double          R0N[3][3];           //!< inertial-to-reference DCM at time of read
+    SingularityFlag singularityFlag;     //!< +1 or -1 during singular configurations, 0 otherwise
+    uint64_t        lastFilterReadTime;  //!< time of last filter read
 
     BSKLogger bskLogger;               //!< BSK Logging
 };
