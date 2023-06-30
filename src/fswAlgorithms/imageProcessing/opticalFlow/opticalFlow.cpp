b894bd69fadf81cfa9fa196f0918a15007e57fc8
Thibaud Teil <thibaud.teil@colorado.edu>
2023-06-30 13:09:57 -0600

    Improve module robustness if features are not found

--- src/fswAlgorithms/imageProcessing/opticalFlow/opticalFlow.cpp
+++ src/fswAlgorithms/imageProcessing/opticalFlow/opticalFlow.cpp
@@ -102,40 +102,49 @@ void OpticalFlow::UpdateState(uint64_t CurrentSimNanos)
                                  this->flowMaxLevel,
                                  criteria);
 
-        featurePayload.timeTag_secondImage = this->sensorTimeTag;
-        featurePayload.timeTag_firstImage = this->firstTimeTag;
+        /*! If optical flow matches features between the images */
+        if (!this->secondFeatures.empty()){
+            featurePayload.timeTag_secondImage = this->sensorTimeTag;
+            featurePayload.timeTag_firstImage = this->firstTimeTag;
 
-        v3Copy(this->firstSpacecraftAttitude, featurePayload.sigma_BN_firstImage);
-        v3Copy(navAttBuffer.sigma_BN, featurePayload.sigma_BN_secondImage);
-        v3Copy(this->firstTargetEphemAttitude, featurePayload.sigma_TN_firstImage);
-        v3Copy(ephemMsgBuffer.sigma_BN, featurePayload.sigma_TN_secondImage);
+            v3Copy(this->firstSpacecraftAttitude, featurePayload.sigma_BN_firstImage);
+            v3Copy(navAttBuffer.sigma_BN, featurePayload.sigma_BN_secondImage);
+            v3Copy(this->firstTargetEphemAttitude, featurePayload.sigma_TN_firstImage);
+            v3Copy(ephemMsgBuffer.sigma_BN, featurePayload.sigma_TN_secondImage);
 
-        for (int i=0; i<this->secondFeatures.size(); i++){
-            featurePayload.keyPoints_firstImage[2*i] = this->firstFeatures[i][0];
-            featurePayload.keyPoints_firstImage[2*i+1] = this->firstFeatures[i][1];
-            featurePayload.keyPoints_secondImage[2*i] = this->secondFeatures[i][0];
-            featurePayload.keyPoints_secondImage[2*i+1] = this->secondFeatures[i][1];
+            for (size_t i=0; i<this->secondFeatures.size(); i++){
+                featurePayload.keyPoints_firstImage[2*i] = this->firstFeatures[i][0];
+                featurePayload.keyPoints_firstImage[2*i+1] = this->firstFeatures[i][1];
+                featurePayload.keyPoints_secondImage[2*i] = this->secondFeatures[i][0];
+                featurePayload.keyPoints_secondImage[2*i+1] = this->secondFeatures[i][1];
+            }
+            featurePayload.cameraID = imageBuffer.cameraID;
+            featurePayload.keyPointsFound = (int)this->secondFeatures.size();
+            featurePayload.valid = true;
+
+            this->keyPointsMsg.write(&featurePayload, this->moduleID, CurrentSimNanos);
+
+            this->secondImagePresent = false;
+            /*! Then reset values for next pair of images depending on to the sliding window setting */
+            if (this->slidingWindowImages){
+                this->firstImagePresent = true;
+                v3Copy(navAttBuffer.sigma_BN, this->firstSpacecraftAttitude);
+                v3Copy(ephemMsgBuffer.sigma_BN, this->firstTargetEphemAttitude);
+                this->firstTimeTag = this->sensorTimeTag;
+                this->firstFeatures = this->secondFeatures;
+            }
+            else{
+                this->firstImagePresent = false;
+                this->firstTimeTag = 0;
+                this->firstFeatures.clear();
+            }
         }
-        featurePayload.cameraID = imageBuffer.cameraID;
-        featurePayload.keyPointsFound = (uint64_t)this->secondFeatures.size();
-        featurePayload.valid = true;
-
-        this->keyPointsMsg.write(&featurePayload, this->moduleID, CurrentSimNanos);
-
-        this->secondImagePresent = false;
-        this->firstFeatures = this->secondFeatures;
-        /*! Then reset values for next pair of images */
-        if (this->slidingWindowImages){
-            this->firstImagePresent = true;
-            v3Copy(navAttBuffer.sigma_BN, this->firstSpacecraftAttitude);
-            v3Copy(ephemMsgBuffer.sigma_BN, this->firstTargetEphemAttitude);
-            this->firstTimeTag = this->sensorTimeTag;
-        }
+        /*! If optical flow did not succeed, reset features to allow for another attempt next image */
         else{
             this->firstImagePresent = false;
-            v3SetZero(this->firstSpacecraftAttitude);
-            v3SetZero(this->firstTargetEphemAttitude);
             this->firstTimeTag = 0;
+            this->firstFeatures.clear();
+            this->secondFeatures.clear();
         }
     }
     /*! - If there is a second image but also a first image, populate the first image buffers */
@@ -154,6 +163,7 @@ void OpticalFlow::UpdateState(uint64_t CurrentSimNanos)
                                 false,
                                 0.04);
 
+        /*! If features are found, save the data*/
         if (!this->firstFeatures.empty()) {
             v3Copy(navAttBuffer.sigma_BN, this->firstSpacecraftAttitude);
             v3Copy(ephemMsgBuffer.sigma_BN, this->firstTargetEphemAttitude);
@@ -161,6 +171,7 @@ void OpticalFlow::UpdateState(uint64_t CurrentSimNanos)
             this->firstImagePresent = true;
             this->secondImagePresent = false;
         }
+        /*! If no features are found, reset data to attempt again */
         else{
             this->firstImagePresent = false;
             this->secondImagePresent = false;
