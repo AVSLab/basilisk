5c2479c4e6ab0fbe56a872e04a6e5f58fd37f168
Julian Hammerl <julian.hammerl@hotmail.com>
2023-09-13 13:43:28 -0600

    Add scenario to integrated example scripts catalog

--- src/tests/test_scenarioTest.py
+++ src/tests/test_scenarioTest.py
@@ -65,6 +65,7 @@ sys.path.append(path + '/../../examples/')
                                         , 'scenarioRendezVous'
                                         , 'scenarioSensorThermal'
                                         , 'scenarioLambertSolver'
+                                        , 'scenarioMomentumDumpingZeroNetForce'
                                         ])
 @pytest.mark.scenarioTest
 def test_scenarioBskScenarios(show_plots, scenarioCase):
