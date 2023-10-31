50a38074004f0c98115506b8ddccfd465e54248f
João Vaz Carneiro <joao.carneiro@colorado.edu>
2023-10-31 13:03:51 -0600

    Introduce new field to scStatesMsgPayload
    The new field corresponds to the accumulated delta-v of the center of
    mass of the spacecraft in the inertial frame.

--- src/architecture/msgPayloadDefC/SCStatesMsgPayload.h
+++ src/architecture/msgPayloadDefC/SCStatesMsgPayload.h
@@ -33,6 +33,7 @@ typedef struct {
     double omegaDot_BN_B[3];          //!< r/s/s Current angular acceleration
     double TotalAccumDVBdy[3];        //!< m/s Accumulated DV of center of mass in body frame coordinates
     double TotalAccumDV_BN_B[3];      //!< m/s Accumulated DV of body frame in body frame coordinates
+    double TotalAccumDV_CN_N[3];      //!< m/s Accumulated DV of center of mass in inertial frame coordinates
     double nonConservativeAccelpntB_B[3];//!< m/s/s Current Spacecraft non-conservative body frame accel
     uint64_t MRPSwitchCount;          //!< -- Number of times that MRPs have switched
 }SCStatesMsgPayload;
