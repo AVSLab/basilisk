02189edff3a1ffef8c9b942da2940676de84c818
Juan Garcia Bonilla <juan@garciabonilla.com>
2023-11-18 17:09:44 -0800

    Update gravity effector to new gravity model class

--- src/simulation/dynamics/_GeneralModuleFiles/gravityEffector.h
+++ src/simulation/dynamics/_GeneralModuleFiles/gravityEffector.h
@@ -17,7 +17,6 @@
 
  */
 
-
 #ifndef GRAVITY_DYN_EFFECTOR_H
 #define GRAVITY_DYN_EFFECTOR_H
 
@@ -28,148 +27,160 @@
 #include "architecture/msgPayloadDefC/SpicePlanetStateMsgPayload.h"
 #include "architecture/utilities/bskLogging.h"
 #include "architecture/messaging/messaging.h"
+#include <memory>
+#include "gravityModel.h"
+#include "pointMassGravityModel.h"
+#include "architecture/utilities/avsEigenSupport.h"
 
-/*! @brief poyhedral class */
-class Polyhedral
-{
-public:
-    unsigned int nVertex;         //!< [-] Number of vertexes
-    unsigned int nFacet;          //!< [-] Number of facets
-    
-    double volPoly;               //!< [-] Volume of the polyhedral
-    double muBody;                //!< [-] Gravitation parameter for the planet
-    
-    Eigen::MatrixXd xyzVertex;    //!< [m] Position of vertex
-    Eigen::MatrixXd orderFacet;   //!< [-] Vertexes of a facet
-
-    Eigen::MatrixXd normalFacet;  //!< [-] Normal of a facet
-
-    BSKLogger bskLogger;          //!< -- BSK Logging
-
-public:
-
-    Polyhedral();
-    ~Polyhedral();
-    bool initializeParameters();            //!< [-] configure polyhedral based on inputs
-    Eigen::Vector3d computeField(const Eigen::Vector3d pos_Pfix);
-    bool polyReady();                       //!< class variable
-};
-
-/*! @brief spherical harmonics class */
-class SphericalHarmonics
-{
-public:
-    unsigned int maxDeg;  //!< [-] Maximum degree of the spherical harmonics
-    double radEquator;    //!< [-] Reference radius for the planet
-    double muBody;        //!< [-] Gravitation parameter for the planet
-    
-    std::vector<std::vector<double>> cBar;  //!< [-] C coefficient set
-    std::vector<std::vector<double>> sBar;  //!< [-] S coefficient set
-    std::vector<std::vector<double>> aBar;  //!< [-] Normalized 'derived' Assoc. Legendre
-    std::vector<std::vector<double>> n1;    //!< [-] What am I
-    std::vector<std::vector<double>> n2;    //!< [-] What am I
-    std::vector<std::vector<double>> nQuot1;//!< [-] What am I
-    std::vector<std::vector<double>> nQuot2;//!< [-] What am I
-
-    BSKLogger bskLogger;                      //!< -- BSK Logging
-
-public:
-
-    SphericalHarmonics();
-    ~SphericalHarmonics();
-    bool initializeParameters();            //!< [-] configure all spher-harm based on inputs
-    double getK(const unsigned int degree); //!< class method
-    Eigen::Vector3d computeField(const Eigen::Vector3d pos_Pfix, unsigned int degree,
-                                                     bool include_zero_degree);
-    bool harmReady();                       //!< class variable
-    
-};
-
-//!@brief Container for gravitational body data
-/*! This class is designed to hold all of the information for a gravity
- body.  The nominal use-case has it initialized at the python level and
- attached to dynamics using the AddGravityBody method.
-
+/** Container for gravitational body data
+ * 
+ * This class is designed to hold all of the information for a gravity
+ * body.  The nominal use-case has it initialized at the python level and
+ * attached to dynamics using the AddGravityBody method.
  */
 class GravBodyData
 {
 public:
-    // Default constructor
-    GravBodyData();
-    ~GravBodyData();
+    friend class GravityEffector;
 
-    void initBody(int64_t moduleID); //!<        Method to initialize the gravity body
+    /** Initializes `localPlanet` to zero except for `localPlanet.J20002Pfix`, which
+     * is initialized to the identity matrix. Additionally, initializes `gravityModel`
+     * to a `PointMassGravityModel`.
+    */
+    GravBodyData();
+
+    /** Initializes the gravityModel, logging and raising an error if
+     * the initialization fails.
+     */
+    void initBody(int64_t moduleID);
+
+    /** Computes the gravitational acceleration
+     *
+     * @param r_I inertial position vector
+     * @param simTimeNanos simulation time (ns)
+     */
     Eigen::Vector3d computeGravityInertial(Eigen::Vector3d r_I, uint64_t simTimeNanos);
-    double computePotentialEnergy(Eigen::Vector3d r_I);
-    void loadEphemeris();           //!< Command to load the ephemeris data
-    void registerProperties(DynParamManager& statesIn);  //!< class method
-    ReadFunctor<SpicePlanetStateMsgPayload> planetBodyInMsg;       //!< planet spice ephemeris input message
+
+    /** Read the ephemeris data from planetBodyInMsg if it's linked.
+     * Otherwise, zeros `this->localPlanet`.
+     */
+    void loadEphemeris();
+
+    /** Creates the following properies in the given statesIn object.
+     * 
+     *      - [planetName].r_PN_N
+     *      - [planetName].v_PN_N
+     *      - [planetName].mu
+     *      - [planetName].J20002Pfix
+     *      - [planetName].J20002Pfix_dot
+     * 
+     * vr_PN_N`, `v_PN_N`, and `mu` are initialized to zero, while `J20002Pfix` and `J20002Pfix_dot`
+     * are initialized to the values stored in `this->localPlanet`. This usually means that
+     * `J20002Pfix` is initialized to the identity matrix and `J20002Pfix_dot` to zero.
+     */
+    void registerProperties(DynParamManager &statesIn);
 
 public:
-    bool isCentralBody=0;           //!<          Flag indicating that object is center
-    bool usePolyhedral=0;           //!<          Flag indicating to use polyhedral model
-    bool useSphericalHarmParams=0;  //!<          Flag indicating to use spherical harmonics perturbations
+    bool isCentralBody = false; /**< Flag indicating that object is center */
 
-    double mu=0;                    //!< [m3/s^2] central body gravitational param
-    double ephemTime;               //!< [s]      Ephemeris time for the body in question
-    double ephIntTime;              //!< [s]      Integration time associated with the ephem data
-    double radEquator=0;            //!< [m]      Equatorial radius for the body
-    double radiusRatio=0;           //!< []       ratio of polar over equatorial radius
-    SpicePlanetStateMsgPayload localPlanet = {};  //!< [-]   Class storage of ephemeris info from scheduled portion
-    uint64_t timeWritten = 0;       //!< [ns]     time the input planet state message was written
-    std::string planetName="";      //!<          Gravitational body name, this is used as the Spice name if spiceInterface is used
-    std::string displayName="";     //!<          this is the name that is displayed in Vizard.  If not set, Vizard shows planetName
-    std::string modelDictionaryKey = ""; //!<     "" will result in using the current default for the celestial body's given name, otherwise key will be matched if possible to available model in internal model dictionary
+    std::shared_ptr<GravityModel> gravityModel; /**< Model used to compute the gravity of the object */
 
-    Polyhedral poly;                //!<          Object that computes the polyhedral gravity field
-    SphericalHarmonics spherHarm;   //!<          Object that computes the spherical harmonics gravity field
-    BSKLogger bskLogger;            //!< -- BSK Logging
-    Eigen::MatrixXd *r_PN_N;        //!< [m]      (state engine property) planet inertial position vector
-    Eigen::MatrixXd *v_PN_N;        //!< [m/s]    (state engine property) planet inertial velocity vector
-    Eigen::MatrixXd *muPlanet;      //!< [m/s]    (state engine property) planet inertial velocity vector
-    Eigen::MatrixXd *J20002Pfix;    //!< [m/s]    (state engine property) planet attitude [PN]
-    Eigen::MatrixXd *J20002Pfix_dot;//!< [m/s]    (state engine property) planet attitude rate [PN_dot]
+    double mu = 0;                       /**< [m3/s^2] central body gravitational param */
+    double radEquator = 0;               /**< [m]      Equatorial radius for the body */
+    double radiusRatio = 1;              /**< []       ratio of polar over equatorial radius */
+    std::string planetName = "";         /**< Gravitational body name, this is used as the Spice name if spiceInterface is used */
+    std::string displayName = "";        /**< This is the name that is displayed in Vizard.  If not set, Vizard shows planetName */
+    std::string modelDictionaryKey = ""; /**< "" will result in using the current default for the celestial body's given name, otherwise key will be matched if possible to available model in internal model dictionary */
 
+    ReadFunctor<SpicePlanetStateMsgPayload> planetBodyInMsg; /**< planet spice ephemeris input message */
+    SpicePlanetStateMsgPayload localPlanet;             /**< [-]   Class storage of ephemeris info from scheduled portion */
+
+    BSKLogger bskLogger; /**< -- BSK Logging */
+
+private:
+    Eigen::MatrixXd *r_PN_N;         /**< [m]      (state engine property) planet inertial position vector */
+    Eigen::MatrixXd *v_PN_N;         /**< [m/s]    (state engine property) planet inertial velocity vector */
+    Eigen::MatrixXd *muPlanet;       /**< [m/s]    (state engine property) planet inertial velocity vector */
+    Eigen::MatrixXd *J20002Pfix;     /**< [m/s]    (state engine property) planet attitude [PN] */
+    Eigen::MatrixXd *J20002Pfix_dot; /**< [m/s]    (state engine property) planet attitude rate [PN_dot] */
+
+    uint64_t timeWritten = 0; /**< [ns]     time the input planet state message was written */
 };
 
-
 /*! @brief gravity effector class */
-class GravityEffector : public SysModel {
+class GravityEffector : public SysModel
+{
 public:
-    GravityEffector();
-    ~GravityEffector();
-    void Reset(uint64_t CurrentSimNanos);
-    void UpdateState(uint64_t CurrentSimNanos);
-    void linkInStates(DynParamManager& statesIn); //!< class method
-    void registerProperties(DynParamManager& statesIn);
+    /** Initializes every `GravBodyData` associated with this `GravityEffector` */
+    void Reset(uint64_t currentSimNanos);
+
+    /** Updates the central body, loads the ephemeris data for every `GravBodyData`,
+     * and writes the output messages. */
+    void UpdateState(uint64_t currentSimNanos);
+
+    /** Links the correlation between times property */
+    void linkInStates(DynParamManager &statesIn);
+
+    /** Registers the gravity, inertial position, and inertial velocity properties.
+     * It also calls `registerProperties` for every associated `GravBodyData`.
+     */
+    void registerProperties(DynParamManager &statesIn);
+
+    /** Calculate gravitational acceleration of s/c wrt inertial (no central body) or wrt central body
+     *  
+     *   @param r_cF_N is position of center of mass of s/c wrt frame
+     *   @param rDot_cF_N is the derivative of above
+     */
     void computeGravityField(Eigen::Vector3d r_cF_N, Eigen::Vector3d rDot_cF_N);
+
+    /** Updates the inertial position and velocity properties */
     void updateInertialPosAndVel(Eigen::Vector3d r_BF_N, Eigen::Vector3d rDot_BF_N);
-    void updateEnergyContributions(Eigen::Vector3d r_CN_N, double & orbPotEnergyContr);  //!< -- Orbital Potential Energy Contributions
-    void setGravBodies(std::vector<GravBodyData *> gravBodies); //!< class method
-    void addGravBody(GravBodyData* gravBody); //!< class method
-    void prependSpacecraftNameToStates(); //!< class method
-    
+
+    /** Computes the Potential Energy Contributions from every associated `GravBodyData` */
+    void updateEnergyContributions(Eigen::Vector3d r_CN_N, double &orbPotEnergyContr);
+
+    /** Sets the `GravBodyData` associated with this effector */
+    void setGravBodies(std::vector<std::shared_ptr<GravBodyData>> gravBodies);
+
+    /** Adds a `GravBodyData` associated with this effector */
+    void addGravBody(std::shared_ptr<GravBodyData> gravBody);
+
+    /** Called to modify property names to prepend them by the string stored in nameOfSpacecraftAttachedTo 
+     * 
+     * This can be used to make property names unique between different `GravityEffector` in a simulation
+     * with multiple dynamic objects.
+    */
+    void prependSpacecraftNameToStates();
+
 private:
-    Eigen::Vector3d getEulerSteppedGravBodyPosition(GravBodyData *bodyData); //!< class method
-    void writeOutputMessages(uint64_t currentSimNanos); //!< class method
+    /**
+        Compute planet position with Euler integration
+        @param bodyData planet data
+    */
+    Eigen::Vector3d getEulerSteppedGravBodyPosition(std::shared_ptr<GravBodyData> bodyData);
+
+    /** Writes to centralBodyOutMsg if it is linked and there is a central body */
+    void writeOutputMessages(uint64_t currentSimNanos);
 
 public:
-    std::string vehicleGravityPropName;            //!< [-] Name of the vehicle mass state
-    std::string systemTimeCorrPropName;            //!< [-] Name of the correlation between times
-    std::vector<GravBodyData*> gravBodies;         //!< [-] Vector of bodies we feel gravity from
-    GravBodyData* centralBody;         //!<  Central body
-    std::string inertialPositionPropName;           //!< [-] Name of the inertial position property
-    std::string inertialVelocityPropName;           //!< [-] Name of the inertial velocity property
-    std::string nameOfSpacecraftAttachedTo;         //!< [-] Name of the s/c this gravity model is attached to
-    BSKLogger bskLogger;                      //!< -- BSK Logging
-    Message<SpicePlanetStateMsgPayload> centralBodyOutMsg;  //!< central planet body state output message
+    std::vector<std::shared_ptr<GravBodyData>> gravBodies; /**< [-] Vector of bodies we feel gravity from */
+    std::shared_ptr<GravBodyData> centralBody;             /**<  Central body */
+
+    std::string vehicleGravityPropName = "g_N";        /**< [-] Name of the vehicle gravity acceleration property */
+    std::string systemTimeCorrPropName = "systemTime"; /**< [-] Name of the correlation between times */
+    std::string inertialPositionPropName = "r_BN_N";   /**< [-] Name of the inertial position property */
+    std::string inertialVelocityPropName = "v_BN_N";   /**< [-] Name of the inertial velocity property */
+    std::string nameOfSpacecraftAttachedTo = "";       /**< [-] Name of the s/c this gravity model is attached to */
+
+    Message<SpicePlanetStateMsgPayload> centralBodyOutMsg; /**< central planet body state output message */
+
+    BSKLogger bskLogger; /**< -- BSK Logging */
 
 private:
-    Eigen::MatrixXd *gravProperty;                  //!< [-] g_N property for output
-    Eigen::MatrixXd *timeCorr;                      //!< [-] Time correlation property
-    Eigen::MatrixXd *inertialPositionProperty;      //!< [m] r_N inertial position relative to system spice zeroBase/refBase coordinate frame, property for output.
-    Eigen::MatrixXd *inertialVelocityProperty;      //!< [m/s] v_N inertial velocity relative to system spice zeroBase/refBase coordinate frame, property for output.
-
+    Eigen::MatrixXd *gravProperty;             /**< [-] g_N property for output */
+    Eigen::MatrixXd *timeCorr;                 /**< [-] Time correlation property */
+    Eigen::MatrixXd *inertialPositionProperty; /**< [m] r_N inertial position relative to system spice zeroBase/refBase coordinate frame, property for output. */
+    Eigen::MatrixXd *inertialVelocityProperty; /**< [m/s] v_N inertial velocity relative to system spice zeroBase/refBase coordinate frame, property for output. */
 };
 
 #endif /* GRAVITY_EFFECTOR_H */
