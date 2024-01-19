7562afe374f715d485b311953af45836684759ff
Riccardo <riccardo.calaon@colorado.edu>
2024-01-19 14:18:15 +0100

    Reset function checks that the input messages are connected

--- src/fswAlgorithms/attGuidance/sunPointRoll/sunPointRoll.cpp
+++ src/fswAlgorithms/attGuidance/sunPointRoll/sunPointRoll.cpp
@@ -42,7 +42,10 @@ void SunPointRoll::SelfInit(){
  */
 void SunPointRoll::Reset(uint64_t CurrentSimNanos)
 {
-
+    if (!this->attNavInMsg.isLinked()) {
+        bskLogger.bskLog(BSK_ERROR, ".attNavInMsg wasn't connected.");
+    }
+    this->resetTime = CurrentSimNanos;
 }
 
 
