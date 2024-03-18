4ddbee974196073c6fe2f853b80ee6f6c2024e2d
Leah Kiner <leahannkiner@gmail.com>
2024-03-18 17:29:50 -0600

    Rename variables t_r and t_s to t_b1
    Both of these variables represent the end of a
    bang segment; therefore they can be merged and
    renamed as a single variable. When smoothing
    is added to the module, these additional profilers
    will be able to re-use this variable.

--- src/simulation/deviceInterface/prescribedLinearTranslation/prescribedLinearTranslation.h
+++ src/simulation/deviceInterface/prescribedLinearTranslation/prescribedLinearTranslation.h
@@ -86,8 +86,7 @@ private:
 
     /* Temporal parameters */
     double tInit;                                                               //!< [s] Simulation time at the beginning of the translation
-    double t_r;                                                                 //!< [s] The simulation time at the end of the first bang segment
-    double t_s;                                                                 //!< [s] The simulation time halfway through the translation
+    double t_b1;                                                                //!< [s] Simulation time at the end of the first bang segment
     double t_c;                                                                 //!< [s] Simulation time at the end of the coast segment
     double t_f;                                                                 //!< [s] Simulation time when the translation is complete
 
