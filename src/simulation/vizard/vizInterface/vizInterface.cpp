d831d14b01af2b39f423dca5a4b26361dc238b5f
jackfox <jack.fox@colorado.edu>
2024-03-20 00:23:26 -0600

    Sync EventDialog responses with broadcast Vizard

--- src/simulation/vizard/vizInterface/vizInterface.cpp
+++ src/simulation/vizard/vizInterface/vizInterface.cpp
@@ -1289,7 +1289,7 @@ void VizInterface::receiveUserInput(uint64_t CurrentSimNanos){
 
     if (receive_status != -1) {
         /*! - Extract message size and data pointer from socket */
-        int vizPointSize = zmq_msg_size(&viz_response);
+        int vizPointSize = (int)zmq_msg_size(&viz_response);
         void* vizPoint = zmq_msg_data(&viz_response);
 
         /*! - Set up and fill VizInput message */
@@ -1307,26 +1307,33 @@ void VizInterface::receiveUserInput(uint64_t CurrentSimNanos){
             outMsgBuffer.keyboardInput = keys;
         }
 
-        /*! - Iterate through VizInput_EventReply objects */
-        for (int i=0; i<msgRecv->replies_size(); i++) {
-            const vizProtobufferMessage::VizInput_EventReply* vier = &(msgRecv->replies(i));
-            // remove "const"ness for compatibility with protobuffer access methods
-            vizProtobufferMessage::VizInput_EventReply* vier_nc;
-            vier_nc = const_cast<vizProtobufferMessage::VizInput_EventReply*>(vier);
-            
-            EventReply* er = new EventReply();
-            er->eventHandlerID = *(vier_nc->mutable_eventhandlerid());
-            er->reply = *(vier_nc->mutable_reply());
-            er->eventHandlerDestroyed = vier_nc->eventhandlerdestroyed();
-            outMsgBuffer.eventReplies.push_back(*er);
-        }
-
         /*! - Receive VizBroadcastSyncSettings */
         const vizProtobufferMessage::VizBroadcastSyncSettings* vbss = &(msgRecv->broadcastsyncsettings());
         // remove "const"ness for compatibility with protobuffer access methods
         vizProtobufferMessage::VizBroadcastSyncSettings* vbss_nc;
         vbss_nc = const_cast<vizProtobufferMessage::VizBroadcastSyncSettings*>(vbss);
-        
+
+        /*! - Iterate through VizEventReply objects */
+        for (int i=0; i<msgRecv->replies_size(); i++) {
+            const vizProtobufferMessage::VizEventReply* ver = &(msgRecv->replies(i));
+            // remove "const"ness for compatibility with protobuffer access methods
+            vizProtobufferMessage::VizEventReply* ver_nc;
+            ver_nc = const_cast<vizProtobufferMessage::VizEventReply*>(ver);
+            
+            EventReply* er = new EventReply();
+            er->eventHandlerID = *(ver_nc->mutable_eventhandlerid());
+            er->reply = *(ver_nc->mutable_reply());
+            er->eventHandlerDestroyed = ver_nc->eventhandlerdestroyed();
+            outMsgBuffer.eventReplies.push_back(*er);
+
+            // populate VizEventReply in VizBroadcastSyncSettings
+            vizProtobufferMessage::VizEventReply* ver_nc_bss = vbss_nc->add_dialogevents();
+            ver_nc_bss->set_eventhandlerid(er->eventHandlerID);
+            ver_nc_bss->set_reply(er->reply);
+            ver_nc_bss->set_eventhandlerdestroyed(er->eventHandlerDestroyed);
+        }
+
+        /*! - Serialize and send BroadcastSyncSettings*/
         uint32_t syncByteCount = (uint32_t) vbss_nc->ByteSizeLong();
         if (syncByteCount > 0 && this->broadcastStream) {
             void* sync_settings = malloc(syncByteCount);
