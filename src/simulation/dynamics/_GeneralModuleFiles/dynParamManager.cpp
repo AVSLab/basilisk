d4ef65070f8c39d13789310fc2e96701d6f4d2ad
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2024-03-23 16:01:54 -0600

    fix unclear error message if stateName is empty string
    If the stateName variable is an empty string then this error message does not
    make this apparent.  Edited the warning string to make this clearer.

--- src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.cpp
+++ src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.cpp
@@ -80,7 +80,7 @@ StateData* DynParamManager::getStateObject(std::string stateName)
             Either the state name was miss-spelled, or the state simply
             doesn't exit in the current simulaiton setup (i.e. asking for the
             hub attitude in a translation only simulation setup */
-        bskLogger.bskLog(BSK_WARNING, "You requested this non-existent state name: %s You either miss-typed the stateName, or you asked for a state that doesn't exist in your simulation setup.", stateName.c_str());
+        bskLogger.bskLog(BSK_WARNING, "You requested this non-existent state name: %s You either miss-typed the stateName, or you asked for a state that doesn't exist in your simulation setup. stateName = ", stateName.c_str());
     }
     
     return(statePtr);
