f6a83da5bf102621cb99715f7ae80b00d962b911
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-03-24 12:38:42 -0600

    Tidy class initialization and constructors of system model class

--- src/architecture/system_model/sys_model_task.h
+++ src/architecture/system_model/sys_model_task.h
@@ -36,9 +36,9 @@ class SysModelTask
 {
 
 public:
-    SysModelTask();
-    SysModelTask(uint64_t InputPeriod, uint64_t FirstStartTime=0); //!< class method
-    ~SysModelTask();
+    SysModelTask()=default;
+    explicit SysModelTask(uint64_t InputPeriod, uint64_t FirstStartTime=0); //!< class method
+    ~SysModelTask()=default;
     void AddNewObject(SysModel *NewModel, int32_t Priority = -1);
     void SelfInitTaskList();
     //void CrossInitTaskList();
@@ -54,16 +54,16 @@ public:
     uint64_t getTaskPeriod() const;
     uint64_t getFirstTaskTime() const;
 public:
-    std::vector<ModelPriorityPair> TaskModels;  //!< -- Array that has pointers to all task sysModels
-    std::string TaskName;  //!< -- Identifier for Task
+    std::vector<ModelPriorityPair> TaskModels{};  //!< -- Array that has pointers to all task sysModels
+    std::string TaskName{};  //!< -- Identifier for Task
     std::string parentProc; //!< -- Process that calls this task
-	bool taskActive;  //!< -- Flag indicating whether the Task has been disabled
+	bool taskActive=true;  //!< -- Flag indicating whether the Task has been disabled
   BSKLogger bskLogger;                      //!< -- BSK Logging
 private:
-    uint64_t NextStartTime; //!< [ns] Next time to start task
-    uint64_t NextPickupTime; //!< [ns] Next time read Task outputs
-    uint64_t TaskPeriod; //!< [ns] Cycle rate for Task
-    uint64_t FirstTaskTime; //!< [ns] Time to start Task for first time.  After this time the normal periodic updates resume.
+    uint64_t NextStartTime=0; //!< [ns] Next time to start task
+    uint64_t NextPickupTime=0; //!< [ns] Next time read Task outputs
+    uint64_t TaskPeriod=100; //!< [ns] Cycle rate for Task
+    uint64_t FirstTaskTime=0; //!< [ns] Time to start Task for first time.  After this time the normal periodic updates resume.
 };
 
 #endif /* _SysModelTask_H_ */
