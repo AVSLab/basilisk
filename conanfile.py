bdf8dc0401b8d935a83b13a71f8ab8eb20333f9a
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-04-12 12:14:37 -0600

    Pin protobuf dependency package recipe id

--- conanfile.py
+++ conanfile.py
@@ -185,7 +185,7 @@ class BasiliskConan(ConanFile):
             self.requires.add("xz_utils/5.4.0")
 
         if self.options.vizInterface or self.options.opNav:
-            self.requires.add("protobuf/3.17.1")
+            self.requires.add("protobuf/3.17.1#ffb2039b66b5a372f7a2a8a0b1ddfd13")
             self.options['zeromq'].encryption = False  # Basilisk does not use data streaming encryption.
             self.requires.add("cppzmq/4.5.0")
 
