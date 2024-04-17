7a693d2642393647d9702b71bc5b59b63064d206
jackfox <jack.fox@colorado.edu>
2024-04-17 13:24:32 -0600

    Clean up comments in vizInterface.h
    Remove unitless [-] identifier throughout
    Alignment

--- src/simulation/vizard/vizInterface/vizInterface.h
+++ src/simulation/vizard/vizInterface/vizInterface.h
@@ -56,39 +56,39 @@ public:
     void addCamMsgToModule(Message<CameraConfigMsgPayload> *tmpMsg);
 
 public:
-    std::vector<VizSpacecraftData> scData;      //!< [-] vector of spacecraft data containers
-    std::vector <ReadFunctor<SpicePlanetStateMsgPayload>> spiceInMsgs;   //!< [-] vector of input messages of planet Spice data
-    std::vector<LocationPbMsg *> locations;       //!< [] vector of ground or spacecraft locations
-    std::vector<GravBodyInfo> gravBodyInformation; //!< [-] vector of gravitational body info
-    std::vector<Message<CameraImageMsgPayload>*> opnavImageOutMsgs;  //!< vector of vizard instrument camera output messages
-    int opNavMode;                              /*!< [int] Set non-zero positive value  if Unity/Viz couple in direct
-                                                 communication. (1 - regular opNav, 2 - performance opNav) */
-    bool saveFile;                              //!< [Bool] Set True if Vizard should save a file of the data.
-    bool liveStream;                            //!< [Bool] Set True if Vizard should receive a live stream of BSK data.
-    std::vector<void* >bskImagePtrs;            /*!< [RUN] vector of permanent pointers for the images to be used in BSK
-                                                     without relying on ZMQ because ZMQ will free it (whenever, who knows) */
+    std::vector<VizSpacecraftData> scData;         //!< vector of spacecraft data containers
+    std::vector <ReadFunctor<SpicePlanetStateMsgPayload>> spiceInMsgs; //!< vector of input messages of planet Spice data
+    std::vector<LocationPbMsg *> locations;        //!< vector of ground or spacecraft locations
+    std::vector<GravBodyInfo> gravBodyInformation; //!< vector of gravitational body info
+    std::vector<Message<CameraImageMsgPayload>*> opnavImageOutMsgs;    //!< vector of vizard instrument camera output messages
+    int opNavMode;                                 /*!< [int] Set non-zero positive value  if Unity/Viz couple in direct
+                                                    communication. (1 - regular opNav, 2 - performance opNav) */
+    bool saveFile;                                 //!< [Bool] Set True if Vizard should save a file of the data.
+    bool liveStream;                               //!< [Bool] Set True if Vizard should receive a live stream of BSK data.
+    std::vector<void* >bskImagePtrs;               /*!< [RUN] vector of permanent pointers for the images to be used in BSK
+                                                        without relying on ZMQ because ZMQ will free it (whenever, who knows) */
 
-    std::vector<ReadFunctor<CameraConfigMsgPayload>> cameraConfInMsgs;        //!< [-] vector of incoming camera data messages
-    std::vector<MsgCurrStatus> cameraConfMsgStatus;                           //!< [-] vector of msg status of incoming camera data
-    std::vector<CameraConfigMsgPayload> cameraConfigBuffers;                  //!< [-] vector of Camera config buffers
+    std::vector<ReadFunctor<CameraConfigMsgPayload>> cameraConfInMsgs;  //!< vector of incoming camera data messages
+    std::vector<MsgCurrStatus> cameraConfMsgStatus;                     //!< vector of msg status of incoming camera data
+    std::vector<CameraConfigMsgPayload> cameraConfigBuffers;            //!< vector of Camera config buffers
         
-    int64_t FrameNumber;                        //!< Number of frames that have been updated for TimeStamp message
-    std::string protoFilename;                  //!< Filename for where to save the protobuff message
-    VizSettings settings;                       //!< [-] container for the Viz settings that can be specified from BSK
-    LiveVizSettings liveSettings;               //!< [-] container for Viz settings that are updated on each time step
-    std::vector<EventDialog *> eventDialogs={}; //!< vector of dialog boxes to show
+    int64_t FrameNumber;                          //!< Number of frames that have been updated for TimeStamp message
+    std::string protoFilename;                    //!< Filename for where to save the protobuff message
+    VizSettings settings;                         //!< container for the Viz settings that can be specified from BSK
+    LiveVizSettings liveSettings;                 //!< container for Viz settings that are updated on each time step
+    std::vector<EventDialog *> eventDialogs={};   //!< vector of dialog boxes to show
 
-    std::string comProtocol;                    //!< Communication protocol to use when connecting to Vizard
-    std::string comAddress;                     //!< Communication address to use when connecting to Vizard
-    std::string comPortNumber;                  //!< Communication port number to use when connecting to Vizard
+    std::string comProtocol;                      //!< Communication protocol to use when connecting to Vizard
+    std::string comAddress;                       //!< Communication address to use when connecting to Vizard
+    std::string comPortNumber;                    //!< Communication port number to use when connecting to Vizard
     
-    ReadFunctor<EpochMsgPayload> epochInMsg;    //!< [-] simulation epoch date/time input msg
-    MsgCurrStatus epochMsgStatus;                   //!< [-] ID of the epoch msg
-    EpochMsgPayload epochMsgBuffer;                   //!< [-] epoch msg data
+    ReadFunctor<EpochMsgPayload> epochInMsg;      //!< simulation epoch date/time input msg
+    MsgCurrStatus epochMsgStatus;                 //!< ID of the epoch msg
+    EpochMsgPayload epochMsgBuffer;               //!< epoch msg data
 
-    Message<VizUserInputMsgPayload> userInputMsg; //!< [-] User input from Vizard
+    Message<VizUserInputMsgPayload> userInputMsg; //!< User input from Vizard
 
-    BSKLogger bskLogger;                        //!< [-] BSK Logging object
+    BSKLogger bskLogger;                          //!< BSK Logging object
 
 
 private:
@@ -97,11 +97,11 @@ private:
     void* requester_socket;
     int firstPass;                                          //!< Flag to intialize the viz at first timestep
 
-    std::vector<MsgCurrStatus>spiceInMsgStatus;             //!< [-] status of the incoming planets' spice data messages
-    std::vector <SpicePlanetStateMsgPayload> spiceMessage;  //!< [-] Spice message copies
-    std::ofstream *outputStream;                            //!< [-] Output file stream opened in reset
+    std::vector<MsgCurrStatus>spiceInMsgStatus;             //!< status of the incoming planets' spice data messages
+    std::vector <SpicePlanetStateMsgPayload> spiceMessage;  //!< Spice message copies
+    std::ofstream *outputStream;                            //!< Output file stream opened in reset
     void receiveUserInput(uint64_t CurrentSimNanos);        //!< request user input from Vizard, save as
-    void requestImage(size_t camCounter, uint64_t CurrentSimNanos);  //!<   request image from Vizard and store it in output img msg
+    void requestImage(size_t camCounter, uint64_t CurrentSimNanos);  //!< request image from Vizard and store it in output img msg
 
 };
 
