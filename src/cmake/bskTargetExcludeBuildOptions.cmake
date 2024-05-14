abe3da00a6f0ade63ff6ae3b152674f7f22d7128
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-05-14 17:53:40 -0600

    Add cielim interface to vizInterface toggle

--- src/cmake/bskTargetExcludeBuildOptions.cmake
+++ src/cmake/bskTargetExcludeBuildOptions.cmake
@@ -1,6 +1,6 @@
 option(BUILD_VIZINTERFACE "Build VizInterface Module" ON)
 if(NOT BUILD_VIZINTERFACE)
-  list(APPEND EXCLUDED_BSK_TARGETS "vizInterface")
+  list(APPEND EXCLUDED_BSK_TARGETS "vizInterface" "cielimInterface")
 endif()
 
 option(BUILD_OPNAV "Build OpNav Modules" OFF)
