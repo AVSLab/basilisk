1cff66173299821aae6b87b262de32162257e3c1
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2024-05-23 15:59:10 -0600

    update the sphinx configuration file
    Add support to work with sphinx version 7.x and sphinx_rtd_theme 2.x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,30 +95,26 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'style_nav_header_background': '#CFB87C'
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#CFB87C',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
-html_context = {
-    'css_files': ['_static/css/custom.css']
-}
+html_css_files = [
+    'css/custom.css',
+]
 
 html_logo = "./_images/static/Basilisk-Logo.png"
-#
-# html_theme_options = {
-#     'canonical_url': '',
-#     'logo_only': False,
-#     'display_version': True,
-#     'prev_next_buttons_location': 'bottom',
-#     'style_external_links': False,
-#     'vcs_pageview_mode': '',
-#     'style_nav_header_background': 'white',
-#     # Toc options
-#     'collapse_navigation': True,
-#     'sticky_navigation': True,
-#     'navigation_depth': 4,
-#     'includehidden': True,
-#     'titles_only': True
-# }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
