0af142dd791ff2e283ccb56f95169a98648398de
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-07-23 13:22:00 -0600

    Remove redundant forward slash character

--- src/architecture/messaging/msgAutoSource/generateMessagePackageInit.py
+++ src/architecture/messaging/msgAutoSource/generateMessagePackageInit.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
                 msgName = className.split('Payload')[0]
                 mainImportFid.write('from Basilisk.architecture.messaging.' + className + ' import *\n')
     mainImportFid.close()
-    oldModulePath = modulePath.split('messaging')[0] + '/cMsgCInterfacePy'
+    oldModulePath = modulePath.split('messaging')[0] + 'cMsgCInterfacePy'
     pathlib.Path(oldModulePath).unlink(missing_ok=True)
     os.symlink(modulePath, oldModulePath)
