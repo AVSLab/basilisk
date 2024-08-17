91fe49acc093f4e7851957d151efe41d49d7282e
Thibaud Teil <thibaud.teil@colorado.edu>
2024-08-17 15:26:18 -0600

    refactor and swig ekfInterface

--- src/fswAlgorithms/_GeneralModuleFiles/ekfInterface.cpp
+++ src/fswAlgorithms/_GeneralModuleFiles/ekfInterface.cpp
@@ -21,7 +21,7 @@
 #include "ekfInterface.h"
 
 EkfInterface::EkfInterface(FilterType type){
-    this->ckfOnlyMode = type;
+    this->filterType = type;
 }
 
 EkfInterface::~EkfInterface() = default;
@@ -32,69 +32,19 @@ EkfInterface::~EkfInterface() = default;
  */
 void EkfInterface::Reset(uint64_t currentSimNanos)
 {
-    this->customReset();
-
-    assert(this->stateInitial.size() == this->covarInitial.rows() &&
-    this->stateInitial.size() == this->covarInitial.cols());
-    assert(this->stateInitial.size() - this->constantRateStates.size() == this->processNoise.rows() &&
-    this->stateInitial.size() - this->constantRateStates.size() == this->processNoise.cols());
-
-    this->state = this->unitConversion * this->stateInitial;
-    this->stateError.resize(this->state.size());
-    this->stateError.setZero();
+    KalmanFilter::Reset(currentSimNanos);
+    this->stateError = Eigen::VectorXd ::Zero(this->state.size());
     this->stateTransitionMatrix = Eigen::MatrixXd::Identity(this->state.size(), this->state.size());
-    this->covar = this->unitConversion*this->unitConversion * this->covarInitial;
-    this->covar.resize(this->state.size(), this->state.size());
-    this->previousFilterTimeTag = (double) currentSimNanos*NANO2SEC;
-    if (this->constantRateStates.size() > 0){
-        this->constantRateStates = this->unitConversion * this->constantRateStates;
-        this->processNoise.resize(this->constantRateStates.size(), this->constantRateStates.size());
+    if (this->stateInitial.hasVelocity()) {
+        this->processNoise.resize(this->state.getVelocityStates().size(), this->state.getVelocityStates().size());
     }
-    else{
-        this->processNoise.resize(this->state.size()/2, this->state.size()/2);
+    else {
+        this->processNoise.resize(this->state.getPositionStates().size(), this->state.getPositionStates().size());
     }
 
+    this->customReset();
 }
 
-/*! Filter update
- @return void
- @param currentSimNanos The clock time at which the function was called (nanoseconds)
- */
-void EkfInterface::UpdateState(uint64_t currentSimNanos)
-{
-    this->customInitializeUpdate();
-    /*! Read all available measurements, add their information to the Measurement container class, then sort the
-     * vector in chronological order */
-    this->readFilterMeasurements();
-    this->orderMeasurementsChronologically();
-    /*! Loop through all of the measurements assuming they are in chronological order by first testing if a value
-     * has been populated in the measurements array*/
-     for (int index =0 ; index < MAX_MEASUREMENT_NUMBER; ++index) {
-         auto measurement = Measurement();
-         if (!this->measurements[index].has_value()){
-             continue;}
-         else{
-             measurement = this->measurements[index].value();}
-         /*! - If the time tag from a valid measurement is new compared to previous step,
-         propagate and update the filter*/
-         if (measurement.timeTag >= this->previousFilterTimeTag && measurement.validity) {
-             /*! - time update to the measurement time and compute pre-fit residuals*/
-             this->timeUpdate(measurement.timeTag);
-             measurement.preFitResiduals = EkfInterface::computeResiduals(measurement);
-             /*! - measurement update and compute post-fit residuals  */
-             this->measurementUpdate(measurement);
-             measurement.postFitResiduals = EkfInterface::computeResiduals(measurement);
-             this->measurements[index] = measurement;
-         }
-     }
-    /*! - If current clock time is further ahead than the last measurement time, then
-    propagate to this current time-step*/
-    if ((double) currentSimNanos * NANO2SEC > this->previousFilterTimeTag) {
-        this->timeUpdate((double) currentSimNanos * NANO2SEC);
-    }
-    this->customFinalizeUpdate();
-    this->writeOutputMessages(currentSimNanos);
-}
 
 /*! Perform the time update for kalman filter.
  @return void
@@ -106,38 +56,37 @@ void EkfInterface::timeUpdate(const double updateTime)
     this->stateTransitionMatrix = Eigen::MatrixXd::Identity(this->state.size(), this->state.size());
     std::array<double, 2> time = {0, dt};
 
-    /*! - Prepare full state and STM vector which is the state and flattened STM concatenated */
-    Eigen::MatrixXd reshapedStm = this->stateTransitionMatrix.reshaped<Eigen::RowMajor>().transpose();
-    Eigen::VectorXd flatStm(Eigen::Map<Eigen::VectorXd>(reshapedStm.data(), reshapedStm.cols()*reshapedStm.rows()));
-    Eigen::VectorXd stateStm(this->state.size() + flatStm.size());
-    stateStm << this->state, flatStm;
-
     /*! - Propagate full state and STM vector using dynamics specified in child class */
-    Eigen::VectorXd propagatedStateStm;
-    propagatedStateStm = propagate(time, stateStm, dt);
+    StateVector stateStm = this->state;
+    stateStm.attachStm(this->stateTransitionMatrix);
+    StateVector propagatedStateStm = this->dynamics.propagate(time, stateStm, dt);
 
-    Eigen::VectorXd propagatedStm;
-    propagatedStm = propagatedStateStm.tail(this->state.size()*this->state.size());
-    Eigen::MatrixXd stmTranspose(Eigen::Map<Eigen::MatrixXd>(propagatedStm.data(), this->state.size(), this->state.size()));
-    this->stateTransitionMatrix = stmTranspose.transpose();
+    this->stateTransitionMatrix = propagatedStateStm.detachStm();
 
     /*! - Unpack propagated states and update state, and state error */
-    this->state = propagatedStateStm.head(this->state.size());
+    this->state = propagatedStateStm;
     this->stateError = this->stateTransitionMatrix * this->stateError;
+    this->stateLogged = this->state.addVector(this->stateError);
 
     /*! - Update the covariance Pbar = Phi*P*Phi^T + Gamma*Q*Gamma^T
      * The process noise mapping will depend on the number of "rate" states */
     Eigen::MatrixXd processNoiseMapping;
-    if (this->constantRateStates.size() > 0) {
-        processNoiseMapping = Eigen::MatrixXd::Identity(this->state.size(), this->state.size());
+    if (!this->state.hasVelocity()) {
+        processNoiseMapping = Eigen::MatrixXd::Identity(this->state.getPositionStates().size(),
+                                                        this->state.getPositionStates().size());
         processNoiseMapping *= pow(dt, 2) / 2;
     }
     else{
-        processNoiseMapping.setZero(this->state.size(), this->state.size()/2);
-        processNoiseMapping.block(0, 0, this->state.size()/2, this->state.size()/2) =
-            pow(dt, 2) / 2 * Eigen::MatrixXd::Identity(this->state.size()/2, this->state.size()/2);
-        processNoiseMapping.block(this->state.size()/2, 0, this->state.size()/2, this->state.size()/2) =
-            dt * Eigen::MatrixXd::Identity(this->state.size()/2, this->state.size()/2);
+        processNoiseMapping.setZero(this->state.getPositionStates().size() + this->state.getVelocityStates().size(),
+                                    this->state.getVelocityStates().size());
+        processNoiseMapping.block(0, 0, this->state.getPositionStates().size(),
+                                  this->state.getPositionStates().size()) =
+            pow(dt, 2) / 2 * Eigen::MatrixXd::Identity(this->state.getPositionStates().size(),
+                                  this->state.getPositionStates().size());
+        processNoiseMapping.block(this->state.getPositionStates().size(), 0, this->state.getVelocityStates().size(),
+                                                        this->state.getVelocityStates().size()) =
+            dt * Eigen::MatrixXd::Identity(this->state.getVelocityStates().size(),
+                                                        this->state.getVelocityStates().size());
     }
     this->covar = this->stateTransitionMatrix*this->covar*this->stateTransitionMatrix.transpose() +
             processNoiseMapping*this->processNoise*processNoiseMapping.transpose();
@@ -150,19 +99,19 @@ void EkfInterface::timeUpdate(const double updateTime)
  @param Measurement
  @return void
  */
-void EkfInterface::measurementUpdate(const Measurement &measurement)
+void EkfInterface::measurementUpdate(const MeasurementModel &measurement)
 {
     /*! - Compute the valid observations delta */
-    Eigen::VectorXd measurementDelta = measurement.observation - measurement.model(this->state);
+    Eigen::VectorXd measurementDelta = measurement.getObservation() - measurement.model(this->state);
     /*! - Compute the measurement matrix at this state */
-    Eigen::MatrixXd measurementMatrix = computeMeasurementMatrix(this->state);
+    Eigen::MatrixXd measurementMatrix = measurement.computeMeasurementMatrix(this->state);
 
     /*! - Compute the Kalman Gain */
-    Eigen::MatrixXd kalmanGain = EkfInterface::computeKalmanGain(this->covar, measurementMatrix, measurement.noise);
+    Eigen::MatrixXd kalmanGain = EkfInterface::computeKalmanGain(this->covar, measurementMatrix, measurement.getMeasurementNoise());
 
     /*! - Update the covariance */
-    EkfInterface::updateCovariance(measurementMatrix, measurement.noise, kalmanGain);
-    if (this->covar.maxCoeff() > this->minCovarNorm || this->ckfOnlyMode == FilterType::Classical){
+    EkfInterface::updateCovariance(measurementMatrix, measurement.getMeasurementNoise(), kalmanGain);
+    if (this->covar.maxCoeff() > this->minCovarNorm || this->filterType == FilterType::Classical){
         /*! - Compute the update with a CKF if the covariance is high at the time of the update to avoid divergence*/
         EkfInterface::ckfUpdate(kalmanGain, measurementDelta, measurementMatrix);
     }
@@ -214,7 +163,7 @@ void EkfInterface::ckfUpdate(const Eigen::MatrixXd &kalmanGain,
                              const Eigen::MatrixXd &measurementMatrix){
 
     this->stateError = this->stateError + kalmanGain*(measurementDelta - measurementMatrix*this->stateError);
-    this->updatedWithCkf = true;
+    this->stateLogged = this->state.addVector(this->stateError);
 }
 
 /*! Extended Kalman Filter Update (the reference state is updated given the state error)
@@ -225,56 +174,29 @@ void EkfInterface::ckfUpdate(const Eigen::MatrixXd &kalmanGain,
 void EkfInterface::ekfUpdate(const Eigen::MatrixXd &kalmanGain, const Eigen::VectorXd &measurementDelta){
 
     this->stateError = kalmanGain*measurementDelta;
-    this->state += this->stateError;
-    this->updatedWithCkf = false;
+
+    this->state = this->state.addVector(this->stateError);
+    this->stateLogged = this->state;
 }
 
 /*! Compute the measurement residuals at a given time.
 @param Measurement
 @return Eigen::VectorXd
  */
-Eigen::VectorXd EkfInterface::computeResiduals(const Measurement &measurement)
+Eigen::VectorXd EkfInterface::computeResiduals(const MeasurementModel &measurement)
 {
-    Eigen::VectorXd measurementDelta(measurement.observation - measurement.model(this->state));
-    Eigen::MatrixXd measurementMatrix = computeMeasurementMatrix(this->state);
+    Eigen::VectorXd measurementDelta(measurement.getObservation() - measurement.model(this->state));
+    Eigen::MatrixXd measurementMatrix = measurement.computeMeasurementMatrix(this->state);
 
     return measurementDelta - measurementMatrix*this->stateError;
 }
 
-/*!- Order the measurements chronologically (standard sort)
- @return void
- */
-void EkfInterface::orderMeasurementsChronologically(){
-    std::sort(this->measurements.begin(), this->measurements.end(),
-              [](std::optional<Measurement> meas1, std::optional<Measurement> meas2){
-                    if (!meas1.has_value()){return false;}
-                    else if (!meas2.has_value()){return true;}
-                    else {return meas1.value().timeTag < meas2.value().timeTag;}
-                                  });
-}
-
-/*! Runge-Kutta 4 (RK4) function for state propagation
-    @param ODEfunction function handle that includes the equations of motion
-    @param X0 initial state
-    @param t0 initial time
-    @param dt time step
-    @return Eigen::VectorXd
-*/
-Eigen::VectorXd EkfInterface::rk4(const std::function<Eigen::VectorXd(double, Eigen::VectorXd)>& ODEfunction,
-                                const Eigen::VectorXd& X0,
-                                double t0,
-                                double dt) const
-{
-    double h = dt;
-
-    Eigen::VectorXd k1 = ODEfunction(t0, X0);
-    Eigen::VectorXd k2 = ODEfunction(t0 + h/2., X0 + h*k1/2.);
-    Eigen::VectorXd k3 = ODEfunction(t0 + h/2., X0 + h*k2/2.);
-    Eigen::VectorXd k4 = ODEfunction(t0 + h, X0 + h*k3);
-
-    Eigen::VectorXd X = X0 + 1./6.*h*(k1 + 2.*k2 + 2.*k3 + k4);
-
-    return X;
+/*! Get the filter dynamics matrix (A = df/dX evaluated at the reference)
+    @return Eigen::VectorXd stateInitial
+    */
+void EkfInterface::setFilterDynamicsMatrix(const std::function<const Eigen::MatrixXd(const double, const StateVector&)>&
+            dynamicsMatrixCalculator){
+    this->dynamics.setDynamicsMatrix(dynamicsMatrixCalculator);
 }
 
 /*! Set a minimum value (infinite norm, meaning maximal term) of the covariance before switching to Extended KF updates.
@@ -283,7 +205,7 @@ Eigen::VectorXd EkfInterface::rk4(const std::function<Eigen::VectorXd(double, Ei
     @return void
     */
 void EkfInterface::setMinimumCovarianceNormForEkf(const double infiniteNorm){
-    assert(this->ckfOnlyMode == FilterType::Extended && "EKF minimum norm set in a Classical implementation: this is "
+    assert(this->filterType == FilterType::Extended && "EKF minimum norm set in a Classical implementation: this is "
                                                         "only used in an extended kalman filter to temporarily use "
                                                         "linear updates when the covariance is high");
     this->minCovarNorm = infiniteNorm;
@@ -293,87 +215,8 @@ void EkfInterface::setMinimumCovarianceNormForEkf(const double infiniteNorm){
     @return double infiniteNorm
     */
 double EkfInterface::getMinimumCovarianceNormForEkf() const {
-    assert(this->ckfOnlyMode == FilterType::Extended && "EKF minimum norm requested in a Classical implementation: "
+    assert(this->filterType == FilterType::Extended && "EKF minimum norm requested in a Classical implementation: "
                                                         "this is only used in an extended kalman filter to temporarily "
                                                         "use linear updates when the covariance is high");
     return this->minCovarNorm;
-}
-
-/*! Set the filter initial state vector
-    @param Eigen::VectorXd initialStateInput
-    @return void
-    */
-void EkfInterface::setInitialState(const Eigen::VectorXd &initialStateInput){
-    this->stateInitial.resize(initialStateInput.size());
-    this->stateInitial << initialStateInput;
-}
-
-/*! Get the filter initial state vector
-    @return Eigen::VectorXd stateInitial
-    */
-Eigen::VectorXd EkfInterface::getInitialState() const {
-    return this->stateInitial;
-}
-
-/*! Set the filter initial state covariance
-    @param Eigen::MatrixXd initialCovarianceInput
-    @return void
-    */
-void EkfInterface::setInitialCovariance(const Eigen::MatrixXd &initialCovarianceInput){
-    this->covarInitial.resize(initialCovarianceInput.rows(), initialCovarianceInput.cols());
-    this->covarInitial << initialCovarianceInput;
-}
-
-/*! Get the filter initial state covariance
-    @return Eigen::MatrixXd covarInitial
-    */
-Eigen::MatrixXd EkfInterface::getInitialCovariance() const {
-    return this->covarInitial;
-}
-
-/*! Set the filter process noise
-    @param Eigen::MatrixXd processNoiseInput
-    @return void
-    */
-void EkfInterface::setProcessNoise(const Eigen::MatrixXd &processNoiseInput){
-    this->processNoise.resize(processNoiseInput.rows(), processNoiseInput.cols());
-    this->processNoise << processNoiseInput;
-}
-
-/*! Get the filter process noise
-    @return Eigen::MatrixXd processNoise
-    */
-Eigen::MatrixXd EkfInterface::getProcessNoise() const {
-    return this->processNoise;
-}
-
-/*! Set a unit conversion factor, for instance if desirable to solve for a state in km in the filter, but Basilisk's
- * outside facing interface is in SI
-    @param double conversion
-    */
-void EkfInterface::setUnitConversionFromSItoState(const double conversion){
-    this->unitConversion = conversion;
-}
-
-/*! Get a unit conversion factor, for instance if desirable to solve for a state in km in the filter, but Basilisk's
- * outside facing interface is in SI
-    @return double unitConversion
-    */
-double EkfInterface::getUnitConversionFromSItoState() const{
-    return this->unitConversion;
-}
-
-/*! Set a constant rate with which the rates will evolve: states(k+1) = states(k) + rateStates*dt
-    @param Eigen::VectorXd rateStates
-    @return void
-    */
-void EkfInterface::setConstantRateStates(const Eigen::VectorXd &rateStates){
-    this->constantRateStates = rateStates;
-}
-
-/*! Get the constant rate
-    @return Eigen::VectorXd rateStates
-    */
-Eigen::VectorXd EkfInterface::getConstantRateStates() const{
-    return this->constantRateStates;
 }
--- src/fswAlgorithms/_GeneralModuleFiles/ekfInterface.h
+++ src/fswAlgorithms/_GeneralModuleFiles/ekfInterface.h
@@ -29,6 +29,9 @@
 #include "architecture/messaging/messaging.h"
 #include "fswAlgorithms/_GeneralModuleFiles/filterInterfaceDefinitions.h"
 #include "fswAlgorithms/_GeneralModuleFiles/measurementModels.h"
+#include "fswAlgorithms/_GeneralModuleFiles/kalmanFilter.h"
+#include "fswAlgorithms/_GeneralModuleFiles/stateModels.h"
+#include "fswAlgorithms/_GeneralModuleFiles/dynamicModels.h"
 
 /*! Enumerator class to set if the filter is meant to be used purely as a linear/classical KF,
  * no reference state updates will be performed in the classical filter, while the EKF updates the reference
@@ -36,59 +39,23 @@
 enum class FilterType {Classical, Extended};
 
 /*! @brief Extended or Classical/Linear Kalman Filter base class. */
-class EkfInterface: public SysModel  {
+class EkfInterface: public KalmanFilter {
 public:
     EkfInterface(FilterType type);
     ~EkfInterface() override;
     void Reset(uint64_t CurrentSimNanos) override;
-    void UpdateState(uint64_t CurrentSimNanos) override;
 
+    void setFilterDynamicsMatrix(const std::function<const Eigen::MatrixXd(const double, const StateVector&)>&
+        dynamicsMatrixCalculator);
     void setMinimumCovarianceNormForEkf(double infiniteNorm);
     double getMinimumCovarianceNormForEkf() const;
-    void setInitialState(const Eigen::VectorXd &initialState);
-    Eigen::VectorXd getInitialState() const;
-    void setInitialCovariance(const Eigen::MatrixXd &initialCovariance);
-    Eigen::MatrixXd getInitialCovariance() const;
-    void setProcessNoise(const Eigen::MatrixXd &processNoise);
-    Eigen::MatrixXd getProcessNoise() const;
-    void setUnitConversionFromSItoState(double conversion);
-    double getUnitConversionFromSItoState() const ;
-    void setConstantRateStates(const Eigen::VectorXd &rateStates);
-    Eigen::VectorXd getConstantRateStates() const;
 
-protected:
-    virtual void customReset(){/* virtual */};
-    virtual void customInitializeUpdate(){/* virtual */};
-    virtual void customFinalizeUpdate(){/* virtual */};
-    virtual Eigen::VectorXd propagate(std::array<double, 2> interval, const Eigen::VectorXd& X0, double dt)=0;
-    virtual Eigen::MatrixXd computeDynamicsMatrix(const Eigen::VectorXd& state)=0;
-    virtual Eigen::MatrixXd computeMeasurementMatrix(const Eigen::VectorXd& state)=0;
-    /*! Read method neads to read incoming messages containing the measurements for the filter.
-     * Their information must be added to the Measurement container class, and added to the measurements vector.
-     * Each measurement must be paired with a measurement model provided in the measurementModels.h */
-    virtual void readFilterMeasurements()=0;
-    virtual void writeOutputMessages(uint64_t CurrentSimNanos)=0;
-
-    Eigen::VectorXd rk4(const std::function<Eigen::VectorXd(double, Eigen::VectorXd)>& ODEfunction,
-                const Eigen::VectorXd& X0,
-                double t0,
-                double dt) const;
-
-    std::array<std::optional<Measurement>, MAX_MEASUREMENT_NUMBER> measurements;  //!< [Measurements] All measurement containers in chronological order
-    double previousFilterTimeTag = 0; //!< [s]  Last filter time-tag
-    double unitConversion = 1; //!< [-] Scale that converts input units (SI) to a desired unit for the inner maths
-    bool updatedWithCkf = true; //!< [-] Flag to signal that filter was last updated with a Linear measurement update
-
-    Eigen::VectorXd state; //!< [-] State estimate for time TimeTag
-    Eigen::VectorXd stateError; //!< [-] State error for time TimeTag
-    Eigen::VectorXd constantRateStates; //!< [-] Constant rate states if the filter doesn not estimate them
-    Eigen::MatrixXd stateTransitionMatrix; //!< [-] State Transition Matrix
-    Eigen::MatrixXd covar; //!< [-] covariance
 
 private:
-    void timeUpdate(const double updateTime);
-    void measurementUpdate(const Measurement &measurement);
-    Eigen::VectorXd computeResiduals(const Measurement &measurement);
+    void timeUpdate(double updateTime) final;
+    void measurementUpdate(const MeasurementModel &measurement) final;
+
+    Eigen::VectorXd computeResiduals(const MeasurementModel &measurement) final;
     Eigen::MatrixXd computeKalmanGain(const Eigen::MatrixXd &covar,
                                     const Eigen::MatrixXd &measurementMatrix,
                                     const Eigen::MatrixXd &measurementNoise) const;
@@ -96,14 +63,10 @@ private:
     void ckfUpdate(const Eigen::MatrixXd &kalmanGain, const Eigen::VectorXd &yMeas, const Eigen::MatrixXd &measurementMatrix);
     void ekfUpdate(const Eigen::MatrixXd &kalmanGain, const Eigen::VectorXd &yMeas);
 
-    void orderMeasurementsChronologically();
-
-    Eigen::MatrixXd processNoise; //!< [-] process noise matrix
-    Eigen::VectorXd stateInitial; //!< [-] State estimate for time TimeTag at previous time
-    Eigen::MatrixXd covarInitial; //!< [-] covariance at previous time
+    Eigen::MatrixXd stateTransitionMatrix; //!< [-] State Transition Matrix
     double minCovarNorm = 1E-5; /*!< [-] Infinite norm after which the filter will begin processing measurements as
     an extended kalman filter */
-    FilterType ckfOnlyMode = FilterType::Extended; //!< [-] flag to know whether the filter is being run as a linear KF or extended KF
+    FilterType filterType = FilterType::Extended; //!< [-] flag to know whether the filter is being run as a linear KF or extended KF
 };
 
 #endif /* EKF_INTERFACE_HPP */
