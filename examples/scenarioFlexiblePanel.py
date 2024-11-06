e5efd502e5936119d25f3b2491bd168ab7d2035b
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2024-11-06 09:52:00 -0500

    add links to the Vizard YouTube videos of these scenarios

--- examples/scenarioFlexiblePanel.py
+++ examples/scenarioFlexiblePanel.py
@@ -40,6 +40,17 @@ comprehensive Vizard simulation which creates an appropriate to-scale model for 
 Illustration of Simulation Results
 ----------------------------------
 
+.. raw:: html
+
+   <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;
+   max-width: 100%; height: auto;">
+        <iframe src="https://www.youtube.com/embed/kbcqUD4MW9c?si=B09rp8jm_m9pbsra"
+        style="position: absolute;
+        top: 0; left: 0; width: 100%; height: 100%;" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+        gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
 .. image:: /_images/Scenarios/scenarioFlexiblePanel_theta.svg
    :align: center
 
--- examples/scenarioRoboticArm.py
+++ examples/scenarioRoboticArm.py
@@ -40,6 +40,17 @@ to-scale model for the defined scenario.
 Illustration of Simulation Results
 ----------------------------------
 
+.. raw:: html
+
+   <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;
+   max-width: 100%; height: auto;">
+        <iframe src="https://www.youtube.com/embed/4mMCCTVHZkA?si=L3Pj9dfy7a8DSwjQ"
+        style="position: absolute;
+        top: 0; left: 0; width: 100%; height: 100%;" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+        gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
 .. image:: /_images/Scenarios/scenarioRoboticArm_theta.svg
    :align: center
 
