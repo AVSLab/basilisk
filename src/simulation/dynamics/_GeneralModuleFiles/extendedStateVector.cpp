8145fa201c262b6b96e3cfa8bf8c007cdacb77e6
Juan Garcia Bonilla <juan@garciabonilla.com>
2024-11-24 02:42:25 -0800

    Adapt ExtendedStateVector::fromStateData for polymorphic StateData

--- src/simulation/dynamics/_GeneralModuleFiles/extendedStateVector.cpp
+++ src/simulation/dynamics/_GeneralModuleFiles/extendedStateVector.cpp
@@ -124,7 +124,7 @@ ExtendedStateVector::fromStateData(const std::vector<DynamicObject*>& dynPtrs,
     for (size_t dynIndex = 0; dynIndex < dynPtrs.size(); dynIndex++) {
         for (auto&& [stateName, stateData] :
              dynPtrs.at(dynIndex)->dynManager.stateContainer.stateMap) {
-            result.emplace(std::make_pair(dynIndex, stateName), functor(stateData));
+            result.emplace(std::make_pair(dynIndex, stateName), functor(*stateData.get()));
         }
     }
 
