6ca86c91fc73d496209542124a459201c2906978
Juan Garcia Bonilla <juan@garciabonilla.com>
2024-11-26 19:50:52 -0800

    Adapt DynParamManager for polymorphic StateData

--- src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.cpp
+++ src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.cpp
@@ -17,177 +17,127 @@
 
  */
 
-
 #include "dynParamManager.h"
+#include <algorithm>
 #include <iostream>
 
-DynParamManager::DynParamManager()
+StateVector::StateVector(const StateVector& other)
 {
-    return;
+    *this = other;
 }
 
-DynParamManager::~DynParamManager()
+StateVector&
+StateVector::operator=(const StateVector& other)
 {
-    return;
+    stateMap.clear();
+    for (const auto& [key, value] : other.stateMap) {
+        stateMap.emplace(key, value->clone());
+    }
+    return *this;
 }
 
-StateData* DynParamManager::registerState(uint32_t nRow, uint32_t nCol,
-    std::string stateName)
+void StateVector::setStates(const StateVector& operand)
 {
-    if (stateName == "") {
-        bskLogger.bskLog(BSK_ERROR, "Your state name can't be an empty string.  Come on.  You get null.");
-        return nullptr;
+    for (auto [it1, it2] = std::tuple{std::begin(this->stateMap), std::begin(operand.stateMap)};
+         it1 != std::end(this->stateMap) && it2 != std::end(operand.stateMap);
+         ++it1, ++it2) {
+        it1->second->setState(it2->second->getState());
     }
+}
 
-    std::map<std::string, StateData>::iterator it;
-    it = stateContainer.stateMap.find(stateName);
-    if(it != stateContainer.stateMap.end())
-    {
-        bskLogger.bskLog(BSK_WARNING, "You created a state with the name: %s more than once.  Go ahead and don't do this.", stateName.c_str());
-        if(it->second.getRowSize() != nRow || it->second.getColumnSize() != nCol)
-        {
-            bskLogger.bskLog(BSK_ERROR, "In addition to that, you tried to change the size of the state in question.  Come on.  You get null.");
-            return nullptr;
-        }
+void StateVector::addStates(const StateVector& operand)
+{
+    for (auto [it1, it2] = std::tuple{std::begin(this->stateMap), std::begin(operand.stateMap)};
+         it1 != std::end(this->stateMap) && it2 != std::end(operand.stateMap);
+         ++it1, ++it2) {
+        it1->second->addState(*it2->second);
     }
-    else
-    {
-        Eigen::MatrixXd stateMatrix;
-        stateMatrix.resize(nRow, nCol);
-        StateData newState(stateName, stateMatrix);
-        stateContainer.stateMap.insert(std::pair<std::string, StateData>
-                              (stateName, newState));
-        it = stateContainer.stateMap.find(stateName);
+}
+
+void StateVector::scaleStates(double scaleFactor)
+{
+    for (const auto& [key, value] : stateMap) {
+        value->scaleState(scaleFactor);
     }
-    return (&(it->second));
+}
+
+void StateVector::propagateStates(double dt)
+{
+    for (const auto& [key, value] : stateMap) {
+        value->propagateState(dt);
+    }
 }
 
 StateData* DynParamManager::getStateObject(std::string stateName)
 {
-    StateData *statePtr;
-    std::map<std::string, StateData>::iterator it;
-
-    statePtr = nullptr;
-    it = stateContainer.stateMap.find(stateName);
-    if(it != stateContainer.stateMap.end())
-    {
-        statePtr = &(it->second);
+    if (stateContainer.stateMap.count(stateName) > 0) {
+        return stateContainer.stateMap.at(stateName).get();
     }
 
-    if (statePtr == nullptr)
-    {
-        /*  The requested state could not be found.
-            Either the state name was miss-spelled, or the state simply
-            doesn't exit in the current simulaiton setup (i.e. asking for the
-            hub attitude in a translation only simulation setup */
-        bskLogger.bskLog(BSK_WARNING, "You requested this non-existent state name: %s You either miss-typed the stateName, or you asked for a state that doesn't exist in your simulation setup. stateName = ", stateName.c_str());
-    }
+    /*  The requested state could not be found.
+        Either the state name was miss-spelled, or the state simply
+        doesn't exit in the current simulaiton setup (i.e. asking for the
+        hub attitude in a translation only simulation setup */
+    bskLogger.bskLog(
+        BSK_WARNING,
+        "You requested this non-existent state name: %s You either miss-typed the stateName, or "
+        "you asked for a state that doesn't exist in your simulation setup.",
+        stateName.c_str());
 
-    return(statePtr);
+    return nullptr;
 }
 
-StateVector DynParamManager::getStateVector()
+void DynParamManager::updateStateVector(const StateVector& newState)
 {
-    return(stateContainer);
+    this->stateContainer.setStates(newState);
 }
 
-void DynParamManager::updateStateVector(const StateVector & newState)
-{
-
-    std::map<std::string, StateData>::iterator it;
-    std::map<std::string, StateData>::const_iterator inIt;
-    for (it = stateContainer.stateMap.begin(), inIt = newState.stateMap.begin();
-         it != stateContainer.stateMap.end() && inIt != newState.stateMap.end(); it++, inIt++)
-    {
-        it->second.setState(inIt->second.getState());
-
-    }
-}
-
-void DynParamManager::propagateStateVector(double dt)
-{
-    std::map<std::string, StateData>::iterator it;
-    for (it = stateContainer.stateMap.begin();
-         it != stateContainer.stateMap.end(); it++)
-    {
-        it->second.propagateState(dt);
-
-    }
-}
-
-StateVector StateVector::operator+(const StateVector& operand)
-{
-    std::map<std::string, StateData>::iterator it;
-    std::map<std::string, StateData>::const_iterator opIt;
-    StateVector outVector;
-    for (it = stateMap.begin(), opIt = operand.stateMap.begin();
-         it != stateMap.end() && opIt != operand.stateMap.end(); it++, opIt++)
-    {
-        StateData newState = it->second + opIt->second;
-        outVector.stateMap.insert(std::pair<std::string, StateData>
-                                  (it->first, newState));
-
-    }
-    return outVector;
-}
-
-StateVector StateVector::operator*(double scaleFactor)
-{
-    StateVector outVector;
-    std::map<std::string, StateData>::iterator it;
-    for (it = stateMap.begin(); it != stateMap.end(); it++)
-    {
-        outVector.stateMap.insert(std::pair<std::string, StateData>
-                                  (it->first, it->second*scaleFactor));
-
-    }
-
-    return outVector;
-}
+void DynParamManager::propagateStateVector(double dt) { this->stateContainer.propagateStates(dt); }
 
 Eigen::MatrixXd* DynParamManager::createProperty(std::string propName,
-    const Eigen::MatrixXd & propValue)
+                                                 const Eigen::MatrixXd& propValue)
 {
     std::map<std::string, Eigen::MatrixXd>::iterator it;
     it = dynProperties.find(propName);
-    if(it == dynProperties.end())
-    {
-        dynProperties.insert(std::pair<std::string, Eigen::MatrixXd>
-                             (propName, propValue));
+    if (it == dynProperties.end()) {
+        dynProperties.insert(std::pair<std::string, Eigen::MatrixXd>(propName, propValue));
     }
-    else{
-        bskLogger.bskLog(BSK_WARNING, "You created the dynamic property: %s more than once.  You shouldn't be doing that.", propName.c_str());
+    else {
+        bskLogger.bskLog(
+            BSK_WARNING,
+            "You created the dynamic property: %s more than once.  You shouldn't be doing that.",
+            propName.c_str());
         it->second = propValue;
     }
-    return(&(dynProperties.find(propName)->second));
+    return (&(dynProperties.find(propName)->second));
 }
 
 Eigen::MatrixXd* DynParamManager::getPropertyReference(std::string propName)
 {
     std::map<std::string, Eigen::MatrixXd>::iterator it;
     it = dynProperties.find(propName);
-    if(it == dynProperties.end())
-    {
-        bskLogger.bskLog(BSK_ERROR, "You requested the property: %s which doesn't exist.  Null returned.", propName.c_str());
+    if (it == dynProperties.end()) {
+        bskLogger.bskLog(BSK_ERROR,
+                         "You requested the property: %s which doesn't exist.  Null returned.",
+                         propName.c_str());
         return nullptr;
     }
-    else
-    {
-        return(&(it->second));
+    else {
+        return (&(it->second));
     }
 }
 
-void DynParamManager::setPropertyValue(const std::string propName,
-                      const Eigen::MatrixXd & propValue)
+void DynParamManager::setPropertyValue(const std::string propName, const Eigen::MatrixXd& propValue)
 {
     std::map<std::string, Eigen::MatrixXd>::iterator it;
     it = dynProperties.find(propName);
-    if(it == dynProperties.end())
-    {
-        bskLogger.bskLog(BSK_ERROR, "You tried to set the property value for: %s which has not been created yet. I can't do that.", propName.c_str());
+    if (it == dynProperties.end()) {
+        bskLogger.bskLog(BSK_ERROR,
+                         "You tried to set the property value for: %s which has not been created "
+                         "yet. I can't do that.",
+                         propName.c_str());
     }
-    else
-    {
+    else {
         it->second = propValue;
     }
 }
--- src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.h
+++ src/simulation/dynamics/_GeneralModuleFiles/dynParamManager.h
@@ -17,48 +17,179 @@
 
  */
 
-
 #ifndef STATE_MANAGER_H
 #define STATE_MANAGER_H
 
-#include <stdint.h>
-#include <map>
-#include <vector>
-#include <Eigen/Dense>
-#include "stateData.h"
 #include "architecture/utilities/bskLogging.h"
+#include "stateData.h"
+#include <Eigen/Dense>
+#include <map>
+#include <stdint.h>
+#include <type_traits>
+#include <vector>
 
-
-/*! state vector class */
+/** StateVector represents an ordered collection of StateData,
+ * with each state having a unique name.
+ */
 class StateVector {
-public:
-    std::map<std::string, StateData> stateMap;          //!< class method
-public:
-    StateVector operator+(const StateVector& operand);  //!< class method
-    StateVector operator*(double scaleFactor);          //!< class method
+  public:
+    /** All states managed by the StateVector, cached
+     * by name.
+     *
+     * StateData classes have virtual methods, and this class
+     * should be capable of supporting subclasses of StateData.
+     * To do so, we need to store StateData objects as pointers.
+     * To facilitate memory management, we use unique_ptr.
+     */
+    std::map<std::string, std::unique_ptr<StateData>> stateMap;
+
+  public:
+    /** Default constructor */
+    StateVector() = default;
+
+    /** Copy constructor */
+    StateVector(const StateVector& other);
+
+    /** Assignment operator */
+    StateVector& operator= (const StateVector&);
+
+    /** Sets the values of the states of this StateVector to a copy the
+     * values of the states of the other StateVector.
+     *
+     * Note that we assume that both StateVectors have the same states
+     * and in the same order !!!*/
+    void setStates(const StateVector& operand);
+
+    /** Adds the states of the given StateVector to the states of
+     * this StateVector.
+     *
+     * Note that we assume that both StateVectors have the same states
+     * and in the same order !!!*/
+    void addStates(const StateVector& operand);
+
+    /** Scales the states of this StateVector by the given factor */
+    void scaleStates(double scaleFactor);
+
+    /** Scales the states of this StateVector by the given delta time */
+    void propagateStates(double dt);
 };
 
-/*! dynamic parameter manager class */
+/** A class that manages a set of states and properties. */
 class DynParamManager {
-public:
-    std::map<std::string, Eigen::MatrixXd> dynProperties; //!< class variable
-    StateVector stateContainer;                             //!< class variable
-    BSKLogger bskLogger;                      //!< -- BSK Logging
-public:
-    DynParamManager();
-    ~DynParamManager();
-    StateData* registerState(uint32_t nRow, uint32_t nCol, std::string stateName); //!< class method
-    StateData* getStateObject(std::string stateName); //!< class method
-    StateVector getStateVector(); //!< class method
-    void updateStateVector(const StateVector & newState); //!< class method
-    void propagateStateVector(double dt); //!< class method
-    Eigen::MatrixXd* createProperty(std::string propName,
-                                    const Eigen::MatrixXd & propValue); //!< class method
-    Eigen::MatrixXd* getPropertyReference(std::string propName); //!< class method
-    void setPropertyValue(const std::string propName,
-                          const Eigen::MatrixXd & propValue); //!< class method
-    
+  public:
+    /** A map of properties managed by this class.
+     *
+     * Properties are matrices of doubles that are defined and updated
+     * by modules. They are not integrated at any point, so it is the
+     * responsability of the user to update them so that they remain
+     * current.
+     */
+    std::map<std::string, Eigen::MatrixXd> dynProperties;
+
+    /** A collection of states managed by this class */
+    StateVector stateContainer;
+
+    /** Logger used by this class */
+    BSKLogger bskLogger;
+
+  public:
+    /** Creates and stores a new state to be managed by this class.
+     *
+     * The state name should be unique: registering two states with the
+     * same name will cause either an error or a warning.
+     *
+     * This method may optionally be templated to create StateData of
+     * subclasses of StateData.
+     */
+    template <typename StateDataType = StateData,
+              std::enable_if_t<std::is_base_of_v<StateData, StateDataType>, bool> = true>
+    StateDataType* registerState(uint32_t nRow, uint32_t nCol, std::string stateName);
+
+    /** Retrieves the handler to a previously registered StateData.
+     *
+     * Calling this method for a state name not previously registered will raise a warning
+     * and return a nullptr.
+     */
+    StateData* getStateObject(std::string stateName);
+
+    /** Creates and stores a new property to be managed by this class.
+     *
+     * The property name should be unique: registering two properties with the
+     * same name will cause a warning or an error.
+     */
+    Eigen::MatrixXd* createProperty(std::string propName, const Eigen::MatrixXd& propValue);
+
+    /** Retrieves the handler to a previously registered property.
+     *
+     * Calling this method for a property name not previously registered
+     * will raise an error.
+     */
+    Eigen::MatrixXd* getPropertyReference(std::string propName);
+
+    /** Sets the value for a property.
+     *
+     * An error will be raised if no property exists with name or the size of the
+     * given matrix is different from the size of the existing property.
+     */
+    void setPropertyValue(const std::string propName, const Eigen::MatrixXd& propValue);
+
+    /** Sets the values of the states managed by this class to a copy the
+     * values of the states of the given StateVector.
+     *
+     * Note that we assume that given StateVector have the same states
+     * and in the same order as this object !!!*/
+    void updateStateVector(const StateVector& newState);
+
+    /** Propagates the states managed by this class a given delta time.  */
+    void propagateStateVector(double dt);
 };
 
+template <typename StateDataType,
+          std::enable_if_t<std::is_base_of_v<StateData, StateDataType>, bool>>
+StateDataType* DynParamManager::registerState(uint32_t nRow, uint32_t nCol, std::string stateName)
+{
+    if (stateName == "") {
+        bskLogger.bskLog(BSK_ERROR,
+                         "Your state name can't be an empty string.  Come on.  You get null.");
+        return nullptr;
+    }
+
+    if (stateContainer.stateMap.count(stateName) > 0) {
+        bskLogger.bskLog(
+            BSK_WARNING,
+            "You created a state with the name: %s more than once.  Go ahead and don't do this.",
+            stateName.c_str());
+
+        auto& stateData = stateContainer.stateMap.at(stateName);
+
+        if (stateData->getRowSize() != nRow || stateData->getColumnSize() != nCol) {
+            bskLogger.bskLog(BSK_ERROR,
+                             "In addition to that, you tried to change the size of the state in "
+                             "question.  Come on.  You get null.");
+            return nullptr;
+        }
+
+        auto casted = dynamic_cast<StateDataType*>(stateData.get());
+        if (!casted) {
+            bskLogger.bskLog(BSK_ERROR,
+                             "In addition to that, you tried to change the StateData type.  Come "
+                             "on.  You get null.");
+            return nullptr;
+        }
+
+        return casted;
+    }
+
+    Eigen::MatrixXd stateMatrix;
+    stateMatrix.resize(nRow, nCol);
+
+    // Emplacing this stateData in the map will wrap the raw
+    // pointer in a unique_ptr, so no worries about leaks.
+    // I didn't emplace `std::make_unique<StateDataType>(...)
+    // because I needed the raw pointer anyway to return it.
+    StateDataType* stateData = new StateDataType(stateName, stateMatrix);
+    stateContainer.stateMap.emplace(stateName, stateData);
+    return stateData;
+}
 
 #endif /* STATE_MANAGER_H */
