e150f00c9a5a2937ff8919ce9681bbdf88353405
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2024-12-28 20:34:41 -0700

    improve variable comment string

--- src/architecture/_GeneralModuleFiles/sys_model.h
+++ src/architecture/_GeneralModuleFiles/sys_model.h
@@ -54,7 +54,7 @@ public:
     /** Called at simulation initialization, resets module to specified time */
     virtual void Reset(uint64_t CurrentSimNanos){};
 
-    std::string ModelTag = "";     //!< name for the algorithm to base off of
+    std::string ModelTag = "";     //!< Basilisk module tag name
     uint64_t CallCounts = 0;       //!< Counts on the model being called
     uint32_t RNGSeed = 0x1badcad1; //!< Giving everyone a random seed for ease of MC
     int64_t moduleID;              //!< Module ID for this module  (handed out by module_id_generator)
