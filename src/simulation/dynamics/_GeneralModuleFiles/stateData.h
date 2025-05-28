5ecc465703a44155b6ab81b00c8e0b3e62002fe6
juan-g-bonilla <juan@garciabonilla.com>
2025-05-27 22:25:21 -0700

    Enhance StateData class to support stochastic dynamics

--- src/simulation/dynamics/_GeneralModuleFiles/stateData.h
+++ src/simulation/dynamics/_GeneralModuleFiles/stateData.h
@@ -30,6 +30,7 @@ class StateData
 public:
     Eigen::MatrixXd state;       //!< [-] State value storage
     Eigen::MatrixXd stateDeriv;  //!< [-] State derivative value storage
+    std::vector<Eigen::MatrixXd> stateDiffusion; //!< [-] State diffusion value storage
     const std::string stateName; //!< [-] Name of the state
     BSKLogger bskLogger;         //!< -- BSK Logging
 
@@ -46,17 +47,65 @@ public:
     /** Destructor */
     virtual ~StateData() = default;
 
+    /** Sets the number of noise sources for this state.
+     *
+     * This is used for stochastic dynamics, where the
+     * evolutions of the state are driven by a set of independent
+     * noise sources:
+     *
+     *  dx = f(t,x)dt + g_0(t,x)dW_0 + g_1(t,x)dW_1 + ... + g_{n-1}(t,x)dW_{n-1}
+     *
+     * where dW_i are independent Wiener processes. The number of
+     * noise sources is equal to the number of diffusion matrices
+     * that are used to drive the stochastic dynamics (n above).
+     *
+     * @param numSources The number of noise sources
+     */
+    void setNumNoiseSources(size_t numSources);
+
+    /** Get how many independent sources of noise drive the dynamics
+     * of this state.
+     *
+     * Any number greater than zero indicates that this state
+     * is driven by a stochastic differential equation.
+     */
+    size_t getNumNoiseSources() const;
+
     /** Updates the value of the state */
     void setState(const Eigen::MatrixXd& newState);
 
     /** Updates the derivative of the value of the state */
     void setDerivative(const Eigen::MatrixXd& newDeriv);
 
+    /** Updates the diffusion of the value of the state.
+     *
+     * This is used for stochastic dynamics, where the
+     * evolutions of the state are driven by a set of independent
+     * noise sources:
+     *
+     *  dx = f(t,x)dt + g_0(t,x)dW_0 + g_1(t,x)dW_1 + ... + g_{n-1}(t,x)dW_{n-1}
+     *
+     * where dW_i are independent Wiener processes. The diffusion
+     * matrices are used to drive the stochastic dynamics (g_i above).
+     *
+     * @param newDiffusion The new diffusion matrix
+     * @param index The index of the diffusion matrix to update.
+     * This must be less than the number of noise sources.
+    */
+    void setDiffusion(const Eigen::MatrixXd& newDiffusion, size_t index);
+
     /** Retrieves a copy of the current state */
     Eigen::MatrixXd getState() const { return state; }
 
     /** Retrieves a copy of the current state derivative */
     Eigen::MatrixXd getStateDeriv() const { return stateDeriv; }
+
+    /** Retrieves a copy of the current state diffusion
+     *
+     * @param index The index of the diffusion matrix to retrieve.
+     * This must be less than the number of noise sources.
+    */
+    Eigen::MatrixXd getStateDiffusion(size_t index) const { return stateDiffusion.at(index); }
 
     /** Returns the name of the state */
     std::string getName() const { return stateName; }
@@ -79,8 +128,20 @@ public:
     /** Adds the values of the other state to this state */
     void addState(const StateData& other);
 
-    /** Propagates the state in time with the stored derivative */
-    virtual void propagateState(double dt);
+    /**
+     * @brief Propagates the state over a time step.
+     *
+     * This method integrates the position state using the state derivative
+     * over the given time step::
+     *
+     *   x += f(t,x)*h + g_0(t,x)*pseudoStep[0] + g_1(t,x)*pseudoStep[1] ...
+     *
+     * @param h The time step for propagation.
+     * @param pseudoStep For states driven by stochastic dynamics, this
+     * represents the random pseudotimestep. The length of this input must
+     * match the number of noise sources of this state (``getNumNoiseSources()``)
+     */
+    virtual void propagateState(double h, std::vector<double> pseudoStep = {});
 };
 
 #endif /* STATE_DATA_H */
