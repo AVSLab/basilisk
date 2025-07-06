3cfb17f29006e145af65b674b3a4ad312ef58f97
Rayan Mazouz <rayan.mazouz@colorado.edu>
2025-07-05 20:38:45 -0600

    Modify main.py in accordance with re-base

--- fault/main.py
+++ fault/main.py
@@ -1,13 +1,10 @@
-from src.core import run
-from src.sweeping import sweep_moving_window
+import argparse
+from src import fid
 
 if __name__ == "__main__":
-    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sweep', action='store_true')
+    parser.add_argument('--sweep', type=int, default=0, help="Set sweep window size (0 for none)")
+    parser.add_argument('--showPlots', action='store_true', help="Display plots")
     args = parser.parse_args()
 
-    if args.sweep:
-        N_vals, fail_rate, avg_delay = sweep_moving_window()
-    else:
-        run(True, 5)
+    fid.run_fid(sweep_window=args.sweep, showPlots=args.showPlots)
