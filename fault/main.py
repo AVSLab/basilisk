26887577547c5a328ba06e221d0953fc54e9aff2
Rayan Mazouz <rayan.mazouz@colorado.edu>
2025-07-06 10:05:54 -0600

    Accomodate main for multiple sweeps

--- fault/main.py
+++ fault/main.py
@@ -1,10 +1,28 @@
 import argparse
 from src.fault import FID
+from src.plot import plot_fid_metrics
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sweep', type=int, default=0, help="Set sweep window size (0 for none)")
+    parser.add_argument(
+        '--sweep', 
+        type=int, 
+        nargs='*', 
+        default=[1, 5, 10, 15, 20, 25, 30, 35, 40]
+    )
     parser.add_argument('--showPlots', action='store_true', help="Display plots")
     args = parser.parse_args()
 
-    FID.run_fid(sweep_window=args.sweep, showPlots=args.showPlots)
+    sweep_values = args.sweep
+    all_fail_rates = []
+    all_avg_delays = []
+    for sweep in args.sweep:
+        fail_rate, avg_delay = FID.run_fid(sweep_window=sweep)
+        all_fail_rates.append(fail_rate)
+        all_avg_delays.append(avg_delay)
+
+    # Plotting
+    plot_fid_metrics(sweep_values=sweep_values,
+                    fail_rates=all_fail_rates,
+                    avg_delays=all_avg_delays)
