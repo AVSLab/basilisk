e03d29d8d36c56360c621e0b2341943e5edcbecb
William Schwend <williamschwend@gmail.com>
2025-09-17 14:01:35 -0600

    create msg payload definition

---
+++ src/architecture/msgPayloadDefC/JointArrayStateMsgPayload.h
@@ -0,0 +1,33 @@
+/*
+ ISC License
+
+ Copyright (c) 2016, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef JOINT_STATE_MESSAGE_H
+#define JOINT_STATE_MESSAGE_H
+
+#include <stdint.h>
+#include "architecture/utilities/macroDefinitions.h"
+
+/*! @brief Joint state FSW msg */
+typedef struct{
+    double thetas[MAX_EFF_CNT];        //!< [rad], joint angles
+    double thetaDots[MAX_EFF_CNT];     //!< [rad/s], joint angle rates
+}JointArrayStateMsgPayload;
+
+
+#endif
