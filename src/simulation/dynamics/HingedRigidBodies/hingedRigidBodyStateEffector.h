dbec9a34ecfde731f2fd888bd5dc642b900bd46f
Andrew Morell <ammorell13@hotmail.com>
2025-10-27 16:23:16 -0600

    formatting clean up

--- src/simulation/dynamics/HingedRigidBodies/hingedRigidBodyStateEffector.h
+++ src/simulation/dynamics/HingedRigidBodies/hingedRigidBodyStateEffector.h
@@ -58,56 +58,67 @@ public:
 
 private:
     static uint64_t effectorID;        //!< [] ID number of this panel
-    double theta;                    //!< [rad] hinged rigid body angle
-    double thetaDot;                 //!< [rad/s] hinged rigid body angle rate
-    double cTheta;                  //!< -- term needed for back substitution
     double u;                        //!< [N-m] optional motor torque
-    Eigen::Vector3d r_HP_P;          //!< [m] vector pointing from primary body frame P origin to Hinge location.  If a single spacecraft body is modeled than P is the same as B
-    Eigen::Matrix3d dcm_HP;          //!< -- DCM from primary body frame to hinge frame
+
+    // Terms needed for back substitution
     Eigen::Vector3d aTheta;         //!< -- term needed for back substitution
     Eigen::Vector3d bTheta;         //!< -- term needed for back substitution
-    Eigen::Matrix3d rTilde_HP_P;     //!< -- Tilde matrix of rHB_B
-    Eigen::Matrix3d dcm_SH;          //!< -- DCM from hinge to hinged rigid body frame, S
-    Eigen::Matrix3d dcm_SP;          //!< -- DCM from body to S frame
-    Eigen::Vector3d omega_PN_S;       //!< [rad/s] omega_BN in S frame components
+    double cTheta;                  //!< -- term needed for back substitution
+
+    // Vector quantities
+    Eigen::Vector3d r_HP_P;          //!< [m] vector pointing from primary body frame P origin to Hinge location.  If a single spacecraft body is modeled than P is the same as B
+    Eigen::Vector3d omega_PN_S;      //!< [rad/s] omega_BN in S frame components
     Eigen::Vector3d sHat1_P;         //!< -- unit direction vector for the first axis of the S frame
     Eigen::Vector3d sHat2_P;         //!< -- unit direction vector for the second axis of the S frame
     Eigen::Vector3d sHat3_P;         //!< -- unit direction vector for the third axis of the S frame
     Eigen::Vector3d r_SP_P;          //!< -- Vector pointing from B to CoM of hinged rigid body in B frame components
-    Eigen::Matrix3d rTilde_SP_P;     //!< -- Tilde matrix of rSB_B
     Eigen::Vector3d rPrime_SP_P;     //!< [m/s] Body time derivative of rSB_B
-    Eigen::Matrix3d rPrimeTilde_SP_P;  //!< -- Tilde matrix of rPrime_SB_B
-    Eigen::Matrix3d ISPrimePntS_P;  //!< [kg-m^2/s] time body derivative IPntS in body frame components
     Eigen::Vector3d omegaLoc_PN_P;  //!< [rad/s] local copy of omegaBN
+    Eigen::Vector3d omega_BN_B{0.0, 0.0, 0.0};  //!< Hub/Inertial angular velocity vector in B frame components
+    Eigen::MRPd sigma_BN{0.0, 0.0, 0.0};       //!< Hub/Inertial attitude represented by MRP
+
+    // Matrix quantities
+    Eigen::Matrix3d dcm_HP;          //!< -- DCM from primary body frame to hinge frame
+    Eigen::Matrix3d dcm_SH;          //!< -- DCM from hinge to hinged rigid body frame, S
+    Eigen::Matrix3d dcm_SP;          //!< -- DCM from body to S frame
+    Eigen::Matrix3d rTilde_HP_P;     //!< -- Tilde matrix of rHB_B
+    Eigen::Matrix3d rTilde_SP_P;     //!< -- Tilde matrix of rSB_B
+    Eigen::Matrix3d rPrimeTilde_SP_P;  //!< -- Tilde matrix of rPrime_SB_B
+    Eigen::Matrix3d ISPrimePntS_P;   //!< [kg-m^2/s] time body derivative IPntS in body frame components
     Eigen::Matrix3d omegaTildeLoc_PN_P; //!< -- tilde matrix of omegaBN
+
+    // Hinged rigid body properties
     Eigen::Vector3d r_SN_N;          //!< [m] position vector of hinge CM S relative to inertial frame
     Eigen::Vector3d v_SN_N;          //!< [m/s] inertial velocity vector of S relative to inertial frame
     Eigen::Vector3d sigma_SN;        //!< -- MRP attitude of panel frame S relative to inertial frame
     Eigen::Vector3d omega_SN_S;      //!< [rad/s] inertial panel frame angular velocity vector
-    Eigen::MRPd sigma_BN{0.0, 0.0, 0.0};         //!< Hub/Inertial attitude represented by MRP
-    Eigen::Vector3d omega_BN_B{0.0, 0.0, 0.0};  //!< Hub/Inertial angular velocity vector in B frame components
-    StateData *thetaState;           //!< -- state manager of theta for hinged rigid body
-    Eigen::MatrixXd *inertialPositionProperty;  //!< [m] r_N inertial position relative to system spice zeroBase/refBase
-    Eigen::MatrixXd *inertialVelocityProperty;  //!< [m] v_N inertial velocity relative to system spice zeroBase/refBase
 
-    StateData *thetaDotState;        //!< -- state manager of thetaDot for hinged rigid body
-    Eigen::MatrixXd *c_B;            //!< [m] Vector from point B to CoM of s/c in B frame components
+    // Hub properties
+    Eigen::MatrixXd* inertialPositionProperty;  //!< [m] r_N inertial position relative to system spice zeroBase/refBase
+    Eigen::MatrixXd* inertialVelocityProperty;  //!< [m] v_N inertial velocity relative to system spice zeroBase/refBase
 
-    Eigen::MatrixXd *cPrime_B;       //!< [m/s] Body time derivative of vector c_B in B frame components
+    Eigen::MatrixXd* c_B;            //!< [m] Vector from point B to CoM of s/c in B frame components
+    Eigen::MatrixXd* cPrime_B;       //!< [m/s] Body time derivative of vector c_B in B frame components
+
+    // States
+    double theta;                    //!< [rad] hinged rigid body angle
+    double thetaDot;                 //!< [rad/s] hinged rigid body angle rate
+    StateData* thetaState;           //!< -- state manager of theta for hinged rigid body
+    StateData* thetaDotState;        //!< -- state manager of thetaDot for hinged rigid body
 
 public:
     HingedRigidBodyStateEffector();  //!< -- Contructor
     ~HingedRigidBodyStateEffector();  //!< -- Destructor
-    void writeOutputStateMessages(uint64_t CurrentClock);
-	void UpdateState(uint64_t CurrentSimNanos);
-    void registerStates(DynParamManager& statesIn);  //!< -- Method for registering the HRB states
-    void linkInStates(DynParamManager& states);  //!< -- Method for getting access to other states
-    void updateContributions(double integTime, BackSubMatrices & backSubContr, Eigen::Vector3d sigma_BN, Eigen::Vector3d omega_BN_B, Eigen::Vector3d g_N);  //!< -- Method for back-sub contributions
-    void computeDerivatives(double integTime, Eigen::Vector3d rDDot_BN_N, Eigen::Vector3d omegaDot_BN_B, Eigen::Vector3d sigma_BN);  //!< -- Method for HRB to compute its derivatives
-    void updateEffectorMassProps(double integTime);  //!< -- Method for giving the s/c the HRB mass props and prop rates
-    void updateEnergyMomContributions(double integTime, Eigen::Vector3d & rotAngMomPntCContr_B, double & rotEnergyContr, Eigen::Vector3d omega_BN_B); //!< -- Computing energy and momentum for HRBs
-    void calcForceTorqueOnBody(double integTime, Eigen::Vector3d omega_BN_B);  //!< -- Force and torque on s/c due to HRBs
-    void prependSpacecraftNameToStates(); //!< class method
+    void writeOutputStateMessages(uint64_t CurrentClock) override;
+	void UpdateState(uint64_t CurrentSimNanos) override;
+    void registerStates(DynParamManager& statesIn) override;  //!< -- Method for registering the HRB states
+    void linkInStates(DynParamManager& states) override;  //!< -- Method for getting access to other states
+    void updateContributions(double integTime, BackSubMatrices & backSubContr, Eigen::Vector3d sigma_BN, Eigen::Vector3d omega_BN_B, Eigen::Vector3d g_N) override;  //!< -- Method for back-sub contributions
+    void computeDerivatives(double integTime, Eigen::Vector3d rDDot_BN_N, Eigen::Vector3d omegaDot_BN_B, Eigen::Vector3d sigma_BN) override;  //!< -- Method for HRB to compute its derivatives
+    void updateEffectorMassProps(double integTime) override;  //!< -- Method for giving the s/c the HRB mass props and prop rates
+    void updateEnergyMomContributions(double integTime, Eigen::Vector3d & rotAngMomPntCContr_B, double & rotEnergyContr, Eigen::Vector3d omega_BN_B) override; //!< -- Computing energy and momentum for HRBs
+    void calcForceTorqueOnBody(double integTime, Eigen::Vector3d omega_BN_B) override;  //!< -- Force and torque on s/c due to HRBs
+    void prependSpacecraftNameToStates() override; //!< class method
 
 private:
     void computePanelInertialStates();
