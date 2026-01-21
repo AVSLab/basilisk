c50ce03ae7dd68c6611676f83217995801c8bb44
William Schwend <williamschwend@gmail.com>
2026-01-21 09:46:36 -0700

    update unit test to validate output message

--- src/fswAlgorithms/attGuidance/locationPointing/_UnitTest/test_locationPointing.py
+++ src/fswAlgorithms/attGuidance/locationPointing/_UnitTest/test_locationPointing.py
@@ -1,12 +1,12 @@
-# 
+#
 #  ISC License
-# 
+#
 #  Copyright (c) 2021, Autonomous Vehicle Systems Lab, University of Colorado Boulder
-# 
+#
 #  Permission to use, copy, modify, and/or distribute this software for any
 #  purpose with or without fee is hereby granted, provided that the above
 #  copyright notice and this permission notice appear in all copies.
-# 
+#
 #  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 #  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 #  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -14,8 +14,8 @@
 #  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 #  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# 
-# 
+#
+#
 
 #   Unit Test Script
 #   Module Name:        locationPointing
@@ -44,7 +44,7 @@ def test_locationPointing(show_plots, r_LS_N, locationType, use3DRate, accuracy)
 
     This unit test ensures that the Attitude Guidance and Attitude Reference messages content are properly computed
     for a series of desired inertial target locations
-    
+
 
     **Test Parameters**
 
@@ -146,7 +146,7 @@ def locationPointingTestFunction(show_plots, r_LS_NIn, locationType, use3DRate, 
         unitTestSim.ExecuteSimulation()
         counter += 1
 
-    truthSigmaBR, truthOmegaBR, truthSigmaRN, truthOmegaRN = \
+    truthSigmaBR, truthOmegaBR, truthSigmaRN, truthOmegaRN_B, truthOmegaRN_N = \
         truthValues(pHat_B, r_LN_N, r_SN_N, scAttRec.sigma_BN, scAttRec.omega_BN_B, eps, timeStep,
                     use3DRate)
 
@@ -175,9 +175,17 @@ def locationPointingTestFunction(show_plots, r_LS_NIn, locationType, use3DRate, 
                                 str(attRefOutMsgRec.times()[i] * macros.NANO2SEC) +
                                 "sec\n")
 
-    for i in range(0, len(truthOmegaRN)):
+    for i in range(0, len(truthOmegaRN_B)):
         # check a vector values
-        if not unitTestSupport.isArrayEqual(attRefOutMsgRec.omega_RN_N[i], truthOmegaRN[i], 3, accuracy):
+        if not unitTestSupport.isArrayEqual(attGuidOutMsgRec.omega_RN_B[i], truthOmegaRN_B[i], 3, accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: " + module.ModelTag + " Module failed omega_RN_B unit test at t=" +
+                                str(attRefOutMsgRec.times()[i] * macros.NANO2SEC) +
+                                "sec\n")
+
+    for i in range(0, len(truthOmegaRN_N)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqual(attRefOutMsgRec.omega_RN_N[i], truthOmegaRN_N[i], 3, accuracy):
             testFailCount += 1
             testMessages.append("FAILED: " + module.ModelTag + " Module failed omega_RN_N unit test at t=" +
                                 str(attRefOutMsgRec.times()[i] * macros.NANO2SEC) +
@@ -205,6 +213,7 @@ def truthValues(pHat_B, r_LN_N, r_SN_N, sigma_BNList, omega_BNList, smallAngle, 
     sigma_BR_Out = []
     omega_BR_B_Out = []
     sigma_RN_Out = []
+    omega_RN_B_Out = []
     omega_RN_N_Out = []
     while counter <= 2:
         sigma_BN = sigma_BNList[counter]
@@ -234,11 +243,12 @@ def truthValues(pHat_B, r_LN_N, r_SN_N, sigma_BNList, omega_BNList, smallAngle, 
         sigma_BR_Out.append(sigma_BR)
         omega_BR_B_Out.append(omega_BR_B)
         sigma_RN_Out.append(RigidBodyKinematics.addMRP(sigma_BNList[counter], -sigma_BR))
+        omega_RN_B_Out.append(omega_BNList[counter] - omega_BR_B_Out[counter])
         omega_RN_N_Out.append(np.transpose(dcmBN).dot(omega_BNList[counter] - omega_BR_B_Out[counter]))
 
         counter += 1
 
-    return sigma_BR_Out, omega_BR_B_Out, sigma_RN_Out, omega_RN_N_Out
+    return sigma_BR_Out, omega_BR_B_Out, sigma_RN_Out, omega_RN_B_Out, omega_RN_N_Out
 
 
 if __name__ == "__main__":
