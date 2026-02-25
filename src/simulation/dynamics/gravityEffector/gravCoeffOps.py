fd1a014630da5c9a2843bc0ae60ae6da87471188
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2026-02-25 08:59:44 -0700

    fix formatting

--- src/simulation/dynamics/gravityEffector/gravCoeffOps.py
+++ src/simulation/dynamics/gravityEffector/gravCoeffOps.py
@@ -20,8 +20,8 @@ import csv
 from Basilisk import __path__
 
 def loadGravFromFile(
-        fileName: str, 
-        spherHarm: "SphericalHarmonicsGravityModel", 
+        fileName: str,
+        spherHarm: "SphericalHarmonicsGravityModel",
         maxDeg: int = 2
     ):
 
@@ -57,7 +57,7 @@ def loadGravFromFileToList(fileName: str, maxDeg: int = 2):
             raise ValueError(f"Requested using Spherical Harmonics of degree {maxDeg}"
                              f", but file '{fileName}' has maximum degree/order of"
                              f"{min(maxDegreeFile, maxOrderFile)}")
-        
+
         if not coefficientsNormalized:
             raise ValueError("Coefficients in given file are not normalized. This is "
                             "not currently supported in Basilisk.")
@@ -84,8 +84,8 @@ def loadGravFromFileToList(fileName: str, maxDeg: int = 2):
             slmRow.append(float(gravRow[3]))
 
         return [clmList, slmList, mu, radEquator]
-        
-        
+
+
 def loadPolyFromFile(fileName: str, poly: "PolyhedralGravityModel"):
     [vertList, faceList, _, _] = loadPolyFromFileToList(fileName)
     poly.xyzVertex = vertList
