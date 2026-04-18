354af7b67e0516e3f272fe6820d9908da8d09c97
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2026-04-18 10:37:31 -0600

    [#1362] Fix mappingInstrument dataName overflow
    Reject mapping point names that exceed DataNodeUsageMsgPayload dataName
    storage before saving them, and use a bounded copy when writing the output
    payload. Add a regression test for the BSK_ERROR path and include the
    BSK-2026-002 release-note snippet.
    
    reported by Daniel Miranda Barcelona (Excal1bur)

--- src/simulation/onboardDataHandling/instrument/mappingInstrument/_UnitTest/test_mappingInstrument.py
+++ src/simulation/onboardDataHandling/instrument/mappingInstrument/_UnitTest/test_mappingInstrument.py
@@ -1,12 +1,12 @@
-# 
+#
 #  ISC License
-# 
+#
 #  Copyright (c) 2022, Autonomous Vehicle Systems Lab, University of Colorado Boulder
-# 
+#
 #  Permission to use, copy, modify, and/or distribute this software for any
 #  purpose with or without fee is hereby granted, provided that the above
 #  copyright notice and this permission notice appear in all copies.
-# 
+#
 #  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 #  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 #  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -14,11 +14,12 @@
 #  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 #  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# 
-# 
+#
+#
 
 import numpy as np
-from Basilisk.architecture import messaging
+import pytest
+from Basilisk.architecture import bskLogging, messaging
 from Basilisk.simulation import mappingInstrument
 from Basilisk.utilities import SimulationBaseClass
 from Basilisk.utilities import macros
@@ -36,6 +37,19 @@ def test_mappingInstrument():
     """
     [testResults, testMessage] = mappingInstrumentTestFunction()
     assert testResults < 1, testMessage
+
+
+def test_mappingInstrument_rejects_long_data_name():
+    r"""
+    This test checks that ``mappingInstrument`` rejects mapping point names that do not fit in the fixed
+    ``DataNodeUsageMsgPayload.dataName`` storage.
+    """
+    module = mappingInstrument.MappingInstrument()
+    accessInMsg = messaging.AccessMsg().write(messaging.AccessMsgPayload())
+    long_data_name = "mapping-point-" + "x" * 128
+
+    with pytest.raises(bskLogging.BasiliskError, match="dataName is 142 characters"):
+        module.addMappingPoint(accessInMsg, long_data_name)
 
 
 def mappingInstrumentTestFunction():
@@ -111,5 +125,3 @@ def mappingInstrumentTestFunction():
 
 if __name__ == "__main__":
     test_mappingInstrument()
-
-
