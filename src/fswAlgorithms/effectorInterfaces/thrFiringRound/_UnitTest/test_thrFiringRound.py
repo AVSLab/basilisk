f249d39d56bda57e78ffb970cb4af58581878f6d
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2026-04-19 15:42:04 -0600

    [#1363] Package MuJoCo arm-control Python modules
    - Add packaging support for pure Python BSK modules under fswAlgorithms, simulation, and architecture
    - Move jointThrAllocation and thrFiringRound into Basilisk.fswAlgorithms
    - Move stateMerge into Basilisk.simulation
    - Update MuJoCo arm-control FSW imports to use standard Basilisk package paths
    - Add module documentation, unit tests, and release notes for the moved Python modules

---
+++ src/fswAlgorithms/effectorInterfaces/thrFiringRound/_UnitTest/test_thrFiringRound.py
@@ -0,0 +1,75 @@
+#
+#  ISC License
+#
+#  Copyright (c) 2026, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import numpy as np
+
+from Basilisk.architecture import messaging
+from Basilisk.fswAlgorithms import thrFiringRound
+
+
+def test_thr_firing_round_output_payload():
+    """
+    **Validation Test Description**
+
+    This unit test verifies that :class:`thrFiringRound.ThrFiringRound` can be
+    constructed from the standard ``Basilisk.fswAlgorithms`` import path and
+    that it converts thruster force commands into rounded on-time commands.
+
+    **Description of Variables Being Tested**
+
+    This unit test checks the output ``OnTimeRequest`` field.
+    """
+    module = thrFiringRound.ThrFiringRound()
+    module.setControlPeriodSec(1.0)  # [s]
+    module.setOnTimeResolutionSec(0.1)  # [s]
+    module.setThrForceMax([1.0, 2.0])  # [N]
+
+    forcePayload = messaging.THRArrayCmdForceMsgPayload()
+    forcePayload.thrForce = [0.24, 2.0, -1.0]  # [N]
+    forceMsg = messaging.THRArrayCmdForceMsg().write(forcePayload)
+    module.thrForceInMsg.subscribeTo(forceMsg)
+
+    module.Reset(0)
+    module.UpdateState(1)
+
+    onTimeOut = module.thrOnTimeOutMsg.read()
+    np.testing.assert_allclose(onTimeOut.OnTimeRequest[:2], [0.2, 1.0])  # [s]
+    np.testing.assert_allclose(onTimeOut.OnTimeRequest[2:], np.zeros(34))  # [s]
+
+
+def test_thr_firing_round_configuration_helpers():
+    """
+    **Validation Test Description**
+
+    This unit test verifies that :class:`thrFiringRound.ThrFiringRound`
+    resolves scalar and vector thrust limit configurations.
+
+    **Description of Variables Being Tested**
+
+    This unit test checks the resolved number of thrusters and maximum thrust
+    vector.
+    """
+    module = thrFiringRound.ThrFiringRound()
+    module.setThrForceMax(3.0)  # [N]
+
+    forceCmd = np.array([1.0, 2.0, 3.0])  # [N]
+    assert module.resolveNumThrusters(forceCmd) == 3
+    np.testing.assert_allclose(module.resolveThrForceMax(3), [3.0, 3.0, 3.0])  # [N]
+
+    module.setThrForceMax([1.0, 2.0])  # [N]
+    assert module.resolveNumThrusters(forceCmd) == 2
+    np.testing.assert_allclose(module.resolveThrForceMax(2), [1.0, 2.0])  # [N]
