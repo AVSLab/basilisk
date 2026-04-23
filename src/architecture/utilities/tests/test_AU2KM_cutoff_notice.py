9e56a2102e34f12a08fd4bbaab2e9584a2740e9c
Carlo Cena <carlo98.cena@gmail.com>
2026-04-23 22:42:24 +0200

    [#1369] Deprecating AU2KM and adding AU2M

---
+++ src/architecture/utilities/tests/test_AU2KM_cutoff_notice.py
@@ -0,0 +1,32 @@
+#
+#  ISC License
+#
+#  Copyright (c) 2026, PIC4SeR & AVS Lab, Politecnico di Torino & Argotec S.R.L., University of Colorado Boulder
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from datetime import date
+import warnings
+
+
+def test_AU2KM_cutoff_notice():
+    """Emit a visible reminder after the cutoff date without failing tests."""
+    if date.today() <= date(2027, 4, 24):
+        return
+
+    warnings.warn(
+        "The cutoff date for 'AU2KM' has passed. Please remove the deprecated AU2KM usage along with this test script.",
+        UserWarning,
+        stacklevel=1,
+    )
