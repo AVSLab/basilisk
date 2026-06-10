f99499eb31c130cd5820c32a41c8cceaf8671848
William Schwend <williamschwend@gmail.com>
2026-06-10 15:18:49 -0600

    [#1416] update msg payload def

--- src/architecture/msgPayloadDefCpp/THRArmConfigMsgPayload.h
+++ src/architecture/msgPayloadDefCpp/THRArmConfigMsgPayload.h
@@ -20,6 +20,7 @@
 #ifndef THR_ARM_CONFIG_MESSAGE_H
 #define THR_ARM_CONFIG_MESSAGE_H
 
+#include <array>
 #include <vector>
 
 /*! @brief Structure used by the messaging system to communicate details about the spacecraft system configuration
@@ -29,15 +30,24 @@ typedef struct
 THRArmConfigMsgPayload
 //@endcond
 {
-    std::vector<int> thrArmIdx;              //!< [-] index for which arm a thruster belongs to
-    std::vector<int> thrArmJointIdx;        //!< [-] index for which hinged joint in an arm is the closest parent joint
-    std::vector<int> armTreeIdx;           //!< [-] index for which kinematic tree each thruster arm belongs to
+    double hubMass;                       //!< [kg] mass of the spacecraft's hub
+    std::vector<double> r_BcB_B;          //!< [m] position vector from the hub body frame origin to the hub center of mass (body frame)
+
+    std::vector<int> bodyArmIdx;          //!< [-] index for which arm a body belongs to
+    std::vector<int> bodyJointIdx;        //!< [-] local joint index of the closest parent joint for each body
+    std::vector<double> bodyMass;         //!< [kg] mass of each arm body
+    std::vector<double> r_LcP_P;          //!< [m] position vector from the parent joint to the body center of mass (parent joint's frame), column-major order
+
     std::vector<int> armJointCount;       //!< [-] list of the number of hinged joints in each thruster arm
     std::vector<double> r_CP_P;           //!< [m] position vector from the parent joint to the child joint(parent joint's frame), column-major order
-    std::vector<double> r_TP_P;           //!< [m] position vector from the closest parent joint to the thruster (parent joint's frame), column-major order
     std::vector<double> shat_P;           //!< [-] spin axis unit vector of a child joint (parent joint's frame), column-major order
+    std::vector<double> dcm_C0P;          //!< [-] direction cosine matrix from the child joint frame zero angle orientation to the parent joint frame, column-major order
+
+    std::vector<int> armTreeIdx;          //!< [-] index for which kinematic tree each thruster arm belongs to
+    std::vector<int> thrArmIdx;           //!< [-] index for which arm a thruster belongs to
+    std::vector<int> thrArmJointIdx;      //!< [-] local joint index of the closest parent joint for each thruster
+    std::vector<double> r_TP_P;           //!< [m] position vector from the closest parent joint to the thruster (parent joint's frame), column-major order
     std::vector<double> fhat_P;           //!< [-] force axis unit vector of a thruster (parent joint's frame), column-major order
-    std::vector<double> dcm_C0P;           //!< [-] direction cosine matrix from the child joint frame zero angle orientation to the parent joint frame, column-major order
 } THRArmConfigMsgPayload;
 
 #endif
