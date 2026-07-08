554af818dd739c6d76055deaa9c14c9f36a1faae
Juan Garcia-Bonilla <gjuaga@amazon.com>
2026-07-07 20:03:20 -0700

    Add RDI1WM weak-order-1 Roessler integrator
    svStochasticIntegratorRDI1WM: the simplest weak Roessler method (weak order 1,
    deterministic order 2) for Ito SDEs with diagonal or scalar noise: two drift stages and
    one diffusion evaluation, driven by a three-point increment.

---
+++ src/simulation/dynamics/Integrators/svStochasticIntegratorRDI1WM.h
@@ -0,0 +1,58 @@
+/*
+ ISC License
+
+ Copyright (c) 2026, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef svStochasticIntegratorRDI1WM_h
+#define svStochasticIntegratorRDI1WM_h
+
+#include "../_GeneralModuleFiles/dynamicObject.h"
+#include "../_GeneralModuleFiles/dynParamManager.h"
+#include "../_GeneralModuleFiles/stochasticRKIntegratorBase.h"
+#include "../_GeneralModuleFiles/extendedStateVector.h"
+
+#include <memory>
+
+/** The RDI1WM stochastic integrator: weak order 1 (deterministic order 2) for Ito
+ * SDEs with diagonal or scalar noise.
+ *
+ * Implementation of the ``RDI1WM`` method (Roessler). It is
+ * the simplest of the weak Roessler methods: two drift stages and one diffusion
+ * evaluation, driven by a three-point distributed increment.
+ *
+ * With three-point increment \f$\hat{I} \in \{-\sqrt{3h},0,+\sqrt{3h}\}\f$ (built from
+ * the Gaussian \f$\Delta W\f$ via ``stochasticWeakRV::threePoint``):
+ *
+ * \f[
+ *   H_2^{(0)} = x_n + a_{021} f(t_n,x_n) h + b_{021} g(t_n,x_n)\,\hat{I}
+ * \f]
+ * \f[
+ *   x_{n+1} = x_n + \big(\alpha_1 f(t_n,x_n) + \alpha_2 f(t_n + c_{02}h, H_2^{(0)})\big) h
+ *                 + \beta_{11} g(t_n,x_n)\,\hat{I}
+ * \f]
+ *
+ * @warning Stochastic integration is in beta.
+ */
+class svStochasticIntegratorRDI1WM : public StochasticRKIntegratorBase {
+public:
+    using StochasticRKIntegratorBase::StochasticRKIntegratorBase;
+
+    /** Performs the integration of the associated dynamic objects up to time currentTime+timeStep */
+    virtual void integrate(double currentTime, double timeStep) override;
+};
+
+#endif /* svStochasticIntegratorRDI1WM_h */
